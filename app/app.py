538ef585e0dd7c0ab50460f2dcf88615c03ab1e9
Kyle Fox <kyle.fox55@gmail.com>
2015-12-01 11:02:30 -0500

    Pointing the root route to the new index.html location

--- app/app.py
+++ app/app.py
@@ -6,7 +6,6 @@ from flask import Flask, request, make_response, session, g, redirect, url_for, 
 import json
 import flask
 
-
 from handlers.aws.session import DynamoInterface, SessionTable, LoginSession
 
 from handlers.loginHandler import LoginHandler
@@ -16,9 +15,12 @@ from fileRoutes import add_file_routes
 from loginRoutes import add_login_routes
 
 # Set parameters
-debugFlag = True # Should be false for prod
-runLocal = True # False for prod
-createTable = False # Should be false for most runs, true for first run with DynamoDB
+debugFlag = True  # Should be false for prod
+runLocal = True  # False for prod
+createTable = False  # Should be false for most runs, true for first run with DynamoDB
+
+# Get the project's root folder
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 # Create application
 app = Flask(__name__)
@@ -27,10 +29,12 @@ app.config.from_object(__name__)
 #Enable AWS Sessions
 app.session_interface = DynamoInterface()
 
+
 # Root will point to index.html
 @app.route("/")
 def root():
-    content = open(os.getcwd()+"/index.html").read()
+    filePath = os.path.join(PROJECT_ROOT, '..', 'index.html')
+    content = open(filePath).read()
     return Response(content, mimetype="text/html")
 
 #Add routes for modules here
@@ -38,5 +42,5 @@ add_file_routes(app)
 add_login_routes(app)
 
 if __name__ == '__main__':
-    SessionTable.setup(app,runLocal,createTable)
+    SessionTable.setup(app, runLocal, createTable)
     app.run(debug=debugFlag)
