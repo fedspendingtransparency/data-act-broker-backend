07f0f4ef2241667c791a1b581eca82d02f5ef9ee
Martin Press <presss_martin@bah.com>
2015-12-09 10:53:00 -0500

    Adding base folders and common objects .

---
+++ addToPath.py
@@ -0,0 +1,10 @@
+""" Add all needed subdirectories to path if they are not there yet """
+
+import os, sys, inspect
+
+filePath = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+neededSubDirs = ["/aws","/models","/utils"]
+for subDir in neededSubDirs:
+    pathToAdd =  filePath + subDir
+    if(not(pathToAdd in sys.path)):
+        sys.path.append(pathToAdd)
