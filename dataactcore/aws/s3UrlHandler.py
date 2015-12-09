ab863f7ba65b72e94a590936b3ee060ae2557f2d
Martin Press <presss_martin@bah.com>
2015-12-09 13:11:57 -0500

    Adding new git ignore.

---
+++ dataactcore/aws/s3UrlHandler.py
@@ -0,0 +1,51 @@
+from datetime import datetime, timedelta
+import boto
+
+class s3UrlHandler:
+    """
+    This class acts a wrapper for S3 URL Signing
+    """
+    BASE_URL = "https://s3.amazonaws.com/"
+    ENABLE_S3 = True
+    URL_LIFETIME = 2000
+
+    def __init__(self,name,user):
+        """
+        Creates the object for signing URLS
+
+        arguments:
+        name -- (String) Name of the S3 bucket
+        user -- (int) User id folder of S3 bucket
+
+        """
+        self.bucketRoute = name
+        self.user  = user
+
+    def _signUrl(self,fileName) :
+        """
+        Creates the object for signing URLS
+
+        arguments:
+
+        fileName -- (String) File name of file to be uploaded to S3.
+
+        returns signed url (String)
+
+        """
+        if(s3UrlHandler.ENABLE_S3) :
+            s3connection = boto.connect_s3()
+            return s3connection.generate_url(s3UrlHandler.URL_LIFETIME, 'PUT', self.bucketRoute, "/"+str(self.user)+"/" +fileName)
+        return s3UrlHandler.BASE_URL + "/"+self.bucketRoute +"/"+self.user+"/" +fileName
+
+    def getSignedUrl(self,fileName):
+        """
+        Signs a URL for PUT requests
+
+        arguments:
+        fileName -- (String) File name of file to be uploaded to S3.
+
+        returns signed url (String)
+        """
+        seconds = int((datetime.utcnow()-datetime(1970,1,1)).total_seconds())
+        self.s3FileName = str(seconds)+"_"+fileName
+        return self._signUrl(self.s3FileName)
