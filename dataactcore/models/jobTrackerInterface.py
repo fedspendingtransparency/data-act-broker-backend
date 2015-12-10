89f5aa21c7648c930b2e5da40d6d3fa68b5ee99b
James_Worcester <Worcester_James@bah.com>
2015-12-10 13:50:57 -0500

    Added ResponseException to hold an exception with an HTTP status response, added method to check if sqlalchemy result is unique, refactored credentials

--- dataactcore/models/jobTrackerInterface.py
+++ dataactcore/models/jobTrackerInterface.py
@@ -3,11 +3,17 @@ import json
 from dataactcore.models.baseInterface import BaseInterface
 import os
 import inspect
+from dataactcore.utils.responseException import ResponseException
+from sqlalchemy.orm.exc import NoResultFound,MultipleResultsFound
 
 class JobTrackerInterface(BaseInterface):
     """ Manages all interaction with the job tracker database
     """
-    dbConfigFile = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) + "/jobTracker.conf" #"jobTracker.conf"
+    path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    lastBackSlash = path.rfind("\\",0,-1)
+    lastForwardSlash = path.rfind("/",0,-1)
+    lastSlash = max([lastBackSlash,lastForwardSlash])
+    dbConfigFile =  path[0:lastSlash] + "/credentials/dbCred.json" #"dbCred.json"
     dbName = "job_tracker"
     # May or may not need constructor here to define config file location
     #def __init__(self):
@@ -15,6 +21,30 @@ class JobTrackerInterface(BaseInterface):
         #self.dbConfigFile =
         #super.__init__()
 
+    @staticmethod
+    def checkJobUnique(queryResult):
+        """ Checks if sqlalchemy queryResult has only one entry, error messages are specific to unique jobs
+
+        Args:
+        queryResult -- sqlalchemy query result
+
+        Returns:
+        True if single result, otherwise exception
+        """
+        # TODO move this code to a more general function in BaseInterface that takes error messages as arguments
+        if(len(queryResult) == 0):
+                # Did not get a result for this job
+            exc = ResponseException("Job ID not found in job_status table")
+            exc.status = 400
+            exc.wrappedException = NoResultFound("Job ID not found in job_status table")
+            raise exc
+        elif(len(queryResult) > 1):
+            # Multiple results for single job ID
+            exc = ResponseException("Conflicting jobs found for this ID")
+            exc.status = 400
+            exc.wrappedException = MultipleResultsFound("Conflicting jobs found for this ID")
+            raise exc
+        return True
 
     def getSession(self):
         return self.session
---
+++ dataactcore/utils/responseException.py
@@ -0,0 +1,5 @@
+class ResponseException(Exception):
+    def __init__(self,message):
+        super(ResponseException,self).__init__(message)
+        self.status = 500 # This will be used for the HTTP response status code, 500 if unspecified
+        self.wrappedException = None # Can be used to wrap another type of exception into a ResponseException
