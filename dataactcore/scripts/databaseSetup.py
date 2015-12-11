b58fff11d3624d95024a8e2a7c7c9b682a1ff54a
Martin Press <presss_martin@bah.com>
2015-12-11 15:59:12 -0500

    Adding base database setup script.

---
+++ dataactcore/scripts/databaseSetup.py
@@ -0,0 +1,24 @@
+import json
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.exc import ProgrammingError, IntegrityError
+
+def runCommands(credDict, sqlCommands,dbName) :
+    dbBaseName = "postgres"
+    baseEngine = sqlalchemy.create_engine("postgresql://"+credDict["username"]+":"+credDict["password"]+"@"+credDict["host"]+":"+credDict["port"]+"/"+dbBaseName, isolation_level = "AUTOCOMMIT")
+    try:
+
+        baseEngine.connect().execute("CREATE DATABASE " + '"' + dbName + '"')
+
+    except ProgrammingError as e:
+        # Happens if DB exists, just print and carry on
+        print(e.message)
+    engine = sqlalchemy.create_engine("postgresql://"+credDict["username"]+":"+credDict["password"]+"@"+credDict["host"]+":"+credDict["port"]+"/"+dbName)
+    connection = engine.connect()
+    for statement in sqlCommands:
+        try:
+            connection.execute(statement)
+        except (ProgrammingError, IntegrityError) as e:
+            # Usually a table exists error, print and continue
+            print(e.message)
