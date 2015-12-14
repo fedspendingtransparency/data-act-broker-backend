a350d069c120e348c807cd58e97de37528557b2d
James_Worcester <Worcester_James@bah.com>
2015-12-14 09:48:02 -0500

    Added models for validation database

--- dataactcore/models/validationModels.py
+++ dataactcore/models/validationModels.py
@@ -1,7 +1,7 @@
 """ These classes define the ORM models to be used by sqlalchemy for the job tracker database """
 
 import sqlalchemy
-from sqlalchemy import Column, Integer, Text, ForeignKey
+from sqlalchemy import Column, Integer, Text, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from dataactcore.models.jobTrackerInterface import JobTrackerInterface
@@ -14,6 +14,7 @@ class Rule(Base):
 
     rule_id = Column(Integer, primary_key=True)
     file_column_id = Column(Integer, ForeignKey("file_columns.file_column_id"))
+    file_columns = relationship("FileColumn", back_populates="file_columns")
     rule_type_id = Column(Integer, ForeignKey("rule_type.rule_type_id"))
     rule_text_1 = Column(Text)
     rule_text_2 = Column(Text)
@@ -33,4 +34,22 @@ class FileColumn(Base):
     file_column_id = Column(Integer, primary_key=True)
     file_id = Column(Integer, ForeignKey("file_type.file_id"))
     file = relationship("FileType", back_populates="file_type")
-    field_types_id = Column(Integer, ForeignKey("field_types"))
+    field_types_id = Column(Integer, ForeignKey("field_type.field_type_id"))
+    field_type = relationship("FieldType", back_populates="field_type")
+    name = Column(Text)
+    description = Column(Text)
+    required = Column(Boolean)
+
+class FieldType(Base):
+    __table__ = "field_type"
+
+    field_type_id = Column(Integer, primary_key=True)
+    name = Column(Text)
+    description = Column(Text)
+
+class FileType(Base):
+    __table__ = "file_type"
+
+    file_id = Column(Integer, primary_key=True)
+    name = Column(Text)
+    description = Column(Text)
