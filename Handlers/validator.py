255a1ca2ec104783e104a27b45f3fe620aa7f676
Martin Press <presss_martin@bah.com>
2015-12-14 14:45:33 -0500

    Added hooks into the validation interface for common queries.

--- Handlers/validator.py
+++ Handlers/validator.py
@@ -1,9 +1,11 @@
-class Validator:
-    """ Checks individual records against specified validation tests
+import interfaces
+class Validator(ValidationInterface):
     """
-    
+    Checks individual records against specified validation tests
+    """
+
     @staticmethod
-    def validate(record,testDef):
+    def validate(record,rules):
         """
         Args:
         record -- dict represenation of a single record
@@ -12,3 +14,22 @@ class Validator:
         Returns:
         True if validation passed, False if failed
         """
+        for fieldName in record :
+            ruleSubset = Validator.getRules(fieldName)
+            for currentRule in ruleSubset :
+                if(not validateor.evaluateRule(record[fieldName],currentRule)):
+                    return False
+        return True
+
+    @staticmethod
+    def getRules(fieldName,rules) :
+        returnList =[]
+        for rule in rules :
+            if( rule.file_column.name == fieldName) :
+                returnList.append(rule)
+    return returnList
+
+    @staticmethod
+    def evaluateRule(data,rule):
+        #TODO The rules
+        return False
--- Interfaces/validationInterface.py
+++ Interfaces/validationInterface.py
@@ -1,6 +1,13 @@
-from models.field import FieldType, FieldConstraint
+import sqlalchemy
+import json
+from dataactcore.models import validationInterface
+from sqlalchemy.orm import subqueryload
+from dataactcore.models.validationModels import Rule, RuleType, FileColumn, FileType
+from sqlalchemy.orm.exc import NoResultFound,MultipleResultsFound
+from dataactcore.utils.responseException import ResponseException
 
-class ValidationInterface:
+
+class ValidationInterface(validationInterface.ValidationInterface) :
     """ Manages all interaction with the validation database
     """
 
@@ -12,7 +19,7 @@ class ValidationInterface:
         Returns:
         array of dicts, each representing a single validation
         """
-
+        pass
     def getFieldsByFile(self,filetype):
         """ Returns a dict of valid field names that can appear in this type of file
 
@@ -22,3 +29,29 @@ class ValidationInterface:
         Returns:
         dict with field names as keys and values "type" and "constraint" pulled from FieldType and FieldConstraint
         """
+        returnDict = {}
+        fileId = self.getFileId(filetype)
+        if(fileId is None) :
+            raise ValueError("Filetype does not exist")
+        queryResult = self.session.query(FileColumn).options(subqueryload("field_type")).filter(FileColumn.file_column_id == fileId).all()
+        for column in queryResult :
+            returnDict[column.name]  = column
+        return returnDict
+
+
+    def getFileId(self,filename) :
+        queryResult = self.session.query(FileType).filter(FileType.name== filename).all()
+        if(len(queryResult) > 0) :
+            return queryResult[0].file_id
+        return None
+
+    def getRulesByFile(self,filetype) :
+        """
+        Arguments
+        filetype -- the int id of the filename
+        returns an list of rules
+        """
+        fileId = self.getFileId(filetype)
+        if(fileId is None) :
+            raise ValueError("Filetype does not exist")
+        return self.session.query(Rule).options(subqueryload("rule_type")).options(subqueryload("file_column")).filter(FileColumn.file_column_id == fileId).all()
