4848e18cab209499b9326835258151775de5252d
Martin Press <presss_martin@bah.com>
2015-12-16 17:25:52 -0500

    Adding threaded route.

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import flask
-
+from threading import Thread
 from flask import Flask, request, make_response, session, g, redirect, url_for, \
-     abort, render_template, flash ,session, Response
+     abort, render_template, flash ,session, Response, copy_current_request_context
 import json
 from handlers.validationManager import ValidationManager
+from dataactcore.utils.jsonResponse import JsonResponse
+from dataactcore.utils.statusCode import StatusCode
 
 debugFlag = True
 
@@ -12,6 +14,20 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 validationManager = ValidationManager()
+
+
+
+@app.route("/validate_threaded/",methods=["POST"])
+def validate_threaded():
+
+    @copy_current_request_context
+    def ThreadedFunction ()  :
+        threadedManager = ValidationManager()
+        threadedManager.validateJob(flask.request)
+
+    thread = Thread(target=ThreadedFunction)
+    thread.start()
+    return JsonResponse.create(StatusCode.OK,{"table":"TESTING"})
 
 @app.route("/validate/",methods=["POST"])
 def validate():
