85df032203bf414dddbcd9840ca89113ce8f3acf
James_Worcester <Worcester_James@bah.com>
2015-12-18 14:19:18 -0500

    Debugging path issues on ec2

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
+import sys
 import flask
 from threading import Thread
 from flask import Flask, request, make_response, session, g, redirect, url_for, \
      abort, render_template, flash ,session, Response, copy_current_request_context
 import json
+print(sys.path())
+open("pathLog","w").write(str(sys.path()))
 from handlers.validationManager import ValidationManager
 from dataactcore.utils.jsonResponse import JsonResponse
 from dataactcore.utils.statusCode import StatusCode
