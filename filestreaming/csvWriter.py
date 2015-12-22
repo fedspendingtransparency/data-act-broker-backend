2b3e01acf6312ec6842a84cb5d5dae3122f26eec
Martin Press <press_martin@bah.com>
2015-12-22 13:18:34 -0500

    Adding in CSV Writer.

---
+++ filestreaming/csvWriter.py
@@ -0,0 +1,79 @@
+import io
+import csv
+import boto
+import smart_open
+from smart_open import smart_open_lib
+
+class CsvWriter(object):
+    """
+    Writes a CSV to an S3 Bucket in a steaming manner
+    use with the "with" python construct
+    """
+
+    BUFFER_SIZE =  (5 * 1024 ** 2)
+
+    def __init__(self,bucket,filename,header) :
+        """
+
+        args
+
+        bucket - the string name of the S3 bucket
+        filename - string filename and path in the S3 bucket
+        header - list of strings for the header
+
+        """
+        print CsvWriter.BUFFER_SIZE
+        self.stream = smart_open.smart_open("s3://"+bucket+"/"+filename, 'wb',min_part_size=CsvWriter.BUFFER_SIZE)
+        self.write(header)
+
+    @staticmethod
+    def doesFileExist(bucket,filename) :
+        """
+
+        bucket - the string name of the S3 bucket
+        filename - string filename and path in the S3 bucket
+
+        returns true if file can be found
+        """
+        s3connection = boto.connect_s3()
+        s3Bucket = s3connection.lookup(bucket)
+        if(s3Bucket is None) :
+            return False
+        if(s3Bucket.lookup(filename) is None ) :
+            return False
+        return True
+
+    def write(self,dataList) :
+        """
+
+        args
+        dataList - list of strings to be written to the S3 bucket
+        Adds a row of csv into the S3 stream
+
+        """
+        ioStream = io.BytesIO()
+        csvFormatter = csv.writer(ioStream)
+        csvFormatter.writerow(dataList)
+        self.stream.write(ioStream.getvalue())
+
+    def __enter__(self) :
+        """
+
+        Return a reference of self in the 'with' block
+
+        """
+        return self
+
+    def __exit__(self, type, value, traceback) :
+        """
+
+        args
+        type - the type of error
+        value - the value of the error
+        traceback - the traceback of the error
+
+        This function calls the smart open exit in the
+        'with' block
+
+        """
+        self.stream.__exit__(type, value, traceback)
