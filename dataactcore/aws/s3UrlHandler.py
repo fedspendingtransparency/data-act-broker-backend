2265f32ab4ed2efbec002f3e58eb9e7306992b6d
Martin Press <press_martin@bah.com>
2015-12-22 18:55:23 -0500

    Updated S3 Signer to work without user id.

--- dataactcore/aws/s3UrlHandler.py
+++ dataactcore/aws/s3UrlHandler.py
@@ -12,7 +12,7 @@ class s3UrlHandler:
     ENABLE_S3 = True
     URL_LIFETIME = 2000
 
-    def __init__(self,name,user):
+    def __init__(self,name):
         """
         Creates the object for signing URLS
 
@@ -22,9 +22,8 @@ class s3UrlHandler:
 
         """
         self.bucketRoute = name
-        self.user  = user
 
-    def _signUrl(self,fileName,method="PUT") :
+    def _signUrl(self,path,fileName,method="PUT") :
         """
         Creates the object for signing URLS
 
@@ -37,10 +36,10 @@ class s3UrlHandler:
         """
         if(s3UrlHandler.ENABLE_S3) :
             s3connection = boto.connect_s3()
-            return s3connection.generate_url(s3UrlHandler.URL_LIFETIME, method, self.bucketRoute, "/"+str(self.user)+"/" +fileName)
-        return s3UrlHandler.BASE_URL + "/"+self.bucketRoute +"/"+self.user+"/" +fileName
+            return s3connection.generate_url(s3UrlHandler.URL_LIFETIME, method, self.bucketRoute, "/"+path+"/" +fileName)
+        return s3UrlHandler.BASE_URL + "/"+self.bucketRoute +"/"+path+"/" +fileName
 
-    def getSignedUrl(self,fileName,method="PUT"):
+    def getSignedUrl(self,path,fileName,method="PUT"):
         """
         Signs a URL for PUT requests
 
@@ -54,7 +53,7 @@ class s3UrlHandler:
             self.s3FileName = str(seconds)+"_"+fileName
         else:
             self.s3FileName = fileName
-        return self._signUrl(self.s3FileName, method)
+        return self._signUrl(path,self.s3FileName, method)
 
     @staticmethod
     def getBucketNameFromConfig():
