6025d2b02d6b012bab69bfdd839cf208bc4cbef6
James_Worcester <Worcester_James@bah.com>
2015-12-22 13:17:09 -0500

    Added status and error type arguments to ResponseException constructor

--- dataactcore/utils/responseException.py
+++ dataactcore/utils/responseException.py
@@ -1,6 +1,8 @@
 class ResponseException(Exception):
-    def __init__(self,message):
+    def __init__(self,message,status = 500,errorType = None):
         super(ResponseException,self).__init__(message)
-        self.status = 500 # This will be used for the HTTP response status code, 500 if unspecified
-        self.errorReportMessage = None
-        self.wrappedException = None # Can be used to wrap another type of exception into a ResponseException
+        self.status = status # This will be used for the HTTP response status code, 500 if unspecified
+        if(errorType == None):
+            self.wrappedException = None # Can be used to wrap another type of exception into a ResponseException
+        else:
+            self.wrappedException = errorType(message)
