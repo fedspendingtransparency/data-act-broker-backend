e1f276bdc10a24774eb7f5015d9601ddd4883b6a
Martin Press <press_martin@bah.com>
2016-01-12 15:53:03 -0500

    Created Schema Loader for CSV.

--- interfaces/validationInterface.py
+++ interfaces/validationInterface.py
@@ -2,7 +2,7 @@ import sqlalchemy
 import json
 from dataactcore.models import validationInterface
 from sqlalchemy.orm import subqueryload
-from dataactcore.models.validationModels import Rule, RuleType, FileColumn, FileType
+from dataactcore.models.validationModels import Rule, RuleType, FileColumn, FileType ,FieldType
 from sqlalchemy.orm.exc import NoResultFound,MultipleResultsFound
 from dataactcore.utils.responseException import ResponseException
 
@@ -10,6 +10,87 @@ from dataactcore.utils.responseException import ResponseException
 class ValidationInterface(validationInterface.ValidationInterface) :
     """ Manages all interaction with the validation database
     """
+
+    def addColumnByFileType(self,fileType,fieldName,required,field_type):
+        """
+        Adds a new column to the schema
+
+        Args:
+        fileType -- One of the set of valid types of files (e.g. Award, AwardFinancial)
+
+        fieldName -- The name of the scheam column
+        required --  marks the column if data is allways required
+        field_type  -- sets the type of data allowed in the column
+        """
+        fileId = self.getFileId(fileType)
+        if(fileId is None) :
+            raise ValueError("Filetype does not exist")
+        newColumn = FileColumn()
+        newColumn.required = False
+        newColumn.name = fieldName
+        newColumn.file_id = fileId
+        field_type = field_type.upper()
+
+        types = self.getDataTypes()
+        #Allow for other names
+        if(field_type == "STR") :
+            field_type = "STRING"
+        elif(field_type  == "FLOAT") :
+            field_type = "DECIMAL"
+        elif(field_type  == "BOOL"):
+            field_type = "BOOLEAN"
+
+        #Check types
+        if field_type in types :
+            newColumn.field_types_id =  types[field_type]
+        else :
+            raise ValueError("Type " +field_type + " is not vaild for  " + str(fieldName) )
+        #Check Required
+        required = required.upper()
+        if( required in ["TRUE","FALSE"]) :
+            if( required == "TRUE") :
+                newColumn.required = True
+        else :
+            raise ValueError("Required is not boolean for " + str(fieldName) )
+        # Save
+        self.session.add(newColumn)
+        self.session.commit()
+
+    def getDataTypes(self) :
+        """"
+        Returns a dictionary of data types that contains the id of the types
+        """
+        dataTypes  = {}
+        queryResult = self.session.query(FieldType).all()
+        for column in queryResult :
+            dataTypes[column.name] = column.field_type_id
+        return dataTypes
+
+    def removeColumnsByFileType(self,fileType) :
+        """
+        Removes the schema for a file
+
+        Args:
+        fileType -- One of the set of valid types of files (e.g. Award, AwardFinancial)
+        """
+        fileId = self.getFileId(fileType)
+        if(fileId is None) :
+            raise ValueError("Filetype does not exist")
+        queryResult = self.session.query(FileColumn).filter(FileColumn.file_id == fileId).delete(synchronize_session='fetch')
+        self.session.commit()
+
+    def removeRulesByFileType(self,fileType) :
+        """
+        Removes the rules for a file
+
+        Args:
+        fileType -- One of the set of valid types of files (e.g. Award, AwardFinancial)
+        """
+        fileId = self.getFileId(fileType)
+        if(fileId is None) :
+            raise ValueError("Filetype does not exist")
+        self.session.execute("DELETE FROM rule where file_column_id in (SELECT file_column_id FROM file_columns WHERE file_id = :param)",{"param":fileId})
+        self.session.commit()
 
     def getFieldsByFileList(self, fileType):
         """ Returns a list of valid field names that can appear in this type of file
