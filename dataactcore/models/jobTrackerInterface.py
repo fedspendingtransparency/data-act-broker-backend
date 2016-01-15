79a8ff71f39526645a684d17892a74cc3dd67537
James_Worcester <Worcester_James@bah.com>
2016-01-15 12:39:48 -0500

    Moved several utility functions for jobTracker into core

--- dataactcore/models/jobTrackerInterface.py
+++ dataactcore/models/jobTrackerInterface.py
@@ -1,6 +1,7 @@
 from dataactcore.models.baseInterface import BaseInterface
-from dataactcore.models.jobModels import JobStatus, JobDependency
+from dataactcore.models.jobModels import JobStatus, JobDependency, Status, Type
 from sqlalchemy.orm import subqueryload, joinedload
+from dataactcore.utils.responseException import ResponseException
 
 class JobTrackerInterface(BaseInterface):
     """ Manages all interaction with the job tracker database
@@ -121,3 +122,47 @@ class JobTrackerInterface(BaseInterface):
         for result in queryResult:
             dependents.append(result.job_id)
         return dependents
+
+    def markStatus(self,jobId,statsType):
+        # Pull JobStatus for jobId
+
+        queryResult = self.session.query(JobStatus).filter(JobStatus.job_id == jobId).all()
+        if(self.checkJobUnique(queryResult)):
+            # Mark it finished
+            queryResult[0].status_id = Status.getStatus(statsType)
+            # Push
+            self.session.commit()
+
+    def checkJobType(self,jobId):
+        """ Job should be of type csv_record_validation, or this is the wrong service
+
+        Args:
+        jobId -- job ID to check
+
+        Returns:
+        True if correct type, False or exception otherwise
+        """
+        queryResult = self.session.query(JobStatus.type_id).filter(JobStatus.job_id == jobId).all()
+        if(self.checkJobUnique(queryResult)):
+            if(queryResult[0].type_id == Type.getType("csv_record_validation")):
+                # Correct type
+                return True
+            else:
+                # Wrong type
+                raise ResponseException("Wrong type of job for this service",StatusCode.CLIENT_ERROR,None,ValidationError.jobError)
+
+    def getStatus(self,jobId):
+        """ Get status for specified job
+
+        Args:
+        jobId -- job to get status for
+
+        Returns:
+        status ID
+        """
+        status = None
+        queryResult = self.session.query(JobStatus.status_id).filter(JobStatus.job_id == jobId).all()
+        if(self.checkJobUnique(queryResult)):
+            status = queryResult[0].status_id
+        self.session.commit()
+        return status
