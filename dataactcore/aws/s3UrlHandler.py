bd62ebada3566acf4684df5e2c4ac5d3b275bfe5
Martin Press <press_martin@bah.com>
2016-01-21 11:51:40 -0500

    Adding options to S3 object to have access to STS.

--- dataactcore/aws/s3UrlHandler.py
+++ dataactcore/aws/s3UrlHandler.py
@@ -3,6 +3,7 @@ import boto
 import os
 import inspect
 import json
+from boto import sts
 
 class s3UrlHandler:
     """
@@ -11,7 +12,8 @@ class s3UrlHandler:
     BASE_URL = "https://s3.amazonaws.com/"
     ENABLE_S3 = True
     URL_LIFETIME = 2000
-
+    STS_LIFETIME = 2000
+    S3_ROLE = ""
     def __init__(self,name = None):
         """
         Creates the object for signing URLS
@@ -22,9 +24,10 @@ class s3UrlHandler:
 
         """
         if(name == None):
-            self.bucketRoute = s3UrlHandler.getBucketNameFromConfig()
+            self.bucketRoute = s3UrlHandler.getValueFromConfig("bucket")
         else:
             self.bucketRoute = name
+        s3UrlHandler.S3_ROLE = s3UrlHandler.getValueFromConfig("role")
 
     def _signUrl(self,path,fileName,method="PUT") :
         """
@@ -54,14 +57,29 @@ class s3UrlHandler:
         returns signed url (String)
         """
         if(method=="PUT"):
-            seconds = int((datetime.utcnow()-datetime(1970,1,1)).total_seconds())
-            self.s3FileName = str(seconds)+"_"+fileName
+            self.s3FileName = s3UrlHandler.getTimeStampedFilename(fileName)
         else:
             self.s3FileName = fileName
         return self._signUrl(path,self.s3FileName, method)
 
     @staticmethod
-    def getBucketNameFromConfig():
+    def getTimestampedFilename(filename) :
+        """
+        Gets a Timestamped file name to prevent conflicts on S3 Uploading
+        """
+        seconds = int((datetime.utcnow()-datetime(1970,1,1)).total_seconds())
+        return str(seconds)+"_"+fileName
+
+    @staticmethod
+    def gettValueFromConfig(value):
+        path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+        bucketFile = open(path+"/s3bucket.json","r").read()
+        bucketDict = json.loads(bucketFile)
+        return bucketDict[value]
+
+
+    @staticmethod
+    def getS3RolefromConfig():
         path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
         bucketFile = open(path+"/s3bucket.json","r").read()
         bucketDict = json.loads(bucketFile)
@@ -78,6 +96,18 @@ class s3UrlHandler:
             return False
         else:
             return True
+
+    def getTemporaryCredentials(self,user):
+        """
+        Gets token that allows for S3 Uploads for seconds set in STS_LIFETIME
+        """
+        stsConnection = boto.connect_sts()
+        role = stsConnection.assume_role(s3UrlHandler.S3_ROLE,"FileUpload"+str(user) duration_seconds=s3UrlHandler.STS_LIFETIME)
+        credentials ={}
+        credentials["AccessKeyId"] =  role.access_key
+        credentials["SecretAccessKey"] = role.secret_key
+        credentials["SessionToken"] = role.session_token
+        return credentials
 
     @staticmethod
     def getFileSize(filename):
