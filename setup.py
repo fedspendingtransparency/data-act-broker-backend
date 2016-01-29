c47d5f222089a2f7a957360062502659afb43cc8
Katie Rose <rose_katherine@bah.com>
2016-01-29 14:28:27 -0500

    renaming for clarity

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='Data Act Core',
+    name='dataactcore',
     version='0.0.1',
     description='A sample Python project',
     long_description=long_description,
