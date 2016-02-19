1df584f2814596bccd420897c376c3548dabd6ed
Martin Press <press_martin@bah.com>
2016-02-19 16:13:35 -0500

    Updated Templates and added getStatus method to the model for user status.

--- dataactcore/models/userModel.py
+++ dataactcore/models/userModel.py
@@ -2,57 +2,8 @@
 
 from sqlalchemy import Column, Integer, Text, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
 
 Base = declarative_base()
-class UserStatus(Base):
-    __tablename__ = 'user_status'
-
-    user_status_id = Column(Integer, primary_key=True)
-    name = Column(Text)
-    description = Column(Text)
-    STATUS_DICT = None
-    STATUS_LIST = ["awaiting_confirmation", "email_confirmed","awaiting_approval","approved","denied"]
-
-    @staticmethod
-    def getStatus(statusName):
-        if(UserStatus.STATUS_DICT == None):
-            UserStatus.STATUS_DICT = {}
-            # Pull status values out of DB
-            for status in UserStatus.STATUS_LIST:
-                UserStatus.STATUS_DICT[status] = UserStatus.setStatus(status)
-        if(not statusName in UserStatus.STATUS_DICT):
-            raise ValueError("Not a valid user status")
-        return UserStatus.STATUS_DICT[statusName]
-
-    @staticmethod
-    def setStatus(name):
-        """  Add id to dict for specified status, if not unique throw an exception
-
-        Arguments:
-        name -- Name of status to get an id for
-
-        Returns:
-        id of the specified status
-        """
-        # Create new session for this
-        from dataactcore.models.userInterface import UserInterface
-        UserStatus.session = UserInterface().Session()
-        queryResult = UserStatus.session.query(UserStatus.user_status_id).filter(UserStatus.name==name).all()
-        UserStatus.session.close()
-        if(len(queryResult) != 1):
-            # Did not get a unique result
-            raise ValueError("Database does not contain a unique ID for type "+name)
-        else:
-            return queryResult[0].user_status_id
-
-class PermissionType(Base):
-    __tablename__ = "permission_type"
-
-    permission_type_id = Column(Integer, primary_key=True)
-    name = Column(Text)
-    description = Column(Text)
-
 class User(Base):
     __tablename__ = 'users'
 
@@ -63,8 +14,29 @@ class User(Base):
     agency = Column(Text)
     title = Column(Text)
     user_status_id = Column(Integer, ForeignKey("user_status.user_status_id"))
-    status = relationship("UserStatus", uselist=False)
-    permissions = Column(Integer)
+
+class UserStatus(Base):
+    __tablename__ = 'user_status'
+    STATUS_DICT = None
+
+    user_status_id = Column(Integer, primary_key=True)
+    name = Column(Text)
+    description = Column(Text)
+    @staticmethod
+    def getStatus(statusName):
+        if(UserStatus.STATUS_DICT == None):
+            UserStatus.STATUS_DICT = {}
+            # Pull status values out of DB
+            # Create new session for this
+            from dataactcore.models.userInterface import UserInterface
+            UserStatus.session = UserInterface().Session() # Create new session
+            queryResult = UserStatus.session.query(UserStatus).all()
+            for status in queryResult:
+                UserStatus.STATUS_DICT[status.name] = status.user_status_id
+            UserStatus.session.close()
+        if(not statusName in UserStatus.STATUS_DICT):
+            raise ValueError("Not a valid User status: " + str(statusName) + ", not found in dict: " + str(UserStatus.STATUS_DICT))
+        return UserStatus.STATUS_DICT[statusName]
 
 class EmailTemplateType(Base):
     __tablename__ = 'email_template_type'
