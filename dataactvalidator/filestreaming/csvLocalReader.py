a1b6c24a733760af6d5aeabf4e5594d51cbc83a2
Martin Press <press_martin@bah.com>
2016-03-10 14:36:03 -0500

    Refactoring out the File reader into an abstract class. Created two new sub classes for it.

---
+++ dataactvalidator/filestreaming/csvLocalReader.py
@@ -0,0 +1,37 @@
+import boto
+import csv
+import os
+from dataactcore.utils.statusCode import StatusCode
+from dataactcore.utils.responseException import ResponseException
+from dataactvalidator.filestreaming.csvAbstractReader import CsvAbstractReader
+from dataactvalidator.validation_handlers.validationError import ValidationError
+from dataactvalidator.filestreaming.fieldCleaner import FieldCleaner
+
+class CsvLocalReader(CsvAbstractReader):
+
+
+    def openFile(self,bucket,filename,csvSchema):
+        try:
+            self.file = open(filename,"r")
+        except :
+            raise ValueError("".join(["Filename provided not found : ",str(filename)]))
+        super(CsvLocalReader,self).openFile(self,bucket,filename,csvSchema)
+
+    def close(self):
+        self.file.close()
+
+    def _getFileSize(self):
+        """
+        Gets the size of the file
+        """
+        return os.path.getsize(filename)
+
+    def _getNextPacket(self):
+        """
+        Gets the next packet from the file returns true if successful
+        """
+        packet  = self.file.read(CsvAbstractReader.BUFFER_SIZE).decode('utf-8')
+        success = True
+        if(packet == ""):
+            success = False
+        return success,packet
---
+++ dataactvalidator/filestreaming/csvS3Reader.py
@@ -0,0 +1,45 @@
+import boto
+import csv
+import os
+from dataactcore.utils.statusCode import StatusCode
+from dataactcore.utils.responseException import ResponseException
+from dataactvalidator.validation_handlers.validationError import ValidationError
+from dataactvalidator.filestreaming.fieldCleaner import FieldCleaner
+
+class CsvS3Reader(object):
+    """
+    Reads data from S3 CSV file
+    """
+
+    def openFile(self,bucket,filename,csvSchema):
+        """ Opens file and prepares to read each record, mapping entries to specified column names
+        Args:
+            bucket : the S3 Bucket
+            filename: The file path for the CSV file in S3
+        Returns:
+        """
+        s3connection = boto.connect_s3()
+        s3Bucket = s3connection.lookup(bucket)
+        self.s3File = s3Bucket.lookup(filename)
+        if(self.s3File == None):
+            raise ValueError("".join(["Filename provided not found on S3: ",str(filename)]))
+
+        super(CsvS3Reader,self).openFile(self,bucket,filename,csvSchema)
+
+    def _getFileSize(self):
+        """
+        Gets the size of the file
+        """
+        return self.s3File.size
+
+    def _getNextPacket(self):
+        """
+        Gets the next packet from the file returns true if successful
+        """
+        offsetCheck = self.packetCounter *  CsvReader.BUFFER_SIZE
+        header ={'Range' : "".join(['bytes=',str(offsetCheck),'-',str(offsetCheck +CsvAbstractReader.BUFFER_SIZE - 1)])}
+        try:
+            packet = self.s3File.get_contents_as_string(headers=header).decode('utf-8')
+            return True, packet
+        except :
+            return False, ""
