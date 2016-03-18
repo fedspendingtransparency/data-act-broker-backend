d0a6c5f52cbaed235658e44dbba3c9cdeb298ee3
Rebecca Sweger <rebecca.sweger@gsa.gov>
2016-03-17 22:46:52 -0400

    simplify test suite creation

--- tests/runTests.py
+++ tests/runTests.py
@@ -5,30 +5,11 @@ from userTests import UserTests
 
 # Create test suite
 suite = unittest.TestSuite()
-# Get lists of method names
-loginMethods = LoginTests.__dict__.keys()
-fileMethods = FileTests.__dict__.keys()
-userMethods = UserTests.__dict__.keys()
+suite.addTests(unittest.makeSuite(LoginTests))
+suite.addTests(unittest.makeSuite(FileTests))
+suite.addTests(unittest.makeSuite(UserTests))
 
-for method in loginMethods:
-    # If test method, add to suite
-    if(method[0:4] == "test"):
-        test =LoginTests(methodName=method)
-        suite.addTest(test)
-
-for method in fileMethods:
-    # If test method, add to suite
-    if(method[0:4] == "test"):
-        test =FileTests(methodName=method)
-        suite.addTest(test)
-
-for method in userMethods:
-    # If test method, add to suite
-    if(method[0:4] == "test"):
-        test =UserTests(methodName=method)
-        suite.addTest(test)
-
-print(str(suite.countTestCases()) + " tests in suite")
+print("{} tests in suite".format(suite.countTestCases()))
 
 # Run tests and store results
 runner = unittest.TextTestRunner(verbosity=2)
