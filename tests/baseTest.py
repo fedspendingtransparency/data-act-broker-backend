90f655c2efdcced2a5fde5b737f16e176d5e3822
Rebecca Sweger <rebecca.sweger@gsa.gov>
2016-03-21 14:46:25 -0400

    remove s3 file size check from unittests; revisit when integration testing

--- tests/baseTest.py
+++ tests/baseTest.py
@@ -61,10 +61,11 @@ class BaseTest(unittest.TestCase):
         self.assertEqual(
             response.headers.get("Content-Type"), "application/json")
 
-        if(fileSize != False):
-            s3FileSize = s3UrlHandler.getFileSize("errors/"+jobTracker.getReportPath(jobId))
-            self.assertGreater(s3FileSize, fileSize - 5)
-            self.assertLess(s3FileSize, fileSize + 5)
+        # TODO: revisit this as an integration test
+        # if(fileSize != False):
+        #     s3FileSize = s3UrlHandler.getFileSize("errors/"+jobTracker.getReportPath(jobId))
+        #     self.assertGreater(s3FileSize, fileSize - 5)
+        #     self.assertLess(s3FileSize, fileSize + 5)
 
         tableName = response.json["table"]
         if(type(stagingRows) == type(False) and not stagingRows):
