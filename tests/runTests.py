fdb21e471019e2cde12d235df9909367b2b23182
Rebecca Sweger <rebecca.sweger@gsa.gov>
2016-03-20 22:41:45 -0400

    run unittests through flask app; add setUp; use unittest asserts

--- tests/runTests.py
+++ tests/runTests.py
@@ -1,70 +1,18 @@
-import unittest, inspect
-from dataactcore.models.baseInterface import BaseInterface
-from dataactvalidator.interfaces.interfaceHolder import InterfaceHolder
+import unittest
 from jobTests import JobTests
 from validatorTests import ValidatorTests
 from fileTypeTests import FileTypeTests
-import cProfile
-import pstats
 
 def runTests():
-    runMany = False # True to run the test suite multiple times
-
-    # Connect to databases
-    BaseInterface.IS_FLASK = False # Tests are not running within a flask app
-    interfaces = InterfaceHolder()
-
-    # Create test suite
+    """Create and run validation test suite."""
     suite = unittest.TestSuite()
+    suite.addTests(unittest.makeSuite(ValidatorTests))
+    suite.addTests(unittest.makeSuite(JobTests))
+    suite.addTests(unittest.makeSuite(FileTypeTests))
 
-    # Get lists of method names
-    validatorMethods = ValidatorTests.__dict__.keys()
-    jobMethods = JobTests.__dict__.keys()
-
-    #validatorMethods = []
-    #jobMethods = [] #["test_rules"]
-
-    for method in validatorMethods:
-        # If test method, add to suite
-        if(str(method[0:4]) == "test"):
-            test =ValidatorTests(methodName=method)
-            suite.addTest(test)
-
-    for method in jobMethods:
-        # If test method, add to suite
-        if(method[0:4] == "test"):
-            test =JobTests(methodName=method,interfaces=interfaces)
-            suite.addTest(test)
-
-
-
-    #print(str(suite.countTestCases()) + " tests in suite")
-
-    # Run tests and store results
+    print("{} tests in suite".format(suite.countTestCases()))
     runner = unittest.TextTestRunner(verbosity=2)
-    if(runMany):
-        for i in range(0,100):
-            result = runner.run(suite)
-            if(len(result.errors) > 0 or len(result.failures) > 0):
-                raise Exception("Test Failed")
-    else:
-        result = runner.run(suite)
-
-    fileSuite = unittest.TestSuite()
-    fileMethods = FileTypeTests.__dict__.keys()
-    #fileMethods = ["test_approp_valid"]
-
-    for method in fileMethods:
-        # If test method, add to suite
-        if(method[0:4] == "test"):
-            test =FileTypeTests(methodName=method,interfaces=interfaces)
-            fileSuite.addTest(test)
-
-    fileResult = runner.run(fileSuite)
-    interfaces.close()
+    runner.run(suite)
 
 if __name__ == '__main__':
     runTests()
-    #cProfile.run("runTests()","stats")
-    #stats = pstats.Stats("stats")
-    #stats.sort_stats("cumulative").print_stats(100)
