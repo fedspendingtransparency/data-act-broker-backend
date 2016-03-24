f461be1f6177d870cbeed3551813419b7fdd0bed
Rebecca Sweger <rebecca.sweger@gsa.gov>
2016-03-24 14:26:35 -0400

    set local flag for backward compatibility

--- dataactcore/config.py
+++ dataactcore/config.py
@@ -2,11 +2,17 @@ from os.path import dirname, join
 import yaml
 
 # set the location of the DATA Act broker config file
-CONFIG = join(dirname(__file__), 'config.yml')
+CONFIG_PATH = join(dirname(__file__), 'config.yml')
 
-with open(CONFIG) as c:
-    configs = yaml.load(c)
-CONFIG_BROKER = configs['broker']
-CONFIG_SERVICES = configs['services']
-CONFIG_DB = configs['db']
-CONFIG_LOGGING = configs['logging']
+with open(CONFIG_PATH) as c:
+    CONFIG_ALL = yaml.load(c)
+CONFIG_BROKER = CONFIG_ALL['broker']
+CONFIG_SERVICES = CONFIG_ALL['services']
+CONFIG_DB = CONFIG_ALL['db']
+CONFIG_LOGGING = CONFIG_ALL['logging']
+
+# for backward-compatibility
+if not CONFIG_BROKER['use_aws']:
+    CONFIG_BROKER['local'] = False
+else:
+    CONFIG_BROKER['local'] = True
