2f5673b77f8c3a91879306093eae5d2819a2c9c2
Rebecca Sweger <rebecca.sweger@gsa.gov>
2016-03-28 15:59:43 -0400

    remove warnings--they show up whenever config is imported

--- dataactcore/config.py
+++ dataactcore/config.py
@@ -2,7 +2,6 @@ import os.path
 from os.path import expanduser, normpath, dirname
 import yaml
 import re
-import warnings
 
 # set the location of the DATA Act broker config file
 CONFIG_PATH = os.path.join(dirname(__file__), 'config.yml')
@@ -29,9 +28,6 @@ else:
     broker_files = CONFIG_BROKER['broker_files']
     if not broker_files:
         broker_files = os.path.join(expanduser('~'), 'data_act_broker')
-        msg = 'No broker file path set in config file. Broker will '\
-                      'use the default location: {}'.format(broker_files)
-        warnings.warn(msg)
     elif len(os.path.splitext(broker_files)[1]):
         # if config's broker_files is set to a actual filename
         # just use the directory
@@ -44,25 +40,16 @@ else:
     error_report_path = CONFIG_SERVICES['error_report_path']
     if not error_report_path:
         error_report_path = os.path.join(expanduser('~'), 'data_act_broker')
-        msg = 'No broker file path set in config file. Broker will '\
-                      'use the default location: {}'.format(error_report_path)
-        warnings.warn(msg)
     normpath(error_report_path)
     CONFIG_SERVICES['error_report_path'] = error_report_path
 
     # if not using AWS and no DynamoDB settings specified, default
     # to localhost and port 8000
     if not CONFIG_DB['dynamo_host']:
-        # TODO: dynamo host doesn't actually do anything...code assumes localhost
+        # TODO: dynamo host/port don't do anything; code assumes localhost/8000
         CONFIG_DB['dynamo_host'] = '0.0.0.0'
-        warnings.warn('No Dynamo host set in config file. Broker will '
-                      'default to 0.0.0.0. Please make sure your local '
-                      'DynamoDB is up and running before starting the broker.')
     if not CONFIG_DB['dynamo_port']:
         CONFIG_DB['dynamo_port'] = 8000
-        warnings.warn('No Dynamo host set in config file. Broker will '
-                      'use port 8000. Please make sure your local '
-                      'DynamoDB is up and running before starting the broker.')
     # TODO: can we test that local dynamo is up and running? if not, route calls hang
 
 # normalize logging path, if given
