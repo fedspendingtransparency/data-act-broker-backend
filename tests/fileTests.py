6c96db507030bd4e0ccc7061fffcf8010504f90a
Rebecca Sweger <rebecca.sweger@gsa.gov>
2016-03-28 23:12:42 -0400

    format header test checking data as a list

--- tests/fileTests.py
+++ tests/fileTests.py
@@ -129,8 +129,8 @@ class FileTests(BaseTest):
         self.assertEqual(json[jobIdDict["appropriations"]]["file_type"],"appropriations")
         self.assertEqual(json[jobIdDict["appropriations"]]["filename"],"approp.csv")
         self.assertEqual(json[jobIdDict["appropriations"]]["file_status"],"complete")
-        self.assertEqual(json[jobIdDict["appropriations"]]["missing_headers"],"missing_header_one, missing_header_two")
-        self.assertEqual(json[jobIdDict["appropriations"]]["duplicated_headers"],"duplicated_header_one, duplicated_header_two")
+        self.assertItemsEqual(json[jobIdDict["appropriations"]]["missing_headers"],["missing_header_one", "missing_header_two"])
+        self.assertItemsEqual(json[jobIdDict["appropriations"]]["duplicated_headers"],["duplicated_header_one", "duplicated_header_two"])
 
     def check_upload_complete(self, jobId):
         """Check status of a broker file submission."""
