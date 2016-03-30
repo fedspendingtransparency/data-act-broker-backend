c4a34f63802fdb61bed717ff49e48cea723acec0
Nipun Monga <monga_nipun@bah.com>
2016-03-30 15:35:57 -0400

    Changing job queue manager server reference to public DNS instead of public IP.

--- dataactcore/utils/jobQueue.py
+++ dataactcore/utils/jobQueue.py
@@ -3,7 +3,7 @@ import requests
 
 # params need to be in a broker config file
 # TODO: add persistant queue to be available across server restarts
-jobQueue = Celery('tasks', backend='rpc://', broker='amqp://user:pass@52.200.1.10:5672//')
+jobQueue = Celery('tasks', backend='rpc://', broker='amqp://user:pass@ec2-52-200-1-10.compute-1.amazonaws.com:5672//')
 
 @jobQueue.task(name='jobQueue.enqueue')
 def enqueue(jobID):
