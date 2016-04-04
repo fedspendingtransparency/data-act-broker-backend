1d9d25d509ad938f7fb1bdc2404b60fff8b03b1f
Rebecca Sweger <rebecca.sweger@gsa.gov>
2016-04-04 10:41:30 -0400

    make db setup behavior consistent with soft & hard resets

--- dataactcore/scripts/setupErrorDB.py
+++ dataactcore/scripts/setupErrorDB.py
@@ -11,32 +11,31 @@ def setupErrorDB(hardReset = False):
     errorDb = ErrorInterface()
     if hardReset:
         errorModels.Base.metadata.drop_all(errorDb.engine)
-        errorModels.Base.metadata.create_all(errorDb.engine)
+    errorModels.Base.metadata.create_all(errorDb.engine)
 
-        # TODO: define these codes as enums in the data model?
+    errorDb.session.commit()
+    errorDb.session.close()
 
-        # insert status types
-        statusList = [(1, 'complete', 'File has been processed'),
-            (2, 'header_error', 'The file has errors in the header row'),
-            (3, 'unknown_error', 'An unknown error has occurred with this file'),
-            (4, 'single_row_error', 'CSV file must have a header row and at least one record'),
-            (5, 'job_error', 'Error occurred in job manager')]
-        for s in statusList:
-            status = Status(status_id=s[0], name=s[1], description=s[2])
-            errorDb.session.add(status)
+    # TODO: define these codes as enums in the data model?
+    # insert status types
+    statusList = [(1, 'complete', 'File has been processed'),
+        (2, 'header_error', 'The file has errors in the header row'),
+        (3, 'unknown_error', 'An unknown error has occurred with this file'),
+        (4, 'single_row_error', 'CSV file must have a header row and at least one record'),
+        (5, 'job_error', 'Error occurred in job manager')]
+    for s in statusList:
+        status = Status(status_id=s[0], name=s[1], description=s[2])
+        errorDb.session.merge(status)
 
-        # insert error types
-        errorList = [(1, 'type_error', 'The value provided was of the wrong type'),
-            (2, 'required_error', 'A required value was not provided'),
-            (3, 'value_error', 'The value provided was invalid'),
-            (4, 'read_error', 'Could not parse this record correctly'),
-            (5, 'write_error', 'Could not write this record into the staging database')]
-        for e in errorList:
-            error = ErrorType(error_type_id=e[0], name=e[1], description=e[2])
-            errorDb.session.add(error)
-
-    else:
-        errorModels.Base.metadata.create_all(errorDb.engine)
+    # insert error types
+    errorList = [(1, 'type_error', 'The value provided was of the wrong type'),
+        (2, 'required_error', 'A required value was not provided'),
+        (3, 'value_error', 'The value provided was invalid'),
+        (4, 'read_error', 'Could not parse this record correctly'),
+        (5, 'write_error', 'Could not write this record into the staging database')]
+    for e in errorList:
+        error = ErrorType(error_type_id=e[0], name=e[1], description=e[2])
+        errorDb.session.merge(error)
 
     errorDb.session.commit()
     errorDb.session.close()
--- dataactcore/scripts/setupUserDB.py
+++ dataactcore/scripts/setupUserDB.py
@@ -12,6 +12,9 @@ def setupUserDB(hardReset = False):
         userModel.Base.metadata.drop_all(userDb.engine)
     userModel.Base.metadata.create_all(userDb.engine)
 
+    userDb.session.commit()
+    userDb.session.close()
+
     # TODO: define these codes as enums in the data model?
 
     # insert status types
@@ -22,18 +25,18 @@ def setupUserDB(hardReset = False):
         (5, 'denied', 'User registration was denied')]
     for s in statusList:
         status = UserStatus(user_status_id=s[0], name=s[1], description=s[2])
-        userDb.session.add(status)
+        userDb.session.merge(status)
 
     # insert user permission types
     typeList = [
         (0, 'agency_user', 'This user is allowed to upload data to be validated'),
         (1, 'website_admin', 'This user is allowed to manage user accounts')]
     for t in typeList:
-        type = PermissionType(permission_type_id=t[0],name=t[1], description=t[2])
-        userDb.session.add(type)
+        type = PermissionType(permission_type_id=t[0], name=t[1], description=t[2])
+        userDb.session.merge(type)
 
     userDb.session.commit()
     userDb.session.close()
 
 if __name__ == '__main__':
-    setupUserDB(hardReset = True)
+    setupUserDB(hardReset=True)
