4c6f26a2f2daf23dc2cf25a25d97ff11f3770038
Rebecca Sweger <rebecca.sweger@gsa.gov>
2016-04-11 15:24:34 -0400

    change file submission wait time from 100 to 20

--- tests/fileTests.py
+++ tests/fileTests.py
@@ -128,17 +128,17 @@ class FileTests(BaseTest):
 
         # First wait for job Id to get a file status
         done = False
-        while not done and ((time() - start) < 100):
+        while not done and ((time() - start) < 20):
             try:
                 self.interfaces.errorDb.checkStatusByJobId(valId)
                 done = True
             except ResponseException:
                 # Does not exist yet, keep trying
                 sleep(1)
-        while (self.interfaces.jobDb.getJobStatus(valId) == "waiting" or self.interfaces.jobDb.getJobStatus(valId) == "running") and ((time() - start) < 100):
-            # If validation does not complete in 100 seconds, give up
+        while (self.interfaces.jobDb.getJobStatus(valId) == "waiting" or self.interfaces.jobDb.getJobStatus(valId) == "running") and ((time() - start) < 20):
+            # If validation does not complete in 20 seconds, give up
             sleep(1)
-        self.assertLess((time() - start),100,"Validation did not complete")
+        self.assertLess((time() - start),20,"Validation did not complete")
         self.assertEqual(self.interfaces.jobDb.getJobStatus(valId),"finished")
         self.assertEqual(self.interfaces.errorDb.checkStatusByJobId(valId),self.interfaces.errorDb.getStatusId("complete"))
         fileSize = self.interfaces.jobDb.getFileSizeById(valId)
