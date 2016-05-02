8e131f16eca52ea417e054bb7f17790ed147ad1c
Nipun Monga <monga_nipun@bah.com>
2016-05-02 11:48:20 -0400

    Changing default error message for timeout

--- dataactcore/utils/timeout.py
+++ dataactcore/utils/timeout.py
@@ -1,12 +1,10 @@
 from functools import wraps
-import errno
-import os
 import signal
 
 class TimeoutError(Exception):
     pass
 
-def timeout(seconds=10, error_message=os.strerror(errno.ETIME)):
+def timeout(seconds=10, error_message="Request timed out"):
     def decorator(func):
         def _handle_timeout(signum, frame):
             raise TimeoutError(error_message)
