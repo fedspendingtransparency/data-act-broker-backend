945741048eacaeecdc592f3ff096077a9a775032
Katie Rose <rose_katherine@bah.com>
2016-05-03 15:05:47 -0400

    Using wildcard for allowed headers

--- dataactbroker/app.py
+++ dataactbroker/app.py
@@ -48,10 +48,10 @@ def createApp():
         JsonResponse.debugMode = app.config['REST_TRACE']
 
         if CONFIG_SERVICES['cross_origin_url'] ==  "*":
-            cors = CORS(app, supports_credentials=True, allow_headers = "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Session-ID", expose_headers = "X-Session-Id")
+            cors = CORS(app, supports_credentials=True, allow_headers = "*", expose_headers = "X-Session-Id")
         else:
             cors = CORS(app, supports_credentials=True, origins=CONFIG_SERVICES['cross_origin_url'],
-                        allow_headers = "X-Session-Id", expose_headers = "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Session-ID")
+                        allow_headers = "X-Session-Id", expose_headers = "*")
         # Enable AWS Sessions
         app.session_interface = DynamoInterface()
         # Set up bcrypt
