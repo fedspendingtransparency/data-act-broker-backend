d4c8a2056030be71c5763a38eb1e26db6289dfd8
Nipun Monga <monga_nipun@bah.com>
2016-05-20 09:56:51 -0400

    Updating get users by type unit test to accommodate for new agency user

--- tests/userTests.py
+++ tests/userTests.py
@@ -155,7 +155,7 @@ class UserTests(BaseTestAPI):
         emails = []
         for admin in agencyUsers:
             emails.append(admin.email)
-        self.assertEqual(len(agencyUsers), 13)
+        self.assertEqual(len(agencyUsers), 14)
         for email in ["realEmail@agency.gov", "waiting@agency.gov",
             "impatient@agency.gov", "watchingPaintDry@agency.gov",
             "approved@agency.gov", "nefarious@agency.gov",]:
