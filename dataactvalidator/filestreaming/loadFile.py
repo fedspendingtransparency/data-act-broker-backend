98016519d3e985c0ce0e5935779f3c9d2e3f5110
James_Worcester <Worcester_James@bah.com>
2016-05-27 15:12:57 -0400

    Added function to load object class

--- dataactvalidator/filestreaming/loadFile.py
+++ dataactvalidator/filestreaming/loadFile.py
@@ -1,9 +1,13 @@
 from dataactvalidator.filestreaming.loaderUtils import LoaderUtils
 from dataactvalidator.interfaces.validatorValidationInterface import ValidatorValidationInterface
-from dataactcore.models.domainModels import CGAC
+from dataactcore.models.domainModels import CGAC,ObjectClass
 
 def loadCgac(filename):
     LoaderUtils.loadCsv(filename,CGAC,ValidatorValidationInterface(),{"cgac":"cgac_code","agency":"agency_name"},{"cgac_code":{"pad_to_length":3,"skip_duplicates":True}})
 
+def loadObjectClass(filename):
+    LoaderUtils.loadCsv(filename,ObjectClass,ValidatorValidationInterface(),{"max oc code":"object_class_code","max object class name":"object_class_name"},{"object_class_code":{"skip_duplicates":True}})
+
 if __name__ == '__main__':
     loadCgac("../config/cgac.csv")
+    loadObjectClass("../config/object_class.csv")
