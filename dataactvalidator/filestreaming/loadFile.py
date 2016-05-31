f4deeca179f1cb26a5a4fe7c91a4c501ab6c9d70
James_Worcester <Worcester_James@bah.com>
2016-05-31 08:56:47 -0400

    Fixing bugs with loading object classes

--- dataactvalidator/filestreaming/loadFile.py
+++ dataactvalidator/filestreaming/loadFile.py
@@ -6,7 +6,7 @@ def loadCgac(filename):
     LoaderUtils.loadCsv(filename,CGAC,ValidatorValidationInterface(),{"cgac":"cgac_code","agency":"agency_name"},{"cgac_code":{"pad_to_length":3,"skip_duplicates":True}})
 
 def loadObjectClass(filename):
-    LoaderUtils.loadCsv(filename,ObjectClass,ValidatorValidationInterface(),{"max oc code":"object_class_code","max object class name":"object_class_name"},{"object_class_code":{"skip_duplicates":True}})
+    LoaderUtils.loadCsv(filename,ObjectClass,ValidatorValidationInterface(),{"max_oc_code":"object_class_code","max_object_class_name":"object_class_name"},{"object_class_code":{"skip_duplicates":True}})
 
 if __name__ == '__main__':
     loadCgac("../config/cgac.csv")
