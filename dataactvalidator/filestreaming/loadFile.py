d7d6c8f0f62b8092ac02a25512602ee7a5e08f60
James_Worcester <Worcester_James@bah.com>
2016-06-03 14:54:32 -0400

    Improved path handling

--- dataactvalidator/filestreaming/loadFile.py
+++ dataactvalidator/filestreaming/loadFile.py
@@ -2,6 +2,7 @@ import os
 from dataactvalidator.filestreaming.loaderUtils import LoaderUtils
 from dataactvalidator.interfaces.validatorValidationInterface import ValidatorValidationInterface
 from dataactcore.models.domainModels import CGAC,ObjectClass
+from dataactcore.config import CONFIG_BROKER
 
 def loadCgac(filename):
     LoaderUtils.loadCsv(filename,CGAC,ValidatorValidationInterface(),{"cgac":"cgac_code","agency":"agency_name"},{"cgac_code":{"pad_to_length":3,"skip_duplicates":True}})
@@ -14,4 +15,4 @@ def loadDomainValues(basePath):
     loadObjectClass(os.path.join(basePath,"object_class.csv"))
 
 if __name__ == '__main__':
-    loadDomainValues("../config/")
+    loadDomainValues(os.path.join(CONFIG_BROKER["path"],"dataactvalidator","config"))
