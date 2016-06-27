37024fc6c3d00f009b98688e4f11729dd24b2b28
Rebecca Sweger <rebecca.sweger@gsa.gov>
2016-06-27 19:26:32 -0400

    update .sql files for short colnames

--- dataactvalidator/config/sqlrules/a18_cross_file.sql
+++ dataactvalidator/config/sqlrules/a18_cross_file.sql
@@ -1,5 +1,10 @@
-SELECT approp.row_number, approp.tas, approp.grossoutlayamountbytas_cpe, SUM(op.grossoutlayamountbyprogramobjectclass_cpe) as grossoutlayamountbyprogramobjectclass_cpe_sum
-FROM appropriation approp
-    JOIN object_class_program_activity op ON approp.tas = op.tas AND approp.submission_id = op.submission_id
+SELECT
+    approp.row_number,
+    approp.tas,
+    approp.gross_outlay_amount_by_tas_cpe,
+    SUM(op.gross_outlay_amount_by_pro_cpe) as gross_outlay_amount_by_pro_cpe_sum
+FROM appropriation approp JOIN object_class_program_activity op
+ON approp.tas = op.tas AND approp.submission_id = op.submission_id
 WHERE approp.submission_id = {}
-GROUP BY approp.row_number, approp.tas, approp.grossoutlayamountbytas_cpe HAVING approp.grossoutlayamountbytas_cpe <> SUM(op.grossoutlayamountbyprogramobjectclass_cpe)
+GROUP BY approp.row_number, approp.tas, approp.gross_outlay_amount_by_tas_cpe
+HAVING approp.gross_outlay_amount_by_tas_cpe <> SUM(op.gross_outlay_amount_by_pro_cpe)
--- dataactvalidator/config/sqlrules/b11_object_class_program_activity_1.sql
+++ dataactvalidator/config/sqlrules/b11_object_class_program_activity_1.sql
@@ -1,4 +1,6 @@
-SELECT op.row_number, op.objectclass
+SELECT
+    op.row_number,
+    op.object_class
 FROM object_class_program_activity AS op
-WHERE op.submission_id = {} AND op.objectclass NOT IN (SELECT object_class_code
-                                                        FROM object_class)
+WHERE op.submission_id = {}
+AND op.object_class NOT IN (SELECT object_class_code FROM object_class)
