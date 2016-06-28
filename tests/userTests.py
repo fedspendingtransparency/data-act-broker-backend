c67d7966aab48013ff54c39962cafb4c6eaf36f1
Nipun Monga <monga_nipun@bah.com>
2016-06-28 09:16:34 -0400

    Updating unit tests to list_users route to check that all users are actually being returned for case with no parameters

--- tests/userTests.py
+++ tests/userTests.py
@@ -144,6 +144,8 @@ class UserTests(BaseTestAPI):
 
         response = self.app.post_json("/v1/list_users/", headers={"x-session-id": self.session_id})
         self.check_response(response, StatusCode.OK)
+        users = response.json["users"]
+        self.assertEqual(len(users), 16)
 
     def test_list_users_bad_status(self):
         """Test getting user list with invalid status."""
