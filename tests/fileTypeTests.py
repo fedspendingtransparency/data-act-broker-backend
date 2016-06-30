8a724d58606bd4fd0e9179ed7e881d578bae64d6
James_Worcester <Worcester_James@bah.com>
2016-06-30 14:09:25 -0400

    A20 and A21 now completed for files B and C

--- tests/fileTypeTests.py
+++ tests/fileTypeTests.py
@@ -111,7 +111,7 @@ class FileTypeTests(BaseTestValidator):
         """Test mixed job with some rows failing."""
         jobId = self.jobIdDict["programMixed"]
         self.passed = self.run_test(
-            jobId, 200, "finished", 21023, 4, "complete", 118, True)
+            jobId, 200, "finished", 21475, 4, "complete", 122, True)
 
     def test_award_fin_valid(self):
         """Test valid job."""
@@ -123,7 +123,7 @@ class FileTypeTests(BaseTestValidator):
         """Test mixed job with some rows failing."""
         jobId = self.jobIdDict["awardFinMixed"]
         self.passed = self.run_test(
-            jobId, 200, "finished", 17222, 5, "complete", 87, True)
+            jobId, 200, "finished", 17504, 5, "complete", 89, True)
         # Test that whitespace is converted to null
         rowThree = self.interfaces.validationDb.session.query(AwardFinancial).filter(AwardFinancial.row_number == 3).filter(AwardFinancial.submission_id == self.interfaces.jobDb.getSubmissionId(jobId)).first()
         self.assertIsNone(rowThree.agencyidentifier)
