29c5bfbe7e43eb20e48ec55e4a18a73dc4710884
James_Worcester <Worcester_James@bah.com>
2016-07-01 14:07:22 -0400

    Updated file loaders to be consistent with new preprocessing

--- tests/fileTypeTests.py
+++ tests/fileTypeTests.py
@@ -99,7 +99,7 @@ class FileTypeTests(BaseTestValidator):
         """Test mixed job with some rows failing."""
         jobId = self.jobIdDict["mixed"]
         self.passed = self.run_test(
-            jobId, 200, "finished", 7038, 4, "complete", 50, True)
+            jobId, 200, "finished", 7032, 4, "complete", 50, True)
 
     def test_program_valid(self):
         """Test valid job."""
@@ -111,7 +111,7 @@ class FileTypeTests(BaseTestValidator):
         """Test mixed job with some rows failing."""
         jobId = self.jobIdDict["programMixed"]
         self.passed = self.run_test(
-            jobId, 200, "finished", 16389, 4, "complete", 107, True)
+            jobId, 200, "finished", 16377, 4, "complete", 107, True)
 
     def test_award_fin_valid(self):
         """Test valid job."""
@@ -123,7 +123,7 @@ class FileTypeTests(BaseTestValidator):
         """Test mixed job with some rows failing."""
         jobId = self.jobIdDict["awardFinMixed"]
         self.passed = self.run_test(
-            jobId, 200, "finished", 13413, 5, "complete", 77, True)
+            jobId, 200, "finished", 13404, 5, "complete", 77, True)
         # Test that whitespace is converted to null
         rowThree = self.interfaces.validationDb.session.query(AwardFinancial).filter(AwardFinancial.parentawardid == "ZZZZ").filter(AwardFinancial.submission_id == self.interfaces.jobDb.getSubmissionId(jobId)).first()
         self.assertIsNone(rowThree.agencyidentifier)
