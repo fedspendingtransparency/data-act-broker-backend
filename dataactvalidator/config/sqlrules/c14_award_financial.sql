0d42d8d7f0feeff8f3b3da494bdc6bd1d28ca81f
Michael Bray <bray_michael@bah.com>
2016-07-05 15:55:47 -0400

    Fixed C8 and C9 to account for NULLs, modified C14 to add PIID, added C17 and C18 implementations

--- dataactvalidator/config/sqlrules/c14_award_financial.sql
+++ dataactvalidator/config/sqlrules/c14_award_financial.sql
@@ -2,4 +2,5 @@ SELECT row_number, fain, uri
 FROM award_financial
 WHERE submission_id = {}
 	AND fain IS NULL
-	AND uri IS NULL;
+	AND uri IS NULL
+	AND piid IS NULL;
--- dataactvalidator/config/sqlrules/c8_award_financial.sql
+++ dataactvalidator/config/sqlrules/c8_award_financial.sql
@@ -1,13 +1,12 @@
 SELECT af.row_number, af.fain, af.uri
 FROM award_financial AS af
-WHERE af.submission_id = {}
-	AND af.fain IS NOT NULL
-	AND af.uri IS NOT NULL
+WHERE af.submission_id = {0}
 	AND af.row_number NOT IN (
 		SELECT af.row_number
 		FROM award_financial AS af
 			JOIN award_financial_assistance AS afa
-				ON af.fain = afa.fain
-				AND af.uri = afa.uri
-				AND af.submission_id = afa.submission_id
+				ON (af.fain IS NOT DISTINCT FROM afa.fain
+				AND af.uri IS NOT DISTINCT FROM afa.uri
+				AND af.submission_id = afa.submission_id)
+		WHERE af.submission_id = {0}
 	);
