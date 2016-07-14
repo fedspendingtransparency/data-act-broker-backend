dd364ced6796552b2569c5aa6bbd8c2258940682
James_Worcester <Worcester_James@bah.com>
2016-07-14 11:47:42 -0400

    Fixed quarter assignment on submissions

--- tests/fileTypeTests.py
+++ tests/fileTypeTests.py
@@ -47,9 +47,11 @@ class FileTypeTests(BaseTestValidator):
         # Create submissions and get IDs back
         submissionIDs = {}
         for i in range(0, 16):
+            if i == 7:
+               # Award financial mixed will be second quarter
+                submissionIDs[i] = cls.insertSubmission(cls.jobTracker, user, datetime(2015,3,15)) 
             submissionIDs[i] = cls.insertSubmission(cls.jobTracker, user)
-        # Submission 12 will be second quarter
-        submissionIDs[11] = cls.insertSubmission(cls.jobTracker, user, datetime(2015,3,15))
+
 
         # Create jobs
         jobDb = cls.jobTracker
