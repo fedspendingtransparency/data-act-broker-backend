773933f4f58507720ee06d112b315a1f921191df
James_Worcester <Worcester_James@bah.com>
2016-08-02 12:23:43 -0400

    Explicitly telling boto to get contents as utf-8

--- dataactvalidator/filestreaming/csvS3Reader.py
+++ dataactvalidator/filestreaming/csvS3Reader.py
@@ -55,7 +55,7 @@ class CsvS3Reader(CsvAbstractReader):
         offsetCheck = self.packetCounter *  CsvAbstractReader.BUFFER_SIZE 
         header ={'Range' : "".join(['bytes=',str(offsetCheck),'-',str(offsetCheck +CsvAbstractReader.BUFFER_SIZE - 1)])}
         try:
-            packet = self.s3File.get_contents_as_string(headers=header)
+            packet = self.s3File.get_contents_as_string(headers=header, encoding='utf-8')
             return True, packet
         except :
             return False, ""
