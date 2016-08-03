d3e6834ffa737bba51bca34e9ca4cd606adebe7d
Nipun Monga <monga_nipun@bah.com>
2016-08-02 20:14:34 -0400

    When updating existing metadata, clear out the download url and error_message fields

--- dataactbroker/handlers/jobHandler.py
+++ dataactbroker/handlers/jobHandler.py
@@ -396,6 +396,9 @@ class JobHandler(JobTrackerInterface):
     def createDFileMeta(self, submission_id, start_date, end_date, type, original_file_name, upload_file_name):
         result = self.session.query(DFileMeta).filter(and_(DFileMeta.submission_id == submission_id, DFileMeta.type == type)).first()
         if result is not None:
+            result.url = ""
+            result.error_message = ""
+            self.session.commit()
             return result.d_file_id
 
         d1_file = DFileMeta(submission_id=submission_id, start_date=start_date, end_date=end_date, type=type,
