157a9468d6075b2dda8d2aa2604af2b5830f833a
Nipun Monga <monga_nipun@bah.com>
2016-08-10 00:36:36 -0400

    Adding tests for status codes in dataactcore/utils/statusCodes.py as proof of concept

---
+++ tests/unit/dataactcore/test_status_code.py
@@ -0,0 +1,21 @@
+from dataactcore.utils.statusCode import StatusCode
+
+class TestStatusCode:
+
+    def setup_class(self):
+        """ Setup for class """
+
+    def teardown_class(self):
+        """ Teardown for class """
+
+    def test_status_ok(self):
+        assert StatusCode.OK == 200
+
+    def test_status_client_error(self):
+        assert StatusCode.CLIENT_ERROR == 400
+
+    def test_status_login_required(self):
+        assert StatusCode.LOGIN_REQUIRED == 401
+
+    def test_status_internal_error(self):
+        assert StatusCode.INTERNAL_ERROR == 500
