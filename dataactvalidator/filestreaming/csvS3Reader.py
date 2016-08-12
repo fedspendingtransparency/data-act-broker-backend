589402ecd8fc681fe05b5ed25cd9251c64d03138
Rebecca Sweger <rebecca.sweger@gsa.gov>
2016-08-11 21:22:09 -0400

    readibility tweaks

--- dataactvalidator/filestreaming/csvS3Reader.py
+++ dataactvalidator/filestreaming/csvS3Reader.py
@@ -1,19 +1,13 @@
 import boto
-import csv
-import os
-from dataactcore.utils.statusCode import StatusCode
-from dataactcore.utils.responseException import ResponseException
 from dataactvalidator.filestreaming.csvAbstractReader import CsvAbstractReader
-from dataactvalidator.validation_handlers.validationError import ValidationError
-from dataactvalidator.filestreaming.fieldCleaner import FieldCleaner
+
 
 class CsvS3Reader(CsvAbstractReader):
     """
     Reads data from S3 CSV file
     """
 
-
-    def openFile(self,region,bucket,filename,csvSchema,bucketName,errorFilename):
+    def openFile(self, region, bucket, filename, csvSchema, bucketName, errorFilename):
         """ Opens file and prepares to read each record, mapping entries to specified column names
         Args:
             bucket : the S3 Bucket
@@ -24,12 +18,13 @@ class CsvS3Reader(CsvAbstractReader):
         s3Bucket = s3connection.lookup(bucket)
         self.s3File = s3Bucket.lookup(filename)
         self.isLocal = False
-        if(self.s3File == None):
-            raise ValueError("".join(["Filename provided not found on S3: ",str(filename)]))
+        if not self.s3File:
+            raise ValueError("Filename {} not found on S3".format(filename))
 
-        super(CsvS3Reader,self).openFile(region,bucket,filename,csvSchema,bucketName,errorFilename)
+        super(CsvS3Reader, self).openFile(
+            region, bucket, filename, csvSchema, bucketName, errorFilename)
 
-    def downloadFile(self,region,bucket,filename,targetLocation):
+    def downloadFile(self, region, bucket, filename, targetLocation):
         """ After opening a file, download entire file to filename """
         s3connection = boto.s3.connect_to_region(region)
         s3Bucket = s3connection.lookup(bucket)
@@ -37,8 +32,9 @@ class CsvS3Reader(CsvAbstractReader):
             raise ValueError("Bucket {} not found in region {}".format(
                 bucket, region))
         self.s3File = s3Bucket.get_key(filename)
-        if(self.s3File == None):
-            raise ValueError("".join(["Filename provided not found on S3: ",str(filename)]))
+        if not self.s3File:
+            raise ValueError("Filename {} not found in bucket {}".format(
+                filename, bucket))
         self.s3File.get_contents_to_filename(targetLocation)
 
     def close(self):
@@ -56,9 +52,10 @@ class CsvS3Reader(CsvAbstractReader):
         Gets the next packet from the file returns true if successful
         """
         offsetCheck = self.packetCounter *  CsvAbstractReader.BUFFER_SIZE 
-        header ={'Range' : "".join(['bytes=',str(offsetCheck),'-',str(offsetCheck +CsvAbstractReader.BUFFER_SIZE - 1)])}
+        header ={'Range': "".join(
+            ['bytes=', str(offsetCheck), '-', str(offsetCheck +CsvAbstractReader.BUFFER_SIZE - 1)])}
         try:
             packet = self.s3File.get_contents_as_string(headers=header)
             return True, packet
-        except :
+        except:
             return False, ""
