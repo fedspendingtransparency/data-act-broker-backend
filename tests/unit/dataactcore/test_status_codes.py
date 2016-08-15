91aa397fa4d7e8124f03a8316ad1fa054a44725c
Nipun Monga <monga_nipun@bah.com>
2016-08-15 18:01:14 -0400

    Cleaning up test_status_codes

--- tests/unit/dataactcore/test_status_codes.py
+++ tests/unit/dataactcore/test_status_codes.py
@@ -1,24 +1,17 @@
 from dataactcore.utils.statusCode import StatusCode
 
-class TestStatusCodes:
 
-    def setup_class(self):
-        """ Setup for class """
+def test_status_ok():
+    assert StatusCode.OK == 200
 
-    def teardown_class(self):
-        """ Teardown for class """
 
-    def test_status_ok(self):
-        assert StatusCode.OK == 200
+def test_status_client_error():
+    assert StatusCode.CLIENT_ERROR == 400
 
-    def test_status_client_error(self):
-        assert StatusCode.CLIENT_ERROR == 400
 
-    def test_status_login_required(self):
-        assert StatusCode.LOGIN_REQUIRED == 401
+def test_status_login_required():
+    assert StatusCode.LOGIN_REQUIRED == 401
 
-    def test_status_internal_error(self):
-        assert StatusCode.INTERNAL_ERROR == 500
 
-    def test_intentional_failure(self):
-        assert StatusCode.OK == 201
+def test_status_internal_error():
+    assert StatusCode.INTERNAL_ERROR == 500
