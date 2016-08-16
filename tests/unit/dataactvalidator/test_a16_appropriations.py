6f4e8449dc34770b99d18ea20c4fe0230540b61f
CM Lubinski <christopher.lubinski@gsa.gov>
2016-08-16 21:05:29 +0000

    Restore a16 unittests

--- tests/unit/dataactvalidator/test_a16_appropriations.py
+++ tests/unit/dataactvalidator/test_a16_appropriations.py
@@ -7,18 +7,15 @@ _FILE = 'a16_appropriations'
 
 def test_success(database):
     award = Appropriation(job_id=1, row_number=1)
-    pass
-    #assert run_sql_rule(_FILE, database.stagingDb, award)
+    assert run_sql_rule(_FILE, database.stagingDb, models=[award]) == 0
 
 
 def test_null_authority(database):
     award = Appropriation(job_id=1, row_number=1, is_first_quarter=True)
-    pass
-    #assert not run_sql_rule(_FILE, database.stagingDb, award)
+    assert run_sql_rule(_FILE, database.stagingDb, models=[award]) == 1
 
 
 def test_nonnull_authority(database):
     award = Appropriation(job_id=1, row_number=1, is_first_quarter=True,
                           budget_authority_unobligat_fyb=5)
-    pass
-    #assert run_sql_rule(_FILE, database.stagingDb, award)
+    assert run_sql_rule(_FILE, database.stagingDb, models=[award]) == 0
