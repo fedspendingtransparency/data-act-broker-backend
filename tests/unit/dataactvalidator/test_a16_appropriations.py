20bbbd13bdd7f4d75b10f29472d0993551530c33
CM Lubinski <christopher.lubinski@gsa.gov>
2016-08-16 16:18:40 +0000

    Get more useful feedback from validation utils
    Rather than a all-or-nothing, return the actual rows. Adds a helper method for
    getting the column names present in the query (which are used when generating
    the UI) and replaces `fetch_sql_tpl` with the equivalent method in SQLLoader

--- tests/unit/dataactvalidator/test_a16_appropriations.py
+++ tests/unit/dataactvalidator/test_a16_appropriations.py
@@ -1,5 +1,5 @@
 from dataactcore.models.stagingModels import Appropriation
-from tests.unit.dataactvalidator.utils import models_are_valid
+from tests.unit.dataactvalidator.utils import error_rows, number_of_errors
 
 
 _FILE = 'a16_appropriations'
@@ -7,15 +7,15 @@ _FILE = 'a16_appropriations'
 
 def test_success(database):
     award = Appropriation(job_id=1, row_number=1)
-    assert models_are_valid(_FILE, database.stagingDb, award)
+    assert error_rows(_FILE, database.stagingDb, award) == []
 
 
 def test_null_authority(database):
     award = Appropriation(job_id=1, row_number=1, is_first_quarter=True)
-    assert not models_are_valid(_FILE, database.stagingDb, award)
+    assert number_of_errors(_FILE, database.stagingDb, award) == 1
 
 
 def test_nonnull_authority(database):
     award = Appropriation(job_id=1, row_number=1, is_first_quarter=True,
                           budget_authority_unobligat_fyb=5)
-    assert models_are_valid(_FILE, database.stagingDb, award)
+    assert error_rows(_FILE, database.stagingDb, award) == []
--- tests/unit/dataactvalidator/utils.py
+++ tests/unit/dataactvalidator/utils.py
@@ -1,26 +1,27 @@
-import os.path
 from random import randint
 
 from dataactvalidator.filestreaming.sqlLoader import SQLLoader
 
 
-# @todo: There's extra logic in SQLLoader around FieldCleaning which we may
-# need to run through here, too
-def fetch_sql_tpl(filename):
-    filename = os.path.join(SQLLoader.sql_rules_path, filename + ".sql")
-    with open(filename, "rU") as f:
-        return f.read()
-
-
-def models_are_valid(rule_file, staging_db, *models):
+def error_rows(rule_file, staging_db, *models):
     """Insert the models into the database (with a random submission id), then
     run the rule SQL against those models"""
     submission_id = randint(1, 9999)
-    sql = fetch_sql_tpl(rule_file).format(submission_id)
+    sql = SQLLoader.readSqlStr(rule_file).format(submission_id)
 
     for model in models:
         model.submission_id = submission_id
         staging_db.session.add(model)
 
     staging_db.session.commit()
-    return staging_db.connection.execute(sql).rowcount == 0
+
+    return staging_db.connection.execute(sql).fetchall()
+
+
+def number_of_errors(rule_file, staging_db, *models):
+    return len(error_rows(rule_file, staging_db, *models))
+
+
+def query_columns(rule_file, staging_db):
+    sql = SQLLoader.readSqlStr(rule_file).format(randint(1, 9999))
+    return staging_db.connection.execute(sql).keys()
