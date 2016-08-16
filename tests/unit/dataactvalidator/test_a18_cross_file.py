3eeb2686a0c159fbbd0fecfc633dd4685dfc81a4
CM Lubinski <christopher.lubinski@gsa.gov>
2016-08-16 16:20:55 +0000

    Add unit tests for a18_cross_file.sql

---
+++ tests/unit/dataactvalidator/test_a18_cross_file.py
@@ -0,0 +1,43 @@
+from random import randint
+
+from dataactcore.models.stagingModels import (
+    Appropriation, ObjectClassProgramActivity)
+from tests.unit.dataactvalidator.utils import number_of_errors, query_columns
+
+
+_FILE = 'a18_cross_file'
+
+
+def test_column_headers(database):
+    expected_subset = set([
+        'row_number', 'allocation_transfer_agency', 'agency_identifier',
+        'beginning_period_of_availa', 'ending_period_of_availabil',
+        'availability_type_code', 'main_account_code', 'sub_account_code',
+        'gross_outlay_amount_by_tas_cpe', 'gross_outlay_amount_by_pro_cpe_sum'
+    ])
+    actual = set(query_columns(_FILE, database.stagingDb))
+    assert (actual & expected_subset) == expected_subset
+
+
+def test_sum_matches(database):
+    op1_val, op2_val = randint(1, 9999), randint(1, 9999)
+    approp_val = op1_val + op2_val
+    op1 = ObjectClassProgramActivity(job_id=1, row_number=1, tas='TAS',
+                                     gross_outlay_amount_by_pro_cpe=op1_val)
+    op2 = ObjectClassProgramActivity(job_id=1, row_number=1, tas='TAS',
+                                     gross_outlay_amount_by_pro_cpe=op2_val)
+    approp = Appropriation(job_id=1, row_number=1, tas='TAS',
+                           gross_outlay_amount_by_tas_cpe=approp_val)
+    assert number_of_errors(_FILE, database.stagingDb, approp, op1, op2) == 0
+
+
+def test_sum_does_not_match(database):
+    op1_val, op2_val = randint(1, 9999), randint(1, 9999)
+    approp_val = op1_val + op2_val + randint(1, 9999)
+    op1 = ObjectClassProgramActivity(job_id=1, row_number=1, tas='TAS',
+                                     gross_outlay_amount_by_pro_cpe=op1_val)
+    op2 = ObjectClassProgramActivity(job_id=1, row_number=1, tas='TAS',
+                                     gross_outlay_amount_by_pro_cpe=op2_val)
+    approp = Appropriation(job_id=1, row_number=1, tas='TAS',
+                           gross_outlay_amount_by_tas_cpe=approp_val)
+    assert number_of_errors(_FILE, database.stagingDb, approp, op1, op2) == 1
