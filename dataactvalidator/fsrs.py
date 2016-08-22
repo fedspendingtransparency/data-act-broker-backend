20c9a3f8bf2e97656cc83cf8e0336a189717a823
CM Lubinski <christopher.lubinski@gsa.gov>
2016-08-22 19:48:50 +0000

    Add conversions between suds and FSRS models
    Includes some tests to spot-check logic

--- dataactvalidator/fsrs.py
+++ dataactvalidator/fsrs.py
@@ -1,10 +1,13 @@
+import itertools
 from urllib.parse import urlparse
 
+from suds import sudsobject
 from suds.client import Client
 from suds.transport.https import HttpAuthenticated
 from suds.xsd import doctor
 
 from dataactcore.config import CONFIG_BROKER
+from dataactcore.models.fsrs import FSRSAward, FSRSSubaward
 
 
 def newClient():
@@ -31,3 +34,82 @@ def newClient():
             password=serviceConfig['password'])
 
     return Client(**options)
+
+
+def soap2Dict(soapObj):
+    """A recursive version of sudsobject.asdict"""
+    if isinstance(soapObj, sudsobject.Object):
+        return {k: soap2Dict(v) for k, v in soapObj}
+    elif isinstance(soapObj, list):
+        return [soap2Dict(v) for v in soapObj]
+    return soapObj
+
+
+def commonAttributes(soapDict):
+    """Awards and SubAwards share some fields; set them up here. Accounts for
+    contained locations and top-paid employees"""
+    fields = (
+        'duns', 'company_name', 'dba_name', 'parent_duns',
+        'parent_company_name', 'naics', 'funding_agency_id',
+        'funding_agency_name', 'funding_office_id', 'funding_office_name',
+        'recovery_model_q1', 'recovery_model_q2'
+    )
+    modelAttrs = {field: soapDict.get(field) for field in fields}
+    modelAttrs['bus_types'] = ','.join(soapDict['bus_types'])
+
+    addrFields = ('city', 'street', 'state', 'country', 'zip', 'district')
+    prefixes = ('company_address', 'principle_place')
+    for prefix, field in itertools.product(prefixes, addrFields):
+        modelAttrs[prefix + '_' + field] = soapDict[prefix].get(field)
+
+    for idx in range(5):
+        idx = str(idx + 1)
+        if 'top_pay_employees' in soapDict:
+            info = soapDict['top_pay_employees']['employee_' + idx]
+            modelAttrs['top_paid_fullname_' + idx] = info['fullname']
+            modelAttrs['top_paid_amount_' + idx] = info['amount']
+
+    return modelAttrs
+
+
+def award2Model(soapDict):
+    """Convert SOAP webservice's dict into a FSRSAward ORM model, including
+    referenced FSRSSubaward"""
+    modelAttrs = commonAttributes(soapDict)
+    fields = (
+        'id', 'internal_id', 'contract_number', 'idv_reference_number',
+        'date_submitted', 'report_period_mon', 'report_period_year',
+        'report_type', 'contract_agency_code', 'contract_idv_agency_code',
+        'contracting_office_aid', 'contracting_office_aname',
+        'contracting_office_id', 'contracting_office_name', 'treasury_symbol',
+        'dollar_obligated', 'date_signed', 'transaction_type', 'program_title'
+    )
+    for field in fields:
+        modelAttrs[field] = soapDict.get(field)
+
+    modelAttrs['subawards'] = [
+        subaward2Model(sub) for sub in soapDict['subcontractors']
+    ]
+
+    return FSRSAward(**modelAttrs)
+
+
+def subaward2Model(soapDict):
+    """Convert SOAP webservice's dict into a FSRSSubaward ORM model"""
+    modelAttrs = commonAttributes(soapDict)
+    fields = (
+        'subcontract_amount', 'subcontract_date', 'subcontract_num',
+        'overall_description', 'recovery_subcontract_amt'
+    )
+    for field in fields:
+        modelAttrs[field] = soapDict.get(field)
+
+    return FSRSSubaward(**modelAttrs)
+
+
+def retrieveRecordsBatch(minId):
+    """The FSRS web service returns records in batches (500 at a time).
+    Retrieve one such batch, converting each result (and sub-results) into
+    dicts"""
+    for report in newClient().service.getData(id=minId)['reports']:
+        yield soap2Dict(report)
--- tests/unit/dataactvalidator/test_fsrs.py
+++ tests/unit/dataactvalidator/test_fsrs.py
@@ -1,4 +1,7 @@
+from datetime import date, datetime
 from unittest.mock import Mock
+
+from suds import sudsobject
 
 from dataactvalidator import fsrs
 
@@ -54,3 +57,82 @@ def test_newClient_auth(monkeypatch):
     call_args = newClient_call_args(monkeypatch, username='u1', password='p2')
     assert call_args['transport'].options.username == 'u1'
     assert call_args['transport'].options.password == 'p2'
+
+
+def test_soap2Dict():
+    today = date.today()
+
+    root = sudsobject.Object()
+    root.text = "Some text"
+    root.num = 1234
+    root.sub = sudsobject.Object()
+    root.sub.date = today
+    root.sub.name = "Sub value"
+    root.children = [sudsobject.Object(), sudsobject.Object()]
+    root.children[0].name = 'Obj 0'
+    root.children[1].name = 'Obj 1'
+    root.children[1].sub = sudsobject.Object()
+    root.children[1].sub.name = "Obj 1's Sub"
+
+    expected = dict(
+        text="Some text", num=1234,
+        sub=dict(date=today, name='Sub value'),
+        children=[
+            dict(name='Obj 0'),
+            dict(name='Obj 1', sub=dict(name="Obj 1's Sub"))
+        ]
+    )
+
+    assert fsrs.soap2Dict(root) == expected
+
+
+def test_commonAttributes():
+    """Spot check that data's imported to the proper fields"""
+    obj = dict(
+        duns='DuNs', recovery_model_q1=True, bus_types=['a', 'b', 'c'],
+        company_address=dict(city='CompanyCity', district='CompanyDist'),
+        principle_place=dict(street='PrincipleStreet'),
+        top_pay_employees=dict(
+            employee_1=dict(fullname='full1', amount='1'),
+            employee_2=dict(fullname='full2', amount='2'),
+            employee_3=dict(fullname='full3', amount='3'),
+            employee_4=dict(fullname='full4', amount='4'),
+            employee_5=dict(fullname='full5', amount='5')
+        )
+    )
+    expected = dict(
+        duns='DuNs', recovery_model_q1=True, bus_types='a,b,c',
+        company_address_city='CompanyCity',
+        company_address_district='CompanyDist',
+        principle_place_street='PrincipleStreet',
+        top_paid_fullname_1='full1', top_paid_amount_1='1',
+        top_paid_fullname_2='full2', top_paid_amount_2='2',
+        top_paid_fullname_3='full3', top_paid_amount_3='3',
+        top_paid_fullname_4='full4', top_paid_amount_4='4',
+        top_paid_fullname_5='full5', top_paid_amount_5='5'
+    )
+    result = fsrs.commonAttributes(obj)
+    for key, value in expected.items():
+        assert result.get(key) == value
+    for key, value in result.items():
+        assert expected.get(key) == value
+
+
+def test_award2Model_subcon2Model(monkeypatch):
+    monkeypatch.setattr(fsrs, 'commonAttributes', lambda _: {})
+    now = datetime.now()
+    award = dict(
+        id=5, internal_id='iii', date_signed=now,
+        subcontractors=[
+            dict(subcontract_amount='45', unused_field='Unused'),
+            dict(subcontract_num='67')
+        ]
+    )
+    result = fsrs.award2Model(award)
+
+    assert result.id == 5
+    assert result.internal_id == 'iii'
+    assert result.date_signed == now
+    assert len(result.subawards) == 2
+    assert result.subawards[0].subcontract_amount == '45'
+    assert result.subawards[1].subcontract_num == '67'
