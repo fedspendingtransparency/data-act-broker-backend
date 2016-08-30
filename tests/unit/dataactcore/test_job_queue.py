ffa5229db965e74adac264009d6f03cb80461817
CM Lubinski <christopher.lubinski@gsa.gov>
2016-08-30 22:03:51 +0000

    f -> d
    Thanks for catching, @nmonga91!

--- tests/unit/dataactcore/test_job_queue.py
+++ tests/unit/dataactcore/test_job_queue.py
@@ -8,17 +8,17 @@ import os
 
 
 @pytest.fixture()
-def mock_file_f_path(tmpdir):
+def mock_file_d_path(tmpdir):
     original = CONFIG_BROKER['d_file_storage_path']
     CONFIG_BROKER['d_file_storage_path'] = str(tmpdir) + os.path.sep
     yield tmpdir
     CONFIG_BROKER['d_file_storage_path'] = original
 
 
-def test_generate_d_file_success(monkeypatch, mock_file_f_path):
+def test_generate_d_file_success(monkeypatch, mock_file_d_path):
     """ Test successful generation of D1 and D2 files """
     local_file_name = "12345_test_file.csv"
-    file_path = mock_file_f_path.join(local_file_name)
+    file_path = mock_file_d_path.join(local_file_name)
 
     assert not os.path.isfile(str(file_path))
 
@@ -37,10 +37,10 @@ def test_generate_d_file_success(monkeypatch, mock_file_f_path):
     assert os.path.isfile(str(file_path))
 
 
-def test_generate_d_file_failure(monkeypatch, mock_file_f_path):
+def test_generate_d_file_failure(monkeypatch, mock_file_d_path):
     """ Test unsuccessful generation of D1 and D2 files """
     local_file_name = "12345_test_file.csv"
-    file_path = mock_file_f_path.join(local_file_name)
+    file_path = mock_file_d_path.join(local_file_name)
 
     assert not os.path.isfile(str(file_path))
 
