6e92b97a5d8e9b42b62d765932cd9c41b4560eb0
CM Lubinski <christopher.lubinski@gsa.gov>
2016-08-30 21:35:30 +0000

    Use a tempdir in the CONFIG_BROKER
    As the `generate_d_file` function accesses `CONFIG_BROKER` directly, we need
    to temporarily modify that global configuration during these tests. Also minor
    PEP8 stuff.

--- tests/unit/dataactcore/test_job_queue.py
+++ tests/unit/dataactcore/test_job_queue.py
@@ -1,16 +1,24 @@
 from dataactcore.utils.jobQueue import JobQueue
 from dataactcore.config import CONFIG_BROKER
 from unittest.mock import Mock
+import pytest
 from pytest import raises
 from dataactcore.utils.responseException import ResponseException
 import os
 
 
-def test_generate_d_file_success(monkeypatch, tmpdir):
+@pytest.fixture()
+def mock_file_f_path(tmpdir):
+    original = CONFIG_BROKER['d_file_storage_path']
+    CONFIG_BROKER['d_file_storage_path'] = str(tmpdir) + os.path.sep
+    yield tmpdir
+    CONFIG_BROKER['d_file_storage_path'] = original
+
+
+def test_generate_d_file_success(monkeypatch, mock_file_f_path):
     """ Test successful generation of D1 and D2 files """
     local_file_name = "12345_test_file.csv"
-
-    file_path = tmpdir.mkdir("tmp").join(local_file_name)
+    file_path = mock_file_f_path.join(local_file_name)
 
     assert not os.path.isfile(str(file_path))
 
@@ -21,18 +29,18 @@ def test_generate_d_file_success(monkeypatch, tmpdir):
     jq = JobQueue()
     monkeypatch.setattr(jq, 'update_d_file_status', Mock())
     monkeypatch.setattr(jq, 'download_file', Mock())
-    monkeypatch.setattr(jq, 'get_xml_response_content', Mock(return_value=result_xml))
+    monkeypatch.setattr(jq, 'get_xml_response_content',
+                        Mock(return_value=result_xml))
 
     jq.generate_d_file(Mock(), 1, 1, Mock(), local_file_name, True)
 
     assert os.path.isfile(str(file_path))
 
 
-def test_generate_d_file_failure(monkeypatch, tmpdir):
+def test_generate_d_file_failure(monkeypatch, mock_file_f_path):
     """ Test unsuccessful generation of D1 and D2 files """
     local_file_name = "12345_test_file.csv"
-
-    file_path = tmpdir.mkdir("tmp").join(local_file_name)
+    file_path = mock_file_f_path.join(local_file_name)
 
     assert not os.path.isfile(str(file_path))
 
@@ -40,7 +48,8 @@ def test_generate_d_file_failure(monkeypatch, tmpdir):
 
     jq = JobQueue()
     monkeypatch.setattr(jq, 'update_d_file_status', Mock())
-    monkeypatch.setattr(jq, 'get_xml_response_content', Mock(return_value=result_xml))
+    monkeypatch.setattr(jq, 'get_xml_response_content',
+                        Mock(return_value=result_xml))
 
-    with raises(ResponseException) as excinfo:
+    with raises(ResponseException):
         jq.generate_d_file(Mock(), 1, 1, Mock(), local_file_name, True)
