a102fc9aba17617b5cbb898fbd33f0fc748b33ed
CM Lubinski <christopher.lubinski@gsa.gov>
2016-09-01 16:05:57 +0000

    Abstract mock file paths to cover more configs
    Previously, we had a `mock_file_d_path` function, which modified the
    CONFIG_BROKER to use a temporary file path for the `d_file_storage_path`
    attribute. The same approach would be helpful for other path configs (starting
    with `broker_files`), so this patch abstracts that effort to allow multiple
    paths to be replaced with temporary dirs.

--- tests/unit/dataactcore/test_job_queue.py
+++ tests/unit/dataactcore/test_job_queue.py
@@ -1,5 +1,4 @@
 from dataactcore.utils import jobQueue
-from dataactcore.config import CONFIG_BROKER
 from unittest.mock import Mock
 import pytest
 from pytest import raises
@@ -7,18 +6,11 @@ from dataactcore.utils.responseException import ResponseException
 import os
 
 
-@pytest.fixture()
-def mock_file_d_path(tmpdir):
-    original = CONFIG_BROKER['d_file_storage_path']
-    CONFIG_BROKER['d_file_storage_path'] = str(tmpdir) + os.path.sep
-    yield tmpdir
-    CONFIG_BROKER['d_file_storage_path'] = original
-
-
-def test_generate_d_file_success(monkeypatch, mock_file_d_path):
+def test_generate_d_file_success(monkeypatch, mock_broker_config_paths):
     """ Test successful generation of D1 and D2 files """
     local_file_name = "12345_test_file.csv"
-    file_path = mock_file_d_path.join(local_file_name)
+    file_path = mock_broker_config_paths['d_file_storage_path'].join(
+        local_file_name)
 
     assert not os.path.isfile(str(file_path))
 
@@ -34,10 +26,11 @@ def test_generate_d_file_success(monkeypatch, mock_file_d_path):
     assert os.path.isfile(str(file_path))
 
 
-def test_generate_d_file_failure(monkeypatch, mock_file_d_path):
+def test_generate_d_file_failure(monkeypatch, mock_broker_config_paths):
     """ Test unsuccessful generation of D1 and D2 files """
     local_file_name = "12345_test_file.csv"
-    file_path = mock_file_d_path.join(local_file_name)
+    file_path = mock_broker_config_paths['d_file_storage_path'].join(
+        local_file_name)
 
     assert not os.path.isfile(str(file_path))
 
