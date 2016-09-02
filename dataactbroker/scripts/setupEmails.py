5562ccf245c803f3cf306e48ebaae242c935afa1
James_Worcester <Worcester_James@bah.com>
2016-09-02 13:39:48 -0400

    Setup emails closes the interface it uses directly

--- dataactbroker/scripts/setupEmails.py
+++ dataactbroker/scripts/setupEmails.py
@@ -73,7 +73,7 @@ def setupEmails():
     template = "[REV_USER_NAME] has shared a DATA Act broker submission with you. Click <a href='[REV_URL]'>here</a> to review their submission. For questions or comments, please email the DATA Act Broker Helpdesk (DATABroker@fiscal.treasury.gov)."
     userDb.loadEmailTemplate("DATA Act Broker - Submission Ready for Review", template, "review_submission")
 
-    InterfaceHolder.closeOne(userDb)
+    userDb.close()
 
 if __name__ == '__main__':
     setupEmails()
