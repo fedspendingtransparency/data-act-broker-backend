2d12eade74cbf823fd121529267c7fbb84bc5d40
CM Lubinski <christopher.lubinski@gsa.gov>
2016-09-08 12:35:55 +0000

    Pull out the enqueue method
    This should make it easier to test and reduce the size of the __init__.

--- dataactcore/utils/jobQueue.py
+++ dataactcore/utils/jobQueue.py
@@ -6,17 +6,28 @@ from dataactcore.config import CONFIG_DB, CONFIG_SERVICES, CONFIG_JOB_QUEUE, CON
 from dataactcore.utils.cloudLogger import CloudLogger
 
 
+def enqueue(jobID):
+    """POST a job to the validator"""
+    CloudLogger.log("Adding job {} to the queue".format(str(jobID)))
+    validatorUrl = '{validator_host}:{validator_port}'.format(
+        **CONFIG_SERVICES)
+    if 'http://' not in validatorUrl:
+        validatorUrl = 'http://' + validatorUrl
+    validatorUrl += '/validate/'
+    params = {
+        'job_id': jobID
+    }
+    response = requests.post(validatorUrl, params)
+    CloudLogger.log("Job {} has completed validation".format(str(jobID)))
+    CloudLogger.log("Validator response: {}".format(str(response.json())))
+    return response.json()
+
 
 class JobQueue:
     def __init__(self, job_queue_url="localhost"):
         # Set up backend persistent URL
         backendUrl = ''.join(['db+', CONFIG_DB['scheme'], '://', CONFIG_DB['username'], ':', CONFIG_DB['password'], '@',
                               CONFIG_DB['host'], '/', CONFIG_DB['job_queue_db_name']])
-
-        # Set up url to the validator for the RESTFul calls
-        validatorUrl = ''.join([CONFIG_SERVICES['validator_host'], ':', str(CONFIG_SERVICES['validator_port'])])
-        if 'http://' not in validatorUrl:
-            validatorUrl = ''.join(['http://', validatorUrl])
 
         # Set up url to the job queue to establish connection
         queueUrl = ''.join(
@@ -27,20 +38,7 @@ class JobQueue:
         self.jobQueue = Celery('tasks', backend=backendUrl, broker=queueUrl)
         self.jobQueue.config_from_object('celeryconfig')
 
-        @self.jobQueue.task(name='jobQueue.enqueue')
-        def enqueue(jobID):
-            # Don't need to worry about the response currently
-            CloudLogger.log("Adding job {} to the queue".format(str(jobID)))
-            url = ''.join([validatorUrl, '/validate/'])
-            params = {
-                'job_id': jobID
-            }
-            response = requests.post(url, params)
-            CloudLogger.log("Job {} has completed validation".format(str(jobID)))
-            CloudLogger.log("Validator response: {}".format(str(response.json())))
-            return response.json()
-
-        self.enqueue = enqueue
+        self.enqueue = self.jobQueue.task(name='jobQueue.enqueue')(enqueue)
 
 if __name__ in ['__main__', 'jobQueue']:
     jobQueue = JobQueue()
