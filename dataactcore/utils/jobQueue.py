36324c50ce33e2f8b0cdd8f0a23e525908d973a0
CM Lubinski <christopher.lubinski@gsa.gov>
2016-09-08 12:35:55 +0000

    Pull out the enqueue method
    This should make it easier to test and reduce the size of the __init__.

--- dataactcore/utils/jobQueue.py
+++ dataactcore/utils/jobQueue.py
@@ -5,17 +5,25 @@ import requests
 from dataactcore.config import CONFIG_DB, CONFIG_SERVICES, CONFIG_JOB_QUEUE, CONFIG_BROKER
 
 
+def enqueue(jobID):
+    """POST a job to the validator"""
+    validatorUrl = '{validator_host}:{validator_port}'.format(
+        **CONFIG_SERVICES)
+    if 'http://' not in validatorUrl:
+        validatorUrl = 'http://' + validatorUrl
+    validatorUrl += '/validate/'
+    params = {
+        'job_id': jobID
+    }
+    response = requests.post(validatorUrl, params)
+    return response.json()
+
 
 class JobQueue:
     def __init__(self, job_queue_url="localhost"):
         # Set up backend persistent URL
         backendUrl = ''.join(['db+', CONFIG_DB['scheme'], '://', CONFIG_DB['username'], ':', CONFIG_DB['password'], '@',
                               CONFIG_DB['host'], '/', CONFIG_DB['job_queue_db_name']])
-
-        # Set up url to the validator for the RESTFul calls
-        validatorUrl = ''.join([CONFIG_SERVICES['validator_host'], ':', str(CONFIG_SERVICES['validator_port'])])
-        if 'http://' not in validatorUrl:
-            validatorUrl = ''.join(['http://', validatorUrl])
 
         # Set up url to the job queue to establish connection
         queueUrl = ''.join(
@@ -25,17 +33,7 @@ class JobQueue:
         # Create remote connection to the job queue
         self.jobQueue = Celery('tasks', backend=backendUrl, broker=queueUrl)
 
-        @self.jobQueue.task(name='jobQueue.enqueue')
-        def enqueue(jobID):
-            # Don't need to worry about the response currently
-            url = ''.join([validatorUrl, '/validate/'])
-            params = {
-                'job_id': jobID
-            }
-            response = requests.post(url, params)
-            return response.json()
-
-        self.enqueue = enqueue
+        self.enqueue = self.jobQueue.task(name='jobQueue.enqueue')(enqueue)
 
 if __name__ in ['__main__', 'jobQueue']:
     jobQueue = JobQueue()
