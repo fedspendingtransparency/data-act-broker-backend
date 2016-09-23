de4c656aee46ba3c9f6bc8ddc66c66879d09576a
James_Worcester <Worcester_James@bah.com>
2016-09-23 15:53:34 -0400

    Second alternative to locals using CONFIG_CATEGORIES holding the actual dicts

--- dataactcore/read_config.py
+++ dataactcore/read_config.py
@@ -4,8 +4,13 @@ from os.path import expanduser, normpath, dirname, abspath
 import yaml
 import re
 
-CONFIG_CATEGORIES = {"broker":"CONFIG_BROKER", "services":"CONFIG_SERVICES", "db":"CONFIG_DB",
-                     "logging":"CONFIG_LOGGING","job-queue":"CONFIG_JOB_QUEUE"}
+CONFIG_BROKER = {}
+CONFIG_SERVICES = {}
+CONFIG_DB = {}
+CONFIG_LOGGING = {}
+CONFIG_JOB_QUEUE = {}
+CONFIG_CATEGORIES = {"broker":CONFIG_BROKER, "services":CONFIG_SERVICES, "db":CONFIG_DB,
+                     "logging":CONFIG_LOGGING,"job-queue":CONFIG_JOB_QUEUE}
 
 # set the location of the DATA Act broker config files
 CONFIG_PATH = os.path.join(dirname(abspath(__file__)), 'config.yml')
@@ -22,10 +27,6 @@ path_list = [CONFIG_PATH, ENV_PATH, SECRET_PATH]
 ALEMBIC_PATH = os.path.join(dirname(abspath(__file__)), 'alembic.ini')
 MIGRATION_PATH = os.path.join(dirname(abspath(__file__)), 'migrations')
 
-CONFIG_DICT = {}
-for value in CONFIG_CATEGORIES.values():
-    CONFIG_DICT[value] = {}
-
 for config_path in path_list:
     try:
         with open(config_path) as c:
@@ -35,59 +36,59 @@ for config_path in path_list:
            ' before starting the DATA Act broker: {}'.format(config_path))
 
     for category_name in CONFIG_CATEGORIES:
-        CONFIG_DICT[CONFIG_CATEGORIES[category_name]].update(CONFIG_ALL.get(category_name, {}))
+        CONFIG_CATEGORIES[category_name].update(CONFIG_ALL.get(category_name, {}))
 
 # Get path to installation
-CONFIG_DICT["CONFIG_BROKER"]['path'] = dirname(dirname(abspath(__file__)))
+CONFIG_BROKER['path'] = dirname(dirname(abspath(__file__)))
 
 # for backward-compatibility, differentiate between local runs and AWS
-if CONFIG_DICT["CONFIG_BROKER"]['use_aws'] is True or CONFIG_DICT["CONFIG_BROKER"]['use_aws'] == "true":
-    CONFIG_DICT["CONFIG_BROKER"]['local'] = False
+if CONFIG_BROKER['use_aws'] is True or CONFIG_BROKER['use_aws'] == "true":
+    CONFIG_BROKER['local'] = False
     # AWS flag is on, so make sure all needed AWS info is present
     required_aws_keys = ['aws_bucket', 'aws_role', 'aws_region', 'aws_create_temp_credentials',
                          'static_files_bucket', 'help_files_path']
     for k in required_aws_keys:
         try:
-            CONFIG_DICT["CONFIG_BROKER"][k]
+            CONFIG_BROKER[k]
         except KeyError as e:
             raise KeyError('Config error: use_aws is True, but the {} key is'
                 ' missing from the config.yml file'.format(k))
-        if not CONFIG_DICT["CONFIG_BROKER"][k]:
+        if not CONFIG_BROKER[k]:
             raise ValueError('Config error: use_aws is True but {} value is '
                  'missing'.format(k))
 
-    help_files_path = CONFIG_DICT["CONFIG_BROKER"]["help_files_path"]
-    CONFIG_DICT["CONFIG_BROKER"]["help_files_path"] = "".join([help_files_path, "/"]) if help_files_path[-1] != "/" else help_files_path
+    help_files_path = CONFIG_BROKER["help_files_path"]
+    CONFIG_BROKER["help_files_path"] = "".join([help_files_path, "/"]) if help_files_path[-1] != "/" else help_files_path
 else:
-    CONFIG_DICT["CONFIG_BROKER"]['local'] = True
-    CONFIG_DICT["CONFIG_BROKER"]['aws_bucket'] = None
-    CONFIG_DICT["CONFIG_BROKER"]['aws_role'] = None
-    CONFIG_DICT["CONFIG_BROKER"]['aws_create_temp_credentials'] = None
-    CONFIG_DICT["CONFIG_BROKER"]['aws_region'] = None
+    CONFIG_BROKER['local'] = True
+    CONFIG_BROKER['aws_bucket'] = None
+    CONFIG_BROKER['aws_role'] = None
+    CONFIG_BROKER['aws_create_temp_credentials'] = None
+    CONFIG_BROKER['aws_region'] = None
 
     # if not using AWS and no error report path specified,
     # default to `data_act_broker` in user's home dir
-    error_report_path = CONFIG_DICT["CONFIG_SERVICES"]['error_report_path']
+    error_report_path = CONFIG_SERVICES['error_report_path']
     if not error_report_path:
         error_report_path = os.path.join(expanduser('~'), 'data_act_broker')
     normpath(error_report_path)
-    CONFIG_DICT["CONFIG_SERVICES"]['error_report_path'] = error_report_path
+    CONFIG_SERVICES['error_report_path'] = error_report_path
 
     # if not using AWS and no DynamoDB settings specified, default
     # to localhost and port 8000
-    if not CONFIG_DICT["CONFIG_DB"]['dynamo_host']:
+    if not CONFIG_DB['dynamo_host']:
         # TODO: dynamo host/port don't do anything; code assumes localhost/8000
-        CONFIG_DICT["CONFIG_DB"]['dynamo_host'] = '127.0.0.1'
-    if not CONFIG_DICT["CONFIG_DB"]['dynamo_port']:
-        CONFIG_DICT["CONFIG_DB"]['dynamo_port'] = 8000
+        CONFIG_DB['dynamo_host'] = '127.0.0.1'
+    if not CONFIG_DB['dynamo_port']:
+        CONFIG_DB['dynamo_port'] = 8000
 
-storage_path = CONFIG_DICT["CONFIG_BROKER"]['d_file_storage_path']
+storage_path = CONFIG_BROKER['d_file_storage_path']
 if storage_path[-1] != os.path.sep:
-    CONFIG_DICT["CONFIG_BROKER"]['d_file_storage_path'] = "".join([storage_path, os.path.sep])
+    CONFIG_BROKER['d_file_storage_path'] = "".join([storage_path, os.path.sep])
 
 # if no broker file path specified,
 # default to `data_act_broker` in user's home dir
-broker_files = CONFIG_DICT["CONFIG_BROKER"]['broker_files']
+broker_files = CONFIG_BROKER['broker_files']
 if not broker_files:
     broker_files = os.path.join(expanduser('~'), 'data_act_broker')
 elif len(os.path.splitext(broker_files)[1]):
@@ -97,49 +98,45 @@ elif len(os.path.splitext(broker_files)[1]):
 normpath(broker_files)
 if broker_files[-1] != os.path.sep:
     broker_files += os.path.sep
-CONFIG_DICT["CONFIG_BROKER"]['broker_files'] = broker_files
+CONFIG_BROKER['broker_files'] = broker_files
 
 # normalize logging path, if given
-log_path = CONFIG_DICT["CONFIG_LOGGING"]['log_files']
+log_path = CONFIG_LOGGING['log_files']
 if log_path:
-    CONFIG_DICT["CONFIG_LOGGING"]['log_files'] = normpath(log_path)
+    CONFIG_LOGGING['log_files'] = normpath(log_path)
 
 # we don't want http:// or ports in the host variables
-CONFIG_DICT["CONFIG_SERVICES"]['broker_api_host'] = re.sub(
-    'http://|:(.*)', '', CONFIG_DICT["CONFIG_SERVICES"]['broker_api_host'])
-CONFIG_DICT["CONFIG_SERVICES"]['validator_host'] = re.sub(
-    'http://|:(.*)', '', CONFIG_DICT["CONFIG_SERVICES"]['validator_host'])
+CONFIG_SERVICES['broker_api_host'] = re.sub(
+    'http://|:(.*)', '', CONFIG_SERVICES['broker_api_host'])
+CONFIG_SERVICES['validator_host'] = re.sub(
+    'http://|:(.*)', '', CONFIG_SERVICES['validator_host'])
 # if hosts in config file are set to 0.0.0.0, override to
 # 127.0.0.1 for cross-platform compatibility
-if CONFIG_DICT["CONFIG_SERVICES"]['broker_api_host'] == '0.0.0.0':
-    CONFIG_DICT["CONFIG_SERVICES"]['broker_api_host'] = '127.0.0.1'
-if CONFIG_DICT["CONFIG_SERVICES"]['validator_host'] == '0.0.0.0':
-    CONFIG_DICT["CONFIG_SERVICES"]['validator_host'] = '127.0.0.1'
-if CONFIG_DICT["CONFIG_DB"]['dynamo_host'] == '0.0.0.0':
-    CONFIG_DICT["CONFIG_DB"]['dynamo_host'] = '127.0.0.1'
+if CONFIG_SERVICES['broker_api_host'] == '0.0.0.0':
+    CONFIG_SERVICES['broker_api_host'] = '127.0.0.1'
+if CONFIG_SERVICES['validator_host'] == '0.0.0.0':
+    CONFIG_SERVICES['validator_host'] = '127.0.0.1'
+if CONFIG_DB['dynamo_host'] == '0.0.0.0':
+    CONFIG_DB['dynamo_host'] = '127.0.0.1'
 
-if CONFIG_DICT["CONFIG_SERVICES"]["broker_api_port"] == 443:
+if CONFIG_SERVICES["broker_api_port"] == 443:
     # Use https
-    CONFIG_DICT["CONFIG_SERVICES"]["protocol"] = "https"
+    CONFIG_SERVICES["protocol"] = "https"
 else:
-    CONFIG_DICT["CONFIG_SERVICES"]["protocol"] = "http"
+    CONFIG_SERVICES["protocol"] = "http"
 
 # Log some values from config
 log_message = ""
-if "values_to_log" in CONFIG_DICT["CONFIG_LOGGING"]:
+if "values_to_log" in CONFIG_LOGGING:
     # If no values specified, don't do logging
-    for category_yaml_name in CONFIG_DICT["CONFIG_LOGGING"]["values_to_log"]:
-        category_name = CONFIG_CATEGORIES[category_yaml_name]
-        category = CONFIG_DICT[category_name]
-        category_message = "### {}".format(category_name)
-        for key in CONFIG_DICT["CONFIG_LOGGING"]["values_to_log"][category_yaml_name]:
+    for category_yaml_name in CONFIG_LOGGING["values_to_log"]:
+        category = CONFIG_CATEGORIES[category_yaml_name]
+        category = category
+        category_message = "### {}".format(category_yaml_name)
+        for key in CONFIG_LOGGING["values_to_log"][category_yaml_name]:
             value = category[key]
             category_message = "{}, {}: {}".format(category_message, key,value)
         log_message = " ".join([log_message, category_message])
-
-# Set up variables to be imported in other files
-for category_name in CONFIG_CATEGORIES.values():
-    locals()[category_name] = CONFIG_DICT[category_name]
 
 # TODO: error-handling for db config?
 # TODO: type checking and fixing for int stuff like ports?
