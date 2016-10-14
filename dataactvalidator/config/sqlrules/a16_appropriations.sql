9d73c064972ed00696bd233e6a970f77fb907f30
James_Worcester <Worcester_James@bah.com>
2016-10-14 12:27:09 -0400

    Working on A16 checking for previous submissions from an agency

--- dataactvalidator/config/sqlrules/a16_appropriations.sql
+++ dataactvalidator/config/sqlrules/a16_appropriations.sql
@@ -2,5 +2,5 @@ SELECT row_number,
 	budget_authority_unobligat_fyb
 FROM appropriation
 WHERE submission_id = {0}
-	AND is_first_quarter = TRUE
+	AND EXISTS (SELECT 1 FROM submission WHERE submission_id <> {0} AND cgac_code = (subquery to get cgac))
 	AND budget_authority_unobligat_fyb IS NOT DISTINCT FROM NULL;
