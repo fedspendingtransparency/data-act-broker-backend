e631efb93f82a4388dcc455880070e037cadd071
Rebecca Sweger <rebecca.sweger@gsa.gov>
2016-10-15 16:55:12 -0400

    Remove database interfaces from ValidatorTest setup
    The db code in the setup class didn't appear to be doing
    anything, so this changeset removes it, along with a
    now-unused function in the validation interface.

--- tests/integration/validatorTests.py
+++ tests/integration/validatorTests.py
@@ -1,4 +1,5 @@
 import unittest
+
 from dataactcore.models.validationModels import FieldType, FileColumn
 from dataactvalidator.validation_handlers.validator import Validator
 from tests.integration.baseTestValidator import BaseTestValidator
@@ -39,7 +40,6 @@ class ValidatorTests(BaseTestValidator):
         column1.required = True
         column1.field_type = stringType
         column1.file_id = 1
-        cls.interfaces.validationDb.populateFile(column1)
 
         column2 = FileColumn()
         column2.file_column_id = 2
@@ -47,7 +47,6 @@ class ValidatorTests(BaseTestValidator):
         column2.required = True
         column2.field_type = floatType
         column2.file_id = 1
-        cls.interfaces.validationDb.populateFile(column2)
 
         column3 = FileColumn()
         column3.file_column_id = 3
@@ -55,7 +54,6 @@ class ValidatorTests(BaseTestValidator):
         column3.required = True
         column3.field_type = booleanType
         column3.file_id = 1
-        cls.interfaces.validationDb.populateFile(column3)
 
         column4 = FileColumn()
         column4.file_column_id = 3
@@ -63,7 +61,6 @@ class ValidatorTests(BaseTestValidator):
         column4.required = True
         column4.field_type = intType
         column4.file_id = 1
-        cls.interfaces.validationDb.populateFile(column4)
 
         column5 = FileColumn()
         column5.file_column_id = 3
@@ -71,7 +68,6 @@ class ValidatorTests(BaseTestValidator):
         column5.required = False
         column5.field_type = intType
         column5.file_id = 1
-        cls.interfaces.validationDb.populateFile(column5)
 
         column6 = FileColumn()
         column6.file_column_id = 6
@@ -79,7 +75,6 @@ class ValidatorTests(BaseTestValidator):
         column6.required = False
         column6.field_type = stringType
         column6.file_id = 1
-        cls.interfaces.validationDb.populateFile(column6)
 
         column7 = FileColumn()
         column7.file_column_id = 7
@@ -87,7 +82,6 @@ class ValidatorTests(BaseTestValidator):
         column7.required = False
         column7.field_type = longType
         column7.file_id = 1
-        cls.interfaces.validationDb.populateFile(column7)
 
         cls.schema = {
             "test1": column1,
