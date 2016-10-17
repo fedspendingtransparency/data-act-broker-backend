1bd46464eaa5b6b7314ef7e8a9c9e4f0e719f637
Michael Bray <bray_michael@bah.com>
2016-10-17 14:13:15 -0400

    Updating C8 to account for both FAIN and URI being defined in the same record

--- dataactvalidator/config/sqlrules/c8_award_financial.sql
+++ dataactvalidator/config/sqlrules/c8_award_financial.sql
@@ -1,15 +1,22 @@
 SELECT DISTINCT af.row_number, af.fain, af.uri
 FROM award_financial AS af
-	JOIN award_financial_assistance AS afa
-		ON (af.submission_id = afa.submission_id
-			AND (
-				af.fain IS DISTINCT FROM afa.fain
-				OR af.uri IS DISTINCT FROM afa.uri
-			)
-		)
 WHERE af.submission_id = {0}
-	AND NOT EXISTS (
+    AND NOT EXISTS (
         SELECT cgac_code
         FROM cgac
         WHERE cgac_code = af.allocation_transfer_agency
+	)
+	AND (
+		af.row_number NOT IN (
+		SELECT af.row_number
+		FROM award_financial AS af
+			JOIN award_financial_assistance AS afa
+				ON (af.submission_id = afa.submission_id
+					AND (af.fain IS NOT DISTINCT FROM afa.fain
+						AND af.uri IS NOT DISTINCT FROM afa.uri)
+				)
+		WHERE af.submission_id = {0}
+	) OR
+		af.fain IS DISTINCT FROM NULL
+		AND af.uri IS DISTINCT FROM NULL
 	);
