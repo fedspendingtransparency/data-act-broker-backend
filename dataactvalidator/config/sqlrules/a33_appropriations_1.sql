a2de4f9ab5b5d85ea21fc7f97f0fc40ec124f8e0
Michael Bray <bray_michael@bah.com>
2016-10-19 16:16:29 -0400

    Fixed implementation issue in A33 and updated unit tests appropriately

--- dataactvalidator/config/sqlrules/a33_appropriations_1.sql
+++ dataactvalidator/config/sqlrules/a33_appropriations_1.sql
@@ -5,13 +5,15 @@ SELECT DISTINCT NULL as row_number,
 	sf.ending_period_of_availabil,
 	sf.availability_type_code,
 	sf.main_account_code,
-	sf.sub_account_code,
-	NULL as row_number
+	sf.sub_account_code
 FROM sf_133 AS sf
 	JOIN submission AS sub
 		ON sf.period = sub.reporting_fiscal_period
 			AND sf.fiscal_year = sub.reporting_fiscal_year
-			AND sf.agency_identifier = sub.cgac_code
+			AND (
+			    (sf.agency_identifier = sub.cgac_code AND sf.allocation_transfer_agency is null)
+			    OR (sf.allocation_transfer_agency = sub.cgac_code)
+			)
 WHERE sub.submission_id = {0}
 	AND NOT EXISTS (
 		SELECT 1
