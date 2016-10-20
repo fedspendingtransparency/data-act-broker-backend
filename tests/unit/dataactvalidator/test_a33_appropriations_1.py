47c19441cf7ee5a1e5cc3b2edda429f02dec880f
Michael Bray <bray_michael@bah.com>
2016-10-20 11:31:51 -0400

    Updating tests per PR comments

--- tests/unit/dataactvalidator/test_a33_appropriations_1.py
+++ tests/unit/dataactvalidator/test_a33_appropriations_1.py
@@ -24,15 +24,12 @@ def test_success_populated_ata(database):
     tas, period, year, code = 'some-tas', 2, 2002, 'some-code'
 
     sf1 = SF133Factory(tas=tas, period=period, fiscal_year=year,
-                       allocation_transfer_agency=code, agency_identifier=code)
-    sf2 = SF133Factory(tas=tas, period=period, fiscal_year=year,
-                       allocation_transfer_agency=code, agency_identifier=code)
+                       allocation_transfer_agency=code, agency_identifier='some-other-code')
     submission = SubmissionFactory(submission_id=submission_id, reporting_fiscal_period=period,
                                     reporting_fiscal_year=year, cgac_code=code)
     ap = AppropriationFactory(tas=tas, submission_id=submission_id)
 
-    errors = number_of_errors(_FILE, database,
-                              models=[sf1, sf2, ap], submission=submission)
+    errors = number_of_errors(_FILE, database, models=[sf1, ap], submission=submission)
     assert errors == 0
 
 
@@ -43,14 +40,11 @@ def test_success_null_ata(database):
 
     sf1 = SF133Factory(tas=tas, period=period, fiscal_year=year,
                        allocation_transfer_agency=None, agency_identifier=code)
-    sf2 = SF133Factory(tas=tas, period=period, fiscal_year=year,
-                       allocation_transfer_agency=None, agency_identifier=code)
     submission = SubmissionFactory(submission_id=submission_id, reporting_fiscal_period=period,
                                     reporting_fiscal_year=year, cgac_code=code)
     ap = AppropriationFactory(tas=tas, submission_id=submission_id)
 
-    errors = number_of_errors(_FILE, database,
-                              models=[sf1, sf2, ap], submission=submission)
+    errors = number_of_errors(_FILE, database, models=[sf1, ap], submission=submission)
     assert errors == 0
 
 
@@ -61,14 +55,12 @@ def test_failure_populated_ata(database):
 
     sf1 = SF133Factory(tas=tas, period=period, fiscal_year=year,
                        allocation_transfer_agency=code, agency_identifier=code)
-    sf2 = SF133Factory(tas=tas, period=period, fiscal_year=year,
-                       allocation_transfer_agency=code, agency_identifier=code)
     submission = SubmissionFactory(submission_id=submission_id, reporting_fiscal_period=period,
                                     reporting_fiscal_year=year, cgac_code=code)
     ap = AppropriationFactory(tas='a-different-tas', submission_id=submission_id)
 
-    errors = number_of_errors(_FILE, database, models=[sf1, sf2, ap], submission=submission)
-    assert errors == 2
+    errors = number_of_errors(_FILE, database, models=[sf1, ap], submission=submission)
+    assert errors == 1
 
 def test_failure_null_ata(database):
     """ Tests that TAS for SF-133 are not present in File A """
@@ -77,11 +69,9 @@ def test_failure_null_ata(database):
 
     sf1 = SF133Factory(tas=tas, period=period, fiscal_year=year,
                        allocation_transfer_agency=None, agency_identifier=code)
-    sf2 = SF133Factory(tas=tas, period=period, fiscal_year=year,
-                       allocation_transfer_agency=None, agency_identifier=code)
     submission = SubmissionFactory(submission_id=submission_id, reporting_fiscal_period=period,
                                     reporting_fiscal_year=year, cgac_code=code)
     ap = AppropriationFactory(tas='a-different-tas', submission_id=submission_id)
 
-    errors = number_of_errors(_FILE, database, models=[sf1, sf2, ap], submission=submission)
-    assert errors == 2
+    errors = number_of_errors(_FILE, database, models=[sf1, ap], submission=submission)
+    assert errors == 1
