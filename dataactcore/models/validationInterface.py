c5e14131cf9a188bc28ce5b4b003c96d82c22a13
Rebecca Sweger <rebecca.sweger@gsa.gov>
2016-11-04 18:25:03 -0400

    Remove validationInterface.py
    Thus ends the era of validationInterface.py.

--- dataactcore/models/validationInterface.py
+++
@@ -1,7 +0,0 @@
-from dataactcore.models.baseInterface import BaseInterface
-
-class ValidationInterface(BaseInterface):
-    """Manages all interaction with the validation/staging database."""
-
-    def __init__(self):
-        super(ValidationInterface, self).__init__()
