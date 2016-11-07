73683bf73f8e81d838a3ac9325bf8d91d31f4b4a
CM Lubinski <christopher.lubinski@gsa.gov>
2016-11-07 16:59:28 +0000

    Replicate CloudLogger with logging formatter
    This should be a temporary measure, so there's only one test. It's also not
    being actively used anywhere yet

--- dataactcore/logging.py
+++ dataactcore/logging.py
@@ -1,7 +1,11 @@
+from datetime import datetime
+import json
 import logging
 import os.path
+import traceback
 
 from dataactcore.config import CONFIG_LOGGING
+from dataactcore.utils.responseException import ResponseException
 
 
 def deep_merge(left, right):
@@ -28,7 +32,11 @@ DEFAULT_CONFIG = {
     'formatters': {
         'default': {
             'format': "%(asctime)s %(levelname)s:%(name)s:%(message)s"
-        }
+        },
+        'deprecated.exception': {
+            '()': 'dataactcore.logging.DeprecatedJSONFormatter',
+            'format': "%(asctime)s %(levelname)s:%(name)s:%(message)s",
+        },
     },
     'handlers': {
         'default': {
@@ -39,6 +47,11 @@ DEFAULT_CONFIG = {
             'formatter': 'default',
             'class': 'logging.FileHandler',
             'filename': os.path.join(CONFIG_LOGGING['log_files'], 'debug.log')
+        },
+        'deprecated.exception': {
+            'formatter': 'deprecated.exception',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(CONFIG_LOGGING['log_files'], 'error.log')
         },
         'deprecated.info': {
             'formatter': 'default',
@@ -63,6 +76,11 @@ DEFAULT_CONFIG = {
             'level': 'DEBUG',
             'propagate': False
         },
+        'deprecated.exception': {
+            'handlers': ['deprecated.exception'],
+            'level': 'ERROR',
+            'propagate': False
+        },
         'deprecated.info': {
             'handlers': ['deprecated.info'],
             'level': 'DEBUG',
@@ -82,3 +100,44 @@ def configure_logging():
     if 'python_config' in CONFIG_LOGGING:
         config = deep_merge(config, CONFIG_LOGGING['python_config'])
     logging.config.dictConfig(config)
+
+
+class DeprecatedJSONFormatter(logging.Formatter):
+    """Formats messages into the JSON CloudLogger used to generate."""
+    def formatException(self, exc_info):
+        type_, exception, tb = exc_info
+        trace = traceback.extract_tb(tb, 10)
+        data = {
+            'error_log_type': str(type_),
+            'error_log_message': str(exception),
+            'error_log_wrapped_message': '',
+            'error_log_wrapped_type': '',
+            'error_log_trace': trace,
+            'error_timestamp': str(datetime.utcnow()),
+        }
+        if (isinstance(exception, ResponseException) and
+                exception.wrappedException):
+            wrapped = exception.wrappedException
+            data['error_log_wrapped_type'] = str(type(wrapped))
+            data['error_log_wrapped_message'] = str(wrapped)
+        return json.dumps(data)
+
+    def format(self, record):
+        """Copy-pasta of the built in `format` function, except that we don't
+        add newlines in between the message and exception"""
+        record.message = record.getMessage()
+        if self.usesTime():
+            record.asctime = self.formatTime(record, self.datefmt)
+        s = self.formatMessage(record)
+        if record.exc_info:
+            # Cache the traceback text to avoid converting it multiple times
+            # (it's constant anyway)
+            if not record.exc_text:
+                record.exc_text = self.formatException(record.exc_info)
+        if record.exc_text:
+            s = s + record.exc_text
+        # We also don't include the additional stack info, as it's part of the
+        # exception
+        # if record.stack_info:
+        #     s = s + self.formatStack(record.stack_info)
+        return s
