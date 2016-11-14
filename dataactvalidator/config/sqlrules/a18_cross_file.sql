b0e467c01e9012ed04a34279b22204734b24433e
CM Lubinski <christopher.lubinski@gsa.gov>
2016-11-14 00:28:38 +0000

    Update A18 to use tas_id
    Also switches tests to using a factory

--- dataactvalidator/config/sqlrules/a18_cross_file.sql
+++ dataactvalidator/config/sqlrules/a18_cross_file.sql
@@ -11,7 +11,7 @@ SELECT
 	SUM(op.gross_outlay_amount_by_pro_cpe) as gross_outlay_amount_by_pro_cpe_sum
 FROM appropriation AS approp
 	JOIN object_class_program_activity op
-		ON approp.tas = op.tas
+		ON approp.tas_id = op.tas_id
 			AND approp.submission_id = op.submission_id
 WHERE approp.submission_id = {}
 GROUP BY approp.row_number,
