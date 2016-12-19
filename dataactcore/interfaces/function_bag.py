e8412cc7c3a734508fa558ec5f7ac495a09f7c6a
alburde1 <Burdeyny_Alisa@bah.com>
2016-12-19 14:07:00 -0500

    Removing a couple more functions that nothing uses

--- dataactcore/interfaces/function_bag.py
+++ dataactcore/interfaces/function_bag.py
@@ -204,19 +204,6 @@ def getErrorMetricsByJobId(job_id, include_file_types=False, severity_id=None):
 
 """ USER DB FUNCTIONS """
 
-def clearPassword(user):
-    """ Clear a user's password as part of reset process
-
-    Arguments:
-        user - User object
-
-    """
-    sess = GlobalDB.db().session
-    user.salt = None
-    user.password_hash = None
-    sess.commit()
-
-
 def updateLastLogin(user, unlock_user=False):
     """ This updates the last login date to today's datetime for the user to the current date upon successful login.
     """
@@ -253,27 +240,6 @@ def check_correct_password(user, password, bcrypt):
 
     # Check the password with bcrypt
     return bcrypt.check_password_hash(user.password_hash, password + user.salt)
-
-
-def set_user_password(user, password, bcrypt):
-    """ Given a user and a new password, changes the hashed value in the database to match new password.
-
-    Arguments:
-        user - User object
-        password - password to be set
-        bcrypt - bcrypt to use for password hashing
-    Returns:
-         True if successful
-    """
-    sess = GlobalDB.db().session
-    # Generate hash with bcrypt and store it
-    new_salt = uuid.uuid4().hex
-    user.salt = new_salt
-    password_hash = bcrypt.generate_password_hash(password + new_salt, HASH_ROUNDS)
-    user.password_hash = password_hash.decode("utf-8")
-    sess.commit()
-    return True
-
 
 def get_submission_stats(submission_id):
     """Get summarized dollar amounts by submission."""
