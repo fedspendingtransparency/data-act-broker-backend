e0c72271a473ccfc640e56b8111d3f7084d21377
CM Lubinski <christopher.lubinski@gsa.gov>
2017-01-03 22:06:14 +0000

    Look up CARS financial indicator2 field in A33

--- dataactvalidator/config/sqlrules/a33_appropriations_1.sql
+++ dataactvalidator/config/sqlrules/a33_appropriations_1.sql
@@ -1,3 +1,5 @@
+-- Verify that all of the applicable GTASes have an associated entry in the
+-- submission (file A).
 SELECT DISTINCT NULL as row_number,
     sf.allocation_transfer_agency,
 	sf.agency_identifier,
@@ -14,10 +16,13 @@ FROM sf_133 AS sf
 			    (sf.agency_identifier = sub.cgac_code AND sf.allocation_transfer_agency is null)
 			    OR (sf.allocation_transfer_agency = sub.cgac_code)
 			)
+        LEFT JOIN tas_lookup ON (tas_lookup.tas_id = sf.tas_id)
 WHERE sub.submission_id = {0}
 	AND NOT EXISTS (
 		SELECT 1
 		FROM appropriation AS approp
 		WHERE sf.tas IS NOT DISTINCT FROM approp.tas
 			AND approp.submission_id = {0}
-	);
+	)
+        AND tas_lookup.financial_indicator2 IS DISTINCT FROM 'F'
+;
