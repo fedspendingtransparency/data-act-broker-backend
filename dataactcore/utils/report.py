323c9ff75908bbdc2f0c5de5d4c83ab83e63d372
CM Lubinski <christopher.lubinski@gsa.gov>
2017-01-04 16:06:05 +0000

    Add unified method for generating report filenames
    We'll need to surface this method to API users

--- dataactcore/utils/report.py
+++ dataactcore/utils/report.py
@@ -9,18 +9,32 @@ def get_report_path(job, report_type):
     Return the filename for the error report.
     Does not include the folder to avoid conflicting with the S3 getSignedUrl method.
     """
-    path = 'submission_{}_{}_{}_report.csv'.format(job.submission_id, job.file_type.name, report_type)
-    return path
+    return report_file_name(job.submission_id, report_type == 'warning',
+                            job.file_type.name)
 
 
-def get_cross_report_name(submissionId, source_file, target_file):
+def get_cross_report_name(submission_id, source_file, target_file):
     """Return filename for a cross file error report."""
-    return "submission_{}_cross_{}_{}.csv".format(submissionId, source_file, target_file)
+    return report_file_name(submission_id, False, source_file, target_file)
 
 
-def get_cross_warning_report_name(submissionId, source_file, target_file):
+def get_cross_warning_report_name(submission_id, source_file, target_file):
     """Return filename for a cross file warning report."""
-    return "submission_{}_cross_warning_{}_{}.csv".format(submissionId, source_file, target_file)
+    return report_file_name(submission_id, True, source_file, target_file)
+
+
+def report_file_name(submission_id, warning, file_type, cross_type=None):
+    """Format the csv file name for the requested file.
+    @todo: unify these file names"""
+    if cross_type:
+        report_type_str = 'warning_' if warning else ''
+        return "submission_{}_cross_{}{}_{}.csv".format(
+            submission_id, report_type_str, file_type, cross_type)
+    else:
+        report_type_str = 'warning' if warning else 'error'
+        return "submission_{}_{}_{}_report.csv".format(
+            submission_id, file_type, report_type_str)
+
 
 def get_cross_file_pairs():
     """
