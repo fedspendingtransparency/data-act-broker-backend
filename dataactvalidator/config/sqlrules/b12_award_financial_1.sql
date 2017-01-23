eac104693d6ad1406e068b26283aaa00f1eb7835
kkuratsu <kuratsu_kate@bah.com>
2017-01-23 10:40:54 -0500

    Modify B12 to be able to submit blank D/R fields to 4831, 4832, and 4931

--- dataactvalidator/config/sqlrules/b12_award_financial_1.sql
+++ dataactvalidator/config/sqlrules/b12_award_financial_1.sql
@@ -5,15 +5,12 @@ FROM award_financial AS af
 WHERE af.submission_id = {} AND (
     ussgl480100_undelivered_or_fyb IS NOT NULL
     OR ussgl480100_undelivered_or_cpe IS NOT NULL
-    OR ussgl483100_undelivered_or_cpe IS NOT NULL
     OR ussgl488100_upward_adjustm_cpe IS NOT NULL
     OR ussgl490100_delivered_orde_fyb IS NOT NULL
     OR ussgl490100_delivered_orde_cpe IS NOT NULL
-    OR ussgl493100_delivered_orde_cpe IS NOT NULL
     OR ussgl498100_upward_adjustm_cpe IS NOT NULL
     OR ussgl480200_undelivered_or_fyb IS NOT NULL
     OR ussgl480200_undelivered_or_cpe IS NOT NULL
-    OR ussgl483200_undelivered_or_cpe IS NOT NULL
     OR ussgl488200_upward_adjustm_cpe IS NOT NULL
     OR ussgl490200_delivered_orde_cpe IS NOT NULL
     OR ussgl490800_authority_outl_fyb IS NOT NULL
--- dataactvalidator/config/sqlrules/b12_object_class_program_activity_1.sql
+++ dataactvalidator/config/sqlrules/b12_object_class_program_activity_1.sql
@@ -6,7 +6,6 @@ FROM object_class_program_activity as op
 WHERE op.submission_id = {} AND (
     COALESCE(op.ussgl480100_undelivered_or_fyb, 0) <> 0
     OR COALESCE(op.ussgl480100_undelivered_or_cpe, 0) <> 0
-    OR COALESCE(op.ussgl483100_undelivered_or_cpe, 0) <> 0
     OR COALESCE(op.ussgl488100_upward_adjustm_cpe, 0) <> 0
     OR COALESCE(op.ussgl490100_delivered_orde_fyb, 0) <> 0
     OR COALESCE(op.ussgl490100_delivered_orde_cpe, 0) <> 0
