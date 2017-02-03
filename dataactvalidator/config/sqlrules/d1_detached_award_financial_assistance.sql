b63b2b8816f5c88c5e9a62c53ef1cb99621a1482
alburde1 <Burdeyny_Alisa@bah.com>
2017-02-03 11:54:09 -0500

    Fixing aliasing and introducing second half of rule D8

--- dataactvalidator/config/sqlrules/d1_detached_award_financial_assistance.sql
+++ dataactvalidator/config/sqlrules/d1_detached_award_financial_assistance.sql
@@ -3,7 +3,7 @@ SELECT
     row_number,
     record_type,
     fain
-FROM detached_award_financial_assistance as dafa
-WHERE dafa.submission_id = {0}
-    AND dafa.record_type = 2
-    AND dafa.fain IS NULL
+FROM detached_award_financial_assistance
+WHERE submission_id = {0}
+    AND record_type = 2
+    AND (fain = '' OR fain IS NULL)
