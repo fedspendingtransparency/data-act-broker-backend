77315cd15f17914d36c34abc36b2e2ff55120abd
Nipun Monga <nipunmnga@gmail.com>
2017-02-03 15:06:49 -0500

    Adding rule D7 and related tests

---
+++ dataactvalidator/config/sqlrules/d7_detached_award_financial_assistance.sql
@@ -0,0 +1,8 @@
+-- URI is a required field for aggregate records (i.e., when RecordType = 1)
+SELECT
+    row_number,
+    uri
+FROM detached_award_financial_assistance
+WHERE submission_id = {0}
+    AND record_type = 1
+    AND (uri IS NULL or uri = '');
