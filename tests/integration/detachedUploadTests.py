a78d67276efcc7d701942bb80d0773109e9af846
alburde1 <Burdeyny_Alisa@bah.com>
2017-02-14 15:54:10 -0500

    Updating integration tests

--- tests/integration/detachedUploadTests.py
+++ tests/integration/detachedUploadTests.py
@@ -5,6 +5,7 @@ from dataactbroker.app import create_app
 from dataactcore.interfaces.db import GlobalDB
 from dataactcore.models.userModel import User
 from dataactcore.models.jobModels import Submission
+from dataactcore.models.stagingModels import DetachedAwardFinancialAssistance
 from dataactcore.models.lookups import PUBLISH_STATUS_DICT
 
 
@@ -27,6 +28,12 @@ class DetachedUploadTests(BaseTestAPI):
             # setup submission/jobs data for test_check_status
             cls.d2_submission = cls.insert_submission(sess, cls.submission_user_id, cgac_code="SYS",
                                                       start_date="10/2015", end_date="12/2015", is_quarter=True)
+
+            cls.d2_submission_dupe = cls.insert_submission(sess, cls.submission_user_id, cgac_code="SYS",
+                                                           start_date="10/2015", end_date="12/2015", is_quarter=True)
+
+            cls.d2_submission_dupe_2 = cls.insert_submission(sess, cls.submission_user_id, cgac_code="SYS",
+                                                             start_date="10/2015", end_date="12/2015", is_quarter=True)
 
             cls.published_submission = cls.insert_submission(sess, cls.submission_user_id, cgac_code="SYS",
                                                              start_date="10/2015", end_date="12/2015", is_quarter=True,
@@ -61,6 +68,17 @@ class DetachedUploadTests(BaseTestAPI):
         self.assertEqual(response.status_code, 400)
         self.assertEqual("This is not a FABS submission", response.json["message"])
 
+    def test_duplicate_entry(self):
+        self.insert_duplicate_detached_award()
+        submission = {"submission_id": self.d2_submission_dupe}
+        response = self.app.post_json("/v1/submit_detached_file/", submission,
+                                      headers={"x-session-id": self.session_id})
+
+        submission = {"submission_id": self.d2_submission_dupe_2}
+        response = self.app.post_json("/v1/submit_detached_file/", submission,
+                                      headers={"x-session-id": self.session_id}, expect_errors=True)
+        self.assertEqual(response.status_code, 500)
+
     @staticmethod
     def insert_submission(sess, submission_user_id, cgac_code=None, start_date=None, end_date=None,
                           is_quarter=False, publish_status_id=1, d2_submission=True):
@@ -76,3 +94,19 @@ class DetachedUploadTests(BaseTestAPI):
         sess.add(sub)
         sess.commit()
         return sub.submission_id
+
+    def insert_duplicate_detached_award(self):
+        det_award = DetachedAwardFinancialAssistance(datetime_utc=datetime.utcnow(),
+                                                     submission_id=self.d2_submission_dupe,
+                                                     job_id=1, row_number=1, is_valid=True,
+                                                     fain="abc", uri="def", awarding_sub_tier_agency_c="abc",
+                                                     award_modification_amendme="def")
+
+        det_award_2 = DetachedAwardFinancialAssistance(datetime_utc=datetime.utcnow(),
+                                                       submission_id=self.d2_submission_dupe_2,
+                                                       job_id=1, row_number=1, is_valid=True,
+                                                       fain="abc", uri="def", awarding_sub_tier_agency_c="abc",
+                                                       award_modification_amendme="def")
+
+        self.session.add_all([det_award, det_award_2])
+        self.session.commit()
