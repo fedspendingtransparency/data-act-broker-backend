34e2c0ecb6419dc288f2dfbc18aad62e3976aa6e
kkuratsu <kuratsu_kate@bah.com>
2017-02-21 09:29:48 -0500

    Remove allocation_transfer_agency from test

--- tests/unit/dataactvalidator/test_c11_cross_file.py
+++ tests/unit/dataactvalidator/test_c11_cross_file.py
@@ -41,14 +41,14 @@ def test_success(database):
 
     # Not perform when no transaction obligated amount value in the field
     af = AwardFinancialFactory(piid='some_piid', parent_award_id='some_parent_award_id',
-                               allocation_transfer_agency=None, transaction_obligated_amou=None)
+                               transaction_obligated_amou=None)
     ap = AwardProcurementFactory(piid='some_other_piid', parent_award_id='some_parent_award_id')
 
     assert number_of_errors(_FILE, database, models=[af, ap]) == 0
 
     # Not perform when no transaction obligated amount value in the field
     af = AwardFinancialFactory(piid='some_piid', parent_award_id='some_parent_award_id',
-                               allocation_transfer_agency='bad', transaction_obligated_amou=None)
+                               transaction_obligated_amou=None)
     ap = AwardProcurementFactory(piid='some_piid', parent_award_id='some_other_parent_award_id')
 
     assert number_of_errors(_FILE, database, models=[af, ap]) == 0
