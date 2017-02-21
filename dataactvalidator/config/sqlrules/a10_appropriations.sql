7994d4e9126f652da32d08f8364af9a8b322e146
kkuratsu <kuratsu_kate@bah.com>
2017-02-21 10:38:45 -0500

    Add submission id to a10 table name

--- dataactvalidator/config/sqlrules/a10_appropriations.sql
+++ dataactvalidator/config/sqlrules/a10_appropriations.sql
@@ -1,4 +1,4 @@
-WITH appropriation_a10 AS 
+WITH appropriation_a10_{0} AS
 	(SELECT submission_id,
 		row_number,
 		borrowing_authority_amount_cpe,
@@ -8,12 +8,12 @@ SELECT
     approp.row_number,
     approp.borrowing_authority_amount_cpe,
     SUM(sf.amount) as sf_133_amount_sum
-FROM appropriation_a10 as approp
+FROM appropriation_a10_{0} as approp
     INNER JOIN sf_133 as sf ON approp.tas = sf.tas
     INNER JOIN submission as sub ON approp.submission_id = sub.submission_id AND
         sf.period = sub.reporting_fiscal_period AND
         sf.fiscal_year = sub.reporting_fiscal_year
-WHERE approp.submission_id = {} AND
+WHERE approp.submission_id = {0} AND
     sf.line in (1340, 1440)
 GROUP BY approp.row_number, approp.borrowing_authority_amount_cpe
 HAVING approp.borrowing_authority_amount_cpe <> SUM(sf.amount)
