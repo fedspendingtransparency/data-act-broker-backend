f9f4bfe0bec8cde5fece3a4d5ad6bda090d5b913
kkuratsu <kuratsu_kate@bah.com>
2017-02-22 11:20:40 -0500

    Fixed c23 af temp table

--- dataactvalidator/config/sqlrules/c23_award_financial_2.sql
+++ dataactvalidator/config/sqlrules/c23_award_financial_2.sql
@@ -27,7 +27,7 @@ WHERE af.parent_award_id IS NOT NULL
 GROUP BY af.parent_award_id, af.piid
 HAVING
     (SELECT COALESCE(SUM(sub_af.transaction_obligated_amou::numeric),0) AS transaction_sum
-        FROM award_financial as sub_af WHERE submission_id = {0} AND sub_af.parent_award_id = af.parent_award_id AND sub_af.piid = af.piid) <>
+        FROM award_financial_c23_2_{0} as sub_af WHERE sub_af.parent_award_id = af.parent_award_id AND sub_af.piid = af.piid) <>
 		-1*(SELECT COALESCE(SUM(sub_ap.federal_action_obligation),0) AS obligation_sum
 		FROM award_procurement as sub_ap WHERE submission_id = {0} AND sub_ap.parent_award_id = af.parent_award_id AND sub_ap.piid = af.piid)
 		AND NOT EXISTS (SELECT sub_af.allocation_transfer_agency FROM award_financial as sub_af
