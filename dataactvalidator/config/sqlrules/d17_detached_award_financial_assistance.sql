aae957f1c261c78a770a6090245827b27f4ad487
kkuratsu <kuratsu_kate@bah.com>
2017-02-26 23:46:42 -0500

    d17 Tracking ForeignPostalCode properly

---
+++ dataactvalidator/config/sqlrules/d17_detached_award_financial_assistance.sql
@@ -0,0 +1,11 @@
+-- LegalEntityForeignPostalCode is optional for foreign recipients (i.e., when LegalEntityCountryCode != USA)
+-- LegalEntityForeignPostalCode must be blank for domestic recipients when LegalEntityCountryCode is 'USA'
+SELECT
+    row_number,
+    legal_entity_country_code,
+    legal_entity_foreign_posta
+FROM detached_award_financial_assistance
+WHERE submission_id = {0}
+    AND UPPER(legal_entity_country_code) = 'USA'
+    AND legal_entity_foreign_posta IS NOT NULL
+    AND legal_entity_foreign_posta != ''
