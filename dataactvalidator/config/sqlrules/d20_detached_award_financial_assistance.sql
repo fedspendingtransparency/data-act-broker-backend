1a8d88c0d6c3c1d5da09083cc5e5ccf3dd7c191c
kkuratsu <kuratsu_kate@bah.com>
2017-03-01 18:38:14 -0500

    Valid 3-digit FundingAgencyCode

---
+++ dataactvalidator/config/sqlrules/d20_detached_award_financial_assistance.sql
@@ -0,0 +1,12 @@
+-- FundingAgencyCode is an optional field, but when provided must be a valid 3-digit CGAC agency code.
+SELECT
+    row_number,
+    funding_agency_code
+FROM detached_award_financial_assistance as dafa
+WHERE submission_id = {0}
+    AND funding_agency_code != ''
+    AND NOT EXISTS (
+        SELECT cgac_code
+        FROM cgac
+        WHERE cgac_code = dafa.funding_agency_code
+    )
