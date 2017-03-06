6f5617d8e57350e11289d615a64ac960d54d70c1
kkuratsu <kuratsu_kate@bah.com>
2017-03-06 10:24:02 -0500

    Adding an alias to table

--- dataactvalidator/config/sqlrules/d22_detached_award_financial_assistance.sql
+++ dataactvalidator/config/sqlrules/d22_detached_award_financial_assistance.sql
@@ -1,15 +1,15 @@
 -- AwardingAgencyCode is optional. When provided must be a valid 3-digit CGAC agency code.
 -- If the AwardingAgencyCode is blank, it is auto-populated from the AwardingSubTierAgencyCode.
 SELECT
-    row_number,
-    awarding_agency_code,
-    awarding_sub_tier_agency_c
+    dafa.row_number,
+    dafa.awarding_agency_code,
+    dafa.awarding_sub_tier_agency_c
 FROM detached_award_financial_assistance as dafa
-WHERE submission_id = {0}
-    AND (awarding_agency_code != ''
+WHERE dafa.submission_id = {0}
+    AND (dafa.awarding_agency_code != ''
          AND NOT EXISTS (
-            SELECT cgac_code
-            FROM cgac
-            WHERE cgac_code = dafa.awarding_agency_code))
-    OR (awarding_agency_code =''
-        AND (awarding_agency_code != awarding_sub_tier_agency_c))
+            SELECT cgac.cgac_code
+            FROM cgac AS cgac
+            WHERE cgac.cgac_code = dafa.awarding_agency_code))
+    OR (dafa.awarding_agency_code =''
+        AND (dafa.awarding_agency_code != dafa.awarding_sub_tier_agency_c))
--- dataactvalidator/config/sqlrules/d23_detached_award_financial_assistance.sql
+++ dataactvalidator/config/sqlrules/d23_detached_award_financial_assistance.sql
@@ -1,14 +1,14 @@
 -- AwardingSubTierAgencyCode is required for all submissions and cannot be blank.
 -- AwardingSubTierAgencyCode must contain a valid four character numeric code.
 SELECT
-    row_number,
-    awarding_sub_tier_agency_c
+    dafa.row_number,
+    dafa.awarding_sub_tier_agency_c
 FROM detached_award_financial_assistance as dafa
-WHERE submission_id = {0}
-    AND ((awarding_sub_tier_agency_c IS NULL) OR (awarding_sub_tier_agency_c =''))
-    OR (awarding_sub_tier_agency_c != ''
+WHERE dafa.submission_id = {0}
+    AND ((dafa.awarding_sub_tier_agency_c IS NULL) OR (dafa.awarding_sub_tier_agency_c =''))
+    OR (dafa.awarding_sub_tier_agency_c != ''
         AND NOT EXISTS (
-            SELECT sub_tier_agency_code
-            FROM sub_tier_agency
-            WHERE sub_tier_agency_code = dafa.awarding_sub_tier_agency_c)
+            SELECT sta.sub_tier_agency_code
+            FROM sub_tier_agency AS sta
+            WHERE sta.sub_tier_agency_code = dafa.awarding_sub_tier_agency_c)
     )
