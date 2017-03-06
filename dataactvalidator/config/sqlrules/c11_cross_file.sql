cd04df7b0a640e7eb5d2a59964ba6c1d2734ee02
kkuratsu <kuratsu_kate@bah.com>
2017-03-06 14:49:03 -0500

    Pull 'piid is null' out og parenthesis

--- dataactvalidator/config/sqlrules/c11_cross_file.sql
+++ dataactvalidator/config/sqlrules/c11_cross_file.sql
@@ -6,27 +6,25 @@ SELECT
 FROM award_financial AS af
 WHERE af.submission_id = {}
     AND af.transaction_obligated_amou IS NOT NULL
-    AND ((af.piid IS NOT NULL
-          AND af.parent_award_id IS NULL
-          AND NOT EXISTS (
-              SELECT 1
-              FROM award_procurement AS ap
-              WHERE ap.submission_id = af.submission_id
-                  AND ap.piid = af.piid)
-          )
-          OR
-          (af.piid IS NOT NULL
-           AND af.parent_award_id IS NOT NULL
-           AND NOT EXISTS (
-              SELECT 1
-              FROM award_procurement AS ap
-              WHERE ap.submission_id = af.submission_id
-                  AND ap.piid = af.piid
-                  AND ap.parent_award_id IS NOT DISTINCT FROM af.parent_award_id)
-          )
+    AND af.piid IS NOT NULL
+    AND ((af.parent_award_id IS NULL
+        AND NOT EXISTS (
+            SELECT 1
+            FROM award_procurement AS ap
+            WHERE ap.submission_id = af.submission_id
+                AND ap.piid = af.piid)
+        )
+        OR (af.parent_award_id IS NOT NULL
+            AND NOT EXISTS (
+                SELECT 1
+                FROM award_procurement AS ap
+                WHERE ap.submission_id = af.submission_id
+                    AND ap.piid = af.piid
+                    AND ap.parent_award_id IS NOT DISTINCT FROM af.parent_award_id)
+        )
     )
     AND NOT EXISTS (
-          SELECT cgac_code
-          FROM cgac
-          WHERE cgac_code = af.allocation_transfer_agency
+        SELECT cgac_code
+        FROM cgac
+        WHERE cgac_code = af.allocation_transfer_agency
     )
