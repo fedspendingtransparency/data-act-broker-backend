58e3a43c7be7d6fce9e0105693b51e3a125feb64
kkuratsu <kuratsu_kate@bah.com>
2017-03-06 10:50:11 -0500

    Adding more aliases to tables

--- dataactvalidator/config/sqlrules/d20_detached_award_financial_assistance.sql
+++ dataactvalidator/config/sqlrules/d20_detached_award_financial_assistance.sql
@@ -1,10 +1,10 @@
 -- FundingAgencyCode is an optional field, but when provided must be a valid 3-digit CGAC agency code.
 SELECT
-    row_number,
-    funding_agency_code
+    dafa.row_number,
+    dafa.funding_agency_code
 FROM detached_award_financial_assistance AS dafa
-WHERE submission_id = {0}
-    AND funding_agency_code != ''
+WHERE dafa.submission_id = {0}
+    AND dafa.funding_agency_code != ''
     AND NOT EXISTS (
         SELECT cgac_code
         FROM cgac AS cgac
--- dataactvalidator/config/sqlrules/d21_detached_award_financial_assistance.sql
+++ dataactvalidator/config/sqlrules/d21_detached_award_financial_assistance.sql
@@ -1,10 +1,10 @@
 --  FundingSubTierAgencyCode is an optional field, but when provided must be a valid 4-digit sub-tier agency code.
 SELECT
-    row_number,
-    funding_sub_tier_agency_co
+    dafa.row_number,
+    dafa.funding_sub_tier_agency_co
 FROM detached_award_financial_assistance AS dafa
-WHERE submission_id = {0}
-    AND funding_sub_tier_agency_co != ''
+WHERE dafa.submission_id = {0}
+    AND dafa.funding_sub_tier_agency_co != ''
     AND NOT EXISTS (
         SELECT sub_tier_agency_code
         FROM sub_tier_agency AS sta
