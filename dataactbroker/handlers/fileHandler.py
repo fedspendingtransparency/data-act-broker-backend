fe79de382db072cc6688e2f3724daf3e6a9c5ec2
alburde1 <Burdeyny_Alisa@bah.com>
2017-03-13 11:51:32 -0400

    Adding quarterly submission status to check_status route

--- dataactbroker/handlers/fileHandler.py
+++ dataactbroker/handlers/fileHandler.py
@@ -1170,7 +1170,8 @@ def submission_to_dict_for_status(submission):
         'reporting_period_start_date': reporting_date(submission),
         'reporting_period_end_date': reporting_date(submission),
         'jobs': [job_to_dict(job) for job in relevant_jobs],
-        'publish_status': submission.publish_status.name
+        'publish_status': submission.publish_status.name,
+        'quarterly_submission': submission.is_quarter_format
     }
 
 
