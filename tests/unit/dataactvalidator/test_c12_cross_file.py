38039f984fc27c22b18bdb37bd7daeae3fd9ade2
kkuratsu <kuratsu_kate@bah.com>
2017-03-20 12:58:07 -0400

    Remove transaction_obligated_amou

--- tests/unit/dataactvalidator/test_c12_cross_file.py
+++ tests/unit/dataactvalidator/test_c12_cross_file.py
@@ -32,35 +32,17 @@ def test_success(database):
 
     assert number_of_errors(_FILE, database, models=[ap, af]) == 0
 
-    # Not perform when no transaction obligated amount value in the field
-    ap = AwardProcurementFactory(piid='some_piid', parent_award_id='some_parent_award_id', federal_action_obligation=1)
-    af = AwardFinancialFactory(piid='some_other_piid', parent_award_id='some_parent_award_id',
-                               transaction_obligated_amou=None)
-
-    assert number_of_errors(_FILE, database, models=[af, ap]) == 0
-
-    # Not perform when no transaction obligated amount value in the field
-    ap = AwardProcurementFactory(piid='some_piid', parent_award_id='some_parent_award_id', federal_action_obligation=1)
-    af = AwardFinancialFactory(piid='some_piid', parent_award_id='some_other_parent_award_id',
-                               transaction_obligated_amou=None)
-
-    assert number_of_errors(_FILE, database, models=[af, ap]) == 0
-
 
 def test_failure(database):
     """ Unique PIID, ParentAwardId from file D1 doesn't exist in file C during the same reporting period,
         except D1 records with zero FederalActionObligation """
 
-    # Perform when there's a transaction obligated amount value in the field
     ap = AwardProcurementFactory(piid='some_piid', parent_award_id='some_parent_award_id', federal_action_obligation=1)
-    af = AwardFinancialFactory(piid='some_other_piid', parent_award_id='some_parent_award_id',
-                               transaction_obligated_amou='1234')
+    af = AwardFinancialFactory(piid='some_other_piid', parent_award_id='some_parent_award_id')
 
     assert number_of_errors(_FILE, database, models=[af, ap]) == 1
 
-    # Perform when there's a transaction obligated amount value in the field
     ap = AwardProcurementFactory(piid='some_piid', parent_award_id='some_parent_award_id', federal_action_obligation=1)
-    af = AwardFinancialFactory(piid='some_piid', parent_award_id='some_other_parent_award_id',
-                               transaction_obligated_amou='1234')
+    af = AwardFinancialFactory(piid='some_piid', parent_award_id='some_other_parent_award_id')
 
     assert number_of_errors(_FILE, database, models=[af, ap]) == 1
