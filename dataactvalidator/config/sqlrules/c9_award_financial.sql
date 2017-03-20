fc0e2ff905db8c62cf4887c1d4f4ed6f4eab0deb
alburde1 <Burdeyny_Alisa@bah.com>
2017-03-20 11:09:32 -0400

    Fixing functionality for C9
    We don’t actually care about transaction_obligated_amou for checks from
    D2 -> C
    
    Also making sure we check that fain and uri are the same in both files
    (even if one is null)

--- dataactvalidator/config/sqlrules/c9_award_financial.sql
+++ dataactvalidator/config/sqlrules/c9_award_financial.sql
@@ -5,23 +5,14 @@ WHERE afa.submission_id = {0}
         OR COALESCE(CAST(afa.original_loan_subsidy_cost as numeric),0) <> 0
     ) AND (afa.fain IS NOT NULL
             OR afa.uri IS NOT NULL
-	) AND (afa.row_number NOT IN (
+	) AND afa.row_number NOT IN (
             SELECT afa.row_number
             FROM award_financial_assistance AS afa
                 JOIN award_financial AS af
                     ON afa.submission_id = af.submission_id
-                        AND afa.fain = af.fain
+                        AND (afa.fain = af.fain
+                            OR (afa.fain IS NULL AND af.fain IS NULL))
+                        AND (afa.uri = af.uri
+                            OR (afa.uri IS NULL AND af.uri IS NULL))
             WHERE afa.submission_id = {0}
-        ) AND afa.row_number NOT IN (
-            SELECT afa.row_number
-            FROM award_financial_assistance AS afa
-                JOIN award_financial AS af
-                    ON afa.submission_id = af.submission_id
-                        AND afa.uri = af.uri
-            WHERE afa.submission_id = {0}
-        )
-    ) AND NOT EXISTS(
-	      SELECT transaction_obligated_amou
-	      FROM award_financial AS af
-	      WHERE transaction_obligated_amou IS NULL
-    );
+        );
