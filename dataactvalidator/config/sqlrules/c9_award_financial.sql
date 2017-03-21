48e5638be248d155981db282d8e9c15e94337574
alburde1 <Burdeyny_Alisa@bah.com>
2017-03-21 08:46:08 -0400

    Fixing tabbing and updating rule

--- dataactvalidator/config/sqlrules/c9_award_financial.sql
+++ dataactvalidator/config/sqlrules/c9_award_financial.sql
@@ -1,12 +1,12 @@
 WITH award_financial_assistance_c9_{0} AS
     (SELECT submission_id,
-		row_number,
-		federal_action_obligation,
-		original_loan_subsidy_cost,
-		fain,
-		uri
-	FROM award_financial_assistance
-	WHERE submission_id = {0})
+        row_number,
+        federal_action_obligation,
+        original_loan_subsidy_cost,
+        fain,
+        uri
+    FROM award_financial_assistance
+    WHERE submission_id = {0})
 SELECT
     afa.row_number,
     afa.fain,
@@ -16,13 +16,11 @@ WHERE (COALESCE(afa.federal_action_obligation, 0) <> 0
         OR COALESCE(CAST(afa.original_loan_subsidy_cost as numeric),0) <> 0
     ) AND (afa.fain IS NOT NULL
             OR afa.uri IS NOT NULL
-	) AND afa.row_number NOT IN (
+    ) AND afa.row_number NOT IN (
             SELECT afa.row_number
             FROM award_financial_assistance_c9_{0} AS afa
                 JOIN award_financial AS af
                     ON afa.submission_id = af.submission_id
-                        AND (afa.fain = af.fain
-                            OR (afa.fain IS NULL AND af.fain IS NULL))
-                        AND (afa.uri = af.uri
-                            OR (afa.uri IS NULL AND af.uri IS NULL))
+                        AND afa.fain IS NOT DISTINCT FROM af.fain
+                        AND afa.uri IS NOT DISTINCT FROM af.uri
         );
