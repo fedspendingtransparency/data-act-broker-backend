22b8486103527eccad3262406f8a4fc6f6226677
alburde1 <Burdeyny_Alisa@bah.com>
2017-04-18 17:50:31 -0400

    Fixing c23_3

--- dataactvalidator/config/sqlrules/c23_award_financial_3.sql
+++ dataactvalidator/config/sqlrules/c23_award_financial_3.sql
@@ -7,6 +7,14 @@ WITH award_financial_c23_3_{0} AS
     	fain,
     	allocation_transfer_agency
     FROM award_financial
+    WHERE submission_id = {0}),
+award_financial_assistance_c23_3_{0} AS
+    (SELECT submission_id,
+    	federal_action_obligation,
+    	fain,
+    	original_loan_subsidy_cost,
+    	assistance_type
+    FROM award_financial_assistance
     WHERE submission_id = {0})
 SELECT
 	NULL as row_number,
@@ -15,26 +23,25 @@ SELECT
 		FROM award_financial_c23_3_{0} as sub_af
 		WHERE sub_af.fain = af.fain) AS transaction_obligated_amou_sum,
   (SELECT COALESCE(SUM(sub_afa.federal_action_obligation),0) AS obligation_sum
-		FROM award_financial_assistance as sub_afa
-		WHERE submission_id = {0} AND sub_afa.fain = af.fain and
+		FROM award_financial_assistance_c23_3_{0} as sub_afa
+		WHERE sub_afa.fain = af.fain and
 		COALESCE(sub_afa.assistance_type,'') not in ('07','08')) AS federal_action_obligation_sum,
 	(SELECT COALESCE(SUM(sub_afa.original_loan_subsidy_cost::numeric),0) AS obligation_sum
-		FROM award_financial_assistance as sub_afa
-		WHERE submission_id = {0} AND sub_afa.fain = af.fain and
+		FROM award_financial_assistance_c23_3_{0} as sub_afa
+		WHERE sub_afa.fain = af.fain and
 		COALESCE(sub_afa.assistance_type,'') in ('07','08')) AS original_loan_subsidy_cost_sum
 FROM award_financial_c23_3_{0} AS af
-JOIN award_financial_assistance AS afa
-		ON af.fain = afa.fain
-	  AND af.submission_id = afa.submission_id
+JOIN award_financial_assistance_c23_3_{0} AS afa
+    ON af.fain = afa.fain
 GROUP BY af.fain
 HAVING
 		(SELECT COALESCE(SUM(sub_af.transaction_obligated_amou::numeric),0) AS transaction_sum
 		FROM award_financial_c23_3_{0} as sub_af WHERE sub_af.fain = af.fain) <>
 		(-1*(SELECT COALESCE(SUM(sub_afa.federal_action_obligation),0) AS obligation_sum
-		FROM award_financial_assistance as sub_afa
-		WHERE submission_id = {0} AND sub_afa.fain = af.fain and COALESCE(sub_afa.assistance_type,'') not in ('07','08')) -
+		FROM award_financial_assistance_c23_3_{0} as sub_afa
+		WHERE sub_afa.fain = af.fain and COALESCE(sub_afa.assistance_type,'') not in ('07','08')) -
 		(SELECT COALESCE(SUM(sub_afa.original_loan_subsidy_cost::numeric),0) AS obligation_sum
-		FROM award_financial_assistance as sub_afa
-		WHERE submission_id = {0} AND sub_afa.fain = af.fain and COALESCE(sub_afa.assistance_type,'') in ('07','08')))
+		FROM award_financial_assistance_c23_3_{0} as sub_afa
+		WHERE sub_afa.fain = af.fain and COALESCE(sub_afa.assistance_type,'') in ('07','08')))
 		AND NOT EXISTS (SELECT sub_af.allocation_transfer_agency FROM award_financial as sub_af WHERE sub_af.fain = af.fain
 			AND COALESCE(sub_af.allocation_transfer_agency,'') <> '')
