19a3e6dfb8b9c54bc9ce0b612c6ee567a61e4ef1
Katie Rose <klrBAH@users.noreply.github.com>
2017-06-20 16:33:33 -0400

    Revert "Db 1957 csv newline parsing"

--- tests/unit/dataactvalidator/filestreaming/test_csvReader.py
+++ tests/unit/dataactvalidator/filestreaming/test_csvReader.py
@@ -1,5 +1,3 @@
-import csv
-
 from unittest.mock import Mock
 
 from dataactvalidator.filestreaming import csvReader
@@ -20,12 +18,13 @@ def test_count_and_set_headers_flex():
 def test_get_next_record_flex():
     """Verify that we get a list of FlexFields if present"""
     reader = csvReader.CsvReader()
+    reader.delimiter = ','
     reader.column_count = 6
     reader.expected_headers = ['a', 'b', 'c', None, None, None]
     reader.flex_headers = [None, None, None, 'flex_d', 'flex_e', None]
-    reader.csv_reader = csv.reader(['A,"B\n",C,D,E,F'], dialect='excel', delimiter=',')
+    reader._get_line = lambda: 'A,B,C,D,E,F'
     return_dict, flex_fields = reader.get_next_record()
-    assert return_dict == {'a': 'A', 'b': 'B\n', 'c': 'C'}
+    assert return_dict == {'a': 'A', 'b': 'B', 'c': 'C'}
     assert len(flex_fields) == 2
     assert flex_fields[0].header == 'flex_d'
     assert flex_fields[0].cell == 'D'
