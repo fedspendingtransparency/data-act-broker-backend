c2f43a7747830533645f71c4040441b03d0c511c
dpb-bah <boos_daniel@bah.com>
2017-06-21 19:25:43 -0400

    Updating draft of parsing sam files

--- dataactcore/scripts/loadExecComp.py
+++ dataactcore/scripts/loadExecComp.py
@@ -1,14 +1,18 @@
 import logging
 import os
 import sys
-import pd
+import pandas as pd
 import paramiko
 import socket
 import zipfile
+from collections import OrderedDict
+import numpy as np
 
+from dataactcore.models.domainModels import ExecutiveCompensation
 from dataactcore.interfaces.db import GlobalDB
 from dataactcore.logging import configure_logging
 from dataactvalidator.health_check import create_app
+from dataactvalidator.scripts.loaderUtils import insert_dataframe
 from dataactcore.config import CONFIG_BROKER
 
 
@@ -26,10 +30,81 @@ def parse_sam_file(file, sess):
 
     csv_file = os.path.splitext(os.path.basename(file.name))[0]
     zfile = zipfile.ZipFile(file.name)
-    data = pd.read_csv(zfile.open(csv_file), dtype=str, usecols=[1, 11, 49, 90])
 
-    # parse out column 90 (pandas dataframe)
-    # each value will be -> things^more things^other things~things2^more things2^other things2~ etc etc
+    # can't use skipfooter, pandas' c engine doesn't work with skipfooter and the python engine doesn't work with dtype
+    nrows = 0
+    with zfile.open(csv_file) as f:
+        nrows = len(f.readlines()) - 2 # subtract the header and footer
+    column_header_mapping = {
+        "awardee_or_recipient_uniqu"        : 0,
+        "sam_extract"                       : 4,
+        "expiration_date"                   : 7,
+        "activation_date"                   : 9,
+        "ultimate_parent_legal_enti"        : 10,
+        "ultimate_parent_unique_ide"        : 48,
+        "exec_comp_str"                     : 89
+    }
+    column_header_mapping_ordered = OrderedDict(sorted(column_header_mapping.items(), key=lambda c: c[1]))
+    csv_data = pd.read_csv(zfile.open(csv_file), dtype=str, header=None, skiprows=1, nrows = nrows,
+                           usecols=column_header_mapping_ordered.values(), names=column_header_mapping_ordered.keys())
+    total_data = csv_data.copy()
+
+    # skipping when sam_extract == '4' as it's expired
+    total_data = total_data[total_data.sam_extract != '4']
+
+    # parse out executive compensation from row 90
+    parsed_data = pd.DataFrame(total_data["exec_comp_str"].apply(lambda ecs: pd.Series(parse_exec_comp(ecs).values())),
+                               columns = list(parse_exec_comp().keys()))
+    del total_data["exec_comp_str"]
+    total_data = pd.concat([total_data, parsed_data], axis=1)
+
+    # split into 3 dataframes based on row 8 ('1', '2', '3')
+    delete_data = total_data[total_data.sam_extract == '1'].replace(np.nan, "", regex=True)
+    add_data = total_data[total_data.sam_extract == '2'].replace(np.nan, "", regex=True)
+    update_data = total_data[total_data.sam_extract == '3'].replace(np.nan, "", regex=True)
+    for dataframe in [add_data, update_data, delete_data, total_data]:
+        del dataframe["sam_extract"]
+
+    # print(len(add_data))
+    # print(len(update_data))
+    # print(len(delete_data))
+
+    table_name = ExecutiveCompensation.__table__.name
+    insert_dataframe(add_data, table_name, sess.connection())
+    for _, row in update_data.iterrows():
+        sess.query(ExecutiveCompensation).filter_by(awardee_or_recipient_uniqu=row['awardee_or_recipient_uniqu']).\
+            update(row, synchronize_session=False)
+    for _, row in delete_data.iterrows():
+        sess.query(ExecutiveCompensation).filter_by(awardee_or_recipient_uniqu=row['awardee_or_recipient_uniqu']).\
+            delete(synchronize_session=False)
+    sess.commit()
+
+
+def parse_exec_comp(exec_comp_str = None):
+    """
+    Parses the executive compensation string into a dictionary for the ExecutiveCompensation data model
+    :param exec_comp_str: the incoming compensation string
+    :return: dictionary for the ExecutiveCompensation data model
+    """
+    exec_comp_data = OrderedDict()
+    for index in range(1,6):
+        exec_comp_data["high_comp_officer{}_full_na".format(index)] = ""
+        exec_comp_data["high_comp_officer{}_amount".format(index)] = ""
+
+    if isinstance(exec_comp_str, str) and not exec_comp_str.isdigit():
+        high_comp_officers = exec_comp_str.split('~')
+
+        # records have inconsistent values for Null
+        unaccepted_titles = ["x", "n/a", "none"]
+
+        for index, high_comp_officer in enumerate(high_comp_officers):
+            index += 1
+            exec_name,exec_title,exec_comp = high_comp_officer.split('^')
+            if exec_title.lower() not in unaccepted_titles:
+                exec_comp_data["high_comp_officer{}_full_na".format(index)] = exec_name
+                exec_comp_data["high_comp_officer{}_amount".format(index)] = exec_comp
+
+    return exec_comp_data
 
 
 def get_config():
