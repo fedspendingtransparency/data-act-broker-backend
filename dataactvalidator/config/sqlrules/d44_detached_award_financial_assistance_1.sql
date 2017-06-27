df69213cb38b65f00ee8d14691595791ec0cb401
dpb-bah <boos_daniel@bah.com>
2017-06-26 23:33:28 -0400

    Updating rules after clarification

--- dataactvalidator/config/sqlrules/d44_detached_award_financial_assistance_1.sql
+++ dataactvalidator/config/sqlrules/d44_detached_award_financial_assistance_1.sql
@@ -1,18 +1,36 @@
+-- AwardeeOrRecipientUniqueIdentifier is required for AssistanceType of 02, 03, 04, or 05 whose
+-- ActionDate after October 1, 2010.
 -- When ActionType = A, must be active in the System for Award Management (SAM) on the ActionDate of the award.
 -- When ActionType = B, C, or D, DUNS should exist in SAM but need not be active on the ActionDate (i.e., it may be expired).
 
+CREATE OR REPLACE function pg_temp.is_date(str text) returns boolean AS $$
+BEGIN
+    perform CAST(str AS DATE);
+    return TRUE;
+EXCEPTION WHEN others THEN
+    return FALSE;
+END;
+$$ LANGUAGE plpgsql;
+
 SELECT
     row_number,
-    awardee_or_recipient_uniqu,
+    assistance_type,
+    action_date,
     action_type,
-    action_date
+    awardee_or_recipient_uniqu
 FROM detached_award_financial_assistance AS dafa
 WHERE submission_id = {0}
+    AND COALESCE(assistance_type, '') IN ('02', '03', '04', '05')
+    AND (CASE
+        WHEN pg_temp.is_date(COALESCE(action_date, '0'))
+        THEN
+            CAST(action_date as DATE)
+    END) > CAST('10/01/2010' as DATE)
     AND dafa.action_type = 'A'
     AND dafa.row_number NOT IN (
         SELECT DISTINCT dafa.row_number
         FROM detached_award_financial_assistance as dafa
             JOIN executive_compensation AS exec_comp
             ON dafa.awardee_or_recipient_uniqu = exec_comp.awardee_or_recipient_uniqu
-            WHERE CAST(dafa.action_date as VarChar) >= CAST(exec_comp.activation_date as VarChar)
+            WHERE CAST(dafa.action_date as DATE) > CAST(exec_comp.activation_date as DATE)
     )
--- tests/unit/dataactvalidator/test_d44_detached_award_financial_assistance_1.py
+++ tests/unit/dataactvalidator/test_d44_detached_award_financial_assistance_1.py
@@ -6,7 +6,7 @@ _FILE = 'd44_detached_award_financial_assistance_1'
 
 
 def test_column_headers(database):
-    expected_subset = {"row_number", "awardee_or_recipient_uniqu", "action_type", "action_date"}
+    expected_subset = {"row_number", "assistance_type", "action_date", "action_type", "awardee_or_recipient_uniqu"}
     actual = set(query_columns(_FILE, database))
     assert expected_subset == actual
 
@@ -14,39 +14,39 @@ def test_column_headers(database):
 def test_pubished_date_success(database):
     """ Test valid. When ActionType = A, must be active in the System for Award Management (SAM) on the
         ActionDate of the award.
+        AwardeeOrRecipientUniqueIdentifier is required for AssistanceType of 02, 03, 04, or 05 whose
+        ActionDate after October 1, 2010.
         When ActionType = B, C, or D, DUNS should exist in SAM but need not be active on the ActionDate
         (i.e., it may be expired).
     """
-
-    exec_comp_1 = ExecutiveCompensation(awardee_or_recipient_uniqu="111111111", activation_date="20170622")
+    exec_comp_1 = ExecutiveCompensation(awardee_or_recipient_uniqu="111111111", activation_date="06/21/2017")
     det_award_1 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu="111111111", action_type="A",
-                                                          action_date="20170623")
+                                                           assistance_type="02", action_date="06/23/2017")
     # inactive at action date but not A files
-    exec_comp_2 = ExecutiveCompensation(awardee_or_recipient_uniqu="111111112", activation_date="20170623")
-    det_award_2 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu="111111112", action_type="B",
-                                                          action_date="20170622")
-    exec_comp_3 = ExecutiveCompensation(awardee_or_recipient_uniqu="111111113", activation_date="20170623")
-    det_award_3 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu="111111113", action_type="C",
-                                                          action_date="20170622")
-    exec_comp_4 = ExecutiveCompensation(awardee_or_recipient_uniqu="111111114", activation_date="20170623")
-    det_award_4 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu="111111114", action_type="D",
-                                                          action_date="20170622")
+    det_award_2 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu="111111111", action_type="B",
+                                                           assistance_type="03", action_date="06/21/2017")
+    det_award_3 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu="111111111", action_type="C",
+                                                           assistance_type="04", action_date="06/21/2017")
+    det_award_4 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu="111111111", action_type="D",
+                                                           assistance_type="05", action_date="06/21/2017")
 
-    errors = number_of_errors(_FILE, database, models=[det_award_1, exec_comp_1, det_award_2, exec_comp_2, det_award_3,
-                                                       exec_comp_3, det_award_4, exec_comp_4])
+    errors = number_of_errors(_FILE, database, models=[exec_comp_1, det_award_1, det_award_2, det_award_3,
+                                                       det_award_4])
     assert errors == 0
 
 
 def test_pubished_date_failure(database):
     """ Test invalid. When ActionType = A, must be active in the System for Award Management (SAM) on the
         ActionDate of the award.
+        AwardeeOrRecipientUniqueIdentifier is required for AssistanceType of 02, 03, 04, or 05 whose
+        ActionDate after October 1, 2010.
         When ActionType = B, C, or D, DUNS should exist in SAM but need not be active on the ActionDate
         (i.e., it may be expired).
     """
 
-    exec_comp_1 = ExecutiveCompensation(awardee_or_recipient_uniqu="111111111", activation_date="20170623")
-    det_award_1 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu="111111111", action_type="A",
-                                                          action_date="20170622")
+    exec_comp_1 = ExecutiveCompensation(awardee_or_recipient_uniqu="111111111", activation_date="06/23/2017")
+    det_award_1 = DetachedAwardFinancialAssistanceFactory(assistance_type="03", action_date="06/22/2017",
+                                                          awardee_or_recipient_uniqu="111111111", action_type="A")
 
-    errors = number_of_errors(_FILE, database, models=[det_award_1, exec_comp_1])
+    errors = number_of_errors(_FILE, database, models=[exec_comp_1, det_award_1])
     assert errors == 1
