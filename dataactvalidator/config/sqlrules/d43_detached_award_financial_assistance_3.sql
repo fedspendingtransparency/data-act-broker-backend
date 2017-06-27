908fd3da40e7bed4a3c0c8b9a5116193f3c78787
Michael Hess <hess_michael@bah.com>
2017-06-26 20:21:02 -0400

    If no PrimaryPlaceOfPerformanceZIP+4 is provided, a valid PrimaryPlaceOfPerformanceCongressionalDistrict must be provided

---
+++ dataactvalidator/config/sqlrules/d43_detached_award_financial_assistance_3.sql
@@ -0,0 +1,31 @@
+-- If no PrimaryPlaceOfPerformanceZIP+4 is provided, PrimaryPlaceOfPerformanceCongressionalDistrict must be provided.
+-- This congressional district provided must exist in the state indicated by the PrimaryPlaceOfPerformanceCode. The
+-- PrimaryPlaceOfPerformanceCongressionalDistrict may be 90 if the state has more than one congressional district.
+WITH detached_award_financial_assistance_d43_3_{0} AS
+    (SELECT submission_id,
+        row_number,
+        place_of_performance_code,
+        place_of_performance_zip4a,
+        place_of_performance_congr
+    FROM detached_award_financial_assistance
+    WHERE submission_id = {0})
+SELECT
+    dafa.row_number,
+    dafa.place_of_performance_code,
+    dafa.place_of_performance_zip4a,
+    dafa.place_of_performance_congr
+FROM detached_award_financial_assistance_d43_3_{0} AS dafa
+WHERE COALESCE(dafa.place_of_performance_zip4a, '') = '' 
+    AND dafa.place_of_performance_code != '00*****'
+    AND (COALESCE(dafa.place_of_performance_congr, '') = ''
+        OR NOT EXISTS (
+            SELECT DISTINCT sub_dafa.row_number
+            FROM detached_award_financial_assistance_d43_3_{0} AS sub_dafa
+            JOIN zips
+              ON UPPER(LEFT(sub_dafa.place_of_performance_code, 2)) = zips.state_abbreviation
+                AND sub_dafa.place_of_performance_congr = zips.congressional_district_no)
+        OR (dafa.place_of_performance_congr = '90'
+            AND (SELECT COUNT(DISTINCT zips.congressional_district_no)
+                FROM zips
+                WHERE UPPER(LEFT(dafa.place_of_performance_code, 2)) = zips.state_abbreviation) < 2)
+        )
