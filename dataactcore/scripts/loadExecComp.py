d7f98acebd03fbf6294c5959279dad482fa88f72
dpb-bah <boos_daniel@bah.com>
2017-06-27 13:49:11 -0400

    Revert "Updated script after various tests"
    This reverts commit 3a02e1472ccb1e48cc9f4528d6ba7979f51a9bc0.

--- dataactcore/scripts/loadExecComp.py
+++ dataactcore/scripts/loadExecComp.py
@@ -7,7 +7,6 @@ import socket
 import zipfile
 from collections import OrderedDict
 import numpy as np
-import argparse
 
 from dataactcore.models.domainModels import ExecutiveCompensation
 from dataactcore.interfaces.db import GlobalDB
@@ -46,7 +45,7 @@ def parse_sam_file(file, sess):
         "exec_comp_str"                     : 89
     }
     column_header_mapping_ordered = OrderedDict(sorted(column_header_mapping.items(), key=lambda c: c[1]))
-    csv_data = pd.read_csv(zfile.open(csv_file), dtype=str, header=None, skiprows=1, nrows = nrows, sep='|',
+    csv_data = pd.read_csv(zfile.open(csv_file), dtype=str, header=None, skiprows=1, nrows = nrows,
                            usecols=column_header_mapping_ordered.values(), names=column_header_mapping_ordered.keys())
     total_data = csv_data.copy()
 
@@ -54,11 +53,10 @@ def parse_sam_file(file, sess):
     total_data = total_data[total_data.sam_extract != '4']
 
     # parse out executive compensation from row 90
-    lambda_func = (lambda ecs: pd.Series(list(parse_exec_comp(ecs).values())))
-    parsed_data = total_data["exec_comp_str"].apply(lambda_func)
-    parsed_data.columns = list(parse_exec_comp().keys())
+    parsed_data = pd.DataFrame(total_data["exec_comp_str"].apply(lambda ecs: pd.Series(parse_exec_comp(ecs).values())),
+                               columns = list(parse_exec_comp().keys()))
     del total_data["exec_comp_str"]
-    total_data = total_data.join(parsed_data)
+    total_data = pd.concat([total_data, parsed_data], axis=1)
 
     # split into 3 dataframes based on row 8 ('1', '2', '3')
     delete_data = total_data[total_data.sam_extract == '1'].replace(np.nan, "", regex=True)
@@ -66,6 +64,10 @@ def parse_sam_file(file, sess):
     update_data = total_data[total_data.sam_extract == '3'].replace(np.nan, "", regex=True)
     for dataframe in [add_data, update_data, delete_data, total_data]:
         del dataframe["sam_extract"]
+
+    # print(len(add_data))
+    # print(len(update_data))
+    # print(len(delete_data))
 
     table_name = ExecutiveCompensation.__table__.name
     insert_dataframe(add_data, table_name, sess.connection())
@@ -86,15 +88,14 @@ def parse_exec_comp(exec_comp_str = None):
     """
     exec_comp_data = OrderedDict()
     for index in range(1,6):
-        exec_comp_data["high_comp_officer{}_full_na".format(index)] = np.nan
-        exec_comp_data["high_comp_officer{}_amount".format(index)] = np.nan
+        exec_comp_data["high_comp_officer{}_full_na".format(index)] = ""
+        exec_comp_data["high_comp_officer{}_amount".format(index)] = ""
 
     if isinstance(exec_comp_str, str) and not exec_comp_str.isdigit():
         high_comp_officers = exec_comp_str.split('~')
 
         # records have inconsistent values for Null
-        # "see note above" is excluded as it may contain relevant info
-        unaccepted_titles = ["x", "n/a", "na", "null", "none"]
+        unaccepted_titles = ["x", "n/a", "none"]
 
         for index, high_comp_officer in enumerate(high_comp_officers):
             index += 1
@@ -115,29 +116,12 @@ def get_config():
 
     return None, None, None, None, None
 
-def get_parser():
-    parser = argparse.ArgumentParser(description="Get data from SAM and update execution_compensation table")
-    parser.add_argument("--historic", "-i", action="store_true", help='populate based on historical data')
-    parser.add_argument("--test", "-t", type=str, help='test file')
-    return parser
-
 if __name__ == '__main__':
     configure_logging()
-    parser = get_parser()
-    args = parser.parse_args()
-    historic = args.historic
-    test_file = args.test
 
     with create_app().app_context():
         sess = GlobalDB.db().session
         private_key, username, password, host, port = get_config()
-
-        if test_file:
-            class File():
-                def __init__(self, path):
-                    self.name = path
-            parse_sam_file(File(test_file), sess)
-            exit()
 
         if None in (private_key, username, password):
             logger.error("Missing config elements for connecting to SAM")
@@ -153,14 +137,14 @@ if __name__ == '__main__':
         dirlist = sftp.listdir('/current/SAM/6_EXECCOMP')
         print("Dirlist: %s" % dirlist)
 
-        if historic:
-            # pull every daily file
-            for item in dirlist:
-                file = sftp.file(item)
-
-                parse_sam_file(file, sess)
-        else:
-            # pull the latest daily file
-            # parse_sam_file(file, sess)
-            pass
-
+        for item in dirlist:
+            file = sftp.file(item)
+            # if running historical loader
+            #     delete exec comp data
+            #     for each monthly file
+            #         pull all data
+            #         save values as executive_compensation models in the DB
+            # if running daily loader
+            #     pull today's file
+            #     save values as executive_compensation models in the DB
+            parse_sam_file(file, sess)
