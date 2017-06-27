5d74ca96b302db8c93e693174db50c708a1391a1
alburde1 <Burdeyny_Alisa@bah.com>
2017-06-27 14:00:29 -0400

    Fixing tabbing

--- dataactvalidator/config/sqlrules/d43_detached_award_financial_assistance_1.sql
+++ dataactvalidator/config/sqlrules/d43_detached_award_financial_assistance_1.sql
@@ -6,5 +6,5 @@ SELECT
     place_of_perform_country_c
 FROM detached_award_financial_assistance
 WHERE submission_id = {0}
-	AND UPPER(place_of_perform_country_c) != 'USA'
+    AND UPPER(place_of_perform_country_c) != 'USA'
     AND COALESCE(place_of_performance_congr,'') != ''
