862190abe577a70ab489c4af4113692c9360ed35
alburde1 <Burdeyny_Alisa@bah.com>
2017-06-27 13:29:28 -0400

    Updating part 2 to be what we want
    + Wording for part 3 is updated but not rule, will do it in next commit

--- dataactvalidator/config/sqlrules/d43_detached_award_financial_assistance_2.sql
+++ dataactvalidator/config/sqlrules/d43_detached_award_financial_assistance_2.sql
@@ -1,23 +1,8 @@
-WITH detached_award_financial_assistance_d43_2_{0} AS
-    (SELECT submission_id,
-        row_number,
-        place_of_performance_zip4a,
-        place_of_performance_congr,
-     	place_of_performance_code
-    FROM detached_award_financial_assistance
-    WHERE submission_id = {0})
 SELECT
-    dafa.row_number,
-    dafa.place_of_performance_code,
-    dafa.place_of_performance_congr,
-    dafa.place_of_performance_zip4a
-FROM detached_award_financial_assistance_d43_2_{0} AS dafa
-WHERE COALESCE(dafa.place_of_performance_zip4a, '') != ''
-     AND CASE WHEN COALESCE(dafa.place_of_performance_congr, '') != ''
-        THEN NOT EXISTS (SELECT DISTINCT sub_dafa.row_number
-            FROM detached_award_financial_assistance_d43_2_{0} AS sub_dafa
-            JOIN zips
-                ON UPPER(LEFT(sub_dafa.place_of_performance_code,2)) = zips.state_abbreviation
-                 AND sub_dafa.place_of_performance_congr = zips.congressional_district_no)
-        ELSE FALSE
-        END
+    row_number,
+    place_of_performance_congr,
+    place_of_performance_zip4a
+FROM detached_award_financial_assistance
+WHERE submission_id = {0}
+    AND COALESCE(place_of_performance_zip4a, '') = ''
+    AND COALESCE(place_of_performance_congr, '') = ''
