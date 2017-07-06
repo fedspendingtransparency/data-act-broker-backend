e4ce2153f8a8c45ead194f2f6a302344c91c45b3
Daniel Boos <boos_daniel@bah.com>
2017-07-06 17:44:57 -0400

    Using reporting dates instead of action dates for FABS

--- dataactbroker/handlers/fileHandler.py
+++ dataactbroker/handlers/fileHandler.py
@@ -687,13 +687,6 @@ class FileHandler:
             upload_files = []
             request_params = RequestDictionary.derive(self.request)
 
-            # unfortunately, field names in the request don't match
-            # field names in the db/response. create a mapping here.
-            request_job_mapping = {
-                "reporting_period_start_date": "reporting_start_date",
-                "reporting_period_end_date": "reporting_end_date"
-            }
-
             job_data = {}
             # Check for existing submission
             existing_submission_id = request_params.get('existing_submission_id')
@@ -711,42 +704,18 @@ class FileHandler:
                 existing_submission = None
                 existing_submission_obj = None
 
-            for request_field, submission_field in request_job_mapping.items():
-                if request_field in request_params:
-                    job_data[submission_field] = request_params[request_field]
-                # all of those fields are required
-                # unless existing_submission_id exists
-                elif 'existing_submission_id' not in request_params and request_field in request_params:
-                    raise ResponseException('{} is required'.format(request_field), StatusCode.CLIENT_ERROR, ValueError)
-
             if existing_submission_obj is not None:
-                formatted_start_date = existing_submission_obj.reporting_start_date
-                formatted_end_date = existing_submission_obj.reporting_end_date
                 cgac_code = existing_submission_obj.cgac_code
             else:
                 sub_tier_agency = sess.query(SubTierAgency).\
                     filter_by(sub_tier_agency_code=request_params["agency_code"]).one()
                 cgac_code = sub_tier_agency.cgac.cgac_code
-                date_format = '%d/%m/%Y'
-                try:
-                    # convert submission start/end dates from the request into Python date objects
-                    formatted_start_date = datetime.strptime(job_data['reporting_start_date'], date_format).date()
-                    formatted_end_date = datetime.strptime(job_data['reporting_end_date'], date_format).date()
-                except ValueError:
-                    raise ResponseException("Date must be provided as DD/MM/YYYY", StatusCode.CLIENT_ERROR, ValueError)
 
             # get the cgac code associated with this sub tier agency
             job_data["cgac_code"] = cgac_code
             job_data["d2_submission"] = True
-            job_data['reporting_start_date'] = formatted_start_date
-            job_data['reporting_end_date'] = formatted_end_date
-
-            # the front-end is doing date checks, but we'll also do a few server side to ensure everything is correct
-            # when clients call the API directly
-            if job_data.get('reporting_start_date') > job_data.get('reporting_end_date'):
-                raise ResponseException("Submission start date {} is after the end date {}".format(
-                        job_data.get('reporting_start_date'), job_data.get('reporting_end_date')),
-                        StatusCode.CLIENT_ERROR)
+            job_data['reporting_start_date'] = None
+            job_data['reporting_end_date'] = None
 
             if not current_user_can('writer', job_data["cgac_code"]):
                 raise ResponseException("User does not have permission to create jobs for this agency",
@@ -1322,6 +1291,8 @@ def job_to_dict(job):
 
 def reporting_date(submission):
     """Format submission reporting date"""
+    if not (submission.reporting_start_date or submission.reporting_end_date):
+        return None
     if submission.is_quarter_format:
         return 'Q{}/{}'.format(submission.reporting_fiscal_period // 3,
                                submission.reporting_fiscal_year)
@@ -1429,8 +1400,7 @@ def list_submissions(page, limit, certified, sort='modified', order='desc', d2_s
     submission_columns = [Submission.submission_id, Submission.cgac_code, Submission.user_id,
                           Submission.publish_status_id, Submission.d2_submission, Submission.number_of_warnings,
                           Submission.number_of_errors, Submission.updated_at, Submission.reporting_start_date,
-                          Submission.reporting_end_date, Submission.earliest_action_date,
-                          Submission.latest_action_date, Submission.certifying_user_id]
+                          Submission.reporting_end_date, Submission.certifying_user_id]
 
     cgac_columns = [CGAC.cgac_code, CGAC.agency_name]
     user_columns = [User.user_id, User.name, certifying_user.user_id.label('certifying_user_id'),
@@ -1582,10 +1552,8 @@ def serialize_submission(submission):
         "status": status,
         "agency": submission.agency_name if submission.agency_name else 'N/A',
         # @todo why are these a different format?
-        "reporting_start_date": str(submission.reporting_start_date),
-        "reporting_end_date": str(submission.reporting_end_date),
-        "earliest_action_date": str(submission.earliest_action_date) if submission.earliest_action_date else None,
-        "latest_action_date": str(submission.latest_action_date) if submission.latest_action_date else None,
+        "reporting_start_date": str(submission.reporting_start_date) if submission.reporting_start_date else None,
+        "reporting_end_date": str(submission.reporting_end_date) if submission.reporting_end_date else None,
         "user": {"user_id": submission.user_id,
                  "name": submission.name if submission.name else "No User"},
         "certifying_user": submission.certifying_user_name if submission.certifying_user_name else "",
