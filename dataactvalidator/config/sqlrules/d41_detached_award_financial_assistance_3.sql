8809fd1a38d092079775e2bf5a6cd0261fe57f90
alburde1 <Burdeyny_Alisa@bah.com>
2017-07-07 10:34:27 -0400

    Just realized I can simplify one of the rules by adding \- as optional instead of doing 2 checks

--- dataactvalidator/config/sqlrules/d41_detached_award_financial_assistance_3.sql
+++ dataactvalidator/config/sqlrules/d41_detached_award_financial_assistance_3.sql
@@ -20,7 +20,7 @@ WHERE CASE WHEN COALESCE(dafa.place_of_performance_zip4a, '') != ''
                                 ON UPPER(LEFT(sub_dafa.place_of_performance_code, 2)) = zips.state_abbreviation
                                     AND sub_dafa.place_of_performance_zip4a = zips.zip5)
              -- 9 digit zip
-             ELSE (CASE WHEN dafa.place_of_performance_zip4a ~ '^\d\d\d\d\d\d\d\d\d$' OR dafa.place_of_performance_zip4a ~ '^\d\d\d\d\d\-\d\d\d\d$'
+             ELSE (CASE WHEN dafa.place_of_performance_zip4a ~ '^\d\d\d\d\d\-?\d\d\d\d$'
                     THEN dafa.row_number NOT IN (SELECT DISTINCT sub_dafa.row_number
                                     FROM detached_award_financial_assistance_d41_3_{0} AS sub_dafa
                                     JOIN zips
