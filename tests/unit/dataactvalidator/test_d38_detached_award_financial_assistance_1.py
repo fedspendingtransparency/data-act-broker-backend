bfd5909167d2f1d373b5a5b757d4196ca9516e77
alburde1 <Burdeyny_Alisa@bah.com>
2017-07-07 09:23:53 -0400

    Renaming d38 to d38_1 so I can just add the same check to awardingofficecode in the same rule number

--- tests/unit/dataactvalidator/test_d38_detached_award_financial_assistance_1.py
+++ tests/unit/dataactvalidator/test_d38_detached_award_financial_assistance_1.py
@@ -1,7 +1,7 @@
 from tests.unit.dataactcore.factories.staging import DetachedAwardFinancialAssistanceFactory
 from tests.unit.dataactvalidator.utils import number_of_errors, query_columns
 
-_FILE = 'd38_detached_award_financial_assistance'
+_FILE = 'd38_detached_award_financial_assistance_1'
 
 
 def test_column_headers(database):
