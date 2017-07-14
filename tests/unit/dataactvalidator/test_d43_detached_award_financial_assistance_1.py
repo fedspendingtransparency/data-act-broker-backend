4e4b88da57fa846aa468994647757a675c110a3d
alburde1 <Burdeyny_Alisa@bah.com>
2017-07-14 11:59:54 -0400

    Fixing D43
    - Fixing what fields are printed in one section of D43
    - Zip4 and CD only required when country code is USA

--- tests/unit/dataactvalidator/test_d43_detached_award_financial_assistance_1.py
+++ tests/unit/dataactvalidator/test_d43_detached_award_financial_assistance_1.py
@@ -5,7 +5,7 @@ _FILE = 'd43_detached_award_financial_assistance_1'
 
 
 def test_column_headers(database):
-    expected_subset = {"row_number", "place_of_performance_code", "place_of_perform_country_c"}
+    expected_subset = {"row_number", "place_of_perform_country_c", "place_of_performance_congr"}
     actual = set(query_columns(_FILE, database))
     assert expected_subset == actual
 
