bc762a718bf4a0c0341cff03d2984c96773f3742
Daniel Boos <boos_daniel@bah.com>
2017-07-31 14:04:10 -0400

    Properly opening/closing zips/dats, preventing join bugs

--- dataactcore/scripts/loadDUNS.py
+++ dataactcore/scripts/loadDUNS.py
@@ -24,7 +24,7 @@ logger = logging.getLogger(__name__)
 REMOTE_SAM_DIR = '/current/SAM/2_FOUO/UTF-8/'
 
 def get_config():
-    sam_config = CONFIG_BROKER.get('sam')
+    sam_config = CONFIG_BROKER.get('sam_duns')
 
     if sam_config:
         return sam_config.get('username'), sam_config.get('password'), sam_config.get('host'), \
@@ -44,9 +44,7 @@ def activation_check(data, prepopulated_models):
     # if activation_date's already set, keep it, otherwise update it (default)
     lambda_func = (lambda duns_num: pd.Series([prepopulated_models[duns_num].activation_date.strftime("%Y%m%d")
                                                if duns_num in prepopulated_models else np.nan]))
-    old_activation_data = data["awardee_or_recipient_uniqu"].apply(lambda_func)
-    old_activation_data.columns = ["old_activation_date"]
-    data = data.join(old_activation_data)
+    data["old_activation_date"] = data["awardee_or_recipient_uniqu"].apply(lambda_func)
     data.loc[pd.notnull(data["old_activation_date"]), "activation_date"] = data["old_activation_date"]
     del data["old_activation_date"]
     return data
@@ -74,13 +72,13 @@ def clean_sam_data(data):
                 {'awardee_or_recipient_uniqu': {'pad_to_length': 9, 'keep_null': True}}
             )
 
-def parse_sam_file(file, monthly=False):
-    logger.info("starting file " + str(file.name))
+def parse_sam_file(file_path, monthly=False):
+    logger.info("starting file " + str(file_path))
 
-    zip_file = zipfile.ZipFile(file.name)
-    dat_file = os.path.splitext(os.path.basename(file.name))[0]+'.dat'
+    zip_file = zipfile.ZipFile(file_path)
+    dat_file_name = os.path.splitext(os.path.basename(file_path))[0]+'.dat'
     sam_file_type = "MONTHLY" if monthly else "DAILY"
-    dat_file_date = re.findall(".*{}_(.*).dat".format(sam_file_type), dat_file)[0]
+    dat_file_date = re.findall(".*{}_(.*).dat".format(sam_file_type), dat_file_name)[0]
 
     with create_app().app_context():
         sess = GlobalDB.db().session
@@ -92,8 +90,9 @@ def parse_sam_file(file, monthly=False):
 
         # models = {cgac.cgac_code: cgac for cgac in sess.query(CGAC)}
         nrows = 0
-        with zip_file.open(dat_file) as f:
-            nrows = len(f.readlines())
+        with zipfile.ZipFile(file_path) as zip_file:
+            with zip_file.open(dat_file_name) as f:
+                nrows = len(f.readlines())
         block_size = 10000
         batches = nrows//block_size
         # skip the first line again if the last batch is also the first batch
@@ -116,44 +115,46 @@ def parse_sam_file(file, monthly=False):
             nrows = (((batch+1)*block_size)-skiprows) if (batch < batches) else last_block_size
             logger.info('loading rows %s to %s',skiprows+1,nrows+skiprows)
 
-            csv_data = pd.read_csv(zip_file.open(dat_file), dtype=str, header=None, skiprows=skiprows, nrows=nrows, sep='|',
-                                   usecols=column_header_mapping_ordered.values(), names=column_header_mapping_ordered.keys())
+            with zipfile.ZipFile(file_path) as zip_file:
+                with zip_file.open(dat_file_name) as dat_file:
+                    csv_data = pd.read_csv(dat_file, dtype=str, header=None, skiprows=skiprows, nrows=nrows, sep='|',
+                                           usecols=column_header_mapping_ordered.values(),
+                                           names=column_header_mapping_ordered.keys())
 
-            # add deactivation_date column for delete records
-            lambda_func = (lambda sam_extract: pd.Series([dat_file_date if sam_extract == "1" else np.nan]))
-            parsed_data = pd.Series([np.nan], name='deactivation_date') if monthly else csv_data["sam_extract_code"].apply(lambda_func)
-            parsed_data.columns = ["deactivation_date"]
-            csv_data = csv_data.join(parsed_data)
-            csv_data = clean_sam_data(csv_data.where(pd.notnull(csv_data), None))
+                    # add deactivation_date column for delete records
+                    lambda_func = (lambda sam_extract: pd.Series([dat_file_date if sam_extract == "1" else np.nan]))
+                    csv_data["deactivation_date"] = (pd.Series([np.nan], name='deactivation_date')
+                                                     if monthly else csv_data["sam_extract_code"].apply(lambda_func))
+                    csv_data = clean_sam_data(csv_data.where(pd.notnull(csv_data), None))
 
-            if monthly:
-                logger.info("adding all monthly data with bulk load")
-                del csv_data["sam_extract_code"]
-                insert_dataframe(csv_data, DUNS.__table__.name, sess.connection())
-                sess.commit()
-            else:
-                logger.info("splitting daily file into add/update/delete rows")
-                add_data = csv_data[csv_data.sam_extract_code == '2']
-                update_data = csv_data[csv_data.sam_extract_code == '3']
-                delete_data = csv_data[csv_data.sam_extract_code == '1']
-                for dataframe in [add_data, update_data, delete_data]:
-                    del dataframe["sam_extract_code"]
-
-                if not add_data.empty:
-                    try:
-                        logger.info("attempting to bulk load add data")
-                        insert_dataframe(add_data, DUNS.__table__.name, sess.connection())
+                    if monthly:
+                        logger.info("adding all monthly data with bulk load")
+                        del csv_data["sam_extract_code"]
+                        insert_dataframe(csv_data, DUNS.__table__.name, sess.connection())
                         sess.commit()
-                    except IntegrityError:
-                        logger.info("bulk loading add data failed, loading add data by row")
-                        sess.rollback()
-                        load_duns_by_row(add_data, sess, models, prepopulated_models)
-                if not update_data.empty:
-                    logger.info("loading update data by row")
-                    load_duns_by_row(update_data, sess, models, prepopulated_models)
-                if not delete_data.empty:
-                    logger.info("loading delete data by row")
-                    load_duns_by_row(delete_data, sess, models, prepopulated_models)
+                    else:
+                        logger.info("splitting daily file into add/update/delete rows")
+                        add_data = csv_data[csv_data.sam_extract_code == '2']
+                        update_data = csv_data[csv_data.sam_extract_code == '3']
+                        delete_data = csv_data[csv_data.sam_extract_code == '1']
+                        for dataframe in [add_data, update_data, delete_data]:
+                            del dataframe["sam_extract_code"]
+
+                        if not add_data.empty:
+                            try:
+                                logger.info("attempting to bulk load add data")
+                                insert_dataframe(add_data, DUNS.__table__.name, sess.connection())
+                                sess.commit()
+                            except IntegrityError:
+                                logger.info("bulk loading add data failed, loading add data by row")
+                                sess.rollback()
+                                load_duns_by_row(add_data, sess, models, prepopulated_models)
+                        if not update_data.empty:
+                            logger.info("loading update data by row")
+                            load_duns_by_row(update_data, sess, models, prepopulated_models)
+                        if not delete_data.empty:
+                            logger.info("loading delete data by row")
+                            load_duns_by_row(delete_data, sess, models, prepopulated_models)
 
             added_rows+=nrows
             batch+=1
@@ -216,33 +217,32 @@ if __name__ == '__main__':
 
             # generate chronological list of daily and monthy files
             sorted_monthly_file_names = sorted([monthly_file for monthly_file in dirlist if re.match(".*MONTHLY_\d+\.ZIP",
-                                                                                                     monthly_file)])
-            sorted_daily_file_names = sorted([daily_file for daily_file in dirlist if re.match(".*DAILY_\d+\.ZIP", daily_file)])
-
+                                                                                                     monthly_file.upper())])
+            sorted_daily_file_names = sorted([daily_file for daily_file in dirlist if re.match(".*DAILY_\d+\.ZIP",
+                                                                                               daily_file.upper())])
             earliest_monthly_file = sorted_monthly_file_names[0]
             earliest_daily_file = sorted_monthly_file_names[0].replace("MONTHLY", "DAILY")
             sorted_daily_monthly = sorted(sorted_daily_file_names + [earliest_daily_file])
             daily_files_after = sorted_daily_monthly[sorted_daily_monthly.index(earliest_daily_file)+1:]
+            latest_daily_file = sorted_daily_file_names[-1]
 
             # parse the earliest monthly file
-            if local:
-                file = open(os.path.join(root_dir, earliest_monthly_file))
-            else:
-                file = open(os.path.join(root_dir, earliest_monthly_file), 'wb')
-                sftp.getfo(''.join([REMOTE_SAM_DIR, '/', earliest_monthly_file]), file)
-            parse_sam_file(file, monthly=True)
-            file.close()
+            file_path = os.path.join(root_dir, earliest_monthly_file)
             if not local:
-                os.remove(os.path.join(root_dir, earliest_monthly_file))
+                logger.info("Pulling {}".format(earliest_monthly_file))
+                with open(file_path, "wb") as zip_file:
+                    sftp.getfo(''.join([REMOTE_SAM_DIR, '/', earliest_monthly_file]), zip_file)
+            parse_sam_file(file_path, monthly=True)
+            if not local:
+                os.remove(file_path)
 
             # parse all the daily files after that
             for daily_file in daily_files_after:
-                if local:
-                    file = open(os.path.join(root_dir, daily_file))
-                else:
-                    file = open(os.path.join(root_dir, daily_file), 'wb')
-                    sftp.getfo(''.join([REMOTE_SAM_DIR, '/', daily_file]), file)
-                parse_sam_file(file)
-                file.close()
+                file_path = os.path.join(root_dir, daily_file)
                 if not local:
-                    os.remove(os.path.join(root_dir, daily_file))
+                    logger.info("Pulling {}".format(daily_file))
+                    with open(file_path, 'wb') as zip_file:
+                        sftp.getfo(''.join([REMOTE_SAM_DIR, '/', daily_file]), zip_file)
+                parse_sam_file(file_path)
+                if not local:
+                    os.remove(file_path)
