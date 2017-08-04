dc99da6eaf3aac05c529c6007a42ff3028b0272e
Michael Hess <hess_michael@bah.com>
2017-08-04 16:05:09 -0400

    Update historical FABS script to reflect changes to the PublishedAwardFinancialAssistance model

--- dataactcore/scripts/loadHistoricalFabs.py
+++ dataactcore/scripts/loadHistoricalFabs.py
@@ -7,9 +7,6 @@ import zipfile
 import numpy as np
 import pandas as pd
 
-from sqlalchemy.dialects.postgresql import insert
-from sqlalchemy.exc import IntegrityError
-
 from dataactcore.logging import configure_logging
 from dataactcore.config import CONFIG_BROKER
 from dataactcore.interfaces.db import GlobalDB
@@ -37,73 +34,45 @@ def parse_fabs_file(f, sess):
         'principal_place_cc', 'principal_place_country_code', 'principal_place_zip', 'principal_place_cd',
         'cfda_program_title', 'project_description', 'duns_no', 'receip_addr1', 'receip_addr2', 'receip_addr3',
         'face_loan_guran', 'orig_sub_guran', 'recipient_cd', 'rec_flag', 'recipient_country_code', 'uri',
-        'recipient_state_code'
+        'recipient_state_code', 'last_modified_date'
     ])
 
-    (to_create, to_update, to_delete) = format_fabs_data(data)
+    clean_data = format_fabs_data(data)
 
-    # delete records
-    if len(to_delete.index) > 0:
-        logger.info("deleting "+str(len(to_delete.index))+" rows")
-    for index, row in to_delete.iterrows():
-        records = sess.query(PublishedAwardFinancialAssistance).filter_by(
-            awarding_sub_tier_agency_c=row['awarding_sub_tier_agency_c'], fain=row['fain'],
-            award_modification_amendme=row['award_modification_amendme'],  uri=row['uri']
-        )
-        records.delete(synchronize_session='fetch')
+    logger.info("loading {} rows".format(len(clean_data.index)))
 
-    # insert to db
-    to_load = pd.concat([to_create, to_update])
-    if len(to_load.index) > 0:
-        logger.info("loading "+str(len(to_create.index))+" new rows and updating "+str(len(to_update.index))+" rows")
-    try:
-        insert_dataframe(to_load, PublishedAwardFinancialAssistance.__table__.name, sess.connection())
-        sess.commit()
-    except IntegrityError:
-        sess.rollback()
-        load_fabs_by_row(to_load, sess)
-
-
-def load_fabs_by_row(data, sess):
-    count = 1
-    for index, row in data.iterrows():
-        if count % 100000 == 0 or count == len(data.index):
-            logger.info("inserted "+str(count)+" rows")
-        try:
-            statement = insert(PublishedAwardFinancialAssistance).values(**row)
-            sess.execute(statement)
-            sess.commit()
-        except IntegrityError:
-            sess.rollback()
-            stmt = insert(PublishedAwardFinancialAssistance).values(**row)
-            update_dict = {
-                c.name: row[c.name]
-                for c in stmt.excluded
-                if not c.primary_key
-            }
-            new_stmt = stmt.on_conflict_do_update(constraint='uniq_award_mod_sub_tier_fain_uri', set_=update_dict)
-            sess.execute(new_stmt)
-            sess.commit()
-        count = count + 1
+    insert_dataframe(clean_data, PublishedAwardFinancialAssistance.__table__.name, sess.connection())
+    sess.commit()
 
 
 def format_fabs_data(data):
-    # NOTE: commented out lines are due to the PublishedAwardFinancialAssistance model being unfinished
     logger.info("formatting data")
 
-    data['correction_late_ind'] = data.apply(lambda x: format_correction_ind(x), axis=1)
+    # drop rows with null FAIN and URI
+    data = data[~((data['federal_award_id'].isnull()) & (data['uri'].isnull()))].copy()
+
+    proper_casing_cols = ['recipient_name', 'recipient_city_name', 'recipient_county_name', 'receip_addr1',
+                          'receip_addr2', 'receip_addr3']
+    for col in proper_casing_cols:
+        data[col] = data.apply(lambda x: format_proper_casing(x, col), axis=1)
+
+    cols_with_colons = ['action_type', 'assistance_type', 'agency_code', 'recipient_type', 'correction_late_ind']
+    for col in cols_with_colons:
+        data[col] = data.apply(lambda x: remove_data_after_colon(x, col), axis=1)
+
     # data['recipient_city_code'] = data.apply(lambda x: format_integer_code(x, 'recipient_city_code', 5), axis=1)
-    # data['recipient_county_code'] = data.apply(lambda x: format_integer_code(x, 'recipient_county_code', 3), axis=1)
+    data['recipient_county_code'] = data.apply(lambda x: format_integer_code(x, 'recipient_county_code', 3), axis=1)
     data['legal_entity_zip5'] = data.apply(lambda x: format_zip_five(x), axis=1)
     data['legal_entity_zip_last4'] = data.apply(lambda x: format_zip_four(x), axis=1)
-    data['ending_date'] = data.apply(lambda x: format_date(x, 'ending_date'), axis=1)
-    data['starting_date'] = data.apply(lambda x: format_date(x, 'starting_date'), axis=1)
-    data['principal_place_zip'] = data.apply(lambda x: format_full_zip(x), axis=1)
-    data['principal_place_cd'] = data.apply(lambda x: format_principal_place_cd(x), axis=1)
-    # data['place_of_perform_city'] = data.apply(lambda x: format_cc_code(x, False), axis=1)
-    # data['place_of_perform_county_na'] = data.apply(lambda x: format_cc_code(x, True), axis=1)
-    data['record_type'] = data.apply(lambda x: format_record_type(x), axis=1)
     data['total_funding_amount'] = data.apply(lambda x: format_total_funding(x), axis=1)
+    data['starting_date'] = data.apply(lambda x: format_date(x, 'starting_date'), axis=1)
+    data['ending_date'] = data.apply(lambda x: format_date(x, 'ending_date'), axis=1)
+    data['record_type'] = data.apply(lambda x: format_record_type(x), axis=1)
+    data['place_of_perform_county_na'] = data.apply(lambda x: format_cc_code(x, True), axis=1)
+    data['place_of_perform_city'] = data.apply(lambda x: format_cc_code(x, False), axis=1)
+    data['principal_place_zip'] = data.apply(lambda x: format_full_zip(x), axis=1)
+    data['principal_place_cd'] = data.apply(lambda x: format_cd(x, 'principal_place_cd'), axis=1)
+    data['recipient_cd'] = data.apply(lambda x: format_cd(x, 'recipient_cd'), axis=1)
     data['is_historical'] = np.full(len(data.index), True, dtype=bool)
 
     # adding columns missing from historical data
@@ -151,17 +120,18 @@ def format_fabs_data(data):
             'receip_addr2': 'legal_entity_address_line2',
             'receip_addr3': 'legal_entity_address_line3',
             # 'recipient_city_code': 'legal_entity_city_code',
-            # 'recipient_city_name': 'legal_entity_city_name',
+            'recipient_city_name': 'legal_entity_city_name',
             'recipient_cd': 'legal_entity_congressional',
             'recipient_country_code': 'legal_entity_country_code',
-            # 'recipient_county_code': 'legal_entity_county_code',
-            # 'recipient_county_name': 'legal_entity_county_name',
+            'recipient_county_code': 'legal_entity_county_code',
+            'recipient_county_name': 'legal_entity_county_name',
             'legal_entity_foreign_city': 'legal_entity_foreign_city',
             'legal_entity_foreign_posta': 'legal_entity_foreign_posta',
             'legal_entity_foreign_provi': 'legal_entity_foreign_provi',
-            # 'recipient_state_code': 'legal_entity_state_code',
+            'recipient_state_code': 'legal_entity_state_code',
             'legal_entity_zip5': 'legal_entity_zip5',
             'legal_entity_zip_last4': 'legal_entity_zip_last4',
+            'last_modified_date': 'modified_at',
             'non_fed_funding_amount': 'non_federal_funding_amount',
             'orig_sub_guran': 'original_loan_subsidy_cost',
             'ending_date': 'period_of_performance_curr',
@@ -171,15 +141,16 @@ def format_fabs_data(data):
             'principal_place_country_code': 'place_of_perform_country_c',
             'place_of_performance_forei': 'place_of_performance_forei',
             'principal_place_zip': 'place_of_performance_zip4a',
-            # 'place_of_perform_city': 'place_of_perform_city',
-            # 'place_of_perform_county_na': 'place_of_perform_county_na',
-            # 'principal_place_state': 'place_of_perform_state_nam',
+            'place_of_perform_city': 'place_of_performance_city',
+            'place_of_perform_county_na': 'place_of_perform_county_na',
+            'principal_place_state': 'place_of_perform_state_nam',
             'record_type': 'record_type',
             'sai_number': 'sai_number',
             'total_funding_amount': 'total_funding_amount',
             'uri': 'uri'
         }, {
             'place_of_performance_congr': {'pad_to_length': 2, 'keep_null': True},
+            'legal_entity_congressional': {'pad_to_length': 2, 'keep_null': True},
             'awardee_or_recipient_uniqu': {'pad_to_length': 9, 'keep_null': True}
         }
     )
@@ -189,63 +160,50 @@ def format_fabs_data(data):
     cdata = cdata.replace(np.nan, '', regex=True)
     cdata = cdata.applymap(lambda x: str(x).strip() if len(str(x).strip()) else None)
 
-    # drop rows with duplicate UniqueConstraints
-    cdata = cdata[
-        (~cdata.duplicated(
-            subset=['awarding_sub_tier_agency_c', 'award_modification_amendme', 'fain', 'uri'],
-            keep='first'
-        )) |
-        (cdata['awarding_sub_tier_agency_c'].isnull()) | (cdata['award_modification_amendme'].isnull()) |
-        (cdata['fain'].isnull()) | (cdata['uri'].isnull())
-    ]
+    # generate the afa_generated_unique field
+    cdata['afa_generated_unique'] = cdata.apply(lambda x: generate_unique_string(x), axis=1)
 
-    # pull correction and deletion rows from the dataframe
-    to_create = cdata.loc[~cdata['correction_late_delete_ind'].isin(['C', 'D'])]
-    to_update = cdata.loc[cdata['correction_late_delete_ind'] == 'C']
-    to_delete = cdata.loc[cdata['correction_late_delete_ind'] == 'D']
-
-    # pull out all rows that match those in the corrections dataframe
-    matches_to_update = to_create.loc[
-        (to_create['awarding_sub_tier_agency_c'].isin(to_update['awarding_sub_tier_agency_c'])) &
-        (to_create['award_modification_amendme'].isin(to_update['award_modification_amendme'])) &
-        (to_create['fain'].isin(to_update['fain'])) & (to_create['uri'].isin(to_update['uri']))
-    ]
-    # pull out all rows that match those in the deletions dataframe
-    matches_to_delete = to_create.loc[
-        (to_create['awarding_sub_tier_agency_c'].isin(to_delete['awarding_sub_tier_agency_c'])) &
-        (to_create['award_modification_amendme'].isin(to_delete['award_modification_amendme'])) &
-        (to_create['fain'].isin(to_delete['fain'])) & (to_create['uri'].isin(to_delete['uri']))
-    ]
-    # drop exact duplicates
-    to_create = pd.concat([to_create, matches_to_update, matches_to_delete]).drop_duplicates(keep=False)
-
-    return to_create, to_update, to_delete
+    return cdata
 
 
-def format_correction_ind(row):
-    return row['correction_late_ind'][:1]
+def format_proper_casing(row, header):
+    # if string is all caps, implement "Proper Casing"
+    if str(row[header]).isupper():
+        return str(row[header]).title()
+    return row[header]
+
+
+def remove_data_after_colon(row, header):
+    # return the data before the colon in the row, or None
+    if ':' in str(row[header]):
+        return str(row[header]).split(':')[0]
+    return None
 
 
 def format_integer_code(row, header, int_length):
-    # row[header] is an integer of length int_length
+    # ensure row[header] is an integer of length int_length
     value = None
     try:
         value = row[header]
         value = int(value)
     except ValueError:
-        pass
+        value = None
 
     return value if len(str(row[header])) == int_length else None
 
 
 def format_zip_five(row):
+    # remove extra characters from recipient_zip
+    full_zip = re.sub("[^0-9]", "", str(row['recipient_zip']))
     # separate first 5 digits from recipient_zip or set invalid zip to None
-    return str(row['recipient_zip'])[:5] if len(str(row['recipient_zip'])) >= 5 else None
+    return str(full_zip)[:5] if len(str(full_zip)) >= 5 else None
 
 
 def format_zip_four(row):
+    # remove extra characters from recipient_zip
+    full_zip = re.sub("[^0-9]", "", str(row['recipient_zip']))
     # separate last 4 digits from recipient_zip or set invalid zip to None
-    return str(row['recipient_zip'])[5:9] if len(str(row['recipient_zip'])) >= 9 else None
+    return str(full_zip)[5:9] if len(str(full_zip)) >= 9 else None
 
 
 def format_date(row, header):
@@ -259,17 +217,17 @@ def format_full_zip(row):
     return full_zip if len(full_zip) != 5 or len(full_zip) != 9 else None
 
 
-def format_principal_place_cd(row):
-    # remove extra characters from principal_place_cd or set invalid integer to None
-    pop_congr = re.sub("[^0-9]", "", str(row['principal_place_cd']))
+def format_cd(row, column_name):
+    # remove extra characters from row[column_name] or set invalid integer to None
+    congr_district = re.sub("[^0-9]", "", str(row[column_name]))
     try:
-        pop_congr = int(pop_congr)
-        if pop_congr > 53 or pop_congr < 0:
-            pop_congr = None
+        congr_district = int(congr_district)
+        if congr_district < 0 or (congr_district > 53 and congr_district not in [98, 99]):
+            congr_district = None
     except ValueError:
-        pass
+        congr_district = None
 
-    return pop_congr if len(str(pop_congr)) > 0 else None
+    return congr_district if len(str(congr_district)) > 0 else None
 
 
 def format_cc_code(row, is_county):
@@ -298,7 +256,7 @@ def format_record_type(row):
 
 
 def format_total_funding(row):
-    # if total_funding_amount = 0 or nan, set it to fed_funding_amount + non_fed_funding_amount
+    # if total_funding_amount = 0 or NaN, set it to fed_funding_amount + non_fed_funding_amount
     value = 0
     try:
         value = float(row['total_funding_amount'])
@@ -308,6 +266,32 @@ def format_total_funding(row):
         pass
 
     return value
+
+
+def generate_unique_string(row):
+    # create unique string from the awarding_sub_tier_agency_c, award_modification_amendme, fain, and uri
+    astac = row['awarding_sub_tier_agency_c'] if row['awarding_sub_tier_agency_c'] is not None else '-none-'
+    ama = row['award_modification_amendme'] if row['award_modification_amendme'] is not None else '-none-'
+    fain = row['fain'] if row['fain'] is not None else '-none-'
+    uri = row['uri'] if row['uri'] is not None else '-none-'
+    return astac + ama + fain + uri
+
+
+def set_active_rows(sess):
+    logger.info('marking current records as active')
+    sess.execute(
+        "UPDATE published_award_financial_assistance AS all_pafa SET is_active=TRUE " +
+        "FROM (SELECT DISTINCT pafa.published_award_financial_assistance_id " +
+        "FROM published_award_financial_assistance AS pafa " +
+        "INNER JOIN (SELECT max(modified_at) AS modified_at, afa_generated_unique " +
+        "FROM published_award_financial_assistance GROUP BY afa_generated_unique) sub_pafa " +
+        "ON pafa.modified_at = sub_pafa.modified_at AND " +
+        "COALESCE(pafa.afa_generated_unique, '') = COALESCE(sub_pafa.afa_generated_unique, '') " +
+        "WHERE COALESCE(pafa.correction_late_delete_ind, '') != 'D' AND " +
+        "COALESCE(pafa.correction_late_delete_ind, '') != 'd' ) AS selected " +
+        "WHERE all_pafa.published_award_financial_assistance_id = selected.published_award_financial_assistance_id"
+    )
+    sess.commit()
 
 
 def main():
@@ -332,7 +316,10 @@ def main():
             if re.match('^\d{4}_All_(DirectPayments|Grants|Insurance|Loans|Other)_Full_\d{8}.csv.zip', file):
                 parse_fabs_file(open(os.path.join(base_path, file)), sess)
 
+    set_active_rows(sess)
+
     logger.info("Historical FABS script complete")
+
 
 if __name__ == '__main__':
     configure_logging()
