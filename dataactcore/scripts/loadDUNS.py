94e1230432873f1e5176cd49719dd347bfa24984
Daniel Boos <boos_daniel@bah.com>
2017-08-07 12:11:21 -0400

    Reverting caching to preserve memory

--- dataactcore/scripts/loadDUNS.py
+++ dataactcore/scripts/loadDUNS.py
@@ -34,25 +34,26 @@ def get_config():
     return None, None, None, None, None
 
 
-def get_relevant_models(data, all_models, benchmarks=False):
+def get_relevant_models(data, benchmarks=False):
     # Get a list of the duns we're gonna work off of to prevent multiple calls to the database
     if benchmarks:
         get_models = time.time()
     logger.info("getting relevant models")
     duns_found = [duns.strip().zfill(9) for duns in list(data["awardee_or_recipient_uniqu"].unique())]
-    models = {duns_num: duns for duns_num, duns in all_models.items() if duns_num in duns_found}
+    dun_objects_found = sess.query(DUNS).filter(DUNS.awardee_or_recipient_uniqu.in_(duns_found))
+    models = {duns.awardee_or_recipient_uniqu: duns for duns in dun_objects_found}
     logger.info("getting models with activation dates already set")
     activated_models = {duns_num: duns for duns_num, duns in models.items()
-                           if duns.activation_date is not None}
+                        if duns.activation_date is not None}
     if benchmarks:
         logger.info("Getting models took {} seconds".format(time.time() - get_models))
     return models, activated_models
 
 
-def load_duns_by_row(data, sess, models, activated_models, all_models, benchmarks=False):
+def load_duns_by_row(data, sess, models, activated_models, benchmarks=False):
     data = activation_check(data, activated_models, benchmarks).where(pd.notnull(data), None)
     update_duns(models, data, benchmarks=benchmarks)
-    all_models.update(models)
+    sess.add_all(models.values())
 
 
 def activation_check(data, activated_models, benchmarks=False):
@@ -100,7 +101,7 @@ def clean_sam_data(data):
             )
 
 
-def parse_sam_file(file_path, sess, monthly=False, all_models=None, benchmarks=False):
+def parse_sam_file(file_path, sess, monthly=False, benchmarks=False):
     parse_start_time = time.time()
     logger.info("starting file " + str(file_path))
 
@@ -119,13 +120,6 @@ def parse_sam_file(file_path, sess, monthly=False, all_models=None, benchmarks=F
             "legal_business_name": 10
         }
         column_header_mapping_ordered = OrderedDict(sorted(column_header_mapping.items(), key=lambda c: c[1]))
-
-        if not monthly and not all_models:
-            if benchmarks:
-                get_models = time.time()
-            all_models = {duns.awardee_or_recipient_uniqu: duns for duns in sess.query(DUNS)}
-            if benchmarks:
-                logger.info("Getting all models for the first time took {} seconds".format(time.time() - get_models))
 
         # Initial sweep of the file to see rows and possibly what DUNS we're updating
         if benchmarks:
@@ -163,22 +157,36 @@ def parse_sam_file(file_path, sess, monthly=False, all_models=None, benchmarks=F
                     csv_data = csv_data.where(csv_data["awardee_or_recipient_uniqu"].notnull())
                     # cleaning and replacing NaN/NaT with None's
                     csv_data = clean_sam_data(csv_data.where(pd.notnull(csv_data), None))
-                    del csv_data["sam_extract_code"]
 
                     if monthly:
                         logger.info("adding all monthly data with bulk load")
                         if benchmarks:
                             bulk_month_load = time.time()
+                        del csv_data["sam_extract_code"]
                         insert_dataframe(csv_data, DUNS.__table__.name, sess.connection())
-                        sess.commit()
                         if benchmarks:
                             logger.info("Bulk month load took {} seconds".format(time.time()-bulk_month_load))
                     else:
-                        logger.info("loading csv data ({} rows)".format(len(csv_data.index)))
-                        models, activated_models = get_relevant_models(csv_data, all_models,
-                                                                          benchmarks=benchmarks)
-                        load_duns_by_row(csv_data, sess, models, activated_models, all_models,
-                                         benchmarks=benchmarks)
+                        add_data = csv_data[csv_data.sam_extract_code == '2']
+                        update_delete_data = csv_data[(csv_data.sam_extract_code == '3') | (csv_data.sam_extract_code == '1')]
+                        for dataframe in [add_data, update_delete_data]:
+                            del dataframe["sam_extract_code"]
+
+                        if not add_data.empty:
+                            try:
+                                logger.info("attempting to bulk load add data")
+                                insert_dataframe(add_data, DUNS.__table__.name, sess.connection())
+                            except IntegrityError:
+                                logger.info("bulk loading add data failed, loading add data by row")
+                                sess.rollback()
+                                models, activated_models = get_relevant_models(add_data, benchmarks=benchmarks)
+                                logger.info("loading add data ({} rows)".format(len(add_data.index)))
+                                load_duns_by_row(add_data, sess, models, activated_models, benchmarks=benchmarks)
+                        if not update_delete_data.empty:
+                            models, activated_models = get_relevant_models(update_delete_data, benchmarks=benchmarks)
+                            logger.info("loading update_delete data ({} rows)".format(len(update_delete_data.index)))
+                            load_duns_by_row(update_delete_data, sess, models, activated_models, benchmarks=benchmarks)
+                    sess.commit()
 
             added_rows += nrows
             batch += 1
@@ -186,19 +194,17 @@ def parse_sam_file(file_path, sess, monthly=False, all_models=None, benchmarks=F
         if benchmarks:
             logger.info("Parsing {} took {} seconds with {} rows".format(dat_file_name, time.time()-parse_start_time,
                                                                          added_rows))
-        return all_models
 
 
-def process_from_dir(root_dir, file_name, sess, local, monthly=False, all_models=None, benchmarks=False):
+def process_from_dir(root_dir, file_name, sess, local, monthly=False, benchmarks=False):
     file_path = os.path.join(root_dir, file_name)
     if not local:
         logger.info("Pulling {}".format(file_name))
         with open(file_path, "wb") as zip_file:
             sftp.getfo(''.join([REMOTE_SAM_DIR, '/', file_name]), zip_file)
-    all_models = parse_sam_file(file_path, sess, monthly=monthly, all_models=all_models, benchmarks=benchmarks)
+    parse_sam_file(file_path, sess, monthly=monthly, benchmarks=benchmarks)
     if not local:
         os.remove(file_path)
-    return all_models
 
 
 def get_parser():
@@ -273,15 +279,11 @@ if __name__ == '__main__':
             daily_files_after = sorted_daily_monthly[sorted_daily_monthly.index(earliest_daily_file)+1:]
             latest_daily_file = sorted_daily_file_names[-1]
 
-            all_models = None
             if historic:
                 process_from_dir(root_dir, earliest_monthly_file, sess, local, monthly=True, benchmarks=benchmarks)
 
                 for daily_file in daily_files_after:
-                    all_models = process_from_dir(root_dir, daily_file, sess, local, all_models=all_models,
-                                                  benchmarks=benchmarks)
+                    process_from_dir(root_dir, daily_file, sess, local, benchmarks=benchmarks)
             else:
-                all_models = process_from_dir(root_dir, latest_daily_file, sess, local, benchmarks=benchmarks)
-            sess.add_all(all_models.values())
-            sess.commit()
+                process_from_dir(root_dir, latest_daily_file, sess, local, benchmarks=benchmarks)
         sess.close()
