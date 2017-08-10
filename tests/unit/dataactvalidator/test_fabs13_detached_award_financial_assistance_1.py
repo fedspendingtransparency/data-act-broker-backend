7b21c4c6a120743036501a9eed05ab620e074c47
Michael Hess <hess_michael@bah.com>
2017-08-10 11:21:57 -0400

    Change D vaildations to FABS validations, update text for validations messages

---
+++ tests/unit/dataactvalidator/test_fabs13_detached_award_financial_assistance_1.py
@@ -0,0 +1,31 @@
+from tests.unit.dataactcore.factories.staging import DetachedAwardFinancialAssistanceFactory
+from tests.unit.dataactvalidator.utils import number_of_errors, query_columns
+
+_FILE = 'fabs13_detached_award_financial_assistance_1'
+
+
+def test_column_headers(database):
+    expected_subset = {"row_number", "record_type", "legal_entity_zip5"}
+    actual = set(query_columns(_FILE, database))
+    assert expected_subset == actual
+
+
+def test_success(database):
+    """ Test LegalEntityZIP5 must be blank for aggregate records (i.e., when RecordType = 1) record type 2 doesn't
+        affect success """
+    det_award = DetachedAwardFinancialAssistanceFactory(record_type=2, legal_entity_zip5="12345")
+    det_award_2 = DetachedAwardFinancialAssistanceFactory(record_type=2, legal_entity_zip5=None)
+    det_award_null = DetachedAwardFinancialAssistanceFactory(record_type=1, legal_entity_zip5=None)
+    det_award_null_2 = DetachedAwardFinancialAssistanceFactory(record_type=1, legal_entity_zip5='')
+
+    errors = number_of_errors(_FILE, database, models=[det_award, det_award_2, det_award_null, det_award_null_2])
+    assert errors == 0
+
+
+def test_failure(database):
+    """ Test failure when LegalEntityZIP5 isn't blank for aggregate records (i.e., when RecordType = 1) """
+
+    det_award = DetachedAwardFinancialAssistanceFactory(record_type=1, legal_entity_zip5="Test")
+
+    errors = number_of_errors(_FILE, database, models=[det_award])
+    assert errors == 1
---
+++ tests/unit/dataactvalidator/test_fabs41_detached_award_financial_assistance_1.py
@@ -0,0 +1,50 @@
+from tests.unit.dataactcore.factories.staging import DetachedAwardFinancialAssistanceFactory
+from tests.unit.dataactvalidator.utils import number_of_errors, query_columns
+from dataactcore.models.domainModels import CityCode
+
+_FILE = 'fabs41_detached_award_financial_assistance_1'
+
+
+def test_column_headers(database):
+    expected_subset = {"row_number", "place_of_performance_code"}
+    actual = set(query_columns(_FILE, database))
+    assert expected_subset == actual
+
+
+def test_success(database):
+    """ For PrimaryPlaceOfPerformanceCode XX##### city must exist in provided state (zip4 not provided, error).
+        Ignore for all other formats of PrimaryPlaceOfPerformanceCode """
+
+    city_code = CityCode(city_code="10987", state_code="NY")
+    det_award_1 = DetachedAwardFinancialAssistanceFactory(place_of_performance_code="NY*****",
+                                                          place_of_performance_zip4a="2")
+    det_award_2 = DetachedAwardFinancialAssistanceFactory(place_of_performance_code="NY**123",
+                                                          place_of_performance_zip4a="1")
+    det_award_3 = DetachedAwardFinancialAssistanceFactory(place_of_performance_code="NY**123",
+                                                          place_of_performance_zip4a=None)
+    det_award_4 = DetachedAwardFinancialAssistanceFactory(place_of_performance_code="ny10986",
+                                                          place_of_performance_zip4a="12345")
+    det_award_5 = DetachedAwardFinancialAssistanceFactory(place_of_performance_code="Na10987",
+                                                          place_of_performance_zip4a="12345-6789")
+    det_award_6 = DetachedAwardFinancialAssistanceFactory(place_of_performance_code="Ny10987",
+                                                          place_of_performance_zip4a=None)
+    det_award_7 = DetachedAwardFinancialAssistanceFactory(place_of_performance_code="Ny10987",
+                                                          place_of_performance_zip4a='')
+    errors = number_of_errors(_FILE, database, models=[det_award_1, det_award_2, det_award_3, det_award_4, det_award_5,
+                                                       det_award_6, det_award_7, city_code])
+    assert errors == 0
+
+
+def test_failure(database):
+    """ Test failure for PrimaryPlaceOfPerformanceCode XX##### city must exist in provided state
+        (zip4 not provided, error). """
+
+    city_code = CityCode(city_code="10987", state_code="NY")
+    det_award_1 = DetachedAwardFinancialAssistanceFactory(place_of_performance_code="ny10986",
+                                                          place_of_performance_zip4a=None)
+    det_award_2 = DetachedAwardFinancialAssistanceFactory(place_of_performance_code="NY10986",
+                                                          place_of_performance_zip4a='')
+    det_award_3 = DetachedAwardFinancialAssistanceFactory(place_of_performance_code="na10987",
+                                                          place_of_performance_zip4a=None)
+    errors = number_of_errors(_FILE, database, models=[det_award_1, det_award_2, det_award_3, city_code])
+    assert errors == 3
