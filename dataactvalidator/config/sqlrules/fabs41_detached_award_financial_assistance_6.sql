6d39816d6e04a0a150d81e4c09c936e3833f2b2e
Daniel Boos <boos_daniel@bah.com>
2017-08-11 09:05:51 -0400

    Using `place_of_performance_zip4a` for fabs41_6

--- dataactvalidator/config/sqlrules/fabs41_detached_award_financial_assistance_6.sql
+++ dataactvalidator/config/sqlrules/fabs41_detached_award_financial_assistance_6.sql
@@ -1,7 +1,7 @@
 -- If PrimaryPlaceOfPerformanceCode is XX00000, PrimaryPlaceOfPerformanceZip4 must not be 'city-wide'
 SELECT
     row_number,
-    place_of_performance_code
+    place_of_performance_zip4a
 FROM detached_award_financial_assistance
 WHERE submission_id = {0}
     AND UPPER(place_of_performance_code) ~ '^[A-Z][A-Z]00000$'
