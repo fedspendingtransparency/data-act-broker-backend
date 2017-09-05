ca8ce364161cef2f630f1459cf4b8efc3c900711
alburde1 <Burdeyny_Alisa@bah.com>
2017-09-05 11:41:45 -0400

    Speeding up fabs37_2
    + Speeding up rule
    + Fixing sub-table names to match rule name
    + Fixing rule to include checking for trailing zeroes

--- dataactvalidator/config/sqlrules/fabs37_detached_award_financial_assistance_2.sql
+++ dataactvalidator/config/sqlrules/fabs37_detached_award_financial_assistance_2.sql
@@ -2,7 +2,7 @@
 -- This does not apply to correction records (those with CorrectionLateDeleteIndicator = C).
 -- Should not be active (action_date <= archived_date and when archived date exists)
 -- If the ActionDate is < published_date, should trigger a warning.
-WITH detached_award_financial_assistance_d37_2_{0} AS
+WITH detached_award_financial_assistance_fabs37_2_{0} AS
     (SELECT submission_id,
         row_number,
         cfda_number,
@@ -17,16 +17,16 @@ SELECT
     action_type,
     correction_late_delete_ind,
     action_date
-FROM detached_award_financial_assistance_d37_2_{0} AS dafa
+FROM detached_award_financial_assistance_fabs37_2_{0} AS dafa
 WHERE dafa.action_type IN ('B', 'C', 'D')
     AND ((dafa.correction_late_delete_ind != 'C')
         or (dafa.correction_late_delete_ind is null)
     )
     AND dafa.row_number IN (
         SELECT DISTINCT sub_dafa.row_number
-        FROM detached_award_financial_assistance_d37_2_{0} AS sub_dafa
+        FROM detached_award_financial_assistance_fabs37_2_{0} AS sub_dafa
             JOIN cfda_program AS cfda
-            ON (sub_dafa.cfda_number IS NOT DISTINCT FROM to_char(cfda.program_number, 'FM99D999')
+            ON (sub_dafa.cfda_number = to_char(cfda.program_number, 'FM00.000')
             AND ((sub_dafa.action_date <= cfda.published_date)
                  OR ((sub_dafa.action_date >= cfda.archived_date)
                      AND (cfda.archived_date != ''))
