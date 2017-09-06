97d3861721d3964f3d79b122f4aec25784659161
alburde1 <Burdeyny_Alisa@bah.com>
2017-09-06 10:36:40 -0400

    Temporarily update SQL rules/tests to allow 0's for FABS28_2 and FABS29_2

--- tests/unit/dataactvalidator/test_fabs29_detached_award_financial_assistance_2.py
+++ tests/unit/dataactvalidator/test_fabs29_detached_award_financial_assistance_2.py
@@ -15,16 +15,16 @@ def test_success(database):
 
     det_award = DetachedAwardFinancialAssistanceFactory(assistance_type="03", original_loan_subsidy_cost=None)
     det_award_2 = DetachedAwardFinancialAssistanceFactory(assistance_type="05", original_loan_subsidy_cost=None)
+    det_award_3 = DetachedAwardFinancialAssistanceFactory(assistance_type="03", original_loan_subsidy_cost=0)
 
-    errors = number_of_errors(_FILE, database, models=[det_award, det_award_2])
+    errors = number_of_errors(_FILE, database, models=[det_award, det_award_2, det_award_3])
     assert errors == 0
 
 
 def test_failure(database):
     """ OriginalLoanSubsidyCost must be blank for non-loans (i.e., when AssistanceType is not 07 or 08). """
 
-    det_award = DetachedAwardFinancialAssistanceFactory(assistance_type="03", original_loan_subsidy_cost=0)
-    det_award_2 = DetachedAwardFinancialAssistanceFactory(assistance_type="05", original_loan_subsidy_cost=20)
+    det_award = DetachedAwardFinancialAssistanceFactory(assistance_type="05", original_loan_subsidy_cost=20)
 
-    errors = number_of_errors(_FILE, database, models=[det_award, det_award_2])
-    assert errors == 2
+    errors = number_of_errors(_FILE, database, models=[det_award])
+    assert errors == 1
