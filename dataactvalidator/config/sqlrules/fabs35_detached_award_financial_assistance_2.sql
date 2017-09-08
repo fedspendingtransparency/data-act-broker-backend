40ea79700f9bd0ae1bf2c5dfd731870e05e00488
alburde1 <Burdeyny_Alisa@bah.com>
2017-09-08 13:08:34 -0400

    Speeding up fabs41_5
    + also updating the speedup for fabs35_2 a tiny bit

--- dataactvalidator/config/sqlrules/fabs35_detached_award_financial_assistance_2.sql
+++ dataactvalidator/config/sqlrules/fabs35_detached_award_financial_assistance_2.sql
@@ -6,7 +6,7 @@ FROM detached_award_financial_assistance as dafa
 WHERE dafa.submission_id = {0}
     AND COALESCE(dafa.legal_entity_zip5, '') != ''
     AND NOT EXISTS (
-		SELECT DISTINCT z.zip5
+		SELECT *
 		FROM zips as z
 		WHERE z.zip5 = dafa.legal_entity_zip5
 	)
--- dataactvalidator/config/sqlrules/fabs41_detached_award_financial_assistance_5.sql
+++ dataactvalidator/config/sqlrules/fabs41_detached_award_financial_assistance_5.sql
@@ -1,5 +1,7 @@
--- The provided PrimaryPlaceofPerformanceZIP+4 must be in the state specified by PrimaryPlaceOfPerformanceCode. In this specific submission row, the first five digits are valid and located in the correct state, but the last 4 are invalid.
-WITH detached_award_financial_assistance_d41_5_{0} AS
+-- The provided PrimaryPlaceofPerformanceZIP+4 must be in the state specified by PrimaryPlaceOfPerformanceCode.
+-- In this specific submission row, the first five digits are valid and located in the correct state,
+-- but the last 4 are invalid.
+WITH detached_award_financial_assistance_fabs41_5_{0} AS
     (SELECT submission_id,
     	row_number,
     	place_of_performance_code,
@@ -10,18 +12,16 @@ SELECT
     dafa.row_number,
     dafa.place_of_performance_code,
     dafa.place_of_performance_zip4a
-FROM detached_award_financial_assistance_d41_5_{0} AS dafa
+FROM detached_award_financial_assistance_fabs41_5_{0} AS dafa
 WHERE COALESCE(dafa.place_of_performance_zip4a, '') != ''
     AND dafa.place_of_performance_zip4a != 'city-wide'
     AND dafa.place_of_performance_zip4a ~ '^\d\d\d\d\d\-?\d\d\d\d$'
-    AND dafa.row_number IN (SELECT DISTINCT sub_dafa.row_number
-                            FROM detached_award_financial_assistance_d41_5_{0} AS sub_dafa
-                            JOIN zips
-                            ON UPPER(LEFT(sub_dafa.place_of_performance_code, 2)) = zips.state_abbreviation
-                            AND UPPER(LEFT(sub_dafa.place_of_performance_zip4a, 5)) = zips.zip5)
-    AND dafa.row_number NOT IN (SELECT DISTINCT sub_dafa.row_number
-                                FROM detached_award_financial_assistance_d41_5_{0} AS sub_dafa
-                                JOIN zips
-                                ON UPPER(LEFT(sub_dafa.place_of_performance_code, 2)) = zips.state_abbreviation
-                                   AND LEFT(sub_dafa.place_of_performance_zip4a, 5) = zips.zip5
-                                   AND RIGHT(sub_dafa.place_of_performance_zip4a, 4) = zips.zip_last4)
+    AND EXISTS (SELECT *
+                FROM zips
+                WHERE UPPER(LEFT(dafa.place_of_performance_code, 2)) = zips.state_abbreviation
+                    AND LEFT(dafa.place_of_performance_zip4a, 5) = zips.zip5)
+    AND NOT EXISTS (SELECT *
+                    FROM zips
+                    WHERE UPPER(LEFT(dafa.place_of_performance_code, 2)) = zips.state_abbreviation
+                        AND LEFT(dafa.place_of_performance_zip4a, 5) = zips.zip5
+                        AND RIGHT(dafa.place_of_performance_zip4a, 4) = zips.zip_last4)
