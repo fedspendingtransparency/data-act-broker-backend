56f58150a2578d1c40a0bff9d232f47194384c29
alburde1 <Burdeyny_Alisa@bah.com>
2017-09-11 10:44:59 -0400

    Speeding up fabs43_3 and creating the state_congressional table

--- tests/unit/dataactvalidator/test_fabs43_detached_award_financial_assistance_3.py
+++ tests/unit/dataactvalidator/test_fabs43_detached_award_financial_assistance_3.py
@@ -1,6 +1,6 @@
 from tests.unit.dataactcore.factories.staging import DetachedAwardFinancialAssistanceFactory
 from tests.unit.dataactvalidator.utils import number_of_errors, query_columns
-from dataactcore.models.domainModels import Zips
+from dataactcore.models.domainModels import StateCongressional
 
 _FILE = 'fabs43_detached_award_financial_assistance_3'
 
@@ -15,8 +15,8 @@ def test_success(database):
     """ Test PrimaryPlaceOfPerformanceCongressionalDistrict exists in the state indicated by the
         PrimaryPlaceOfPerformanceCode or is 90 in a state with multiple districts or when PrimaryPlaceOfPerformanceCode
         is 00***** """
-    zips1 = Zips(zip5='12345', zip_last4='6789', congressional_district_no="01", state_abbreviation="NY")
-    zips2 = Zips(zip5='98765', zip_last4='4321', congressional_district_no="02", state_abbreviation="NY")
+    state_congr_1 = StateCongressional(congressional_district_no="01", state_code="NY")
+    state_congr_2 = StateCongressional(congressional_district_no="02", state_code="NY")
     det_award_1 = DetachedAwardFinancialAssistanceFactory(place_of_performance_code="NY12345",
                                                           place_of_performance_congr="01")
     det_award_2 = DetachedAwardFinancialAssistanceFactory(place_of_performance_code="ny*****",
@@ -31,7 +31,7 @@ def test_success(database):
                                                           place_of_performance_congr=None)
 
     errors = number_of_errors(_FILE, database, models=[det_award_1, det_award_2, det_award_3, det_award_4, det_award_5,
-                                                       det_award_6, zips1, zips2])
+                                                       det_award_6, state_congr_1, state_congr_2])
     assert errors == 0
 
 
@@ -39,9 +39,9 @@ def test_failure(database):
     """ Test failure PrimaryPlaceOfPerformanceCongressionalDistrict exists in the state indicated by the
         PrimaryPlaceOfPerformanceCode or is 90 in a state with multiple districts or when PrimaryPlaceOfPerformanceCode
         is 00***** """
-    zips1 = Zips(zip5='12345', zip_last4='6789', congressional_district_no="01", state_abbreviation="NY")
-    zips2 = Zips(zip5='98765', zip_last4='4321', congressional_district_no="02", state_abbreviation="NY")
-    zips3 = Zips(zip5='12345', zip_last4='4321', congressional_district_no="01", state_abbreviation="PA")
+    state_congr_1 = StateCongressional(congressional_district_no="01", state_code="NY")
+    state_congr_2 = StateCongressional(congressional_district_no="02", state_code="NY")
+    state_congr_3 = StateCongressional(congressional_district_no="01", state_code="PA")
     det_award_1 = DetachedAwardFinancialAssistanceFactory(place_of_performance_code="nY12345",
                                                           place_of_performance_congr="03")
     det_award_2 = DetachedAwardFinancialAssistanceFactory(place_of_performance_code="PA12345",
@@ -51,6 +51,6 @@ def test_failure(database):
     det_award_4 = DetachedAwardFinancialAssistanceFactory(place_of_performance_code="00*****",
                                                           place_of_performance_congr="01")
 
-    errors = number_of_errors(_FILE, database, models=[det_award_1, det_award_2, det_award_3, det_award_4, zips1, zips2,
-                                                       zips3])
+    errors = number_of_errors(_FILE, database, models=[det_award_1, det_award_2, det_award_3, det_award_4,
+                                                       state_congr_1, state_congr_2, state_congr_3])
     assert errors == 4
