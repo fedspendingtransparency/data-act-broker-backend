8a828f73c9da2a497d437fe33c04147164572e58
Michael Hess <hess_michael@bah.com>
2017-09-13 20:46:27 -0400

    Flake8 and update tests

--- tests/unit/dataactbroker/test_domainRoutes.py
+++ tests/unit/dataactbroker/test_domainRoutes.py
@@ -22,23 +22,25 @@ def test_list_agencies_limits(monkeypatch, user_constants, domain_app):
     user = UserFactory()
     cgac = CGACFactory()
     frec_cgac = CGACFactory()
-    frec = FRECFactory(cgac=cgac)
-    cgac_sub_tiers = [SubTierAgencyFactory(sub_tier_agency_code=str(i), cgac=cgac, frec=None, is_frec=False,
-                                           sub_tier_agency_name="Test Subtier Agency "+str(i)) for i in range(1)]
-    frec_sub_tiers = [SubTierAgencyFactory(sub_tier_agency_code=str(1+i), cgac=frec_cgac, frec=frec, is_frec=True,
-                                           sub_tier_agency_name="Test Subtier Agency "+str(1+i)) for i in range(1)]
+    frec = FRECFactory(cgac=frec_cgac)
+    sub_tiers = [SubTierAgencyFactory(sub_tier_agency_code='0', cgac=cgac, frec=None, is_frec=False,
+                                      sub_tier_agency_name="Test Subtier Agency 0"),
+                 SubTierAgencyFactory(sub_tier_agency_code='1', cgac=frec_cgac, frec=frec, is_frec=True,
+                                      sub_tier_agency_name="Test Subtier Agency 1")]
     user.affiliations = [UserAffiliation(cgac=cgac, frec=None, permission_type_id=2),
                          UserAffiliation(cgac=None, frec=frec, permission_type_id=2)]
-    sess.add_all([cgac] + [frec_cgac] + [frec] + cgac_sub_tiers + frec_sub_tiers + [user])
+    sess.add_all([cgac] + [frec_cgac] + [frec] + sub_tiers + [user])
     sess.commit()
     monkeypatch.setattr(domainRoutes, 'g', Mock(user=user))
 
-    res = domain_app.get('/v1/list_agencies/').data.decode('UTF-8')
-    res = json.loads(res)
-    assert res['cgac_agency_list'] in [[{'agency_name': cgac.agency_name, 'cgac_code': cgac.cgac_code, 'priority': 1}],
-                                       [{'agency_name': cgac.agency_name, 'cgac_code': cgac.cgac_code, 'priority': 2}]]
-    assert res['frec_agency_list'] in [[{'agency_name': frec.agency_name, 'frec_code': frec.frec_code, 'priority': 1}],
-                                       [{'agency_name': frec.agency_name, 'frec_code': frec.frec_code, 'priority': 2}]]
+    result = domain_app.get('/v1/list_agencies/').data.decode('UTF-8')
+    res = json.loads(result)
+    assert len(res['cgac_agency_list']) == 1
+    assert len(res['frec_agency_list']) == 1
+    assert res['cgac_agency_list'][0]['agency_name'] == cgac.agency_name
+    assert res['cgac_agency_list'][0]['cgac_code'] == cgac.cgac_code
+    assert res['frec_agency_list'][0]['agency_name'] == frec.agency_name
+    assert res['frec_agency_list'][0]['frec_code'] == frec.frec_code
 
 
 def test_list_agencies_superuser(database, monkeypatch, domain_app):
@@ -46,15 +48,21 @@ def test_list_agencies_superuser(database, monkeypatch, domain_app):
     sess = GlobalDB.db().session
     user = UserFactory(website_admin=True)
     cgacs = [CGACFactory(cgac_code=str(i)) for i in range(3)]
-    sub_tiers = [SubTierAgencyFactory(sub_tier_agency_code=str(1230 + i), cgac=cgacs[i], frec=None, is_frec=False,
-                                      sub_tier_agency_name="Test Subtier Agency "+str(i)) for i in range(3)]
-    sess.add_all(cgacs + sub_tiers + [user])
+    frec_cgac = CGACFactory()
+    frecs = [FRECFactory(frec_code=str(i), cgac=frec_cgac) for i in range(3)]
+    cgac_sub_tiers = [SubTierAgencyFactory(sub_tier_agency_code=str(i), cgac=cgacs[i], frec=None, is_frec=False,
+                                           sub_tier_agency_name="Test Subtier Agency "+str(i)) for i in range(3)]
+    frec_sub_tiers = [SubTierAgencyFactory(sub_tier_agency_code=str(3+i), cgac=frec_cgac, frec=frecs[i], is_frec=True,
+                                           sub_tier_agency_name="Test Subtier Agency "+str(3+i)) for i in range(3)]
+    sess.add_all(cgacs + [frec_cgac] + frecs + cgac_sub_tiers + frec_sub_tiers + [user])
     sess.commit()
     monkeypatch.setattr(domainRoutes, 'g', Mock(user=user))
 
     result = domain_app.get('/v1/list_agencies/').data.decode('UTF-8')
-    result = json.loads(result)
-    result = {el['cgac_code'] for el in result['cgac_agency_list']}
+    response = json.loads(result)
+    result = {el['cgac_code'] for el in response['cgac_agency_list']}
+    assert result == {'0', '1', '2'}    # i.e. all of them
+    result = {el['frec_code'] for el in response['frec_agency_list']}
     assert result == {'0', '1', '2'}    # i.e. all of them
 
 
@@ -63,8 +71,8 @@ def test_list_agencies_all(monkeypatch, user_constants, domain_app):
     sess = GlobalDB.db().session
     user = UserFactory()
     cgacs = [CGACFactory(cgac_code=str(i)) for i in range(3)]
-    frec_cgac = CGACFactory(cgac_code='3')
-    frecs = [FRECFactory(frec_code=str(i), cgac=cgacs[i]) for i in range(3)]
+    frec_cgac = CGACFactory()
+    frecs = [FRECFactory(frec_code=str(i), cgac=frec_cgac) for i in range(3)]
     cgac_sub_tiers = [SubTierAgencyFactory(sub_tier_agency_code=str(i), cgac=cgacs[i], frec=None, is_frec=False,
                                            sub_tier_agency_name="Test Subtier Agency "+str(i)) for i in range(3)]
     frec_sub_tiers = [SubTierAgencyFactory(sub_tier_agency_code=str(3+i), cgac=frec_cgac, frec=frecs[i], is_frec=True,
@@ -77,6 +85,6 @@ def test_list_agencies_all(monkeypatch, user_constants, domain_app):
     result = domain_app.get('/v1/list_all_agencies/').data.decode('UTF-8')
     response = json.loads(result)
     result = {el['cgac_code'] for el in response['agency_list']}
-    assert result == {'0', '1', '2'}  # i.e. all of them
+    assert result == {'0', '1', '2'}    # i.e. all of them
     result = {el['frec_code'] for el in response['shared_agency_list']}
     assert result == {'0', '1', '2'}    # i.e. all of them
