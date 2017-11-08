24f25c9e3066d3d741d5d749ab0a47c7665f9f91
alburde1 <Burdeyny_Alisa@bah.com>
2017-11-08 16:29:44 -0500

    Speeding up c23_award_financial_1

--- dataactvalidator/config/sqlrules/c23_award_financial_1.sql
+++ dataactvalidator/config/sqlrules/c23_award_financial_1.sql
@@ -2,35 +2,35 @@
 -- TransactionObligatedAmount submitted in the reporting period should match (in inverse) the sum of the
 -- FederalActionObligation amounts reported in D1 for the same timeframe, regardless of modifications.
 WITH award_financial_c23_1_{0} AS
-    (SELECT submission_id,
-    	piid,
-    	transaction_obligated_amou,
-    	parent_award_id,
-    	allocation_transfer_agency
+    (SELECT piid,
+	allocation_transfer_agency,
+	transaction_obligated_amou,
+	parent_award_id
     FROM award_financial
     WHERE submission_id = {0}),
+award_financial_grouped_c23_1_{0} AS
+    (SELECT piid,
+    	COALESCE(SUM(transaction_obligated_amou), 0) AS sum_ob_amount
+    FROM award_financial_c23_1_{0}
+    WHERE COALESCE(parent_award_id, '') = ''
+    GROUP BY piid),
 award_procurement_c23_1_{0} AS
-    (SELECT submission_id,
-    	piid,
-    	federal_action_obligation
+    (SELECT piid,
+    	COALESCE(SUM(federal_action_obligation), 0) AS sum_fed_amount
     FROM award_procurement
-    WHERE submission_id = {0})
+    WHERE submission_id = {0}
+    GROUP BY piid)
 SELECT
 	NULL as row_number,
 	af.piid,
-	(SELECT COALESCE(SUM(sub_af.transaction_obligated_amou::numeric),0) AS transaction_sum
-		FROM award_financial_c23_1_{0} as sub_af WHERE sub_af.piid = af.piid AND COALESCE(sub_af.parent_award_id,'') = '') AS transaction_obligated_amou_sum,
-    (SELECT COALESCE(SUM(sub_ap.federal_action_obligation), 0) AS obligation_sum
-		FROM award_procurement_c23_1_{0} as sub_ap WHERE sub_ap.piid = af.piid) AS federal_action_obligation_sum
-FROM award_financial_c23_1_{0} AS af
+	af.sum_ob_amount AS transaction_obligated_amou_sum,
+	ap.sum_fed_amount AS federal_action_obligation_sum
+FROM award_financial_grouped_c23_1_{0} AS af
 JOIN award_procurement_c23_1_{0} AS ap
     ON af.piid = ap.piid
-    AND (af.parent_award_id IS NULL OR af.parent_award_id = '')
-GROUP BY af.piid
-HAVING
-		(SELECT COALESCE(SUM(sub_af.transaction_obligated_amou::numeric),0) AS transaction_sum
-			FROM award_financial_c23_1_{0} as sub_af WHERE sub_af.piid = af.piid AND COALESCE(sub_af.parent_award_id,'') = '') <>
-		-1*(SELECT COALESCE(SUM(sub_ap.federal_action_obligation),0) AS obligation_sum
-			FROM award_procurement_c23_1_{0} as sub_ap WHERE sub_ap.piid = af.piid)
-		AND NOT EXISTS (SELECT sub_af.allocation_transfer_agency FROM award_financial as sub_af WHERE sub_af.piid = af.piid
+WHERE af.sum_ob_amount <> -1*ap.sum_fed_amount
+	AND NOT EXISTS (
+		SELECT *
+		FROM award_financial_c23_1_{0} AS sub_af
+		WHERE sub_af.piid = af.piid
 			AND COALESCE(sub_af.allocation_transfer_agency,'') <> '')
