4ae602ffae67fe6ac5b69bd96346afca97d1a04d
alburde1 <Burdeyny_Alisa@bah.com>
2017-12-11 09:07:42 -0500

    Fixing tests and adding some for the new functions/calculations

--- tests/unit/dataactbroker/test_pullFPDSData.py
+++ tests/unit/dataactbroker/test_pullFPDSData.py
@@ -2,7 +2,7 @@ import xmltodict
 import os
 
 from dataactcore.config import CONFIG_BROKER
-from dataactcore.models.domainModels import SubTierAgency, CGAC
+from dataactcore.models.domainModels import SubTierAgency, CGAC, Zips
 
 from dataactcore.scripts import pullFPDSData
 
@@ -22,29 +22,96 @@ def test_extract_text():
     assert pullFPDSData.extract_text({'#text': "test", '@description': "different content"}) == "test"
 
 
+def test_get_county_by_zip(database):
+    """ Test that getting the county by zip works """
+    sess = database.session
+    zip_code = Zips(zip5='12345', zip_last4='6789', county_number='000')
+    sess.add(zip_code)
+    sess.commit()
+
+    assert pullFPDSData.get_county_by_zip(sess, 'abcde') is None
+    assert pullFPDSData.get_county_by_zip(sess, '123456789') == '000'
+    assert pullFPDSData.get_county_by_zip(sess, '12345') == '000'
+    assert pullFPDSData.get_county_by_zip(sess, '123459876') == '000'
+    assert pullFPDSData.get_county_by_zip(sess, '12345678') is None
+    assert pullFPDSData.get_county_by_zip(sess, '56789') is None
+
+
 def test_calculate_remaining_fields(database):
     """ Test that calculate_remaining_fields calculates fields based on content in the DB and inserts 999 for the code
         if the sub tier agency doesn't exist """
+    sess = database.session
     cgac = CGAC(cgac_id=1, cgac_code='1700', agency_name='test name')
+    zip_code = Zips(zip5='12345', zip_last4='6789', county_number='123')
     sub_tier = SubTierAgency(sub_tier_agency_code='0000', cgac_id=1)
-    database.session.add(cgac)
-    database.session.add(sub_tier)
-    database.session.commit()
+    sess.add(cgac)
+    sess.add(zip_code)
+    sess.add(sub_tier)
+    sess.commit()
+
+    county_by_name = {'GA': {'COUNTY ONE': '123', 'GA COUNTY TWO': '321'},
+                      'MD': {'JUST ONE MD': '024'}}
+    county_by_code = {'MD': {'024': 'JUST ONE MD'},
+                      'GA': {'123': 'COUNTY ONE'}}
 
     tmp_obj = pullFPDSData.calculate_remaining_fields({'awarding_sub_tier_agency_c': "0000",
-                                                       'funding_sub_tier_agency_co': None},
-                                                      {sub_tier.sub_tier_agency_code: sub_tier})
+                                                       'funding_sub_tier_agency_co': None,
+                                                       'place_of_perform_county_na': 'JUST ONE MD',
+                                                       'place_of_performance_state': 'MD',
+                                                       'place_of_perform_country_c': 'USA',
+                                                       'place_of_performance_zip4a': None,
+                                                       'legal_entity_zip4': '987654321',
+                                                       'legal_entity_country_code': 'USA',
+                                                       'legal_entity_state_code': 'GA'},
+                                                      sess,
+                                                      {sub_tier.sub_tier_agency_code: sub_tier},
+                                                      county_by_name,
+                                                      county_by_code)
     tmp_obj_2 = pullFPDSData.calculate_remaining_fields({'awarding_sub_tier_agency_c': None,
                                                          'funding_sub_tier_agency_co': "0001",
-                                                         'funding_sub_tier_agency_na': "Not Real"},
-                                                        {sub_tier.sub_tier_agency_code: sub_tier})
+                                                         'funding_sub_tier_agency_na': "Not Real",
+                                                         'place_of_perform_county_na': 'JUST ONE MD',
+                                                         'place_of_performance_state': 'GA',
+                                                         'place_of_perform_country_c': 'USA',
+                                                         'place_of_performance_zip4a': None,
+                                                         'legal_entity_zip4': '123456789',
+                                                         'legal_entity_country_code': 'USA',
+                                                         'legal_entity_state_code': 'GA'},
+                                                        sess,
+                                                        {sub_tier.sub_tier_agency_code: sub_tier},
+                                                        county_by_name,
+                                                        county_by_code)
+    tmp_obj_3 = pullFPDSData.calculate_remaining_fields({'awarding_sub_tier_agency_c': None,
+                                                         'funding_sub_tier_agency_co': None,
+                                                         'funding_sub_tier_agency_na': None,
+                                                         'place_of_perform_county_na': None,
+                                                         'place_of_performance_state': None,
+                                                         'place_of_perform_country_c': 'USA',
+                                                         'place_of_performance_zip4a': '123456789',
+                                                         'legal_entity_zip4': '12345',
+                                                         'legal_entity_country_code': 'USA',
+                                                         'legal_entity_state_code': 'GA'},
+                                                        sess,
+                                                        {sub_tier.sub_tier_agency_code: sub_tier},
+                                                        county_by_name,
+                                                        county_by_code)
     assert tmp_obj['awarding_agency_code'] == '1700'
     assert tmp_obj['awarding_agency_name'] == 'test name'
+    assert tmp_obj['place_of_perform_county_co'] == '024'
+    assert tmp_obj['legal_entity_county_code'] is None
+    assert tmp_obj['legal_entity_county_name'] is None
     assert tmp_obj_2['funding_agency_code'] == '999'
     assert tmp_obj_2['funding_agency_name'] is None
+    assert tmp_obj_2['place_of_perform_county_co'] is None
+    assert tmp_obj_2['legal_entity_county_code'] == '123'
+    assert tmp_obj_2['legal_entity_county_name'] == 'COUNTY ONE'
+    assert tmp_obj_3['place_of_perform_county_co'] == '123'
+    assert tmp_obj_3['legal_entity_county_code'] == '123'
+    assert tmp_obj_3['legal_entity_county_name'] == 'COUNTY ONE'
 
 
 def test_process_data(database):
+    sess = database.session
     fpds_file = os.path.join(CONFIG_BROKER['path'], 'tests', 'unit', 'data', 'fpdsXML.txt')
     f = open(fpds_file, "r")
     resp = f.read()
@@ -55,9 +122,10 @@ def test_process_data(database):
 
     listed_data = pullFPDSData.list_data(resp_data['feed']['entry'])
 
-    tmp_obj_award = pullFPDSData.process_data(listed_data[0]['content']['award'], atom_type='award',
-                                              sub_tier_list={})
-    tmp_obj_idv = pullFPDSData.process_data(listed_data[1]['content']['IDV'], atom_type='IDV', sub_tier_list={})
+    tmp_obj_award = pullFPDSData.process_data(listed_data[0]['content']['award'], sess=sess, atom_type='award',
+                                              sub_tier_list={}, county_by_name={}, county_by_code={})
+    tmp_obj_idv = pullFPDSData.process_data(listed_data[1]['content']['IDV'], sess=sess, atom_type='IDV',
+                                            sub_tier_list={}, county_by_name={}, county_by_code={})
 
     assert tmp_obj_award['piid'] == '0001'
     assert tmp_obj_award['major_program'] is None
