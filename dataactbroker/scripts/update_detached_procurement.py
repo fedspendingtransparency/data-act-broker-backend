ca961137160d7cbcdd2a94b1d60964951b0ca814
Sonia Hinson <sonia.hinson@gmail.com>
2018-02-09 16:58:10 -0500

    Adding functionality to update transactions when the agency code list change affects a subtier

--- dataactbroker/scripts/update_detached_procurement.py
+++ dataactbroker/scripts/update_detached_procurement.py
@@ -1,5 +1,5 @@
 import logging
-import datetime
+import argparse
 
 from dataactcore.logging import configure_logging
 from dataactcore.interfaces.db import GlobalDB
@@ -10,20 +10,19 @@ logger = logging.getLogger(__name__)
 
 # sess: Passing session into the function for modifying db
 # type: will be either 'awarding' or 'funding' to determine which agency type we are checking
-def update_table(sess, type):
-    logger.info('updating ' + type)
-    invalid = sess.execute("select count(*) from detached_award_procurement where " + type + "_agency_code='999'")
-    invalid_count = invalid.fetchone()[0]
-    logger.info("{} invalid {} rows found".format(invalid_count, type))
+def update_table(sess, agency_type, args):
+    logger.info('updating ' + agency_type)
+
     # The name of the column depends on the type because of limited string length
     suffix = 'o' if type == "funding" else ''
 
-    updated_at = datetime.datetime.utcnow().strftime('%Y-%m-%d %I:%M:%S.%f')
+    invalid_count, condition = set_update_condition(agency_type, suffix, sess, args.update_date)
+
     # updates table based off the parent of the sub_tier_agency code
     sess.execute(
-        "UPDATE detached_award_procurement set updated_at = '" + updated_at + "', "
-        + type + "_agency_code = agency.agency_code, " +
-        type + "_agency_name = agency.agency_name from ( " +
+        "UPDATE detached_award_procurement SET" +
+        + agency_type + "_agency_code = agency.agency_code, " +
+        agency_type + "_agency_name = agency.agency_name from ( " +
         "SELECT sub.sub_tier_agency_code, sub.cgac_id, sub.frec_id, sub.is_frec, CASE WHEN sub.is_frec " +
         "THEN (SELECT agency_name from frec WHERE frec.frec_id = sub.frec_id) " +
         "ELSE (SELECT agency_name from cgac where cgac.cgac_id = sub.cgac_id) end agency_name, " +
@@ -33,13 +32,32 @@ def update_table(sess, type):
         "from sub_tier_agency sub " +
         "INNER JOIN cgac ON cgac.cgac_id = sub.cgac_id " +
         "INNER JOIN frec ON frec.frec_id = sub.frec_id ) agency " +
-        "where detached_award_procurement." + type + "_agency_code = '999' " +
-        "and detached_award_procurement." + type + "_sub_tier_agency_c" + suffix +
+        "WHERE " + condition +
+        "and detached_award_procurement." + agency_type + "_sub_tier_agency_c" + suffix +
         " = agency.sub_tier_agency_code "
     )
-    sess.commit()
-    invalid = sess.execute("select count(*) from detached_award_procurement where " + type + "_agency_code='999'")
-    print_report(invalid_count, invalid.fetchone()[0], type)
+    # sess.commit()
+    if args.missing_agency:
+        invalid = sess.execute("select count(*) from" + condition)
+        print_report(invalid_count, invalid.fetchone()[0], type)
+
+
+def set_update_condition(agency_type, suffix, sess, update_date=None):
+
+    if update_date:
+        updated_subtiers = sess.execute(
+            "SELECT sub_tier_agency_code from sub_tier_agency where updated_at::date='{}'::date".format(update_date)
+        )
+        subtier_list = ",".join(["\'{}\'".format(str(x[0])) for x in updated_subtiers.fetchall() if len(x[0]) == 4])
+        sql_statement = "detached_award_procurement."+agency_type+"_sub_tier_agency_c"+suffix+" in ("+subtier_list+")"
+    else:
+        sql_statement = "detached_award_procurement." + agency_type + "_agency_code = '999' "
+
+    invalid = sess.execute("select count(*) from " + sql_statement)
+    invalid_count = invalid.fetchone()[0]
+    logger.info("{} invalid {} rows found".format(invalid_count, agency_type))
+
+    return invalid_count, sql_statement
 
 
 # data logging
@@ -49,11 +67,23 @@ def print_report(initial, final, type):
 
 
 def main():
-    sess = GlobalDB.db().session
+    parser = argparse.ArgumentParser(description='Update contract transaction rows based on updates to the agency list')
+    parser.add_argument('--missing_agency', help='Perform an update on 999 agency codes', action='store_true',
+                        required=False, default=False)
+    parser.add_argument('-d', '--update_date', help='Date subtier were updated at to run updates on the' +
+                                                    'toptier agencies derived in transactions. Format: YYYY/MM/DD',
+                        type=str, required=False)
+    args = parser.parse_args()
 
-    update_table(sess, 'awarding')
-    update_table(sess, 'funding')
-    logger.info("Procurement Update Complete")
+    if not args.update_date and not args.missing_agency:
+        logger.error('Missing either update_date or missing_agency argument')
+    else:
+        sess = GlobalDB.db().session
+
+        update_table(sess, 'awarding', args)
+        update_table(sess, 'funding', args)
+        logger.info("Procurement Update Complete")
+
 
 if __name__ == '__main__':
     configure_logging()
