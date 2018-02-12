69940dd28883da5e31376325111e5b59e85ed1c5
Sonia Hinson <sonia.hinson@gmail.com>
2018-02-12 13:25:47 -0500

    Adding ability to update based on subtier code

--- dataactbroker/scripts/update_detached_procurement.py
+++ dataactbroker/scripts/update_detached_procurement.py
@@ -14,50 +14,55 @@ def update_table(sess, agency_type, args):
     logger.info('updating ' + agency_type)
 
     # The name of the column depends on the type because of limited string length
-    suffix = 'o' if type == "funding" else ''
+    suffix = 'o' if agency_type == "funding" else ''
 
-    invalid_count, condition = set_update_condition(agency_type, suffix, sess, args.update_date)
+    invalid_count, condition_sql = set_update_condition(agency_type, suffix, sess, args.subtier_code)
 
     # updates table based off the parent of the sub_tier_agency code
     sess.execute(
-        "UPDATE detached_award_procurement SET" +
-        + agency_type + "_agency_code = agency.agency_code, " +
-        agency_type + "_agency_name = agency.agency_name from ( " +
-        "SELECT sub.sub_tier_agency_code, sub.cgac_id, sub.frec_id, sub.is_frec, CASE WHEN sub.is_frec " +
-        "THEN (SELECT agency_name from frec WHERE frec.frec_id = sub.frec_id) " +
-        "ELSE (SELECT agency_name from cgac where cgac.cgac_id = sub.cgac_id) end agency_name, " +
-        "CASE WHEN sub.is_frec " +
-        "THEN (SELECT frec_code from frec WHERE frec.frec_id = sub.frec_id) " +
-        "ELSE (SELECT cgac_code from cgac where cgac.cgac_id = sub.cgac_id) end agency_code " +
-        "from sub_tier_agency sub " +
-        "INNER JOIN cgac ON cgac.cgac_id = sub.cgac_id " +
-        "INNER JOIN frec ON frec.frec_id = sub.frec_id ) agency " +
-        "WHERE " + condition +
-        "and detached_award_procurement." + agency_type + "_sub_tier_agency_c" + suffix +
-        " = agency.sub_tier_agency_code "
+        """
+        UPDATE detached_award_procurement SET
+        {agency_type}_agency_code = agency.agency_code,
+        {agency_type}_agency_name = agency.agency_name FROM (
+        SELECT sub.sub_tier_agency_code, sub.cgac_id, sub.frec_id, sub.is_frec, CASE WHEN sub.is_frec
+        THEN (SELECT agency_name from frec WHERE frec.frec_id = sub.frec_id)
+        ELSE (SELECT agency_name from cgac where cgac.cgac_id = sub.cgac_id) END agency_name,
+        CASE WHEN sub.is_frec
+        THEN (SELECT frec_code from frec WHERE frec.frec_id = sub.frec_id)
+        ELSE (SELECT cgac_code from cgac where cgac.cgac_id = sub.cgac_id) END agency_code
+        FROM sub_tier_agency sub
+        INNER JOIN cgac ON cgac.cgac_id = sub.cgac_id
+        INNER JOIN frec ON frec.frec_id = sub.frec_id ) agency
+        WHERE {condition_sql}
+        AND detached_award_procurement.{agency_type}_sub_tier_agency_c{suffix} = agency.sub_tier_agency_code;
+        """.format(agency_type=agency_type, condition_sql=condition_sql, suffix=suffix)
     )
-    # sess.commit()
+    sess.commit()
     if args.missing_agency:
-        invalid = sess.execute("select count(*) from" + condition)
-        print_report(invalid_count, invalid.fetchone()[0], type)
+        final_invalid_count = get_invalid_count(condition_sql, sess)
+        print(final_invalid_count)
+        print_report(invalid_count, final_invalid_count, agency_type)
 
 
-def set_update_condition(agency_type, suffix, sess, update_date=None):
+def set_update_condition(agency_type, suffix, sess, subtier_code=None):
 
-    if update_date:
-        updated_subtiers = sess.execute(
-            "SELECT sub_tier_agency_code from sub_tier_agency where updated_at::date='{}'::date".format(update_date)
-        )
-        subtier_list = ",".join(["\'{}\'".format(str(x[0])) for x in updated_subtiers.fetchall() if len(x[0]) == 4])
-        sql_statement = "detached_award_procurement."+agency_type+"_sub_tier_agency_c"+suffix+" in ("+subtier_list+")"
+    if subtier_code:
+        sql_statement = "detached_award_procurement.{}_sub_tier_agency_c{} = '{}' ".format(agency_type,
+                                                                                           suffix, subtier_code)
     else:
         sql_statement = "detached_award_procurement." + agency_type + "_agency_code = '999' "
 
-    invalid = sess.execute("select count(*) from " + sql_statement)
-    invalid_count = invalid.fetchone()[0]
+    invalid_count = get_invalid_count(sql_statement, sess)
     logger.info("{} invalid {} rows found".format(invalid_count, agency_type))
 
     return invalid_count, sql_statement
+
+
+def get_invalid_count(sql_statement, sess):
+    invalid = sess.execute("select count(*) from detached_award_procurement where " + sql_statement + ";")
+    invalid_count = invalid.fetchone()[0]
+
+    return invalid_count
 
 
 # data logging
@@ -68,15 +73,16 @@ def print_report(initial, final, type):
 
 def main():
     parser = argparse.ArgumentParser(description='Update contract transaction rows based on updates to the agency list')
-    parser.add_argument('--missing_agency', help='Perform an update on 999 agency codes', action='store_true',
+    parser.add_argument('-a', '--missing_agency', help='Perform an update on 999 agency codes', action='store_true',
                         required=False, default=False)
-    parser.add_argument('-d', '--update_date', help='Date subtier were updated at to run updates on the' +
-                                                    'toptier agencies derived in transactions. Format: YYYY/MM/DD',
+    parser.add_argument('-s', '--subtier_code', help='Select specific subtier to update. Must be a 4-digit code',
                         type=str, required=False)
     args = parser.parse_args()
 
-    if not args.update_date and not args.missing_agency:
+    if not args.subtier_code and not args.missing_agency:
         logger.error('Missing either update_date or missing_agency argument')
+    elif args.subtier_code and len(args.subtier_code) != 4:
+        logger.error('Subtier not a correct format, must be 4 digits')
     else:
         sess = GlobalDB.db().session
 
