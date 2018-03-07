5e844d4c957ea4f9510e460cc30fdff637106fdd
alburde1 <Burdeyny_Alisa@bah.com>
2018-03-07 12:30:56 -0500

    Updating FABS24
    + PrimaryPlaceOfPerformanceCountryCode is no longer a required field
    + FABS24.1 (renamed from FABS24) is only for record types 1 and 2
    + For record type 3, PrimaryPlaceOfPerformanceCountryCode must be blank

--- dataactvalidator/config/sqlrules/fabs24_detached_award_financial_assistance_1.sql
+++ dataactvalidator/config/sqlrules/fabs24_detached_award_financial_assistance_1.sql
@@ -1,10 +1,12 @@
 -- PrimaryPlaceOfPerformanceCountryCode must contain a valid three character GENC Standard Edition 3.0 (Update 4)
--- country code.
+-- country code for record type 1 and 2.
 SELECT
     dafa.row_number,
+    dafa.record_type,
     dafa.place_of_perform_country_c
 FROM detached_award_financial_assistance AS dafa
 WHERE submission_id={0}
+    AND record_type IN (1, 2)
     AND NOT EXISTS (
         SELECT 1
         FROM country_code AS cc
---
+++ tests/unit/dataactvalidator/test_fabs24_detached_award_financial_assistance_2.py
@@ -0,0 +1,30 @@
+from tests.unit.dataactcore.factories.staging import DetachedAwardFinancialAssistanceFactory
+from tests.unit.dataactvalidator.utils import number_of_errors, query_columns
+
+_FILE = 'fabs24_detached_award_financial_assistance_2'
+
+
+def test_column_headers(database):
+    expected_subset = {"row_number", "record_type", "place_of_perform_country_c"}
+    actual = set(query_columns(_FILE, database))
+    assert expected_subset == actual
+
+
+def test_success(database):
+    """ PrimaryPlaceOfPerformanceCountryCode must be blank for record type 3. """
+    det_award = DetachedAwardFinancialAssistanceFactory(place_of_perform_country_c="USA", record_type=1)
+    det_award_2 = DetachedAwardFinancialAssistanceFactory(place_of_perform_country_c="uKr", record_type=2)
+    det_award_3 = DetachedAwardFinancialAssistanceFactory(place_of_perform_country_c="", record_type=3)
+    det_award_4 = DetachedAwardFinancialAssistanceFactory(place_of_perform_country_c=None, record_type=3)
+
+    errors = number_of_errors(_FILE, database, models=[det_award, det_award_2, det_award_3, det_award_4])
+    assert errors == 0
+
+
+def test_failure(database):
+    """ PrimaryPlaceOfPerformanceCountryCode must be blank for record type 3. """
+
+    det_award = DetachedAwardFinancialAssistanceFactory(place_of_perform_country_c="USA", record_type=3)
+
+    errors = number_of_errors(_FILE, database, models=[det_award])
+    assert errors == 1
