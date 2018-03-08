ab0239b7b28d5228d88e6741dc7cd1d818b4d1f5
alburde1 <Burdeyny_Alisa@bah.com>
2018-03-08 10:37:54 -0500

    Updating FABS31
    - AwardeeOrRecipientUniqueIdentifier must be blank for record type 3,
    adding it to the ignore list for lots of validations

--- dataactvalidator/config/sqlrules/fabs31_detached_award_financial_assistance_4.sql
+++ dataactvalidator/config/sqlrules/fabs31_detached_award_financial_assistance_4.sql
@@ -1,6 +1,7 @@
 -- For AssistanceType of 02, 03, 04, or 05 whose ActionDate is after October 1, 2010,
 -- AwardeeOrRecipientUniqueIdentifier must be found in our records, unless the record
--- is an aggregate record (RecordType=1) or individual recipient (BusinessTypes includes 'P').
+-- is an aggregate or PII-redacted non-aggregate record (RecordType=1 or 3) or individual recipient
+-- (BusinessTypes includes 'P').
 CREATE OR REPLACE function pg_temp.is_date(str text) returns boolean AS $$
 BEGIN
     perform CAST(str AS DATE);
@@ -29,7 +30,7 @@ SELECT
     business_types,
     record_type
 FROM detached_award_financial_assistance_fabs31_4_{0} AS dafa
-WHERE NOT (record_type = 1
+WHERE NOT (record_type IN (1, 3)
         OR UPPER(business_types) LIKE '%%P%%'
     )
     AND awardee_or_recipient_uniqu ~ '^\d\d\d\d\d\d\d\d\d$'
--- tests/unit/dataactvalidator/test_fabs31_detached_award_financial_assistance_4.py
+++ tests/unit/dataactvalidator/test_fabs31_detached_award_financial_assistance_4.py
@@ -15,7 +15,8 @@ def test_column_headers(database):
 def test_pubished_date_success(database):
     """ For AssistanceType of 02, 03, 04, or 05 whose ActionDate is after October 1, 2010,
         AwardeeOrRecipientUniqueIdentifier must be found in our records, unless the record
-        is an aggregate record (RecordType=1) or individual recipient (BusinessTypes includes 'P')."""
+        is an aggregate or PII-redacted non-aggregate record (RecordType=1 or 3) or individual recipient
+        (BusinessTypes includes 'P')."""
 
     duns_1 = DUNS(awardee_or_recipient_uniqu="111111111")
     det_award_1 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu="111111111",
@@ -34,30 +35,33 @@ def test_pubished_date_success(database):
                                                           assistance_type="03", action_date="10/02/2010",
                                                           record_type=1, business_types="A")
     det_award_5 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu="111111112",
+                                                          assistance_type="03", action_date="10/02/2010",
+                                                          record_type=1, business_types="A")
+    det_award_6 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu="111111112",
                                                           assistance_type="04", action_date="10/02/2010",
                                                           record_type=2, business_types="P")
     # Handled by d31_2
-    det_award_6 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu="",
+    det_award_7 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu="",
                                                           assistance_type="05", action_date="10/02/2010",
                                                           record_type=2, business_types="A")
-    det_award_7 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu=None,
+    det_award_8 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu=None,
                                                           assistance_type="02", action_date="10/02/2010",
                                                           record_type=2, business_types="A")
     # Handled by d31_3
-    det_award_8 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu="ABCDEFGHI",
+    det_award_9 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu="ABCDEFGHI",
                                                           assistance_type="03", action_date="10/02/2010",
                                                           record_type=2, business_types="A")
 
-    errors = number_of_errors(_FILE, database, models=[duns_1, det_award_1, det_award_2, det_award_3,
-                                                       det_award_4, det_award_5, det_award_6, det_award_7,
-                                                       det_award_8])
+    errors = number_of_errors(_FILE, database, models=[duns_1, det_award_1, det_award_2, det_award_3, det_award_4,
+                                                       det_award_5, det_award_6, det_award_7, det_award_8, det_award_9])
     assert errors == 0
 
 
 def test_pubished_date_failure(database):
     """ Test invalid for For AssistanceType of 02, 03, 04, or 05 whose ActionDate is after October 1, 2010,
         AwardeeOrRecipientUniqueIdentifier must be found in our records, unless the record is an aggregate
-        record (RecordType=1) or individual recipient (BusinessTypes includes 'P')."""
+        or PII-redacted non-aggregate record (RecordType=1 or 3) or individual recipient
+        (BusinessTypes includes 'P')."""
 
     duns_1 = DUNS(awardee_or_recipient_uniqu="111111111")
     det_award_1 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu="111111112",
