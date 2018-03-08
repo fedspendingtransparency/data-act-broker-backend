73c679507c9e6c925539afb70b098157d1a5792e
alburde1 <Burdeyny_Alisa@bah.com>
2018-03-08 09:46:10 -0500

    Updating FABS14.1
    - legal entity zip last4 must be blank for record type 3

--- tests/unit/dataactvalidator/test_fabs14_detached_award_financial_assistance_1.py
+++ tests/unit/dataactvalidator/test_fabs14_detached_award_financial_assistance_1.py
@@ -11,21 +11,26 @@ def test_column_headers(database):
 
 
 def test_success(database):
-    """ Test LegalEntityZIPLast4 must be blank for aggregate records (i.e., when RecordType = 1) record type 2 doesn't
-        affect success """
+    """ Test LegalEntityZIPLast4 must be blank for aggregate and PII-redacted non-aggregate records
+        (i.e., when RecordType = 1 or 3). Record type 2 doesn't affect success """
     det_award = DetachedAwardFinancialAssistanceFactory(record_type=2, legal_entity_zip_last4="12345")
     det_award_2 = DetachedAwardFinancialAssistanceFactory(record_type=2, legal_entity_zip_last4=None)
-    det_award_null = DetachedAwardFinancialAssistanceFactory(record_type=1, legal_entity_zip_last4=None)
-    det_award_null_2 = DetachedAwardFinancialAssistanceFactory(record_type=1, legal_entity_zip_last4='')
+    det_award_3 = DetachedAwardFinancialAssistanceFactory(record_type=1, legal_entity_zip_last4=None)
+    det_award_4 = DetachedAwardFinancialAssistanceFactory(record_type=1, legal_entity_zip_last4='')
+    det_award_5 = DetachedAwardFinancialAssistanceFactory(record_type=3, legal_entity_zip_last4=None)
+    det_award_6 = DetachedAwardFinancialAssistanceFactory(record_type=3, legal_entity_zip_last4='')
 
-    errors = number_of_errors(_FILE, database, models=[det_award, det_award_2, det_award_null, det_award_null_2])
+    errors = number_of_errors(_FILE, database, models=[det_award, det_award_2, det_award_3, det_award_4, det_award_5,
+                                                       det_award_6])
     assert errors == 0
 
 
 def test_failure(database):
-    """ Test failure when LegalEntityZIPLast4 isn't blank for aggregate records (i.e., when RecordType = 1) """
+    """ Test failure when LegalEntityZIPLast4 isn't blank for aggregate and PII-redacted non-aggregate records
+        (i.e., when RecordType = 1 or 3) """
 
     det_award = DetachedAwardFinancialAssistanceFactory(record_type=1, legal_entity_zip_last4="Test")
+    det_award_2 = DetachedAwardFinancialAssistanceFactory(record_type=3, legal_entity_zip_last4="Test")
 
-    errors = number_of_errors(_FILE, database, models=[det_award])
-    assert errors == 1
+    errors = number_of_errors(_FILE, database, models=[det_award, det_award_2])
+    assert errors == 2
