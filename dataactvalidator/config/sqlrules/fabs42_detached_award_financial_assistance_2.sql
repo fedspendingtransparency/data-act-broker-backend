5c425c2b5454329b8954853c1197e6111b971d73
Sonia Hinson <sonia.hinson@gmail.com>
2018-03-09 10:19:46 -0500

    Updating rules 42.2
    Updating 42.2 to  apply to RecordType = 1 and RecordType = 3 OR when PrimaryPlaceOfPerformanceCountryCode = USA

--- dataactvalidator/config/sqlrules/fabs42_detached_award_financial_assistance_2.sql
+++ dataactvalidator/config/sqlrules/fabs42_detached_award_financial_assistance_2.sql
@@ -1,10 +1,13 @@
 -- PrimaryPlaceOfPerformanceForeignLocationDescription must be blank for domestic recipients
--- (i.e., when PrimaryPlaceOfPerformanceCountryCode = USA).
+-- (i.e., when PrimaryPlaceOfPerformanceCountryCode = USA) or for for aggregate and PII-redacted non-aggregate
+-- records (RecordType=1 or 3).
 SELECT
     row_number,
     place_of_performance_forei,
-    place_of_perform_country_c
+    place_of_perform_country_c,
+    record_type
 FROM detached_award_financial_assistance
 WHERE submission_id = {0}
     AND COALESCE(place_of_performance_forei, '') <> ''
-    AND UPPER(place_of_perform_country_c) = 'USA';
+    AND (UPPER(place_of_perform_country_c) = 'USA'
+    OR record_type in (1, 3));
