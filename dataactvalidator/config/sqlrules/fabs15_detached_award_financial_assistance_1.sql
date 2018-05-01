da1a96014052da1154419623d377d6487f939a19
Sonia Hinson <sonia.hinson@gmail.com>
2018-05-01 10:28:01 -0400

    Updating FABS15.1 to include record type 2 and 3
    FABS15.1 is updated to prevent legitimate foreign aggregate records from being validated. Adding a check for record type 2 and 3  and updating rule text given by Schema.

--- dataactvalidator/config/sqlrules/fabs15_detached_award_financial_assistance_1.sql
+++ dataactvalidator/config/sqlrules/fabs15_detached_award_financial_assistance_1.sql
@@ -1,11 +1,14 @@
 -- LegalEntityForeignCityName is required for foreign recipients (i.e., when LegalEntityCountryCode <> USA)
+-- for non-aggregate and PII-redacted non-aggregate records (RecordType = 2 or 3).
 SELECT
     row_number,
     legal_entity_country_code,
+    record_type,
     legal_entity_foreign_city
 FROM detached_award_financial_assistance
 WHERE submission_id = {0}
     AND UPPER(legal_entity_country_code) <> 'USA'
+    AND record_type IN (2, 3)
     AND (legal_entity_foreign_city IS NULL
         OR legal_entity_foreign_city = ''
     );
--- tests/unit/dataactvalidator/test_fabs15_detached_award_financial_assistance_1.py
+++ tests/unit/dataactvalidator/test_fabs15_detached_award_financial_assistance_1.py
@@ -5,36 +5,51 @@ _FILE = 'fabs15_detached_award_financial_assistance_1'
 
 
 def test_column_headers(database):
-    expected_subset = {"row_number", "legal_entity_country_code", "legal_entity_foreign_city"}
+    expected_subset = {"row_number", "legal_entity_country_code", "legal_entity_foreign_city", "record_type"}
     actual = set(query_columns(_FILE, database))
     assert expected_subset == actual
 
 
 def test_success(database):
     """ Test success LegalEntityForeignCityName is required for foreign recipients
-    (i.e., when LegalEntityCountryCode != USA) """
+    (i.e., when LegalEntityCountryCode != USA) for non-aggregate
+    and PII-redacted non-aggregate records (RecordType = 2 or 3)"""
 
     det_award_1 = DetachedAwardFinancialAssistanceFactory(legal_entity_country_code="Japan",
-                                                          legal_entity_foreign_city="Tokyo")
+                                                          legal_entity_foreign_city="Tokyo",
+                                                          record_type=2)
     det_award_2 = DetachedAwardFinancialAssistanceFactory(legal_entity_country_code="UK",
-                                                          legal_entity_foreign_city="Manchester")
+                                                          legal_entity_foreign_city="Manchester",
+                                                          record_type=3)
     det_award_3 = DetachedAwardFinancialAssistanceFactory(legal_entity_country_code="USA",
-                                                          legal_entity_foreign_city=None)
+                                                          legal_entity_foreign_city=None,
+                                                          record_type=2)
     det_award_4 = DetachedAwardFinancialAssistanceFactory(legal_entity_country_code="USA",
-                                                          legal_entity_foreign_city="")
+                                                          legal_entity_foreign_city="",
+                                                          record_type=3)
+    det_award_5 = DetachedAwardFinancialAssistanceFactory(legal_entity_country_code="UK",
+                                                          legal_entity_foreign_city="",
+                                                          record_type=1)
+    det_award_6 = DetachedAwardFinancialAssistanceFactory(legal_entity_country_code="CAN",
+                                                          legal_entity_foreign_city=None,
+                                                          record_type=1)
 
-    errors = number_of_errors(_FILE, database, models=[det_award_1, det_award_2, det_award_3, det_award_4])
+    errors = number_of_errors(_FILE, database, models=[det_award_1, det_award_2, det_award_3, det_award_4, det_award_5,
+                                                       det_award_6])
     assert errors == 0
 
 
 def test_failure(database):
     """ Test failure LegalEntityForeignCityName is required for foreign recipients
-    (i.e., when LegalEntityCountryCode != USA) """
+    (i.e., when LegalEntityCountryCode != USA) for non-aggregate
+    and PII-redacted non-aggregate records (RecordType = 2 or 3)"""
 
     det_award = DetachedAwardFinancialAssistanceFactory(legal_entity_country_code="Japan",
-                                                        legal_entity_foreign_city=None)
+                                                        legal_entity_foreign_city=None,
+                                                        record_type=2)
     det_award_2 = DetachedAwardFinancialAssistanceFactory(legal_entity_country_code="Canada",
-                                                          legal_entity_foreign_city="")
+                                                          legal_entity_foreign_city="",
+                                                          record_type=3)
 
     errors = number_of_errors(_FILE, database, models=[det_award, det_award_2])
     assert errors == 2
