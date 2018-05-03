dfaa0601892c6bc41978168412ff3d3bea700867
alburde1 <Burdeyny_Alisa@bah.com>
2018-05-03 12:17:39 -0400

    Updating FABS16 to include record_type 1

--- tests/unit/dataactvalidator/test_fabs16_detached_award_financial_assistance.py
+++ tests/unit/dataactvalidator/test_fabs16_detached_award_financial_assistance.py
@@ -5,25 +5,30 @@ _FILE = 'fabs16_detached_award_financial_assistance'
 
 
 def test_column_headers(database):
-    expected_subset = {"row_number", "legal_entity_country_code", "legal_entity_foreign_provi"}
+    expected_subset = {"row_number", "legal_entity_country_code", "legal_entity_foreign_provi", "record_type"}
     actual = set(query_columns(_FILE, database))
     assert expected_subset == actual
 
 
 def test_success(database):
-    """ LegalEntityForeignProvinceName must be blank for foreign recipients
-    (i.e., when LegalEntityCountryCode = USA). Foreign reign recipients don't affect success."""
+    """ LegalEntityForeignProvinceName must be blank for foreign recipients (i.e., when LegalEntityCountryCode = USA)
+        or RecordType = 1. Foreign reign recipients don't affect success."""
 
     det_award = DetachedAwardFinancialAssistanceFactory(legal_entity_country_code="Japan",
-                                                        legal_entity_foreign_provi="Yamashiro")
+                                                        legal_entity_foreign_provi="Yamashiro",
+                                                        record_type=2)
     det_award_2 = DetachedAwardFinancialAssistanceFactory(legal_entity_country_code="UK",
-                                                          legal_entity_foreign_provi="")
+                                                          legal_entity_foreign_provi="",
+                                                          record_type=2)
     det_award_3 = DetachedAwardFinancialAssistanceFactory(legal_entity_country_code="UK",
-                                                          legal_entity_foreign_provi=None)
+                                                          legal_entity_foreign_provi=None,
+                                                          record_type=1)
     det_award_4 = DetachedAwardFinancialAssistanceFactory(legal_entity_country_code="USA",
-                                                          legal_entity_foreign_provi=None)
+                                                          legal_entity_foreign_provi=None,
+                                                          record_type=3)
     det_award_5 = DetachedAwardFinancialAssistanceFactory(legal_entity_country_code="USA",
-                                                          legal_entity_foreign_provi="")
+                                                          legal_entity_foreign_provi="",
+                                                          record_type=2)
 
     errors = number_of_errors(_FILE, database, models=[det_award, det_award_2, det_award_3, det_award_4, det_award_5])
     assert errors == 0
@@ -31,10 +36,14 @@ def test_success(database):
 
 def test_failure(database):
     """ Test failure LegalEntityForeignProvinceName must be blank for foreign recipients
-    (i.e., when LegalEntityCountryCode = USA) """
+        (i.e., when LegalEntityCountryCode = USA) or RecordType = 1 """
 
     det_award = DetachedAwardFinancialAssistanceFactory(legal_entity_country_code="USA",
-                                                        legal_entity_foreign_provi="Test")
+                                                        legal_entity_foreign_provi="Test",
+                                                        record_type=2)
+    det_award_2 = DetachedAwardFinancialAssistanceFactory(legal_entity_country_code="UK",
+                                                          legal_entity_foreign_provi="Test",
+                                                          record_type=1)
 
-    errors = number_of_errors(_FILE, database, models=[det_award])
-    assert errors == 1
+    errors = number_of_errors(_FILE, database, models=[det_award, det_award_2])
+    assert errors == 2
