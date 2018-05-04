8dd52374738c510f3caa85ba3c67e5d958d70334
Sonia Hinson <sonia.hinson@gmail.com>
2018-05-04 13:27:39 -0400

    Update FABS44.3
    - Fixing error that triggers when the provided district doesn't match the ZIP, rather than the list of valid districts in that state as intended.
    - Conformint it to rule FABS43.4 (currently implemented as 43.3 in the Broker), where PPoP CD is checked against the list of valid CDs in the state (including 00, 98, and 99 states/territories)

--- dataactvalidator/config/sqlrules/fabs44_detached_award_financial_assistance_3.sql
+++ dataactvalidator/config/sqlrules/fabs44_detached_award_financial_assistance_3.sql
@@ -1,5 +1,7 @@
--- If LegalEntityCongressionalDistrict is provided, it must be valid in the 5-digit zip code indicated by
--- LegalEntityZIP5.
+-- If LegalEntityCongressionalDistrict is provided, it must be valid in the state or territory
+-- indicated by LegalEntityZIP5. Districts that were created under the 2000 census or later are considered valid.
+-- Retrieve the state code from the zip5 and congressional district and then check to make sure the
+-- submission's congressional district exists within the state's congressional districts
 SELECT
     row_number,
     legal_entity_zip5,
@@ -8,9 +10,11 @@ FROM detached_award_financial_assistance AS dafa
 WHERE submission_id = {0}
     AND COALESCE(legal_entity_zip5, '') <> ''
     AND COALESCE(legal_entity_congressional, '') <> ''
-    AND NOT EXISTS (
-        SELECT 1
-        FROM zips AS z
-        WHERE z.zip5 = dafa.legal_entity_zip5
-            AND z.congressional_district_no = dafa.legal_entity_congressional
-    );
+    AND dafa.legal_entity_congressional
+        NOT IN (SELECT sc.congressional_district_no
+    	        FROM state_congressional AS sc,
+    	 	        zips as z
+    	        WHERE z.congressional_district_no = dafa.legal_entity_congressional
+    	 	        AND z.zip5 = dafa.legal_entity_zip5
+    	            AND z.state_abbreviation = sc.state_code
+    	            AND sc.census_year IS NULL OR sc.census_year >= 2000);
--- tests/unit/dataactvalidator/test_fabs44_detached_award_financial_assistance_3.py
+++ tests/unit/dataactvalidator/test_fabs44_detached_award_financial_assistance_3.py
@@ -1,5 +1,5 @@
 from tests.unit.dataactcore.factories.staging import DetachedAwardFinancialAssistanceFactory
-from tests.unit.dataactcore.factories.domain import ZipsFactory
+from tests.unit.dataactcore.factories.domain import ZipsFactory, StateCongressionalFactory
 from tests.unit.dataactvalidator.utils import number_of_errors, query_columns
 
 _FILE = 'fabs44_detached_award_financial_assistance_3'
@@ -14,24 +14,34 @@ def test_column_headers(database):
 def test_success(database):
     """ Test If LegalEntityCongressionalDistrict is provided, it must be valid in the 5-digit zip code indicated by
         LegalEntityZIP5. """
-    zip1 = ZipsFactory(zip5="12345", congressional_district_no="01")
+    zip1 = ZipsFactory(zip5="12345", congressional_district_no="01", state_abbreviation="AB")
+    zip2 = ZipsFactory(zip5="23456", congressional_district_no="02", state_abbreviation="CD")
+    sc1 = StateCongressionalFactory(state_code="AB", congressional_district_no="01", census_year=None)
+    sc2 = StateCongressionalFactory(state_code="CD", congressional_district_no="02", census_year=2000)
+
     det_award_1 = DetachedAwardFinancialAssistanceFactory(legal_entity_zip5="12345", legal_entity_congressional="")
     det_award_2 = DetachedAwardFinancialAssistanceFactory(legal_entity_zip5="", legal_entity_congressional="01")
     det_award_3 = DetachedAwardFinancialAssistanceFactory(legal_entity_zip5="", legal_entity_congressional="")
     det_award_4 = DetachedAwardFinancialAssistanceFactory(legal_entity_zip5="12345", legal_entity_congressional=None)
+
     det_award_5 = DetachedAwardFinancialAssistanceFactory(legal_entity_zip5="12345", legal_entity_congressional="01")
+    det_award_6 = DetachedAwardFinancialAssistanceFactory(legal_entity_zip5="23456", legal_entity_congressional="02")
 
     errors = number_of_errors(_FILE, database, models=[det_award_1, det_award_2, det_award_3, det_award_4, det_award_5,
-                                                       zip1])
+                                                       det_award_6, zip1, zip2, sc1, sc2])
     assert errors == 0
 
 
 def test_failure(database):
     """ Test failure If LegalEntityCongressionalDistrict is provided, it must be valid in the 5-digit zip code indicated
         by LegalEntityZIP5. """
-    zip1 = ZipsFactory(zip5="12345", congressional_district_no="01")
+    zip1 = ZipsFactory(zip5="12345", congressional_district_no="01", state_abbreviation="AB")
+    sc1 = StateCongressionalFactory(state_code="AB", congressional_district_no="01", census_year=None)
+    sc2 = StateCongressionalFactory(state_code="AB", congressional_district_no="02", census_year=1999)
+
     det_award_1 = DetachedAwardFinancialAssistanceFactory(legal_entity_zip5="12345", legal_entity_congressional="02")
     det_award_2 = DetachedAwardFinancialAssistanceFactory(legal_entity_zip5="12346", legal_entity_congressional="01")
+    det_award_3 = DetachedAwardFinancialAssistanceFactory(legal_entity_zip5="12345", legal_entity_congressional="03")
 
-    errors = number_of_errors(_FILE, database, models=[det_award_1, det_award_2, zip1])
-    assert errors == 2
+    errors = number_of_errors(_FILE, database, models=[det_award_1, det_award_2, det_award_3, zip1, sc1, sc2])
+    assert errors == 3
