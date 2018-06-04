5f7bc688a56c55f7deea8d74779306b95e8184d8
Sonia Hinson <sonia.hinson@gmail.com>
2018-06-04 11:40:11 -0400

    Dev 1049
    * Rule should use the `FYQ` column to check every PA in the submitted file against the PAs that apply to the quarter being certified
    * The rule should not trigger at all for recertifications of FY17Q2 and FY17Q3.
    * Update Error text in sqlrules.csv for B9/B10
    * Update related unit tests

--- dataactvalidator/config/sqlrules/b9_award_financial.sql
+++ dataactvalidator/config/sqlrules/b9_award_financial.sql
@@ -1,15 +1,15 @@
 -- Must be a valid program activity name and code for the corresponding TAS/TAFS as defined in Section 82 of OMB
 -- Circular A-11. If the program activity is unknown, enter "0000" and "Unknown/Other" as the code and name,
--- respectively.
+-- respectively. The rule should not trigger at all for re-certifications of FY17Q2 and FY17Q3.
 WITH award_financial_b9_{0} AS
-    (SELECT submission_id,
-        row_number,
-        agency_identifier,
-        main_account_code,
-        program_activity_name,
-        program_activity_code
-    FROM award_financial
-    WHERE submission_id = {0})
+    (SELECT af.submission_id,
+        af.row_number,
+        af.agency_identifier,
+        af.main_account_code,
+        af.program_activity_name,
+        af.program_activity_code
+    FROM award_financial AS af
+    WHERE af.submission_id = {0})
 SELECT
     af.row_number,
     af.agency_identifier,
@@ -17,8 +17,12 @@ SELECT
     af.program_activity_name,
     af.program_activity_code
 FROM award_financial_b9_{0} AS af
+     INNER JOIN submission AS sub
+        ON af.submission_id = sub.submission_id
 WHERE af.program_activity_code <> '0000'
     AND UPPER(af.program_activity_name) <> 'UNKNOWN/OTHER'
+    AND NOT ((sub.reporting_fiscal_year, sub.reporting_fiscal_period) IN (('2017', 6), ('2017', 9))
+        AND sub.publish_status_id <> 1)
     AND NOT EXISTS (
         SELECT 1
         FROM program_activity AS pa
@@ -26,8 +30,6 @@ WHERE af.program_activity_code <> '0000'
             AND af.main_account_code = pa.account_number
             AND UPPER(COALESCE(af.program_activity_name, '')) = UPPER(pa.program_activity_name)
             AND COALESCE(af.program_activity_code, '') = pa.program_activity_code
-            AND CAST(pa.budget_year AS INTEGER) IN (2016, 2017, 2018)  -- temporarily hardcoded to 2016-2018
-                                                   -- (SELECT reporting_fiscal_year
-                                                   --  FROM submission
-                                                   --  WHERE submission_id = {0})
+            AND pa.fiscal_year_quarter = 'FY' || sub.reporting_fiscal_year || 'Q' || sub.reporting_fiscal_period / 3
+
     );
