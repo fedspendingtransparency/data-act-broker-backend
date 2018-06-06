4a69e3c87da9d82d3f4eb640a23bf80ab89d1f1e
Sharbesh Adhikari <adhikari_sharbesh@bah.com>
2018-06-06 11:26:56 -0400

    extracted country codes from domain

--- dataactvalidator/scripts/load_country_codes.py
+++ dataactvalidator/scripts/load_country_codes.py
@@ -14,50 +14,42 @@ from dataactvalidator.scripts.loaderUtils import clean_data, insert_dataframe
 logger = logging.getLogger(__name__)
 
 
-def load_country_codes(filename):
-    """Load country code lookup table."""
-    model = CountryCode
+def load_country_codes(base_path):
+    """Load all domain value files.
+
+    Args
+        base_path: directory that contains the domain values files.
+    """
+
+    if CONFIG_BROKER["use_aws"]:
+        s3connection = boto.s3.connect_to_region(CONFIG_BROKER['aws_region'])
+        s3bucket = s3connection.lookup(CONFIG_BROKER['sf_133_bucket'])
+        filename = s3bucket.get_key("country_codes.csv").generate_url(expires_in=600)
+    else:
+        filename = os.path.join(base_path, "country_codes.csv")
+
+    logger.info('Loading country codes file: ' + "country_codes.csv")
 
     with create_app().app_context():
         sess = GlobalDB.db().session
         # for object class, delete and replace values
-        sess.query(model).delete()
+        sess.query(CountryCode).delete()
 
         data = pd.read_csv(filename, dtype=str)
         data = clean_data(
             data,
-            model,
+            CountryCode,
             {"country_code": "country_code", "country_name": "country_name"},
             {}
         )
         # de-dupe
         data.drop_duplicates(subset=['country_code'], inplace=True)
         # insert to db
-        table_name = model.__table__.name
+        table_name = CountryCode.__table__.name
         num = insert_dataframe(data, table_name, sess.connection())
         sess.commit()
 
     logger.info('{} records inserted to {}'.format(num, table_name))
 
-
-def load_domain_values(base_path):
-    """Load all domain value files.
-
-    Parameters
-    ----------
-        base_path : directory that contains the domain values files.
-    """
-    if CONFIG_BROKER["use_aws"]:
-        s3connection = boto.s3.connect_to_region(CONFIG_BROKER['aws_region'])
-        s3bucket = s3connection.lookup(CONFIG_BROKER['sf_133_bucket'])
-        country_codes_file = s3bucket.get_key("country_codes.csv").generate_url(expires_in=600)
-    else:
-        country_codes_file = os.path.join(base_path, "country_codes.csv")
-
-    logger.info('Loading country codes file: ' + "country_codes.csv")
-    load_country_codes(country_codes_file)
-
-
 if __name__ == '__main__':
     configure_logging()
-    load_domain_values(os.path.join(CONFIG_BROKER["path"], "dataactvalidator", "config"))
