74d6f0ca5f3e05a4a5332f5629eaea4e7782fa55
Daniel Boos <boos_daniel@bah.com>
2018-06-25 13:24:44 -0400

    Removing redundant comments

--- dataactcore/scripts/update_fabs_parent_duns.py
+++ dataactcore/scripts/update_fabs_parent_duns.py
@@ -111,16 +111,12 @@ if __name__ == '__main__':
         args = parser.parse_args()
         sess = GlobalDB.db().session
 
-        # Update the names of the historic duns that don't have names associated with them
         logger.info("Gathering historical parent duns names via SAM service")
         update_historic_parent_names()
 
-        # Update the FABS whose action_date falls within a yearly SAMS snapshot of DUNS data
         logger.info("Updating FABS with action dates matching the years within the parent duns")
         sess.execute(FABS_PARENT_DUNS_SQL_MATCH)
-        
-        # Update the FABS whose action_dates do not fall within a yearly snapshot
-        # Using the earliest year provided
+
         logger.info("Updating FABS with action dates not matching the parent duns, using the earliest match")
         sess.execute(FABS_PARENT_DUNS_SQL_EARLIEST)
         sess.close()
