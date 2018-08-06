631b0179583b1380dd1c52340220ef194ce3115b
Michael Hess <hess_michael@bah.com>
2018-08-06 13:47:57 -0400

    Allow for the string "None" in multipart as well

--- dataactcore/utils/requestDictionary.py
+++ dataactcore/utils/requestDictionary.py
@@ -46,9 +46,8 @@ class RequestDictionary:
                 request_data = request.form.to_dict()
                 request_data['_files'] = request.files
                 for key, value in request_data.items():
-                    if value == 'null':
+                    if value in ('null', 'None'):
                         request_data[key] = None
-
                 return request_data
             else:
                 raise ValueError("Invalid Content-Type : " + content_type)
