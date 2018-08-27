7a894a00412599fee5c5cf62b8babf9c7eff1ee7
alburde1 <Burdeyny_Alisa@bah.com>
2018-08-27 13:23:32 -0400

    Add tests

---
+++ tests/unit/dataactvalidator/test_fabs44_detached_award_financial_assistance_4.py
@@ -0,0 +1,36 @@
+from tests.unit.dataactcore.factories.staging import DetachedAwardFinancialAssistanceFactory
+from tests.unit.dataactvalidator.utils import number_of_errors, query_columns
+
+
+_FILE = 'fabs44_detached_award_financial_assistance_4'
+
+
+def test_column_headers(database):
+    expected_subset = {"row_number", "legal_entity_congressional", "record_type"}
+    actual = set(query_columns(_FILE, database))
+    assert expected_subset == actual
+
+
+def test_success(database):
+    """ Test LegalEntityCongressionalDistrict must be blank for aggregate records (RecordType = 1). """
+    det_award_1 = DetachedAwardFinancialAssistanceFactory(legal_entity_congressional=None,
+                                                          record_type=1)
+
+    det_award_2 = DetachedAwardFinancialAssistanceFactory(legal_entity_congressional="",
+                                                          record_type=1)
+
+    det_award_3 = DetachedAwardFinancialAssistanceFactory(legal_entity_congressional="01",
+                                                          record_type=2)
+
+    errors = number_of_errors(_FILE, database, models=[det_award_1, det_award_2, det_award_3])
+    assert errors == 0
+
+
+def test_failure(database):
+    """ Test failure LegalEntityCongressionalDistrict must be blank for aggregate records (RecordType = 1). """
+
+    det_award_1 = DetachedAwardFinancialAssistanceFactory(legal_entity_congressional="01",
+                                                          record_type=1)
+
+    errors = number_of_errors(_FILE, database, models=[det_award_1])
+    assert errors == 1
