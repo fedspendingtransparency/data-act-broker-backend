64203b474307f1d781abffbd4a22b1828c4036cc
alburde1 <Burdeyny_Alisa@bah.com>
2018-09-10 13:50:45 -0400

    Updating FABS38.2 based on ticket, funding office code must be valid if provided

--- dataactvalidator/config/sqlrules/fabs38_detached_award_financial_assistance_2.sql
+++ dataactvalidator/config/sqlrules/fabs38_detached_award_financial_assistance_2.sql
@@ -1,8 +1,12 @@
--- AwardingOfficeCode must be six characters long.
+-- When provided, FundingOfficeCode must be a valid value from the Federal Hierarchy.
 SELECT
     row_number,
-    awarding_office_code
-FROM detached_award_financial_assistance
+    funding_office_code
+FROM detached_award_financial_assistance AS dafa
 WHERE submission_id = {0}
-    AND COALESCE(awarding_office_code, '') <> ''
-    AND LENGTH(awarding_office_code) <> 6;
+    AND COALESCE(funding_office_code, '') <> ''
+    AND NOT EXISTS (
+        SELECT 1
+        FROM office
+        WHERE office.office_code = dafa.funding_office_code
+    );
