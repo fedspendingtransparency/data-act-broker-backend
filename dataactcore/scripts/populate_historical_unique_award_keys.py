39f53542418d63a58543dafa951a982b8a615b99
alburde1 <Burdeyny_Alisa@bah.com>
2018-10-17 11:05:15 -0400

    Cleaning up the script so it's less confusing/hard to read

--- dataactcore/scripts/populate_historical_unique_award_keys.py
+++ dataactcore/scripts/populate_historical_unique_award_keys.py
@@ -13,6 +13,41 @@ from dataactvalidator.health_check import create_app
 
 logger = logging.getLogger(__name__)
 
+
+def update_keys(model, model_type, filter_content, update_years, concat_list):
+    """ Run through the loop to update award keys for the given table.
+
+        Args:
+            model: the table model to be used
+            model_type: whether the table being updated is FABS or FPDS
+            filter_content: what we want in the base filter
+            update_years: an array of years to work with
+            concat_list: the concatenation to update with
+    """
+    base_query = sess.query(model)
+    if model_type == 'FPDS':
+        base_query = base_query.filter(model.pulled_from == filter_content)
+    else:
+        if filter_content == 'aggregate':
+            base_query = base_query.filter(model.record_type == '1')
+        else:
+            base_query = base_query.filter(model.record_type != '1')
+
+    logger.info('Populating unique_award_key for {} {} records...'.format(model_type, filter_content))
+    logger.info('Starting records before {}...'.format(update_years[0]))
+    base_query.filter(func.cast_as_date(model.action_date) < '{}-01-01'.format(years[0])).\
+        update({model.unique_award_key: concat_list}, synchronize_session=False)
+    sess.commit()
+
+    for year in update_years:
+        logger.info('Starting records in {}...'.format(year))
+        base_query.filter(func.cast_as_date(model.action_date) >= '{}-01-01'.format(year),
+                          func.cast_as_date(model.action_date) <= '{}-12-31'.format(year)). \
+            update({model.unique_award_key: concat_list}, synchronize_session=False)
+        sess.commit()
+
+    logger.info('{} {} records populated.\n'.format(model_type, filter_content))
+
 if __name__ == '__main__':
     with create_app().app_context():
         configure_logging()
@@ -25,131 +60,36 @@ if __name__ == '__main__':
             years.append(str(i))
 
         # FPDS awards
-        logger.info('Populating unique_award_key for FPDS award records...')
         dap = DetachedAwardProcurement
-        logger.info('Starting records before {}...'.format(years[0]))
-        sess.query(dap).filter(dap.pulled_from == 'award',
-                               func.cast_as_date(dap.action_date) < '{}-01-01'.format(years[0])).\
-            update({dap.unique_award_key: func.concat(func.coalesce(dap.piid, '-none-'), '_',
-                                                      func.coalesce(dap.agency_id, '-none-'), '_',
-                                                      func.coalesce(dap.parent_award_id, '-none-'), '_',
-                                                      func.coalesce(dap.referenced_idv_agency_iden, '-none-'))},
-                   synchronize_session=False)
-        sess.commit()
-        for year in years:
-            logger.info('Starting records in {}...'.format(year))
-            sess.query(dap).filter(dap.pulled_from == 'award',
-                                   func.cast_as_date(dap.action_date) >= '{}-01-01'.format(year),
-                                   func.cast_as_date(dap.action_date) <= '{}-12-31'.format(year)).\
-                update({dap.unique_award_key: func.concat(func.coalesce(dap.piid, '-none-'), '_',
-                                                          func.coalesce(dap.agency_id, '-none-'), '_',
-                                                          func.coalesce(dap.parent_award_id, '-none-'), '_',
-                                                          func.coalesce(dap.referenced_idv_agency_iden, '-none-'))},
-                       synchronize_session=False)
-            sess.commit()
-
-        logger.info('FPDS award records populated.\n')
+        update_keys(dap, 'FPDS', 'award', years, func.concat(func.coalesce(dap.piid, '-none-'), '_',
+                                                             func.coalesce(dap.agency_id, '-none-'), '_',
+                                                             func.coalesce(dap.parent_award_id, '-none-'), '_',
+                                                             func.coalesce(dap.referenced_idv_agency_iden, '-none-')))
 
         # FPDS IDV
-        logger.info('Populating unique_award_key for FPDS IDV records...')
-        logger.info('Starting records before {}...'.format(years[0]))
-        sess.query(dap).filter(dap.pulled_from == 'IDV',
-                               func.cast_as_date(dap.action_date) < '{}-01-01'.format(years[0])).\
-            update({dap.unique_award_key: func.concat('IDV_', func.coalesce(dap.piid, '-none-'), '_',
-                                                      func.coalesce(dap.agency_id, '-none-'))},
-                   synchronize_session=False)
-        sess.commit()
-        for year in years:
-            logger.info('Starting records in {}...'.format(year))
-            sess.query(dap).filter(dap.pulled_from == 'IDV',
-                                   func.cast_as_date(dap.action_date) >= '{}-01-01'.format(year),
-                                   func.cast_as_date(dap.action_date) <= '{}-12-31'.format(year)). \
-                update({dap.unique_award_key: func.concat('IDV_', func.coalesce(dap.piid, '-none-'), '_',
-                                                          func.coalesce(dap.agency_id, '-none-'))},
-                       synchronize_session=False)
-            sess.commit()
-        logger.info('FPDS IDV records populated.\n')
+        update_keys(dap, 'FPDS', 'IDV', years, func.concat('IDV_', func.coalesce(dap.piid, '-none-'), '_',
+                                                           func.coalesce(dap.agency_id, '-none-')))
 
         # unpublished FABS record type 1
-        logger.info('Populating unique_award_key for unpublished FABS aggregate records...')
         dafa = DetachedAwardFinancialAssistance
-        logger.info('Starting records before {}...'.format(years[0]))
-        sess.query(dafa).filter(dafa.record_type == '1',
-                                func.cast_as_date(dafa.action_date) < '{}-01-01'.format(years[0])).\
-            update({dafa.unique_award_key: func.concat('AGG_', func.coalesce(dafa.uri, '-none-'), '_',
-                                                       func.coalesce(dafa.awarding_sub_tier_agency_c, '-none-'))},
-                   synchronize_session=False)
-        sess.commit()
-        for year in years:
-            logger.info('Starting records in {}...'.format(year))
-            sess.query(dafa).filter(dafa.record_type == '1',
-                                    func.cast_as_date(dafa.action_date) >= '{}-01-01'.format(year),
-                                    func.cast_as_date(dafa.action_date) <= '{}-12-31'.format(year)). \
-                update({dafa.unique_award_key: func.concat('AGG_', func.coalesce(dafa.uri, '-none-'), '_',
-                                                           func.coalesce(dafa.awarding_sub_tier_agency_c, '-none-'))},
-                       synchronize_session=False)
-            sess.commit()
-        logger.info('Unpublished FABS aggregate records populated.\n')
+        update_keys(dafa, 'unpublished FABS', 'aggregate', years,
+                    func.concat('AGG_', func.coalesce(dafa.uri, '-none-'), '_',
+                                func.coalesce(dafa.awarding_sub_tier_agency_c, '-none-')))
 
         # unpublished FABS record type not 1
-        logger.info('Populating unique_award_key for unpublished FABS non-aggregated records...')
-        logger.info('Starting records before {}...'.format(years[0]))
-        sess.query(dafa).filter(dafa.record_type != '1',
-                                func.cast_as_date(dafa.action_date) < '{}-01-01'.format(years[0])).\
-            update({dafa.unique_award_key: func.concat('NON_', func.coalesce(dafa.fain, '-none-'), '_',
-                                                       func.coalesce(dafa.awarding_sub_tier_agency_c, '-none-'))},
-                   synchronize_session=False)
-        sess.commit()
-        for year in years:
-            logger.info('Starting records in {}...'.format(year))
-            sess.query(dafa).filter(dafa.record_type != '1',
-                                    func.cast_as_date(dafa.action_date) >= '{}-01-01'.format(year),
-                                    func.cast_as_date(dafa.action_date) <= '{}-12-31'.format(year)). \
-                update({dafa.unique_award_key: func.concat('NON_', func.coalesce(dafa.fain, '-none-'), '_',
-                                                           func.coalesce(dafa.awarding_sub_tier_agency_c, '-none-'))},
-                       synchronize_session=False)
-            sess.commit()
-        logger.info('Unpublished FABS non-aggregated records populated.\n')
+        update_keys(dafa, 'unpublished FABS', 'non-aggregated', years,
+                    func.concat('NON_', func.coalesce(dafa.fain, '-none-'), '_',
+                                func.coalesce(dafa.awarding_sub_tier_agency_c, '-none-')))
 
         # published FABS record type 1
-        logger.info('Populating unique_award_key for published FABS aggregate records...')
         pafa = PublishedAwardFinancialAssistance
-        logger.info('Starting records before {}...'.format(years[0]))
-        sess.query(pafa).filter(pafa.record_type == '1',
-                                func.cast_as_date(pafa.action_date) < '{}-01-01'.format(years[0])).\
-            update({pafa.unique_award_key: func.concat('AGG_', func.coalesce(pafa.uri, '-none-'), '_',
-                                                       func.coalesce(pafa.awarding_sub_tier_agency_c, '-none-'))},
-                   synchronize_session=False)
-        sess.commit()
-        for year in years:
-            logger.info('Starting records in {}...'.format(year))
-            sess.query(pafa).filter(pafa.record_type == '1',
-                                    func.cast_as_date(pafa.action_date) >= '{}-01-01'.format(year),
-                                    func.cast_as_date(pafa.action_date) <= '{}-12-31'.format(year)). \
-                update({pafa.unique_award_key: func.concat('AGG_', func.coalesce(pafa.uri, '-none-'), '_',
-                                                           func.coalesce(pafa.awarding_sub_tier_agency_c, '-none-'))},
-                       synchronize_session=False)
-            sess.commit()
-        logger.info('Published FABS aggregate records populated.\n')
+        update_keys(pafa, 'published FABS', 'aggregate', years,
+                    func.concat('AGG_', func.coalesce(pafa.uri, '-none-'), '_',
+                                func.coalesce(pafa.awarding_sub_tier_agency_c, '-none-')))
 
         # published FABS record type not 1
-        logger.info('Populating unique_award_key for published FABS non-aggregated records...')
-        logger.info('Starting records before {}...'.format(years[0]))
-        sess.query(pafa).filter(pafa.record_type != '1',
-                                func.cast_as_date(pafa.action_date) < '{}-01-01'.format(years[0])).\
-            update({pafa.unique_award_key: func.concat('NON_', func.coalesce(pafa.fain, '-none-'), '_',
-                                                       func.coalesce(pafa.awarding_sub_tier_agency_c, '-none-'))},
-                   synchronize_session=False)
-        sess.commit()
-        for year in years:
-            logger.info('Starting records in {}...'.format(year))
-            sess.query(pafa).filter(pafa.record_type != '1',
-                                    func.cast_as_date(pafa.action_date) >= '{}-01-01'.format(year),
-                                    func.cast_as_date(pafa.action_date) <= '{}-12-31'.format(year)). \
-                update({pafa.unique_award_key: func.concat('NON_', func.coalesce(pafa.fain, '-none-'), '_',
-                                                           func.coalesce(pafa.awarding_sub_tier_agency_c, '-none-'))},
-                       synchronize_session=False)
-            sess.commit()
-        logger.info('Published FABS non-aggregated records populated.\n')
+        update_keys(pafa, 'published FABS', 'non-aggregated', years,
+                    func.concat('NON_', func.coalesce(pafa.fain, '-none-'), '_',
+                                func.coalesce(pafa.awarding_sub_tier_agency_c, '-none-')))
 
         sess.close()
