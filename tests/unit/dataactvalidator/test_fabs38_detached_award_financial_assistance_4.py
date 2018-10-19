059dbd3c2d3a45adff31995c953946196e7c58c9
alburde1 <Burdeyny_Alisa@bah.com>
2018-10-19 13:28:57 -0400

    Updating tests to match changes to rules

--- tests/unit/dataactvalidator/test_fabs38_detached_award_financial_assistance_4.py
+++ tests/unit/dataactvalidator/test_fabs38_detached_award_financial_assistance_4.py
@@ -12,9 +12,11 @@ def test_column_headers(database):
 
 
 def test_success(database):
-    """ Test when provided, AwardingOfficeCode must be a valid value from the Federal Hierarchy. """
+    """ Test when provided, AwardingOfficeCode must be a valid value from the Federal Hierarchy, including being
+        designated specifically as an Assistance/Grant Office in the hierarchy.
+    """
 
-    office = OfficeFactory(office_code='12345a')
+    office = OfficeFactory(office_code='12345a', grant_office=True)
     det_award_1 = DetachedAwardFinancialAssistanceFactory(awarding_office_code='12345a')
     # test ignore case
     det_award_2 = DetachedAwardFinancialAssistanceFactory(awarding_office_code='12345A')
@@ -25,10 +27,15 @@ def test_success(database):
 
 
 def test_failure(database):
-    """ Test fail when provided, AwardingOfficeCode must be a valid value from the Federal Hierarchy. """
+    """ Test fail when provided, AwardingOfficeCode must be a valid value from the Federal Hierarchy, including being
+        designated specifically as an Assistance/Grant Office in the hierarchy.
+    """
 
-    office = OfficeFactory(office_code='123456')
+    office = OfficeFactory(office_code='123456', grant_office=True)
+    office = OfficeFactory(office_code='987654', grant_office=False)
     det_award_1 = DetachedAwardFinancialAssistanceFactory(awarding_office_code='12345')
     det_award_2 = DetachedAwardFinancialAssistanceFactory(awarding_office_code='1234567')
-    errors = number_of_errors(_FILE, database, models=[office, det_award_1, det_award_2])
-    assert errors == 2
+    # Test fail if grant office is false even if code matches
+    det_award_3 = DetachedAwardFinancialAssistanceFactory(awarding_office_code='987654')
+    errors = number_of_errors(_FILE, database, models=[office, det_award_1, det_award_2, det_award_3])
+    assert errors == 3
