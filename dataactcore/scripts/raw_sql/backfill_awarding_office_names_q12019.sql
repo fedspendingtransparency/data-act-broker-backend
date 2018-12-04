52404c4afef603fc766cfe543ed9fc311515ed71
Michael Hess <hess_michael@bah.com>
2018-12-04 16:22:34 -0500

    Removing the end dates from the scripts because we want to cover the time between 01/01/2019 and deploy as well

--- dataactcore/scripts/raw_sql/backfill_awarding_office_names_q12019.sql
+++ dataactcore/scripts/raw_sql/backfill_awarding_office_names_q12019.sql
@@ -2,6 +2,6 @@ UPDATE published_award_financial_assistance AS pafa
 SET awarding_office_name = office.office_name
 FROM office
 WHERE pafa.awarding_office_code = office.office_code
-  AND cast_as_date(pafa.action_date) >= '2018/10/01' AND cast_as_date(pafa.action_date) < '2018/01/01'
+  AND cast_as_date(pafa.action_date) >= '2018/10/01'
   AND pafa.awarding_office_name IS NULL
   AND pafa.awarding_office_code IS NOT NULL;
