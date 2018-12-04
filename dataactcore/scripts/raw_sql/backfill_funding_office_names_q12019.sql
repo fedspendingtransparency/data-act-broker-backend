345679383cb06f1ec5c8969cd10055e296fb174f
Michael Hess <hess_michael@bah.com>
2018-12-04 14:38:02 -0500

    Create a run_sql python script for reusability, add two SQL files for backfilling data

---
+++ dataactcore/scripts/raw_sql/backfill_funding_office_names_q12019.sql
@@ -0,0 +1,7 @@
+UPDATE published_award_financial_assistance AS pafa
+SET funding_office_name = office.office_name
+FROM office
+WHERE pafa.funding_office_code = office.office_code
+  AND cast_as_date(pafa.action_date) >= '2018/10/01' AND cast_as_date(pafa.action_date) < '2018/01/01'
+  AND pafa.funding_office_name IS NULL
+  AND pafa.funding_office_code IS NOT NULL;
