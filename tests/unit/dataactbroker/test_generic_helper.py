a3e9b3adc5954b626c29e657e9840df2bb73a369
Daniel Boos <boos_daniel@bah.com>
2018-12-21 20:52:48 -0500

    Unit test sanity check generate_raw_quoted_query

--- tests/unit/dataactbroker/test_generic_helper.py
+++ tests/unit/dataactbroker/test_generic_helper.py
@@ -1,6 +1,9 @@
 import pytest
+import datetime
+from sqlalchemy import func, or_
 
-from dataactbroker.helpers.generic_helper import year_period_to_dates
+from dataactbroker.helpers.generic_helper import year_period_to_dates, generate_raw_quoted_query
+from dataactcore.models.jobModels import FileGeneration
 
 from dataactcore.utils.responseException import ResponseException
 
@@ -59,3 +62,34 @@ def test_year_period_to_dates_year_failure():
 
     assert resp_except.value.status == 400
     assert str(resp_except.value) == error_text
+
+
+def test_generate_raw_quoted_query(database):
+    sess = database.session
+    # Using FileGeneration for example
+
+    # Testing various filter logic
+    q = sess.query(FileGeneration.created_at).filter(
+        or_(FileGeneration.file_generation_id == 1, FileGeneration.request_date > datetime.datetime(2018, 1, 15, 0, 0)),
+        FileGeneration.agency_code.like('A'),
+        FileGeneration.file_path.is_(None),
+        FileGeneration.agency_type.in_(['awarding', 'funding']),
+        FileGeneration.agency_type.in_([('test',)]),
+        FileGeneration.is_cached_file.is_(True)
+    )
+    expected = "SELECT file_generation.created_at  " \
+               "FROM file_generation  " \
+               "WHERE " \
+               "(file_generation.file_generation_id = 1 OR file_generation.request_date > '2018-01-15 00:00:00') " \
+               "AND file_generation.agency_code LIKE 'A' " \
+               "AND file_generation.file_path IS NULL " \
+               "AND file_generation.agency_type IN ('awarding', 'funding') " \
+               "AND file_generation.agency_type IN ('(''test'',)') " \
+               "AND file_generation.is_cached_file IS true"
+    assert generate_raw_quoted_query(q) == expected
+
+    # Testing funcs
+    q = sess.query(func.max(FileGeneration.file_generation_id).label("Test Label"))
+    expected = 'SELECT max(file_generation.file_generation_id) AS "Test Label"  ' \
+               'FROM file_generation'
+    assert generate_raw_quoted_query(q) == expected
