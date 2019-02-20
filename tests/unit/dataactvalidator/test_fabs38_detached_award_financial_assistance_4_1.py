144f1a629a6b9d840f23a032e678db517eb59591
alburde1 <Burdeyny_Alisa@bah.com>
2019-02-20 11:27:03 -0500

    Updating rule names for DEV-1621 and DEV-1622

--- tests/unit/dataactvalidator/test_fabs38_detached_award_financial_assistance_4_1.py
+++ tests/unit/dataactvalidator/test_fabs38_detached_award_financial_assistance_4_1.py
@@ -2,7 +2,7 @@ from tests.unit.dataactcore.factories.domain import OfficeFactory
 from tests.unit.dataactcore.factories.staging import DetachedAwardFinancialAssistanceFactory
 from tests.unit.dataactvalidator.utils import number_of_errors, query_columns
 
-_FILE = 'fabs38_detached_award_financial_assistance_4'
+_FILE = 'fabs38_detached_award_financial_assistance_4_1'
 
 
 def test_column_headers(database):
