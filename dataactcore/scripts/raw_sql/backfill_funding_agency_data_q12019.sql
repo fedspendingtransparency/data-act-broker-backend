b1de97434bbe4f490ed4bf6d19655c7b902fe99b
alburde1 <Burdeyny_Alisa@bah.com>
2019-04-16 13:29:58 -0400

    Fixing tab vs space... stupid copy-paste

--- dataactcore/scripts/raw_sql/backfill_funding_agency_data_q12019.sql
+++ dataactcore/scripts/raw_sql/backfill_funding_agency_data_q12019.sql
@@ -1,5 +1,5 @@
 WITH agency_list AS
-	(SELECT (CASE WHEN sta.is_frec
+    (SELECT (CASE WHEN sta.is_frec
                 THEN frec.frec_code
                 ELSE cgac.cgac_code
                 END) AS agency_code,
@@ -15,19 +15,19 @@ WITH agency_list AS
         INNER JOIN frec
             ON frec.frec_id = sta.frec_id),
 office_list AS
-	(SELECT office.office_code,
-		agency_list.agency_code,
-		agency_list.agency_name,
-		agency_list.sub_tier_code,
+    (SELECT office.office_code,
+        agency_list.agency_code,
+        agency_list.agency_name,
+        agency_list.sub_tier_code,
         agency_list.sub_tier_name
     FROM office
-    	INNER JOIN agency_list
-    		ON agency_list.sub_tier_code = office.sub_tier_code)
+        INNER JOIN agency_list
+            ON agency_list.sub_tier_code = office.sub_tier_code)
 UPDATE published_award_financial_assistance AS pafa
 SET funding_sub_tier_agency_co = office_list.sub_tier_code,
-	funding_sub_tier_agency_na = office_list.sub_tier_name,
-	funding_agency_code = office_list.agency_code,
-	funding_agency_name = office_list.agency_name
+    funding_sub_tier_agency_na = office_list.sub_tier_name,
+    funding_agency_code = office_list.agency_code,
+    funding_agency_name = office_list.agency_name
 FROM office_list
 WHERE pafa.funding_office_code = office_list.office_code
     AND cast_as_date(pafa.action_date) >= '2018/10/01'
