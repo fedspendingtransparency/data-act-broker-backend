eea8f96fbd6319d9c9a404f9e73f6a5d7f20b9e0
alburde1 <Burdeyny_Alisa@bah.com>
2019-05-29 11:51:36 -0400

    Update the exec comp script to actually do things

--- dataactcore/scripts/derive_historical_transaction_exec_comp.py
+++ dataactcore/scripts/derive_historical_transaction_exec_comp.py
@@ -3,15 +3,61 @@ import argparse
 import re
 
 from dataactcore.config import CONFIG_BROKER
+from dataactcore.interfaces.db import GlobalDB
 from dataactcore.logging import configure_logging
+from dataactcore.scripts.load_exec_comp import parse_exec_comp_file, create_temp_exec_comp_table
 from dataactcore.utils.duns import get_client, REMOTE_SAM_EXEC_COMP_DIR
 from dataactvalidator.health_check import create_app
 
 logger = logging.getLogger(__name__)
 
 
+def update_transactions(sess, exec_comp_data, file_date):
+    """ Update FABS and FPDS transactions with historical executive compensation data.
+    
+        Arguments:
+            sess: database connection
+            exec_comp_data: pandas dataframe representing exec comp data
+            file_date: date on the monthly file to use as min date
+    """
+
+    temp_table_name = 'temp_exec_comp_update'
+    create_temp_exec_comp_table(sess, temp_table_name, exec_comp_data)
+
+    update_sql = """
+            UPDATE {update_table}
+            SET
+                high_comp_officer1_amount = tmp.high_comp_officer1_amount,
+                high_comp_officer1_full_na = tmp.high_comp_officer1_full_na,
+                high_comp_officer2_amount = tmp.high_comp_officer2_amount,
+                high_comp_officer2_full_na = tmp.high_comp_officer2_full_na,
+                high_comp_officer3_amount = tmp.high_comp_officer3_amount,
+                high_comp_officer3_full_na = tmp.high_comp_officer3_full_na,
+                high_comp_officer4_amount = tmp.high_comp_officer4_amount,
+                high_comp_officer4_full_na = tmp.high_comp_officer4_full_na,
+                high_comp_officer5_amount = tmp.high_comp_officer5_amount,
+                high_comp_officer5_full_na = tmp.high_comp_officer5_full_na,
+                last_exec_comp_mod_date = tmp.last_exec_comp_mod_date
+            FROM {table_name} AS tmp
+            WHERE {update_table}.awardee_or_recipient_uniqu = tmp.awardee_or_recipient_uniqu
+                AND cast_as_date({update_table}.action_date) >= cast_as_date({file_date});
+        """
+    # Update FABS
+    logger.info('Updating FABS based on {}, starting at {}'.format(temp_table_name, file_date))
+    sess.execute(update_sql.format(update_table='published_award_financial_assistance', table_name=temp_table_name,
+                                   file_date=file_date))
+
+    # Update FPDS
+    logger.info('Updating FPDS based on {}, starting at {}'.format(temp_table_name, file_date))
+    sess.execute(update_sql.format(update_table='detached_award_procurement', table_name=temp_table_name,
+                                   file_date=file_date))
+
+    logger.info('Dropping {}'.format(temp_table_name))
+    sess.execute('DROP TABLE {};'.format(temp_table_name))
+
+
 def main():
-    logger.info("Starting historical transaction executive compensation backfill.")
+    logger.info('Starting historical transaction executive compensation backfill.')
 
     parser = argparse.ArgumentParser(description='Backfill historical executive compensation data for transactions.')
     parser.add_argument('-k', '--ssh_key', help='private key used to access the API remotely', required=True)
@@ -27,6 +73,14 @@ def main():
                                                                                              monthly_file)])
 
     logger.info(sorted_monthly_file_names)
+    sess = GlobalDB.db().session
+
+    for monthly_file in sorted_monthly_file_names:
+        file_date = re.match('.*(\d{8}).*', monthly_file).group(1)
+
+        logger.info('Starting {} monthly file'.format(file_date))
+        exec_comp_data = parse_exec_comp_file(monthly_file, root_dir, sftp=sftp, ssh_key=args.ssh_key)
+        update_transactions(sess, exec_comp_data, file_date)
 
 if __name__ == '__main__':
     with create_app().app_context():
