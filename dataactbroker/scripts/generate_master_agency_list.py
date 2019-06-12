31ee6d4d0f5206cda9a7af64207d087276a3c0ed
Daniel Boos <boos_daniel@bah.com>
2019-06-12 14:04:02 -0400

    load authoritative agency list

--- dataactbroker/scripts/generate_master_agency_list.py
+++ dataactbroker/scripts/generate_master_agency_list.py
@@ -1,5 +1,3 @@
-import os
-import sys
 import argparse
 import logging
 import datetime
@@ -13,6 +11,7 @@ from dataactvalidator.health_check import create_app
 from dataactvalidator.scripts.loader_utils import pad_function, insert_dataframe
 
 logger = logging.getLogger(__name__)
+
 
 def pad_columns(df, pad_column_dict):
     """ Zero-pads columns in the dataframe provided
@@ -65,15 +64,15 @@ def create_temp_agency_list_table(sess, data):
     """
     logger.info('Making temp temp_agency_list table')
     create_table_sql = """
-            CREATE TABLE IF NOT EXISTS temp_agency_list (
-                cgac text,
-                fpds_dep_id text,
-                name text,
-                abbreviation text,
-                subtier_code text,
-                subtier_name text
-            );
-        """
+        CREATE TABLE IF NOT EXISTS temp_agency_list (
+            cgac text,
+            fpds_dep_id text,
+            name text,
+            abbreviation text,
+            subtier_code text,
+            subtier_name text
+        );
+    """
     sess.execute(create_table_sql)
     # Truncating in case we didn't clear out this table after a failure in the script
     sess.execute('TRUNCATE TABLE temp_agency_list;')
@@ -130,28 +129,28 @@ def create_temp_agency_list_codes_table(sess, data):
     """
     logger.info('Making temp temp_agency_list_codes table')
     create_table_sql = """
-            CREATE TABLE IF NOT EXISTS temp_agency_list_codes (
-                cgac text,
-                fpds_dep_id text,
-                name text,
-                abbreviation text,
-                registered_broker text,
-                registered_asp text,
-                original_source text,
-                subtier_code text,
-                subtier_name text,
-                subtier_abbr text,
-                frec text,
-                frec_entity_desc text,
-                subtier_source text,
-                subtier_in_fpds_asp text,
-                subtier_registered_asp text,
-                original_name text,
-                original_subtier_name text,
-                comment text,
-                is_frec text
-            );
-        """
+        CREATE TABLE IF NOT EXISTS temp_agency_list_codes (
+            cgac text,
+            fpds_dep_id text,
+            name text,
+            abbreviation text,
+            registered_broker text,
+            registered_asp text,
+            original_source text,
+            subtier_code text,
+            subtier_name text,
+            subtier_abbr text,
+            frec text,
+            frec_entity_desc text,
+            subtier_source text,
+            subtier_in_fpds_asp text,
+            subtier_registered_asp text,
+            original_name text,
+            original_subtier_name text,
+            comment text,
+            is_frec text
+        );
+    """
     sess.execute(create_table_sql)
     # Truncating in case we didn't clear out this table after a failure in the script
     sess.execute('TRUNCATE TABLE temp_agency_list_codes;')
@@ -183,7 +182,7 @@ def merge_broker_lists():
             "comment",
             "is_frec"
         )
-        SELECT 
+        SELECT
             tal.cgac AS cgac,
             tal.fpds_dep_id AS fpds_dep_id,
             tal.name AS name,
@@ -215,11 +214,101 @@ def merge_broker_lists():
     logger.info('Added {} rows from agency_list.csv to agency_codes_list.csv'.format(inserted_rows.rowcount))
 
 
-def generate_master_agency_list(sess, agency_list, agency_list_codes):
+def load_temp_authoritative_agency_list(sess, authoritative_agency_list_path):
+    """ Loads authoritative_agency_list.csv into a temporary table named temp_authoritative_agency_list
+
+        Args:
+            sess: database connection
+            authoritative_agency_list_path: uri to authoritative agency list csv
+    """
+    logger.info('Loading authoritative_agency_list.csv')
+    authoritative_agency_list_names = [
+        'cgac',
+        'fpds_dep_id',
+        'name',
+        'abbreviation',
+        'registered_broker',
+        'registered_asp',
+        'original_source',
+        'subtier_code',
+        'subtier_name',
+        'subtier_abbr',
+        'admin_org_name',
+        'admin_org',
+        'frec',
+        'frec_entity_desc',
+        'subtier_source',
+        'subtier_in_fpds_asp',
+        'subtier_registered_asp',
+        'original_name',
+        'original_subtier_name',
+        'is_frec',
+        'mission',
+        'website',
+        'congressional_justification',
+        'icon_filename'
+    ]
+    with RetrieveFileFromUri(authoritative_agency_list_path, 'r').get_file_object() as f:
+        authoritative_agency_list_df = pd.read_csv(f, dtype=str, skiprows=1, names=authoritative_agency_list_names)
+    pad_column_dict = {
+        'cgac': 3,
+        'fpds_dep_id': 4,
+        'subtier_code': 4
+    }
+    authoritative_agency_list_df = pad_columns(authoritative_agency_list_df, pad_column_dict)
+    create_temp_authoritative_agency_list_table(sess, authoritative_agency_list_df)
+
+
+def create_temp_authoritative_agency_list_table(sess, data):
+    """ Creates a temporary authoritative agency list table with the given name and data.
+
+        Args:
+            sess: database connection
+            data: pandas dataframe representing the authoritative agency list csv
+    """
+    logger.info('Making temp temp_authoritative_agency_list table')
+    create_table_sql = """
+        CREATE TABLE IF NOT EXISTS temp_authoritative_agency_list (
+            cgac text,
+            fpds_dep_id text,
+            name text,
+            abbreviation text,
+            registered_broker text,
+            registered_asp text,
+            original_source text,
+            subtier_code text,
+            subtier_name text,
+            subtier_abbr text,
+            admin_org_name text,
+            admin_org text,
+            frec text,
+            frec_entity_desc text,
+            subtier_source text,
+            subtier_in_fpds_asp text,
+            subtier_registered_asp text,
+            original_name text,
+            original_subtier_name text,
+            is_frec text,
+            mission text,
+            website text,
+            congressional_justification text,
+            icon_filename text
+        );
+    """
+    sess.execute(create_table_sql)
+    # Truncating in case we didn't clear out this table after a failure in the script
+    sess.execute('TRUNCATE TABLE temp_authoritative_agency_list;')
+    insert_dataframe(data, 'temp_authoritative_agency_list', sess.connection())
+    sess.commit()
+
+
+def generate_master_agency_list(sess, agency_list, agency_list_codes, authoritative_agency_list):
     """ Main script algorithm that generates the master agency list """
     load_temp_agency_list(sess, agency_list)
     load_temp_agency_list_codes(sess, agency_list_codes)
     merge_broker_lists()
+    load_temp_authoritative_agency_list(sess, authoritative_agency_list)
+
 
 if __name__ == '__main__':
     now = datetime.datetime.now()
@@ -227,13 +316,15 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Merge broker agency lists')
     parser.add_argument('-acl', '--agency_list_codes', type=str, required=True, help="URI for agency_list_codes.csv")
     parser.add_argument('-al', '--agency_list', type=str, required=True, help="URI for agency_list.csv")
+    parser.add_argument('-aal', '--authoritative_agency_list', type=str, required=True,
+                        help="URI for authoritative_agency_list.csv")
 
     with create_app().app_context():
         logger.info("Begin generating master agency list")
         sess = GlobalDB.db().session
         args = parser.parse_args()
 
-        generate_master_agency_list(sess, args.agency_list, args.agency_list_codes)
+        generate_master_agency_list(sess, args.agency_list, args.agency_list_codes, args.authoritative_agency_list)
 
         duration = str(datetime.datetime.now() - now)
         logger.info("Generating master agency list took {} seconds".format(duration))
