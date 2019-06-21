78b9996c33f1cd4e18f8765c089efda1248fd0fd
Daniel Boos <boos_daniel@bah.com>
2019-06-21 18:12:04 -0400

    flake8

--- dataactvalidator/scripts/load_agencies.py
+++ dataactvalidator/scripts/load_agencies.py
@@ -136,7 +136,7 @@ def load_frec(file_name):
         {"frec": {"keep_null": False}, "cgac_code": {"pad_to_length": 3}, "frec_code": {"pad_to_length": 4}}
     )
     # de-dupe
-    data = data[data.frec_cgac=='TRUE']
+    data = data[data.frec_cgac == 'TRUE']
     data.drop_duplicates(subset=['frec_code'], inplace=True)
     # create foreign key dicts
     cgac_dict = {str(cgac.cgac_code): cgac.cgac_id for
