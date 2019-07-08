98bbae0ccca8a7c85c311d11c0a0783f111524f6
alburde1 <Burdeyny_Alisa@bah.com>
2019-07-08 15:16:54 -0400

    Adding ignoring deletes to FABS rules 26-30

--- tests/unit/dataactvalidator/test_fabs30_detached_award_financial_assistance.py
+++ tests/unit/dataactvalidator/test_fabs30_detached_award_financial_assistance.py
@@ -5,7 +5,7 @@ _FILE = 'fabs30_detached_award_financial_assistance'
 
 
 def test_column_headers(database):
-    expected_subset = {"row_number", "business_funds_indicator"}
+    expected_subset = {'row_number', 'business_funds_indicator'}
     actual = set(query_columns(_FILE, database))
     assert expected_subset == actual
 
@@ -13,18 +13,21 @@ def test_column_headers(database):
 def test_success(database):
     """ BusinessFundsIndicator must contain one of the following values: REC or NON. Case doesn't matter """
 
-    det_award = DetachedAwardFinancialAssistanceFactory(business_funds_indicator="REC")
-    det_award_2 = DetachedAwardFinancialAssistanceFactory(business_funds_indicator="non")
+    det_award = DetachedAwardFinancialAssistanceFactory(business_funds_indicator='REC', correction_delete_indicatr='c')
+    det_award_2 = DetachedAwardFinancialAssistanceFactory(business_funds_indicator='non', correction_delete_indicatr='')
+    # Ignore correction delete indicator of D
+    det_award_3 = DetachedAwardFinancialAssistanceFactory(business_funds_indicator='red',
+                                                          correction_delete_indicatr='d')
 
-    errors = number_of_errors(_FILE, database, models=[det_award, det_award_2])
+    errors = number_of_errors(_FILE, database, models=[det_award, det_award_2, det_award_3])
     assert errors == 0
 
 
 def test_failure(database):
     """ BusinessFundsIndicator must contain one of the following values: REC or NON. """
 
-    det_award = DetachedAwardFinancialAssistanceFactory(business_funds_indicator="RECs")
-    det_award_2 = DetachedAwardFinancialAssistanceFactory(business_funds_indicator="red")
+    det_award = DetachedAwardFinancialAssistanceFactory(business_funds_indicator='RECs', correction_delete_indicatr='C')
+    det_award_2 = DetachedAwardFinancialAssistanceFactory(business_funds_indicator='red', correction_delete_indicatr='')
 
     errors = number_of_errors(_FILE, database, models=[det_award, det_award_2])
     assert errors == 2
