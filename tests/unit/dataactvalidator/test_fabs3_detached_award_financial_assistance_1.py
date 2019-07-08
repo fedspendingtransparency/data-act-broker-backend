90b78c15312b3193ff7ed844bf1c81c6339ddaba
alburde1 <Burdeyny_Alisa@bah.com>
2019-07-08 10:35:17 -0400

    Adding ignoring deletes to FABS rules 1-5
    - FABS2 rules not affected

--- tests/unit/dataactvalidator/test_fabs3_detached_award_financial_assistance_1.py
+++ tests/unit/dataactvalidator/test_fabs3_detached_award_financial_assistance_1.py
@@ -5,29 +5,39 @@ _FILE = 'fabs3_detached_award_financial_assistance_1'
 
 
 def test_column_headers(database):
-    expected_subset = {"row_number", "action_type", "record_type"}
+    expected_subset = {'row_number', 'action_type', 'record_type'}
     actual = set(query_columns(_FILE, database))
     assert expected_subset == actual
 
 
 def test_success(database):
     """ Tests success for when Action type is required for non-aggregate and PII-redacted non-aggregate records
-        (i.e., when RecordType = 2 or 3) """
-    det_award_1 = DetachedAwardFinancialAssistanceFactory(record_type=2, action_type="B")
-    det_award_2 = DetachedAwardFinancialAssistanceFactory(record_type=3, action_type="B")
-    det_award_3 = DetachedAwardFinancialAssistanceFactory(record_type=1, action_type="")
-    det_award_4 = DetachedAwardFinancialAssistanceFactory(record_type=1, action_type=None)
+        (i.e., when RecordType = 2 or 3).
+    """
+    det_award_1 = DetachedAwardFinancialAssistanceFactory(record_type=2, action_type='B', correction_delete_indicatr='')
+    det_award_2 = DetachedAwardFinancialAssistanceFactory(record_type=3, action_type='B',
+                                                          correction_delete_indicatr=None)
+    # Ignore record type 1
+    det_award_3 = DetachedAwardFinancialAssistanceFactory(record_type=1, action_type='', correction_delete_indicatr='c')
+    det_award_4 = DetachedAwardFinancialAssistanceFactory(record_type=1, action_type=None,
+                                                          correction_delete_indicatr='C')
+    # Ignore correction delete indicator of D
+    det_award_5 = DetachedAwardFinancialAssistanceFactory(record_type=3, action_type=None,
+                                                          correction_delete_indicatr='D')
 
-    errors = number_of_errors(_FILE, database, models=[det_award_1, det_award_2, det_award_3, det_award_4])
+    errors = number_of_errors(_FILE, database, models=[det_award_1, det_award_2, det_award_3, det_award_4, det_award_5])
     assert errors == 0
 
 
 def test_failure(database):
     """ Tests that failure for when Action type is required for non-aggregate and PII-redacted non-aggregate records
-        (i.e., when RecordType = 2 or 3) """
-    det_award_1 = DetachedAwardFinancialAssistanceFactory(record_type=2, action_type='')
-    det_award_2 = DetachedAwardFinancialAssistanceFactory(record_type=2, action_type=None)
-    det_award_3 = DetachedAwardFinancialAssistanceFactory(record_type=3, action_type=None)
+        (i.e., when RecordType = 2 or 3).
+    """
+    det_award_1 = DetachedAwardFinancialAssistanceFactory(record_type=2, action_type='', correction_delete_indicatr='c')
+    det_award_2 = DetachedAwardFinancialAssistanceFactory(record_type=2, action_type=None,
+                                                          correction_delete_indicatr='')
+    det_award_3 = DetachedAwardFinancialAssistanceFactory(record_type=3, action_type=None,
+                                                          correction_delete_indicatr=None)
 
     errors = number_of_errors(_FILE, database, models=[det_award_1, det_award_2, det_award_3])
     assert errors == 3
--- tests/unit/dataactvalidator/test_fabs4_detached_award_financial_assistance_2.py
+++ tests/unit/dataactvalidator/test_fabs4_detached_award_financial_assistance_2.py
@@ -5,27 +5,30 @@ _FILE = 'fabs4_detached_award_financial_assistance_2'
 
 
 def test_column_headers(database):
-    expected_subset = {"row_number", "action_date"}
+    expected_subset = {'row_number', 'action_date'}
     actual = set(query_columns(_FILE, database))
     assert expected_subset == actual
 
 
 def test_success(database):
-    """ Tests if value of action date is be between 19991001 and 20991231
-        (i.e., a date between 10/01/1999 and 12/31/2099) """
-    det_award_1 = DetachedAwardFinancialAssistanceFactory(action_date="20120725")
-    det_award_2 = DetachedAwardFinancialAssistanceFactory(action_date=None)
-    det_award_3 = DetachedAwardFinancialAssistanceFactory(action_date="5")
+    """ Tests if value of action date is between 19991001 and 20991231 (a date between 10/01/1999 and 12/31/2099). """
+    det_award_1 = DetachedAwardFinancialAssistanceFactory(action_date='20120725', correction_delete_indicatr='')
+    det_award_2 = DetachedAwardFinancialAssistanceFactory(action_date=None, correction_delete_indicatr='C')
+    # Ignore if not a valid date, different rule covers this
+    det_award_3 = DetachedAwardFinancialAssistanceFactory(action_date='5', correction_delete_indicatr=None)
+    # Ignore correction delete indicator of D
+    det_award_4 = DetachedAwardFinancialAssistanceFactory(action_date='19990131', correction_delete_indicatr='d')
 
-    errors = number_of_errors(_FILE, database, models=[det_award_1, det_award_2, det_award_3])
+    errors = number_of_errors(_FILE, database, models=[det_award_1, det_award_2, det_award_3, det_award_4])
     assert errors == 0
 
 
 def test_failure(database):
-    """ Tests if value of action date is not be between 19991001 and 20991231
-        (i.e., a date between 10/01/1999 and 12/31/2099) """
-    det_award_1 = DetachedAwardFinancialAssistanceFactory(action_date="19990131")
-    det_award_2 = DetachedAwardFinancialAssistanceFactory(action_date="21000101")
+    """ Tests if value of action date is not between 19991001 and 20991231 (i.e., a date between 10/01/1999 and
+        12/31/2099).
+    """
+    det_award_1 = DetachedAwardFinancialAssistanceFactory(action_date='19990131', correction_delete_indicatr='c')
+    det_award_2 = DetachedAwardFinancialAssistanceFactory(action_date='21000101', correction_delete_indicatr=None)
 
     errors = number_of_errors(_FILE, database, models=[det_award_1, det_award_2])
     assert errors == 2
