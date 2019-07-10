922290ecec5c9618bfeb48ded1b3cf126595ed63
alburde1 <Burdeyny_Alisa@bah.com>
2019-07-10 12:48:56 -0400

    Fixing some WITH names that got missed somehow

--- dataactvalidator/config/sqlrules/fabs41_detached_award_financial_assistance_1.sql
+++ dataactvalidator/config/sqlrules/fabs41_detached_award_financial_assistance_1.sql
@@ -1,6 +1,6 @@
 -- For PrimaryPlaceOfPerformanceCode XX##### or XX####R, where PrimaryPlaceOfPerformanceZIP+4 is blank or "city-wide":
 -- city code ##### or ####R must be valid and exist in the provided state.
-WITH detached_award_financial_assistance_d41_1_{0} AS
+WITH detached_award_financial_assistance_fabs41_1_{0} AS
     (SELECT submission_id,
         row_number,
         place_of_performance_code,
@@ -11,14 +11,14 @@ WITH detached_award_financial_assistance_d41_1_{0} AS
 SELECT
     dafa.row_number,
     dafa.place_of_performance_code
-FROM detached_award_financial_assistance_d41_1_{0} AS dafa
+FROM detached_award_financial_assistance_fabs41_1_{0} AS dafa
 WHERE UPPER(dafa.place_of_performance_code) ~ '^[A-Z][A-Z]\d\d\d\d[\dR]$'
     AND (COALESCE(dafa.place_of_performance_zip4a, '') = ''
          OR dafa.place_of_performance_zip4a = 'city-wide'
     )
     AND dafa.row_number NOT IN (
         SELECT DISTINCT sub_dafa.row_number
-        FROM detached_award_financial_assistance_d41_1_{0} AS sub_dafa
+        FROM detached_award_financial_assistance_fabs41_1_{0} AS sub_dafa
         JOIN city_code
             ON SUBSTRING(sub_dafa.place_of_performance_code, 3, 5) = city_code.city_code
                 AND UPPER(SUBSTRING(sub_dafa.place_of_performance_code, 1, 2)) = city_code.state_code
