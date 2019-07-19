3811c57e8f42ddd87c7541163037036b3c7f958b
Daniel Boos <boos_daniel@bah.com>
2019-07-19 17:06:38 -0400

    moving exec_comp functions to duns.py

--- dataactcore/scripts/load_exec_comp.py
+++ dataactcore/scripts/load_exec_comp.py
@@ -1,10 +1,6 @@
 import logging
 import os
-import pandas as pd
-import zipfile
 import re
-from collections import OrderedDict
-import numpy as np
 import argparse
 import datetime
 import json
@@ -14,220 +10,37 @@ from dataactcore.interfaces.db import GlobalDB
 from dataactcore.logging import configure_logging
 from dataactvalidator.health_check import create_app
 from dataactcore.config import CONFIG_BROKER
-from dataactcore.utils.duns import get_client, REMOTE_SAM_EXEC_COMP_DIR
-from dataactvalidator.scripts.loader_utils import insert_dataframe
+from dataactcore.utils.duns import get_client, REMOTE_SAM_EXEC_COMP_DIR, parse_exec_comp_file, update_exec_comp_duns
 
 logger = logging.getLogger(__name__)
 
 
-def parse_exec_comp_file(filename, root_dir, sftp=None, ssh_key=None, metrics=None):
-    """ Parses the executive compensation file to update corresponding DUNS records
+def process_from_dir(root_dir, file_name, sess, sftp=None, ssh_key=None, metrics=None):
+    """ Process the SAM file found locally or remotely
 
         Args:
-            filename: name of file to import
-            root_dir: working directory
-            sftp: connection to remote server
-            ssh_key: ssh_key for reconnecting
-            metrics: dictionary representing metrics of the script
-
-        Raises:
-            Exception: couldn't extract the last exec comp modification date, this generally means the filename provided
-                doesn't match the expected format.
+            root_dir: the folder containing the SAM file
+            file_name: the name of the SAM file
+            sess: the database connection
+            sftp: the sftp client to pull the CSV from
+            metrics: dictionary representing metrics data for the load
     """
     if not metrics:
-        metrics = {
-            'files_processed': [],
-            'records_received': 0,
-            'records_processed': 0
-        }
+        metrics = {}
 
-    file_path = os.path.join(root_dir, filename)
+    file_path = os.path.join(root_dir, file_name)
     if sftp:
         if sftp.sock.closed:
             # Reconnect if channel is closed
             ssh_client = get_client(ssh_key=ssh_key)
             sftp = ssh_client.open_sftp()
-        with open(os.path.join(root_dir, filename), 'wb') as file:
-            sftp.getfo(''.join([REMOTE_SAM_EXEC_COMP_DIR, '/', filename]), file)
+        with open(os.path.join(root_dir, file_name), 'wb') as file:
+            sftp.getfo(''.join([REMOTE_SAM_EXEC_COMP_DIR, '/', file_name]), file)
 
-    logger.info('starting file ' + file_path)
-    metrics['files_processed'].append(filename)
-
-    csv_file = os.path.splitext(filename)[0]+'.dat'
-    zfile = zipfile.ZipFile(file_path)
-
-    # can't use skipfooter, pandas' c engine doesn't work with skipfooter and the python engine doesn't work with dtype
-    nrows = 0
-    with zfile.open(csv_file) as zip_file:
-        nrows = len(zip_file.readlines()) - 2  # subtract the header and footer
-    column_header_mapping = {
-        'awardee_or_recipient_uniqu': 0,
-        'sam_extract': 4,
-        'exec_comp_str': 89
-    }
-    column_header_mapping_ordered = OrderedDict(sorted(column_header_mapping.items(), key=lambda c: c[1]))
-    with zfile.open(csv_file) as zip_file:
-        csv_data = pd.read_csv(zip_file, dtype=str, header=None, skiprows=1, nrows=nrows, sep='|',
-                               usecols=column_header_mapping_ordered.values(),
-                               names=column_header_mapping_ordered.keys())
-    total_data = csv_data.copy()
-    metrics['records_received'] += len(total_data.index)
-    total_data = total_data[total_data.awardee_or_recipient_uniqu.notnull() &
-                            total_data.sam_extract.isin(['2', '3', 'A', 'E'])]
-    metrics['records_processed'] += len(total_data.index)
-    del total_data['sam_extract']
-    # Note: we're splitting these up cause it vastly saves memory parsing only the records that are populated
-    blank_exec = total_data[total_data.exec_comp_str.isnull()]
-    pop_exec = total_data[total_data.exec_comp_str.notnull()]
-
-    # parse out executive compensation from row 90 for populated records
-    lambda_func = (lambda ecs: pd.Series(list(parse_exec_comp(ecs).values())))
-    parsed_data = pop_exec['exec_comp_str'].apply(lambda_func)
-    parsed_data.columns = list(parse_exec_comp().keys())
-    del pop_exec['exec_comp_str']
-    pop_exec = pop_exec.join(parsed_data)
-
-    # leave blanks
-    del blank_exec['exec_comp_str']
-    blank_exec = blank_exec.assign(**parse_exec_comp())
-
-    # setup the final dataframe
-    total_data = pd.concat([pop_exec, blank_exec])
-    total_data.replace('', np.nan, inplace=True)
-    last_exec_comp_mod_date_str = re.findall('[0-9]{8}', filename)
-    if not last_exec_comp_mod_date_str:
-        raise Exception('Last Executive Compensation Mod Date not found in filename.')
-    last_exec_comp_mod_date = datetime.datetime.strptime(last_exec_comp_mod_date_str[0], '%Y%m%d').date()
-    total_data = total_data.assign(last_exec_comp_mod_date=last_exec_comp_mod_date)
-
+    exec_comp_data = parse_exec_comp_file(daily_file, root_dir, sftp=sftp, ssh_key=ssh_key, metrics=metrics)
+    update_exec_comp_duns(sess, exec_comp_data, metrics=metrics)
     if sftp:
-        os.remove(os.path.join(root_dir, filename))
-
-    return total_data
-
-
-def create_temp_exec_comp_table(sess, table_name, data):
-    """ Creates a temporary executive compensation table with the given name and data.
-
-        Args:
-            sess: database connection
-            table_name: what to name the table being created
-            data: pandas dataframe representing exec comp data
-    """
-    logger.info('Making {} table'.format(table_name))
-    create_table_sql = """
-            CREATE TABLE IF NOT EXISTS {} (
-                awardee_or_recipient_uniqu TEXT,
-                high_comp_officer1_amount TEXT,
-                high_comp_officer1_full_na TEXT,
-                high_comp_officer2_amount TEXT,
-                high_comp_officer2_full_na TEXT,
-                high_comp_officer3_amount TEXT,
-                high_comp_officer3_full_na TEXT,
-                high_comp_officer4_amount TEXT,
-                high_comp_officer4_full_na TEXT,
-                high_comp_officer5_amount TEXT,
-                high_comp_officer5_full_na TEXT,
-                last_exec_comp_mod_date DATE
-            );
-        """.format(table_name)
-    sess.execute(create_table_sql)
-    # Truncating in case we didn't clear out this table after a failure in the script
-    sess.execute('TRUNCATE TABLE {};'.format(table_name))
-    insert_dataframe(data, table_name, sess.connection())
-
-
-def update_exec_comp_duns(sess, exec_comp_data, metrics=None):
-    """ Takes in a dataframe of exec comp data and updates associated DUNS
-
-        Args:
-            sess: database connection
-            exec_comp_data: pandas dataframe representing exec comp data
-            metrics: dictionary representing metrics of the script
-
-        Returns:
-            list of DUNS updated
-    """
-    if not metrics:
-        metrics = {
-            'updated_duns': []
-        }
-
-    temp_table_name = 'temp_exec_comp_update'
-    create_temp_exec_comp_table(sess, temp_table_name, exec_comp_data)
-
-    # Note: this can work just by getting the row count from the following SQL
-    #       but this can run multiple times on possibly the same DUNS over several days,
-    #       so it'll be more accurate to keep track of which DUNS get updated
-    logger.info('Getting list of DUNS that will be updated for metrics')
-    update_sql = """
-        SELECT duns.awardee_or_recipient_uniqu
-        FROM duns
-        JOIN temp_exec_comp_update AS tecu ON duns.awardee_or_recipient_uniqu=tecu.awardee_or_recipient_uniqu;
-    """
-    duns_list = [row['awardee_or_recipient_uniqu'] for row in sess.execute(update_sql).fetchall()]
-
-    logger.info('Updating DUNS based on temp_exec_comp_update')
-    update_sql = """
-        UPDATE duns
-        SET
-            high_comp_officer1_amount = tecu.high_comp_officer1_amount,
-            high_comp_officer1_full_na = tecu.high_comp_officer1_full_na,
-            high_comp_officer2_amount = tecu.high_comp_officer2_amount,
-            high_comp_officer2_full_na = tecu.high_comp_officer2_full_na,
-            high_comp_officer3_amount = tecu.high_comp_officer3_amount,
-            high_comp_officer3_full_na = tecu.high_comp_officer3_full_na,
-            high_comp_officer4_amount = tecu.high_comp_officer4_amount,
-            high_comp_officer4_full_na = tecu.high_comp_officer4_full_na,
-            high_comp_officer5_amount = tecu.high_comp_officer5_amount,
-            high_comp_officer5_full_na = tecu.high_comp_officer5_full_na,
-            last_exec_comp_mod_date = tecu.last_exec_comp_mod_date
-        FROM temp_exec_comp_update AS tecu
-        WHERE duns.awardee_or_recipient_uniqu=tecu.awardee_or_recipient_uniqu;
-    """
-    sess.execute(update_sql)
-
-    logger.info('Dropping {}'.format(temp_table_name))
-    sess.execute('DROP TABLE {};'.format(temp_table_name))
-
-    sess.commit()
-    metrics['updated_duns'].extend(duns_list)
-
-
-def parse_exec_comp(exec_comp_str=None):
-    """ Parses the executive compensation string into a dictionary of exec comp data
-
-        Args:
-            exec_comp_str: the incoming compensation string
-
-        Returns:
-            dictionary of exec comp data
-    """
-    exec_comp_data = OrderedDict()
-    for index in range(1, 6):
-        exec_comp_data['high_comp_officer{}_full_na'.format(index)] = np.nan
-        exec_comp_data['high_comp_officer{}_amount'.format(index)] = np.nan
-
-    if isinstance(exec_comp_str, str) and not exec_comp_str.isdigit():
-        high_comp_officers = exec_comp_str.split('~')
-
-        # records have inconsistent values for Null
-        # 'see note above' is excluded as it may contain relevant info
-        unaccepted_titles = ['x', 'n/a', 'na', 'null', 'none', '. . .', 'no one', 'no  one']
-
-        for index, high_comp_officer in enumerate(high_comp_officers):
-            index += 1
-            exec_comp_split = high_comp_officer.split('^')
-            if len(exec_comp_split) != 3:
-                continue
-            exec_name = exec_comp_split[0]
-            exec_title = exec_comp_split[1]
-            exec_comp = exec_comp_split[2]
-            if exec_title.lower() not in unaccepted_titles and exec_name.lower() not in unaccepted_titles:
-                exec_comp_data['high_comp_officer{}_full_na'.format(index)] = exec_name
-                exec_comp_data['high_comp_officer{}_amount'.format(index)] = exec_comp
-
-    return exec_comp_data
+        os.remove(file_path)
 
 
 def get_parser():
@@ -290,9 +103,7 @@ if __name__ == '__main__':
 
         # load in earliest monthly file for historic
         if historic and sorted_monthly_file_names:
-            exec_comp_data = parse_exec_comp_file(sorted_monthly_file_names[0], root_dir, sftp=sftp, ssh_key=ssh_key,
-                                                  metrics=metrics)
-            update_exec_comp_duns(sess, exec_comp_data, metrics=metrics)
+            process_from_dir(root_dir, sorted_monthly_file_names[0], sess, sftp=sftp, ssh_key=ssh_key, metrics=metrics)
 
         # load in daily files after depending on params
         if sorted_daily_file_names:
@@ -320,8 +131,7 @@ if __name__ == '__main__':
 
             # load daily files
             for daily_file in daily_files_after:
-                exec_comp_data = parse_exec_comp_file(daily_file, root_dir, sftp=sftp, ssh_key=ssh_key, metrics=metrics)
-                update_exec_comp_duns(sess, exec_comp_data, metrics=metrics)
+                process_from_dir(root_dir, daily_file, sess, sftp=sftp, ssh_key=ssh_key, metrics=metrics)
         sess.close()
 
     metrics['records_updated'] = len(set(metrics['updated_duns']))
