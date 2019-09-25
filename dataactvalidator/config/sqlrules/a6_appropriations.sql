5ec5d9568c559fde87eecd9fa2223670d898aee8
alburde1 <Burdeyny_Alisa@bah.com>
2019-09-25 11:08:19 -0400

    Add Expected Value to all single file error/warning files
    + Set up infrastructure to handle either in-rule or pre-defined string expected values

--- dataactvalidator/config/sqlrules/a6_appropriations.sql
+++ dataactvalidator/config/sqlrules/a6_appropriations.sql
@@ -9,7 +9,7 @@ WITH appropriation_a6_{0} AS
 SELECT
     approp.row_number,
     approp.total_budgetary_resources_cpe,
-    sf.amount AS sf_133_amount
+    sf.amount AS "expected_value_GTAS SF133 Line 1910"
 FROM appropriation_a6_{0} AS approp
     INNER JOIN sf_133 AS sf
         ON approp.tas = sf.tas
