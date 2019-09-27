2d0bac74ecf7fb954ab76e4f63b09aa091fa5cdd
alburde1 <Burdeyny_Alisa@bah.com>
2019-09-27 12:28:52 -0400

    Updating B expected values SQL and tests

--- tests/unit/dataactvalidator/test_b14_object_class_program_activity.py
+++ tests/unit/dataactvalidator/test_b14_object_class_program_activity.py
@@ -1,15 +1,28 @@
 from dataactcore.models.stagingModels import ObjectClassProgramActivity
 from dataactcore.models.domainModels import SF133
-from tests.unit.dataactvalidator.utils import number_of_errors
+from tests.unit.dataactvalidator.utils import number_of_errors, query_columns
 
 
 _FILE = 'b14_object_class_program_activity'
 _TAS = 'b14_object_class_program_activity_tas'
 
 
+def test_column_headers(database):
+    expected_subset = {'row_number', 'ussgl480100_undelivered_or_cpe_sum', 'ussgl480100_undelivered_or_fyb_sum',
+                       'ussgl480200_undelivered_or_cpe_sum', 'ussgl480200_undelivered_or_fyb_sum',
+                       'ussgl488100_upward_adjustm_cpe_sum', 'ussgl488200_upward_adjustm_cpe_sum',
+                       'ussgl490100_delivered_orde_cpe_sum', 'ussgl490100_delivered_orde_fyb_sum',
+                       'ussgl490200_delivered_orde_cpe_sum', 'ussgl490800_authority_outl_cpe_sum',
+                       'ussgl490800_authority_outl_fyb_sum', 'ussgl498100_upward_adjustm_cpe_sum',
+                       'ussgl498200_upward_adjustm_cpe_sum', 'expected_value_GTAS SF133 Line 2004'}
+    actual = set(query_columns(_FILE, database))
+    assert (actual & expected_subset) == expected_subset
+
+
 def test_success(database):
     """ Tests that SF 133 amount sum for line 2004 matches the calculation from Appropriation based on the fields below
-        for the specified fiscal year and period """
+        for the specified fiscal year and period
+    """
     tas = "".join([_TAS, "_success"])
 
     # This uses the default submission created in utils for 10/2015 which is period 1 of FY 2016
@@ -39,7 +52,8 @@ def test_success(database):
 
 def test_failure(database):
     """ Tests that SF 133 amount sum for line 2004 does not match the calculation from Appropriation based on
-        the fields below for the specified fiscal year and period """
+        the fields below for the specified fiscal year and period
+    """
     tas = "".join([_TAS, "_failure"])
     sf = SF133(line=2004, tas=tas, period=1, fiscal_year=2016, amount=5, agency_identifier="sys",
                main_account_code="000", sub_account_code="000")
