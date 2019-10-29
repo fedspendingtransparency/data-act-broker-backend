bff507100967d205f95c500ca2438a00cdaa905a
Daniel Boos <boos_daniel@bah.com>
2019-10-29 17:45:57 -0400

    updating SF133 loader test to account for display_tas

--- tests/unit/dataactvalidator/test_scripts_load_sf133.py
+++ tests/unit/dataactvalidator/test_scripts_load_sf133.py
@@ -11,7 +11,7 @@ from tests.unit.dataactcore.factories.domain import SF133Factory, TASFactory
 FINGERPRINT_COLS = [
     'availability_type_code', 'sub_account_code', 'allocation_transfer_agency', 'fiscal_year',
     'beginning_period_of_availa', 'ending_period_of_availabil', 'main_account_code', 'agency_identifier',
-    'period', 'created_at', 'updated_at', 'tas']
+    'period', 'created_at', 'updated_at', 'tas', 'display_tas']
 
 
 def test_fill_blank_sf133_lines_types():
@@ -19,7 +19,7 @@ def test_fill_blank_sf133_lines_types():
     function (that'd be a regression)."""
     data = pd.DataFrame(
         # We'll only pay attention to two of these fields
-        [[1440, 3041046.31] + list('ABCDEFGHIJKL')], columns=['line', 'amount'] + FINGERPRINT_COLS
+        [[1440, 3041046.31] + list('ABCDEFGHIJKLL')], columns=['line', 'amount'] + FINGERPRINT_COLS
     )
     result = load_sf133.fill_blank_sf133_lines(data)
     assert result['amount'][0] == 3041046.31
@@ -33,10 +33,10 @@ def test_fill_blank_sf133_lines():
         # rows. FINGERPRINT1 has rows for line numbers 1 and 2, while
         # FINGERPRINT2 has rows for line numbers 2 and 3. We want both to have
         # line numbers 1 through 3
-        [[1, 1] + list('FINGERPRINT1'),
-         [2, 2] + list('FINGERPRINT1'),
-         [2, 2] + list('FINGERPRINT2'),
-         [3, 3] + list('FINGERPRINT2')],
+        [[1, 1] + list('FINGERPRINT11'),
+         [2, 2] + list('FINGERPRINT11'),
+         [2, 2] + list('FINGERPRINT22'),
+         [3, 3] + list('FINGERPRINT22')],
         columns=['line', 'amount'] + FINGERPRINT_COLS
     )
     result = load_sf133.fill_blank_sf133_lines(data)
