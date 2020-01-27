1dd089e9a5656452bec32d4b73abbb0bfd32a425
Daniel Boos <boos_daniel@bah.com>
2020-01-27 13:09:47 -0500

    Making the sub_business_types column NULL if the DUNS business types are empty

--- dataactcore/scripts/raw_sql/populate_subaward_table_grants.sql
+++ dataactcore/scripts/raw_sql/populate_subaward_table_grants.sql
@@ -298,7 +298,10 @@ SELECT
          THEN fsrs_subgrant.awardee_address_zip
          ELSE NULL
     END AS "sub_legal_entity_foreign_posta",
-    array_to_string(subgrant_duns.business_types, ',') AS "sub_business_types",
+    CASE WHEN cardinality(duns.business_types_codes) > 0
+         THEN array_to_string(subgrant_duns.business_types, ',')
+         ELSE NULL
+    END AS "sub_business_types",
     fsrs_subgrant.principle_place_city AS "sub_place_of_perform_city_name",
     fsrs_subgrant.principle_place_state AS "sub_place_of_perform_state_code",
     fsrs_subgrant.principle_place_state_name AS "sub_place_of_perform_state_name",
