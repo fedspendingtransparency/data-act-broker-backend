0af7883980d4d49861be35580657b235670352fe
Daniel Boos <boos_daniel@bah.com>
2020-01-27 11:21:06 -0500

    unused imports

--- dataactbroker/scripts/backfill_fsrs_fields.py
+++ dataactbroker/scripts/backfill_fsrs_fields.py
@@ -6,7 +6,6 @@ import datetime
 from dataactcore.interfaces.db import GlobalDB
 from dataactcore.logging import configure_logging
 from dataactbroker.fsrs import config_valid, fetch_and_replace_batch, GRANT, PROCUREMENT, config_state_mappings
-from dataactcore.models.fsrs import FSRSProcurement, FSRSGrant
 from dataactvalidator.health_check import create_app
 
 logger = logging.getLogger(__name__)
