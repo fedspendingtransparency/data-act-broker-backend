94205b2503be3ea44f605ec4089bcbf7de5fcc5f
Daniel Boos <boos_daniel@bah.com>
2020-05-07 15:31:29 -0400

    B5 per DAIMS 2.0

--- dataactvalidator/config/sqlrules/b5_object_class_program_activity_1.sql
+++ dataactvalidator/config/sqlrules/b5_object_class_program_activity_1.sql
@@ -1,5 +1,6 @@
 -- File B (object class program activity): GrossOutlayAmountByProgramObjectClass_FYB =
--- GrossOutlaysUndeliveredOrdersPrepaidTotal_FYB + GrossOutlaysDeliveredOrdersPaidTotal_FYB.
+-- GrossOutlaysUndeliveredOrdersPrepaidTotal_FYB + GrossOutlaysDeliveredOrdersPaidTotal_FYB
+-- for the same date context and TAS/DEFC combination.
 SELECT
     row_number,
     gross_outlay_amount_by_pro_fyb,
@@ -8,6 +9,7 @@ SELECT
     COALESCE(gross_outlay_amount_by_pro_fyb, 0) - (COALESCE(gross_outlays_undelivered_fyb, 0) +
                                                    COALESCE(gross_outlays_delivered_or_fyb, 0)) AS "difference",
     display_tas AS "uniqueid_TAS",
+    disaster_emergency_fund_code AS "uniqueid_DEFC",
     program_activity_code AS "uniqueid_ProgramActivityCode",
     object_class AS "uniqueid_ObjectClass"
 FROM object_class_program_activity
