6efbd75dbebf7da4f4950ca9ebda98df6e358068
Daniel Boos <boos_daniel@bah.com>
2020-05-11 14:09:11 -0400

    Updating C17 per DAIMS 2.0

--- dataactvalidator/config/sqlrules/c17_award_financial.sql
+++ dataactvalidator/config/sqlrules/c17_award_financial.sql
@@ -1,4 +1,4 @@
--- TransactionObligatedAmount required on rows that do not include values for USSGL accounts balances or subtotals
+-- TransactionObligatedAmount and USSGL related balances and subtotals cannot be provided on the same row.
 SELECT
     row_number,
     transaction_obligated_amou,
@@ -8,24 +8,37 @@ SELECT
     uri AS "uniqueid_URI"
 FROM award_financial
 WHERE submission_id = {0}
-    AND transaction_obligated_amou IS NULL
-    AND COALESCE(ussgl480100_undelivered_or_cpe, 0) = 0
-    AND COALESCE(ussgl480100_undelivered_or_fyb, 0) = 0
-    AND COALESCE(ussgl480200_undelivered_or_cpe, 0) = 0
-    AND COALESCE(ussgl480200_undelivered_or_fyb, 0) = 0
-    AND COALESCE(ussgl483100_undelivered_or_cpe, 0) = 0
-    AND COALESCE(ussgl483200_undelivered_or_cpe, 0) = 0
-    AND COALESCE(ussgl487100_downward_adjus_cpe, 0) = 0
-    AND COALESCE(ussgl487200_downward_adjus_cpe, 0) = 0
-    AND COALESCE(ussgl488100_upward_adjustm_cpe, 0) = 0
-    AND COALESCE(ussgl488200_upward_adjustm_cpe, 0) = 0
-    AND COALESCE(ussgl490100_delivered_orde_cpe, 0) = 0
-    AND COALESCE(ussgl490100_delivered_orde_fyb, 0) = 0
-    AND COALESCE(ussgl490200_delivered_orde_cpe, 0) = 0
-    AND COALESCE(ussgl490800_authority_outl_cpe, 0) = 0
-    AND COALESCE(ussgl490800_authority_outl_fyb, 0) = 0
-    AND COALESCE(ussgl493100_delivered_orde_cpe, 0) = 0
-    AND COALESCE(ussgl497100_downward_adjus_cpe, 0) = 0
-    AND COALESCE(ussgl497200_downward_adjus_cpe, 0) = 0
-    AND COALESCE(ussgl498100_upward_adjustm_cpe, 0) = 0
-    AND COALESCE(ussgl498200_upward_adjustm_cpe, 0) = 0;
+    AND COALESCE(transaction_obligated_amou, 0) > 0
+    AND (
+        COALESCE(ussgl480100_undelivered_or_cpe, 0) > 0
+        OR COALESCE(ussgl480100_undelivered_or_fyb, 0) > 0
+        OR COALESCE(ussgl480200_undelivered_or_cpe, 0) > 0
+        OR COALESCE(ussgl480200_undelivered_or_fyb, 0) > 0
+        OR COALESCE(ussgl483100_undelivered_or_cpe, 0) > 0
+        OR COALESCE(ussgl483200_undelivered_or_cpe, 0) > 0
+        OR COALESCE(ussgl487100_downward_adjus_cpe, 0) > 0
+        OR COALESCE(ussgl487200_downward_adjus_cpe, 0) > 0
+        OR COALESCE(ussgl488100_upward_adjustm_cpe, 0) > 0
+        OR COALESCE(ussgl488200_upward_adjustm_cpe, 0) > 0
+        OR COALESCE(ussgl490100_delivered_orde_cpe, 0) > 0
+        OR COALESCE(ussgl490100_delivered_orde_fyb, 0) > 0
+        OR COALESCE(ussgl490200_delivered_orde_cpe, 0) > 0
+        OR COALESCE(ussgl490800_authority_outl_cpe, 0) > 0
+        OR COALESCE(ussgl490800_authority_outl_fyb, 0) > 0
+        OR COALESCE(ussgl493100_delivered_orde_cpe, 0) > 0
+        OR COALESCE(ussgl497100_downward_adjus_cpe, 0) > 0
+        OR COALESCE(ussgl497200_downward_adjus_cpe, 0) > 0
+        OR COALESCE(ussgl498100_upward_adjustm_cpe, 0) > 0
+        OR COALESCE(ussgl498200_upward_adjustm_cpe, 0) > 0
+        OR COALESCE(gross_outlay_amount_by_awa_cpe, 0) > 0
+        OR COALESCE(gross_outlay_amount_by_awa_fyb, 0) > 0
+        OR COALESCE(gross_outlays_delivered_or_cpe, 0) > 0
+        OR COALESCE(gross_outlays_delivered_or_fyb, 0) > 0
+        OR COALESCE(gross_outlays_undelivered_cpe, 0) > 0
+        OR COALESCE(gross_outlays_undelivered_fyb, 0) > 0
+        OR COALESCE(obligations_delivered_orde_cpe, 0) > 0
+        OR COALESCE(obligations_delivered_orde_fyb, 0) > 0
+        OR COALESCE(obligations_incurred_byawa_cpe, 0) > 0
+        OR COALESCE(obligations_undelivered_or_cpe, 0) > 0
+        OR COALESCE(obligations_undelivered_or_fyb, 0) > 0
+    );
