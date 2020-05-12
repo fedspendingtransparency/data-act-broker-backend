84199d0f517e312f47c7fb9414877058d4065855
Daniel Boos <boos_daniel@bah.com>
2020-05-12 15:32:24 -0400

    Missed updating b3 for uniqueid_DisasterEmergencyFundCode

--- dataactvalidator/config/sqlrules/b3_object_class_program_activity_1.sql
+++ dataactvalidator/config/sqlrules/b3_object_class_program_activity_1.sql
@@ -6,7 +6,7 @@ SELECT
     ussgl480100_undelivered_or_fyb,
     COALESCE(obligations_undelivered_or_fyb, 0) - COALESCE(ussgl480100_undelivered_or_fyb, 0) AS "difference",
     display_tas AS "uniqueid_TAS",
-    disaster_emergency_fund_code AS "uniqueid_DEFC",
+    disaster_emergency_fund_code AS "uniqueid_DisasterEmergencyFundCode",
     program_activity_code AS "uniqueid_ProgramActivityCode",
     object_class AS "uniqueid_ObjectClass"
 FROM object_class_program_activity
