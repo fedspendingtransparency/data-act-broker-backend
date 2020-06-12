78b7aee64ef3505bda2bfbe1dab0f412cdf5273f
alburde1 <Burdeyny_Alisa@bah.com>
2020-06-12 13:08:38 -0400

    Updating B21 to ignore lines that aren't 2190 or 3020 or have monetary amounts of 0 in SF133

--- dataactvalidator/config/sqlrules/b21_object_class_program_activity.sql
+++ dataactvalidator/config/sqlrules/b21_object_class_program_activity.sql
@@ -1,6 +1,11 @@
--- For monthly submissions, all TAS and COVID-19 Disaster Emergency Fund Code combinations required to be reported to
--- GTAS are reported in File B, with the exception of Financing Accounts (or when all monetary amounts are zero for the
--- TAS).
+-- For monthly submissions, all TAS and Disaster Emergency Fund Code combinations required to be reported to GTAS are
+-- reported in File B, with the exception of Financing Accounts (or when all obligation and outlay monetary amounts are
+-- zero for the TAS).
+WITH limited_lines_sf_133_b21_{0} AS
+    (SELECT *
+    FROM sf_133
+    WHERE line IN (2190, 3020)
+        AND amount <> 0)
 SELECT DISTINCT
     NULL AS row_number,
     sf.allocation_transfer_agency,
@@ -13,7 +18,7 @@ SELECT DISTINCT
     sf.disaster_emergency_fund_code,
     sf.display_tas AS "uniqueid_TAS",
     sf.disaster_emergency_fund_code AS "uniqueid_DisasterEmergencyFundCode"
-FROM sf_133 AS sf
+FROM limited_lines_sf_133_b21_{0} AS sf
     JOIN submission AS sub
         ON sf.period = sub.reporting_fiscal_period
         AND sf.fiscal_year = sub.reporting_fiscal_year
