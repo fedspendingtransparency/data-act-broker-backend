e482ce418aa3207f9da26fd45eb5e65b4ecee334
Daniel Boos <boos_daniel@bah.com>
2020-10-22 13:11:14 -0400

    Adding config to update the SQS endpoint url

--- dataactcore/aws/sqsHandler.py
+++ dataactcore/aws/sqsHandler.py
@@ -107,6 +107,10 @@ def sqs_queue(region_name=CONFIG_BROKER['aws_region'], queue_name=CONFIG_BROKER[
         return SQSMockQueue()
     else:
         # stuff that's in get_queue
-        sqs = boto3.resource('sqs', region_name)
+        endpoint_url_format = 'https://sqs.{}.amazonaws.com'
+        if CONFIG_BROKER['use_legacy_queue_endpoint']:
+            endpoint_url_format = 'https://{}.queue.amazonaws.com'
+        endpoint_url = endpoint_url_format.format(region_name)
+        sqs = boto3.resource('sqs', endpoint_url=endpoint_url)
         queue = sqs.get_queue_by_name(QueueName=queue_name)
         return queue
