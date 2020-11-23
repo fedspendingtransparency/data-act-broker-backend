731067ec2e10c7ac0e0f37806b19ede094a701de
Daniel Boos <boos_daniel@bah.com>
2020-11-23 16:17:29 -0500

    proxy type http, target host/port injection

--- dataactcore/utils/duns.py
+++ dataactcore/utils/duns.py
@@ -61,7 +61,9 @@ def get_client(ssh_key=None):
 
     https_proxy = os.environ.get('HTTPS_PROXY')
     if https_proxy:
-        para_proxy = paramiko.ProxyCommand('nc --proxy-type https --proxy {} %h %p'.format(https_proxy[7:]))
+        para_proxy = paramiko.ProxyCommand('nc --proxy-type http --proxy {} {} {}'.format(https_proxy[7:],
+                                                                                          connect_args['hostname'],
+                                                                                          '22'))
         connect_args['sock'] = para_proxy
 
     client.connect(**connect_args)
