874cbce91d3395f8df8e2c7cc2fb00d29e4729d8
Daniel Boos <boos_daniel@bah.com>
2021-05-06 17:03:12 -0400

    Took too much out

--- tests/unit/dataactbroker/test_update_historical_duns.py
+++ tests/unit/dataactbroker/test_update_historical_duns.py
@@ -92,6 +92,9 @@ def mock_get_duns_props_from_sam(duns_list):
             'high_comp_officer5_amount': '10'
         }
     }
+    for duns in duns_list:
+        if duns in duns_mappings:
+            results = results.append(pd.DataFrame(duns_mappings[duns]), sort=True)
     return results
 
 
