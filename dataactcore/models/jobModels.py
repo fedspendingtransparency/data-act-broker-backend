a63c95e0a879b4bbb7554bf6fe75f179571dcf07
Alisa Burdeyny <Burdeyny_Alisa@bah.com>
2021-05-11 13:43:07 -0400

    Creating a migration to add the 4 new columns
    + Also fixing a model that had the wrong type name in it (it seems that all of the actual tables have the correct name, no idea what happened here)

--- dataactcore/models/jobModels.py
+++ dataactcore/models/jobModels.py
@@ -342,6 +342,6 @@ class FileGeneration(Base):
                        default='D1', server_default='D1')
     file_path = Column(Text)
     is_cached_file = Column(Boolean, nullable=False, default=False)
-    file_format = Column(Enum('csv', 'txt', name='generation_file_format'), nullable=False, index=True,
+    file_format = Column(Enum('csv', 'txt', name='generation_file_formats'), nullable=False, index=True,
                          default='csv', server_default='csv')
     element_numbers = Column(Boolean, nullable=False, default=False, server_default="False")
