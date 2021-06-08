ba1eb67ffc1ef66e35bf7a16050673b8a726849b
Daniel Boos <boos_daniel@bah.com>
2021-06-08 14:11:39 -0400

    flake8 spaces

--- tests/unit/dataactvalidator/test_a33_appropriations_1.py
+++ tests/unit/dataactvalidator/test_a33_appropriations_1.py
@@ -261,6 +261,7 @@ def test_failure_populated_011_fr_entity_cgac(database):
     errors = number_of_errors(_FILE, database, models=[cgac, frec, sf1, ts1, ap], submission=submission)
     assert errors == 1
 
+
 def test_success_populated_011_fr_entity_frec(database):
     """ Tests that TAS for SF-133 are present in File A for FREC submissions """
     submission_id = randint(1000, 10000)
@@ -307,7 +308,7 @@ def test_financing_tas(database):
         reporting_fiscal_period=gtas_1.period,
         reporting_fiscal_year=gtas_1.fiscal_year,
         cgac_code=gtas_1.agency_identifier,
-        frec_code = None
+        frec_code=None
     )
 
     errors = number_of_errors(_FILE, database, models=[gtas_1, gtas_2, cars_1, cars_2], submission=submission_1)
@@ -325,7 +326,7 @@ def test_financing_tas(database):
         reporting_fiscal_period=gtas_3.period,
         reporting_fiscal_year=gtas_3.fiscal_year,
         cgac_code=gtas_3.agency_identifier,
-        frec_code = None
+        frec_code=None
     )
 
     errors = number_of_errors(_FILE, database, models=[gtas_3, gtas_4, cars_3, cars_4], submission=submission_2)
