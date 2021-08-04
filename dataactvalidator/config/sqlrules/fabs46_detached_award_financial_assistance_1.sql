6b9a19eebc823b69bb56566921a4c71a411d31d6
Alisa Burdeyny <Burdeyny_Alisa@bah.com>
2021-08-04 14:14:36 -0400

    Adding FABS46.1

---
+++ dataactvalidator/config/sqlrules/fabs46_detached_award_financial_assistance_1.sql
@@ -0,0 +1,11 @@
+-- IndirectCostFederalShareAmount must be blank for non-grants/non-cooperative agreements
+-- (AssistanceType = 06, 07, 08, 09, 10, or 11).
+SELECT
+    row_number,
+    indirect_federal_sharing,
+    assistance_type,
+    afa_generated_unique AS "uniqueid_AssistanceTransactionUniqueKey"
+FROM detached_award_financial_assistance
+WHERE submission_id = {0}
+    AND indirect_federal_sharing IS NOT NULL
+    AND COALESCE(assistance_type, '') IN ('06', '07', '08', '09', '10', '11');
