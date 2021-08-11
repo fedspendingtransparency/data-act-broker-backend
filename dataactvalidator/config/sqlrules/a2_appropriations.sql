fccf2ee5cb53f4c86fdabca6f152fb691ba531b5
Alisa Burdeyny <Burdeyny_Alisa@bah.com>
2021-08-11 10:12:53 -0400

    Fixing indent

--- dataactvalidator/config/sqlrules/a2_appropriations.sql
+++ dataactvalidator/config/sqlrules/a2_appropriations.sql
@@ -22,9 +22,9 @@ FROM appropriation AS approp
         AND sf.fiscal_year = sub.reporting_fiscal_year
         AND sf.line = 1902
 WHERE approp.submission_id = {0}
-AND COALESCE(total_budgetary_resources_cpe, 0) <>
-    COALESCE(budget_authority_appropria_cpe, 0) +
-    COALESCE(budget_authority_unobligat_fyb, 0) +
-    COALESCE(adjustments_to_unobligated_cpe, 0) +
-    COALESCE(other_budgetary_resources_cpe, 0) +
-    COALESCE(sf.amount, 0);
+    AND COALESCE(total_budgetary_resources_cpe, 0) <>
+        COALESCE(budget_authority_appropria_cpe, 0) +
+        COALESCE(budget_authority_unobligat_fyb, 0) +
+        COALESCE(adjustments_to_unobligated_cpe, 0) +
+        COALESCE(other_budgetary_resources_cpe, 0) +
+        COALESCE(sf.amount, 0);
