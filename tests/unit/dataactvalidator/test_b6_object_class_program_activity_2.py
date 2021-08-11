5252196c92d43d5cf4167f313fc14888951faa65
Daniel Boos <boos_daniel@bah.com>
2021-08-11 15:34:17 -0400

    B6 Updates

--- tests/unit/dataactvalidator/test_b6_object_class_program_activity_2.py
+++ tests/unit/dataactvalidator/test_b6_object_class_program_activity_2.py
@@ -8,7 +8,9 @@ _FILE = 'b6_object_class_program_activity_2'
 def test_column_headers(database):
     expected_subset = {'row_number', 'gross_outlays_undelivered_cpe', 'ussgl480200_undelivered_or_cpe',
                        'ussgl483200_undelivered_or_cpe', 'ussgl488200_upward_adjustm_cpe', 'difference', 'uniqueid_TAS',
-                       'uniqueid_DisasterEmergencyFundCode', 'uniqueid_ProgramActivityCode', 'uniqueid_ObjectClass'}
+                       'uniqueid_DisasterEmergencyFundCode', 'uniqueid_ProgramActivityCode',
+                       'uniqueid_ProgramActivityName', 'uniqueid_ObjectClass',
+                       'uniqueid_ByDirectReimbursableFundingSource'}
     actual = set(query_columns(_FILE, database))
     assert (actual & expected_subset) == expected_subset
 
