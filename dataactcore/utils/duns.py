c7e6ff236758f9db48d67a5a4e42510e8c75c34d
Daniel Boos <29313570+dpb-bah@users.noreply.github.com>
2021-10-07 15:26:28 -0400

    Adding additional check for response content

--- dataactcore/utils/duns.py
+++ dataactcore/utils/duns.py
@@ -602,7 +602,8 @@ def is_nonexistent_file_error(e):
             bool whether the error is a nonexistent file http error
     """
     no_file_msg = 'The File does not exist with the provided parameters.'
-    return e.response is not None and (json.loads(e.response.content).get('detail') == no_file_msg)
+    return (e.response is not None and e.response.get('content') is not None
+            and json.loads(e.response.content).get('detail') == no_file_msg)
 
 
 def give_up(e):
