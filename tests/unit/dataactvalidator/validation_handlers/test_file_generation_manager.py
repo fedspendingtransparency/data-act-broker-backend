5eda09d167f1f1ae15628841cd97a1c0839ae8b6
Alisa Burdeyny <Burdeyny_Alisa@bah.com>
2022-01-05 14:06:49 -0500

    Updating test factories to match the new types

--- tests/unit/dataactvalidator/validation_handlers/test_file_generation_manager.py
+++ tests/unit/dataactvalidator/validation_handlers/test_file_generation_manager.py
@@ -4,6 +4,7 @@ import re
 import pytest
 
 from datetime import datetime
+from distutils.util import strtobool
 from unittest.mock import Mock
 
 from dataactbroker.helpers import generation_helper
@@ -21,6 +22,34 @@ from tests.unit.dataactcore.factories.domain import TASFactory, SF133Factory, Du
 from tests.unit.dataactcore.factories.staging import (
     AwardFinancialAssistanceFactory, AwardProcurementFactory, DetachedAwardProcurementFactory,
     PublishedAwardFinancialAssistanceFactory)
+
+
+d1_booleans = ['small_business_competitive', 'city_local_government', 'county_local_government',
+               'inter_municipal_local_gove', 'local_government_owned', 'municipality_local_governm',
+               'school_district_local_gove', 'township_local_government', 'us_state_government',
+               'us_federal_government', 'federal_agency', 'federally_funded_research', 'us_tribal_government',
+               'foreign_government', 'community_developed_corpor', 'labor_surplus_area_firm',
+               'corporate_entity_not_tax_e', 'corporate_entity_tax_exemp', 'partnership_or_limited_lia',
+               'sole_proprietorship', 'small_agricultural_coopera', 'international_organization',
+               'us_government_entity', 'emerging_small_business', 'c8a_program_participant',
+               'sba_certified_8_a_joint_ve', 'dot_certified_disadvantage', 'self_certified_small_disad',
+               'historically_underutilized', 'small_disadvantaged_busine', 'the_ability_one_program',
+               'historically_black_college', 'c1862_land_grant_college', 'c1890_land_grant_college',
+               'c1994_land_grant_college', 'minority_institution', 'private_university_or_coll', 'school_of_forestry',
+               'state_controlled_instituti', 'tribal_college', 'veterinary_college', 'educational_institution',
+               'alaskan_native_servicing_i', 'community_development_corp', 'native_hawaiian_servicing',
+               'domestic_shelter', 'manufacturer_of_goods', 'hospital_flag', 'veterinary_hospital',
+               'hispanic_servicing_institu', 'foundation', 'woman_owned_business', 'minority_owned_business',
+               'women_owned_small_business', 'economically_disadvantaged', 'joint_venture_women_owned',
+               'joint_venture_economically', 'veteran_owned_business', 'service_disabled_veteran_o', 'contracts',
+               'grants', 'receives_contracts_and_gra', 'airport_authority', 'council_of_governments',
+               'housing_authorities_public', 'interstate_entity', 'planning_commission', 'port_authority',
+               'transit_authority', 'subchapter_s_corporation', 'limited_liability_corporat',
+               'foreign_owned_and_located', 'american_indian_owned_busi', 'alaskan_native_owned_corpo',
+               'indian_tribe_federally_rec', 'native_hawaiian_owned_busi', 'tribally_owned_business',
+               'asian_pacific_american_own', 'black_american_owned_busin', 'hispanic_american_owned_bu',
+               'native_american_owned_busi', 'subcontinent_asian_asian_i', 'other_minority_owned_busin',
+               'for_profit_organization', 'nonprofit_organization', 'other_not_for_profit_organ', 'us_local_government']
 
 
 def test_tas_concats():
@@ -439,6 +468,9 @@ def test_generate_awarding_d1(database):
                      'ordering_period_end_date', 'action_date', 'last_modified', 'solicitation_date']:
             expected1.append(re.sub(r"[-]", r"", str(dap_one.__dict__[value]))[0:8])
             expected2.append(re.sub(r"[-]", r"", str(dap_two.__dict__[value]))[0:8])
+        elif value in d1_booleans:
+            expected1.append(str(dap_one.__dict__[value])[0:1].lower() if dap_one.__dict__[value] is not None else None)
+            expected2.append(str(dap_two.__dict__[value])[0:1].lower() if dap_two.__dict__[value] is not None else None)
         else:
             expected1.append(str(dap_one.__dict__[value]))
             expected2.append(str(dap_two.__dict__[value]))
@@ -481,6 +513,9 @@ def test_generate_awarding_d1_alternate_headers(database):
                      'ordering_period_end_date', 'action_date', 'last_modified', 'solicitation_date']:
             expected1.append(re.sub(r"[-]", r"", str(dap_one.__dict__[value]))[0:8])
             expected2.append(re.sub(r"[-]", r"", str(dap_two.__dict__[value]))[0:8])
+        elif value in d1_booleans:
+            expected1.append(str(dap_one.__dict__[value])[0:1].lower() if dap_one.__dict__[value] is not None else None)
+            expected2.append(str(dap_two.__dict__[value])[0:1].lower() if dap_two.__dict__[value] is not None else None)
         else:
             expected1.append(str(dap_one.__dict__[value]))
             expected2.append(str(dap_two.__dict__[value]))
@@ -523,6 +558,9 @@ def test_generate_funding_d1(database):
                      'ordering_period_end_date', 'action_date', 'last_modified', 'solicitation_date']:
             expected1.append(re.sub(r"[-]", r"", str(dap_one.__dict__[value]))[0:8])
             expected2.append(re.sub(r"[-]", r"", str(dap_two.__dict__[value]))[0:8])
+        elif value in d1_booleans:
+            expected1.append(str(dap_one.__dict__[value])[0:1].lower() if dap_one.__dict__[value] is not None else None)
+            expected2.append(str(dap_two.__dict__[value])[0:1].lower() if dap_two.__dict__[value] is not None else None)
         else:
             expected1.append(str(dap_one.__dict__[value]))
             expected2.append(str(dap_two.__dict__[value]))
@@ -651,6 +689,9 @@ def test_generate_txt_d1(database):
                      'ordering_period_end_date', 'action_date', 'last_modified', 'solicitation_date']:
             expected1.append(re.sub(r"[-]", r"", str(dap_one.__dict__[value]))[0:8])
             expected2.append(re.sub(r"[-]", r"", str(dap_two.__dict__[value]))[0:8])
+        elif value in d1_booleans:
+            expected1.append(str(dap_one.__dict__[value])[0:1].lower() if dap_one.__dict__[value] is not None else None)
+            expected2.append(str(dap_two.__dict__[value])[0:1].lower() if dap_two.__dict__[value] is not None else None)
         else:
             expected1.append(str(dap_one.__dict__[value]))
             expected2.append(str(dap_two.__dict__[value]))
