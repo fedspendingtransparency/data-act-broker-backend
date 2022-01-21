3bb512c03ca256bfbe518e049e9507780e492ff6
Alisa Burdeyny <Burdeyny_Alisa@bah.com>
2022-01-21 13:52:38 -0500

    Fixing FABS31.5

--- dataactvalidator/config/sqlrules/fabs31_detached_award_financial_assistance_5.sql
+++ dataactvalidator/config/sqlrules/fabs31_detached_award_financial_assistance_5.sql
@@ -1,18 +1,15 @@
--- When ActionDate is after October 1, 2010 and ActionType = A, AwardeeOrRecipientUEI and AwardeeOrRecipientDUNS
--- should (when provided) have an active registration in SAM as of the ActionDate.
+-- When ActionDate is after October 1, 2010 and ActionType = A, AwardeeOrRecipientUEI should (when provided) have an
+-- active registration in SAM as of the ActionDate.
 WITH detached_award_financial_assistance_fabs31_5_{0} AS
     (SELECT
         row_number,
         action_date,
         action_type,
-        awardee_or_recipient_uniqu,
         uei,
         afa_generated_unique
     FROM detached_award_financial_assistance AS dafa
     WHERE submission_id = {0}
-        AND (COALESCE(awardee_or_recipient_uniqu, '') <> ''
-            OR COALESCE(uei, '') <> ''
-        )
+        AND COALESCE(uei, '') <> ''
         AND UPPER(action_type) = 'A'
         AND (CASE WHEN is_date(COALESCE(action_date, '0'))
              THEN CAST(action_date AS DATE)
@@ -22,39 +19,14 @@ SELECT
     dafa.row_number,
     dafa.action_date,
     dafa.action_type,
-    dafa.awardee_or_recipient_uniqu,
     dafa.uei,
     dafa.afa_generated_unique AS "uniqueid_AssistanceTransactionUniqueKey"
 FROM detached_award_financial_assistance_fabs31_5_{0} AS dafa
-WHERE COALESCE(dafa.awardee_or_recipient_uniqu, '') = ''
-    AND NOT EXISTS (
+WHERE NOT EXISTS (
         SELECT 1
         FROM duns
         WHERE (
             UPPER(dafa.uei) = UPPER(duns.uei)
-            AND (CASE WHEN is_date(COALESCE(dafa.action_date, '0'))
-                 THEN CAST(dafa.action_date AS DATE)
-                 END) >= CAST(duns.registration_date AS DATE)
-            AND (CASE WHEN is_date(COALESCE(dafa.action_date, '0'))
-                 THEN CAST(dafa.action_date AS DATE)
-                 END) < CAST(duns.expiration_date AS DATE)
-        )
-    )
-UNION
-SELECT
-    dafa.row_number,
-    dafa.action_date,
-    dafa.action_type,
-    dafa.awardee_or_recipient_uniqu,
-    dafa.uei,
-    dafa.afa_generated_unique AS "uniqueid_AssistanceTransactionUniqueKey"
-FROM detached_award_financial_assistance_fabs31_5_{0} AS dafa
-WHERE COALESCE(dafa.awardee_or_recipient_uniqu, '') <> ''
-    AND NOT EXISTS (
-        SELECT 1
-        FROM duns
-        WHERE (
-            dafa.awardee_or_recipient_uniqu = duns.awardee_or_recipient_uniqu
             AND (CASE WHEN is_date(COALESCE(dafa.action_date, '0'))
                  THEN CAST(dafa.action_date AS DATE)
                  END) >= CAST(duns.registration_date AS DATE)
--- tests/unit/dataactvalidator/test_fabs31_detached_award_financial_assistance_5.py
+++ tests/unit/dataactvalidator/test_fabs31_detached_award_financial_assistance_5.py
@@ -6,70 +6,39 @@ _FILE = 'fabs31_detached_award_financial_assistance_5'
 
 
 def test_column_headers(database):
-    expected_subset = {'row_number', 'action_date', 'action_type', 'awardee_or_recipient_uniqu',
-                       'uei', 'uniqueid_AssistanceTransactionUniqueKey'}
+    expected_subset = {'row_number', 'action_date', 'action_type', 'uei', 'uniqueid_AssistanceTransactionUniqueKey'}
     actual = set(query_columns(_FILE, database))
     assert expected_subset == actual
 
 
 def test_pubished_date_success(database):
+    """ Test success for when ActionDate is after October 1, 2010 and ActionType = A, AwardeeOrRecipientUEI should
+        (when provided) have an active registration in SAM as of the ActionDate.
     """
-        Test success for when ActionDate is after October 1, 2010 and ActionType = A, AwardeeOrRecipientUEI and
-        AwardeeOrRecipientDUNS should (when provided) have an active registration in SAM as of the ActionDate.
-    """
-    duns_1 = DUNS(awardee_or_recipient_uniqu='111111111', uei='111111111111', registration_date='01/01/2017',
-                  expiration_date='01/01/2018')
-    duns_2 = DUNS(awardee_or_recipient_uniqu='222222222', uei='22222222222e', registration_date='01/01/2018',
-                  expiration_date='01/01/2019')
-    det_award_1 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu='111111111', uei=None,
-                                                          action_type='a', action_date='06/22/2017',
-                                                          correction_delete_indicatr='')
-    det_award_2 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu='', uei='22222222222E',
-                                                          action_type='a', action_date='06/22/2018',
-                                                          correction_delete_indicatr='')
-    # Note: if both duns and uei are both provided, it will check against duns only
-    #       they _should_ both point to the same recipient and FABS 31.8 will enforce this
-    det_award_3 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu='111111111', uei='22222222222e',
-                                                          action_type='a', action_date='06/22/2017',
+    duns = DUNS(uei='11111111111E', registration_date='01/01/2017', expiration_date='01/01/2018')
+    det_award_1 = DetachedAwardFinancialAssistanceFactory(uei=duns.uei, action_type='a', action_date='06/22/2017',
                                                           correction_delete_indicatr='')
     # Ignore different action type
-    det_award_4 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu='111111111', uei='111111111111',
-                                                          action_type='B', action_date='06/20/2017',
+    det_award_2 = DetachedAwardFinancialAssistanceFactory(uei='12345', action_type='B', action_date='06/20/2019',
                                                           correction_delete_indicatr='')
     # Ignore Before October 1, 2010
-    det_award_5 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu='111111111', uei='111111111111',
-                                                          action_type='a', action_date='09/30/2010',
+    det_award_3 = DetachedAwardFinancialAssistanceFactory(uei='12345', action_type='a', action_date='09/30/2010',
                                                           correction_delete_indicatr=None)
     # Ignore correction delete indicator of D
-    det_award_6 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu='111111111', uei='111111111111',
-                                                          action_type='A', action_date='06/20/2020',
+    det_award_4 = DetachedAwardFinancialAssistanceFactory(uei='12345', action_type='A', action_date='06/20/2020',
                                                           correction_delete_indicatr='d')
 
-    errors = number_of_errors(_FILE, database, models=[duns_1, duns_2, det_award_1, det_award_2, det_award_3,
-                                                       det_award_4, det_award_5, det_award_6])
+    errors = number_of_errors(_FILE, database, models=[duns, det_award_1, det_award_2, det_award_3, det_award_4])
     assert errors == 0
 
 
 def test_pubished_date_failure(database):
+    """ Test failure for when ActionDate is after October 1, 2010 and ActionType = A, AwardeeOrRecipientUEI should
+        (when provided) have an active registration in SAM as of the ActionDate.
     """
-        Test failure for when ActionDate is after October 1, 2010 and ActionType = A, AwardeeOrRecipientUEI and
-        AwardeeOrRecipientDUNS should (when provided) have an active registration in SAM as of the ActionDate.
-    """
-    duns_1 = DUNS(awardee_or_recipient_uniqu='111111111', uei='111111111111', registration_date='01/01/2017',
-                  expiration_date='01/01/2018')
-    duns_2 = DUNS(awardee_or_recipient_uniqu='222222222', uei='222222222222', registration_date='01/01/2018',
-                  expiration_date='01/01/2019')
-    det_award_1 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu='111111111', uei='',
-                                                          action_type='A', action_date='06/20/2020',
-                                                          correction_delete_indicatr='')
-    det_award_2 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu=None, uei='111111111111',
-                                                          action_type='A', action_date='06/20/2020',
-                                                          correction_delete_indicatr='')
-    # Note: if both duns and uei are both provided, it will check against duns only
-    #       they _should_ both point to the same recipient and FABS 31.8 will enforce this
-    det_award_3 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu='111111111111', uei='222222222222',
-                                                          action_type='A', action_date='06/20/2018',
+    duns= DUNS(uei='111111111111', registration_date='01/01/2017', expiration_date='01/01/2018')
+    det_award_1 = DetachedAwardFinancialAssistanceFactory(uei='12345', action_type='A', action_date='06/20/2020',
                                                           correction_delete_indicatr='')
 
-    errors = number_of_errors(_FILE, database, models=[duns_1, duns_2, det_award_1, det_award_2, det_award_3])
-    assert errors == 3
+    errors = number_of_errors(_FILE, database, models=[duns, det_award_1])
+    assert errors == 1
