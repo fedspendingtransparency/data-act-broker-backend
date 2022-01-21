64075eb1c475b5e263760a9d96b9c136dfd3e555
Alisa Burdeyny <Burdeyny_Alisa@bah.com>
2022-01-21 11:51:08 -0500

    Updating FABS31.1

--- dataactvalidator/config/sqlrules/fabs31_detached_award_financial_assistance_1.sql
+++ dataactvalidator/config/sqlrules/fabs31_detached_award_financial_assistance_1.sql
@@ -1,11 +1,10 @@
--- AwardeeOrRecipientDUNS and AwardeeOrRecipientUEI Fields must be blank for aggregate and PII-redacted non-aggregate
--- records (RecordType = 1 or 3, regardless of the BusinessTypes value) and individual recipients (BusinessTypes
--- includes 'P', regardless of the RecordType value).
+-- AwardeeOrRecipientUEI Fields must be blank for aggregate and PII-redacted non-aggregate records (RecordType = 1 or 3,
+-- regardless of the BusinessTypes value) and individual recipients (BusinessTypes includes 'P', regardless of the
+-- RecordType value).
 SELECT
     row_number,
     record_type,
     business_types,
-    awardee_or_recipient_uniqu,
     uei,
     afa_generated_unique AS "uniqueid_AssistanceTransactionUniqueKey"
 FROM detached_award_financial_assistance
@@ -13,7 +12,5 @@ WHERE submission_id = {0}
     AND (record_type IN (1, 3)
         OR UPPER(business_types) LIKE '%%P%%'
     )
-    AND (COALESCE(awardee_or_recipient_uniqu, '') <> ''
-        OR COALESCE(uei, '') <> ''
-    )
+    AND COALESCE(uei, '') <> ''
     AND UPPER(COALESCE(correction_delete_indicatr, '')) <> 'D';
