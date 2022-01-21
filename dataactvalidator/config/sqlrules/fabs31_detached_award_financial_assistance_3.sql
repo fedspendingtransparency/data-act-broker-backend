f0715a6eb944d1eaa9987b25fedaffc25a89a459
Alisa Burdeyny <Burdeyny_Alisa@bah.com>
2022-01-21 12:29:44 -0500

    Fixing FABS31.3

--- dataactvalidator/config/sqlrules/fabs31_detached_award_financial_assistance_3.sql
+++ dataactvalidator/config/sqlrules/fabs31_detached_award_financial_assistance_3.sql
@@ -1,18 +1,10 @@
 -- When AwardeeOrRecipientUEI is provided, it must be twelve characters.
--- When AwardeeOrRecipientDUNS is provided, it must be nine digits.
 SELECT
     row_number,
-    awardee_or_recipient_uniqu,
     uei,
     afa_generated_unique AS "uniqueid_AssistanceTransactionUniqueKey"
 FROM detached_award_financial_assistance AS dafa
 WHERE submission_id = {0}
-    AND (
-        (COALESCE(awardee_or_recipient_uniqu, '') <> ''
-            AND awardee_or_recipient_uniqu !~ '^\d{{9}}$'
-        )
-        OR (COALESCE(uei, '') <> ''
-            AND uei !~ '^[a-zA-Z\d]{{12}}$'
-        )
-    )
+    AND COALESCE(uei, '') <> ''
+    AND uei !~ '^[a-zA-Z\d]{{12}}$'
     AND UPPER(COALESCE(correction_delete_indicatr, '')) <> 'D';
--- tests/unit/dataactvalidator/test_fabs31_detached_award_financial_assistance_3.py
+++ tests/unit/dataactvalidator/test_fabs31_detached_award_financial_assistance_3.py
@@ -5,58 +5,29 @@ _FILE = 'fabs31_detached_award_financial_assistance_3'
 
 
 def test_column_headers(database):
-    expected_subset = {'row_number', 'awardee_or_recipient_uniqu', 'uei', 'uniqueid_AssistanceTransactionUniqueKey'}
+    expected_subset = {'row_number', 'uei', 'uniqueid_AssistanceTransactionUniqueKey'}
     actual = set(query_columns(_FILE, database))
     assert expected_subset == actual
 
 
 def test_success(database):
-    """
-        Test success for when AwardeeOrRecipientUEI is provided, it must be twelve characters.
-        When AwardeeOrRecipientDUNS is provided, it must be nine digits.
-    """
-    det_award_1 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu='000000001', uei='123456789aBc',
-                                                          correction_delete_indicatr='')
-    det_award_2 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu='', uei='123456789aBc',
-                                                          correction_delete_indicatr='c')
-    det_award_3 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu='103493922', uei='abc000000000',
-                                                          correction_delete_indicatr='C')
-    det_award_4 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu='100000000', uei='000000000000',
-                                                          correction_delete_indicatr=None)
-    det_award_5 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu='100000000', uei=None,
-                                                          correction_delete_indicatr=None)
-    det_award_6 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu='', uei='000000000000',
-                                                          correction_delete_indicatr=None)
-    det_award_7 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu='', uei=None,
-                                                          correction_delete_indicatr=None)
+    """ Test success for when AwardeeOrRecipientUEI is provided, it must be twelve characters. """
+    det_award_1 = DetachedAwardFinancialAssistanceFactory(uei='123456789aBc', correction_delete_indicatr='')
+    det_award_2 = DetachedAwardFinancialAssistanceFactory(uei='abc000000000', correction_delete_indicatr='C')
+    det_award_3 = DetachedAwardFinancialAssistanceFactory(uei='000000000000', correction_delete_indicatr=None)
+    det_award_4 = DetachedAwardFinancialAssistanceFactory(uei=None, correction_delete_indicatr=None)
 
     # Ignore correction delete indicator of D
-    det_award_8 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu='00000000A', uei='2',
-                                                          correction_delete_indicatr='d')
+    det_award_5 = DetachedAwardFinancialAssistanceFactory(uei='2', correction_delete_indicatr='d')
 
-    errors = number_of_errors(_FILE, database, models=[det_award_1, det_award_2, det_award_3, det_award_4,
-                                                       det_award_5, det_award_6, det_award_7, det_award_8])
+    errors = number_of_errors(_FILE, database, models=[det_award_1, det_award_2, det_award_3, det_award_4, det_award_5])
     assert errors == 0
 
 
 def test_failure(database):
-    """
-        Test failure for when AwardeeOrRecipientUEI is provided, it must be twelve characters.
-        When AwardeeOrRecipientDUNS is provided, it must be nine digits.
-    """
-    det_award_1 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu='00000000A', uei='123456789aBc',
-                                                          correction_delete_indicatr='')
-    det_award_2 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu='ABCDEFGHI', uei='1234567s89aBc',
-                                                          correction_delete_indicatr='c')
-    det_award_3 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu='AAA', uei='123456789aBc',
-                                                          correction_delete_indicatr='C')
-    det_award_4 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu='AAAAAAAAAAA', uei='123456789aBc',
-                                                          correction_delete_indicatr=None)
-    det_award_5 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu='123456789', uei='1',
-                                                          correction_delete_indicatr=None)
-    det_award_6 = DetachedAwardFinancialAssistanceFactory(awardee_or_recipient_uniqu='123456789', uei='123456789ABcd',
-                                                          correction_delete_indicatr=None)
+    """ Test failure for when AwardeeOrRecipientUEI is provided, it must be twelve characters. """
+    det_award_1 = DetachedAwardFinancialAssistanceFactory(uei='2', correction_delete_indicatr='')
+    det_award_2 = DetachedAwardFinancialAssistanceFactory(uei='1234567s89aBc', correction_delete_indicatr='c')
 
-    errors = number_of_errors(_FILE, database, models=[det_award_1, det_award_2, det_award_3, det_award_4, det_award_5,
-                                                       det_award_6])
-    assert errors == 6
+    errors = number_of_errors(_FILE, database, models=[det_award_1, det_award_2])
+    assert errors == 2
