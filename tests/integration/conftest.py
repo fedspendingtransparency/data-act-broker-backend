f214cc850acccf9beaf7f7dd552ab5dff959e4c8
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-22 09:53:05 +0800

    Delete tests directory

--- tests/integration/conftest.py
+++
@@ -1,9 +0,0 @@
-from unittest.mock import Mock
-
-from dataactbroker.handlers.aws.sesEmail import SesEmail
-
-
-def pytest_runtest_setup(item):
-    """Big-ol hack. For the sake of our integration tests, mock out SesEmail.
-    Ideally, we can be pin-point our mocks in the future"""
-    SesEmail.send = Mock()
