8af82b10f1298aa65a051a0aee47ad01478afe9e
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-22 09:52:04 +0800

    Delete dataactvalidator directory

--- dataactvalidator/config/sqlrules/b12_award_financial_2.sql
+++
@@ -1,9 +0,0 @@
--- Valid reimbursable flag indicator values are "r" and "d"
-SELECT
-    row_number,
-    by_direct_reimbursable_fun,
-    display_tas AS "uniqueid_TAS",
-    object_class AS "uniqueid_ObjectClass"
-FROM award_financial
-WHERE submission_id = {0}
-    AND COALESCE(UPPER(by_direct_reimbursable_fun), '') NOT IN ('', 'R', 'D');
--- dataactvalidator/config/sqlrules/b12_object_class_program_activity_2.sql
+++
@@ -1,9 +0,0 @@
--- Valid reimbursable flag indicator values are "R" and "D"
-SELECT
-    row_number,
-    by_direct_reimbursable_fun,
-    display_tas AS "uniqueid_TAS",
-    object_class AS "uniqueid_ObjectClass"
-FROM object_class_program_activity
-WHERE submission_id = {0}
-    AND COALESCE(UPPER(by_direct_reimbursable_fun), '') NOT IN ('', 'R', 'D');
