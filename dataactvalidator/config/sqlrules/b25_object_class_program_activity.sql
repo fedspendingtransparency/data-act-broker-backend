7cc54cafd507796b1fe0f92f49ae00fffc434648
Alisa Burdeyny <Burdeyny_Alisa@bah.com>
2022-12-21 15:02:52 -0500

    Commit to restart travis

--- dataactvalidator/config/sqlrules/b25_object_class_program_activity.sql
+++ dataactvalidator/config/sqlrules/b25_object_class_program_activity.sql
@@ -1,5 +1,5 @@
 -- ObligationsIncurredByProgramObjectClass_CPE = value for GTAS SF 133 line #2190 for the same reporting period for the
--- TAS and DEFC combination
+-- TAS and DEFC combination.
 WITH object_class_program_activity_b25_{0} AS
     (SELECT submission_id,
         row_number,
