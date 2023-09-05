a69f106070f90ec0f6093d26813992d22100d577
Daniel Boos <29313570+dpb-bah@users.noreply.github.com>
2023-09-05 12:47:16 -0400

    Consistent order ON COMMIT DROP

--- dataactcore/scripts/raw_sql/link_broken_subaward_grants.sql
+++ dataactcore/scripts/raw_sql/link_broken_subaward_grants.sql
@@ -1,4 +1,4 @@
-CREATE TEMPORARY TABLE ON COMMIT DROP unlinked_subs AS
+CREATE TEMPORARY TABLE unlinked_subs ON COMMIT DROP AS
     (
         SELECT id,
             prime_id,
@@ -10,7 +10,7 @@ CREATE TEMPORARY TABLE ON COMMIT DROP unlinked_subs AS
             AND subaward.subaward_type = 'sub-grant'
     );
 
-CREATE TEMPORARY TABLE ON COMMIT DROP aw_pf AS
+CREATE TEMPORARY TABLE aw_pf ON COMMIT DROP AS
     (SELECT pf.fain AS fain,
         pf.award_description AS award_description,
         pf.record_type AS record_type,
