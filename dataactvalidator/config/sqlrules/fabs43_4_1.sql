1be21ae92a52d7ecac45320d81df0be64ac69b10
Alisa Burdeyny <Burdeyny_Alisa@bah.com>
2023-12-01 12:43:51 -0500

    Splitting FABS43.4 into 2 parts

---
+++ dataactvalidator/config/sqlrules/fabs43_4_1.sql
@@ -0,0 +1,48 @@
+-- If PrimaryPlaceOfPerformanceCongressionalDistrict is provided, it must be valid in the state or territory indicated
+-- by the PrimaryPlaceOfPerformanceCode. Data with an ActionDate before 20230103 (the date the 2020 redistricting took
+-- full effect) will be evaluated based on the USPS source data from prior to the 2020 redistricting. The
+-- PrimaryPlaceOfPerformanceCongressionalDistrict may be 90 if the state has more than one congressional district or
+-- PrimaryPlaceOfPerformanceCode is 00*****.
+WITH fabs43_4_1_{0} AS
+    (SELECT submission_id,
+        row_number,
+        place_of_performance_code,
+        place_of_performance_congr,
+        correction_delete_indicatr,
+        action_date,
+        afa_generated_unique
+    FROM fabs
+    WHERE submission_id = {0})
+SELECT
+    row_number,
+    place_of_performance_code,
+    place_of_performance_congr,
+    action_date,
+    afa_generated_unique AS "uniqueid_AssistanceTransactionUniqueKey"
+FROM fabs43_4_1_{0} AS fabs
+WHERE COALESCE(place_of_performance_code, '') <> ''
+    AND CASE WHEN COALESCE(place_of_performance_congr, '') <> ''
+            THEN ((place_of_performance_congr <> '90'
+                    AND row_number NOT IN (
+                        SELECT DISTINCT sub_fabs.row_number
+                        FROM fabs43_4_1_{0} AS sub_fabs
+                        JOIN state_congressional AS sc_1
+                            ON UPPER(LEFT(sub_fabs.place_of_performance_code, 2)) = sc_1.state_code
+                            AND sub_fabs.place_of_performance_congr = sc_1.congressional_district_no
+                            AND COALESCE(sc_1.census_year, 2010) <= 2010
+                        )
+                )
+                OR (place_of_performance_congr = '90'
+                    AND place_of_performance_code <> '00*****'
+                    AND (SELECT COUNT(DISTINCT sc_2.congressional_district_no)
+                        FROM state_congressional AS sc_2
+                        WHERE UPPER(LEFT(fabs.place_of_performance_code, 2)) = sc_2.state_code
+                            AND COALESCE(sc_2.census_year, 2010) <= 2010) < 2
+                )
+            )
+            ELSE FALSE
+        END
+    AND (CASE WHEN is_date(COALESCE(action_date, '0'))
+              THEN CAST(action_date AS DATE)
+              END) < CAST('01/03/2023' AS DATE)
+    AND UPPER(COALESCE(correction_delete_indicatr, '')) <> 'D';
