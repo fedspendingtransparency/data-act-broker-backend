677304ae8ec48fab43437349d888ea40d97c39a2
Alisa Burdeyny <Burdeyny_Alisa@bah.com>
2024-02-12 14:45:14 -0500

    Updating tests

--- tests/unit/dataactcore/scripts/test_get_fsrs_updates.py
+++ tests/unit/dataactcore/scripts/test_get_fsrs_updates.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, datetime, timedelta
 from decimal import Decimal
 
 from dataactcore.scripts.pipeline.get_fsrs_updates import get_award_updates
@@ -8,8 +8,8 @@ from tests.unit.dataactcore.factories.staging import PublishedFABSFactory
 
 def test_ignore_earlier_date(database):
     sess = database.session
-    pf1 = PublishedFABSFactory(modified_at=date(2024, 2, 1), record_type=2, assistance_type='04', fain='tooearly')
-    pf2 = PublishedFABSFactory(modified_at=date(2024, 2, 4), record_type=2, assistance_type='04', fain='justright')
+    pf1 = PublishedFABSFactory(updated_at=date(2024, 2, 1), record_type=2, assistance_type='04', fain='tooearly')
+    pf2 = PublishedFABSFactory(updated_at=date(2024, 2, 4), record_type=2, assistance_type='04', fain='justright')
     sess.add_all([pf1, pf2])
     sess.commit()
 
@@ -20,44 +20,12 @@ def test_ignore_earlier_date(database):
     assert result_list[0].federal_award_id == 'justright'
 
 
-def test_ignore_assistance_type(database):
-    sess = database.session
-    pf1 = PublishedFABSFactory(modified_at=date(2024, 2, 4), record_type=2, assistance_type='04', fain='rightassist')
-    pf2 = PublishedFABSFactory(modified_at=date(2024, 2, 4), record_type=2, assistance_type='01', fain='badassist')
-    sess.add_all([pf1, pf2])
-    sess.commit()
-
-    results = get_award_updates('02/03/2024')
-    result_list = results.all()
-
-    assert len(result_list) == 1
-    assert result_list[0].federal_award_id == 'rightassist'
-
-
-def test_ignore_record_type(database):
-    sess = database.session
-    pf1 = PublishedFABSFactory(modified_at=date(2024, 2, 4), record_type=2, assistance_type='04', fain='rightrecord')
-    pf2 = PublishedFABSFactory(modified_at=date(2024, 2, 4), record_type=1, assistance_type='04', fain='badrecord')
-    sess.add_all([pf1, pf2])
-    sess.commit()
-
-    results = get_award_updates('02/03/2024')
-    result_list = results.all()
-
-    assert len(result_list) == 1
-    assert result_list[0].federal_award_id == 'rightrecord'
-
-
 def test_status(database):
     sess = database.session
-    pf1 = PublishedFABSFactory(modified_at=date(2024, 2, 4), record_type=2, assistance_type='04', fain='activerecord',
-                               is_active=False)
-    pf2 = PublishedFABSFactory(modified_at=date(2024, 1, 4), record_type=2, assistance_type='04', fain='activerecord',
-                               is_active=True)
-    pf3 = PublishedFABSFactory(modified_at=date(2024, 2, 4), record_type=2, assistance_type='04', fain='inactiverecord',
-                               is_active=False)
-    pf4 = PublishedFABSFactory(modified_at=date(2024, 2, 1), record_type=2, assistance_type='04', fain='inactiverecord',
-                               is_active=False)
+    pf1 = PublishedFABSFactory(updated_at=date(2024, 2, 4), afa_generated_unique='activerecord', is_active=False)
+    pf2 = PublishedFABSFactory(updated_at=date(2024, 1, 4), afa_generated_unique='activerecord', is_active=True)
+    pf3 = PublishedFABSFactory(updated_at=date(2024, 2, 4), afa_generated_unique='inactiverecord', is_active=False)
+    pf4 = PublishedFABSFactory(updated_at=date(2024, 2, 1), afa_generated_unique='inactiverecord', is_active=False)
     sess.add_all([pf1, pf2, pf3, pf4])
     sess.commit()
 
@@ -72,23 +40,31 @@ def test_status(database):
             assert result.status == 'inactive'
 
 
+def test_ignore_duplicates(database):
+    sess = database.session
+    pf1 = PublishedFABSFactory(updated_at=datetime.now() - timedelta(hours=1), afa_generated_unique='activerecord',
+                               is_active=False, award_description='older entry')
+    pf2 = PublishedFABSFactory(updated_at=datetime.now() + timedelta(minutes=1), afa_generated_unique='activerecord',
+                               is_active=True, award_description='newer entry')
+    sess.add_all([pf1, pf2])
+    sess.commit()
+
+    results = get_award_updates(datetime.now().strftime('%m/%d/%y'))
+    result_list = results.all()
+
+    assert len(result_list) == 1
+    assert result_list[0].project_description == 'newer entry'
+
+
 def test_grouping(database):
     sess = database.session
-    pf1 = PublishedFABSFactory(modified_at=date(2024, 2, 1), record_type=2, assistance_type='04', fain='record',
-                               period_of_performance_star=date(2000, 3, 22),
-                               period_of_performance_curr=date(2025, 9, 5),
+    pf1 = PublishedFABSFactory(updated_at=date(2024, 2, 1), unique_award_key='record', action_date=date(2000, 3, 22),
                                is_active=True, federal_action_obligation=25.5)
-    pf2 = PublishedFABSFactory(modified_at=date(2024, 2, 1), record_type=2, assistance_type='04', fain='record',
-                               period_of_performance_star=date(2003, 3, 22),
-                               period_of_performance_curr=date(2035, 8, 5),
+    pf2 = PublishedFABSFactory(updated_at=date(2024, 2, 1), unique_award_key='record', action_date=date(2003, 3, 22),
                                is_active=True, federal_action_obligation=30.25)
-    pf3 = PublishedFABSFactory(modified_at=date(2024, 2, 4), record_type=2, assistance_type='04', fain='record',
-                               period_of_performance_star=date(2023, 3, 22),
-                               period_of_performance_curr=date(2025, 9, 5),
+    pf3 = PublishedFABSFactory(updated_at=date(2024, 2, 4), unique_award_key='record', action_date=date(2023, 3, 22),
                                is_active=True, federal_action_obligation=4.3)
-    pf4 = PublishedFABSFactory(modified_at=date(2024, 1, 4), record_type=2, assistance_type='04', fain='record',
-                               period_of_performance_star=date(2023, 3, 22),
-                               period_of_performance_curr=date(2025, 9, 5),
+    pf4 = PublishedFABSFactory(updated_at=date(2024, 1, 4), unique_award_key='record', action_date=date(2023, 3, 22),
                                is_active=False, federal_action_obligation=100)
     sess.add_all([pf1, pf2, pf3, pf4])
     sess.commit()
@@ -97,6 +73,5 @@ def test_grouping(database):
     result_list = results.all()
 
     assert len(result_list) == 1
-    assert result_list[0].starting_date == '2000-03-22'
-    assert result_list[0].ending_date == '2035-08-05'
-    assert result_list[0].total_fed_funding_amount == Decimal('60.05')
+    assert result_list[0].base_obligation_date == '2000-03-22'
+    assert result_list[0].last_modified_date == '2024-02-04'
