18f1e306fcdbd48b4e5f5190c92fbbf32aa6c9ca
Alisa Burdeyny <Burdeyny_Alisa@bah.com>
2024-03-01 10:56:10 -0500

    Small test update

--- tests/unit/dataactcore/scripts/test_generate_full_sam_fabs_export.py
+++ tests/unit/dataactcore/scripts/test_generate_full_sam_fabs_export.py
@@ -30,18 +30,22 @@ def test_cumulative_sum(database):
                                original_loan_subsidy_cost=9.5, federal_action_obligation=11, unique_award_key='abc')
     pf4 = PublishedFABSFactory(action_date=date(2000, 1, 30), is_active=True, fain='firstdate', assistance_type='06',
                                original_loan_subsidy_cost=13, federal_action_obligation=5, unique_award_key='abc')
-    sess.add_all([pf1, pf2, pf3, pf4])
+    pf5 = PublishedFABSFactory(action_date=date(2000, 1, 30), is_active=True, fain='otherkey', assistance_type='08',
+                               original_loan_subsidy_cost=5, federal_action_obligation=9, unique_award_key='def')
+    sess.add_all([pf1, pf2, pf3, pf4, pf5])
     sess.commit()
 
     results = get_awards()
     result_list = results.all()
 
-    assert len(result_list) == 4
+    assert len(result_list) == 5
     for result in result_list:
         if result.federal_award_id == 'firstdate':
             assert result.total_fed_funding_amount == 5
         elif result.federal_award_id == 'seconddate':
             # If there are 2 in the same date they should get rolled up together
             assert result.total_fed_funding_amount == 65
-        else:
+        elif result.federal_award_id == 'thirddate':
             assert result.total_fed_funding_amount == 76
+        else:
+            assert result.total_fed_funding_amount == 5
