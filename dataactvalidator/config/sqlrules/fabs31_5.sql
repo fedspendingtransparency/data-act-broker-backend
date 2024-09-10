1bfd2ae968a35cac35da8365062c755a789fc9e5
Daniel Boos <29313570+dpb-bah@users.noreply.github.com>
2024-09-10 15:02:32 -0400

    Removing leftover check in FABS 31.5, removing extra redundant select

--- dataactvalidator/config/sqlrules/fabs31_5.sql
+++ dataactvalidator/config/sqlrules/fabs31_5.sql
@@ -31,19 +31,7 @@ active_recipient_{0} AS
                  END) >= CAST(sam_recipient.registration_date AS DATE)
             AND (CASE WHEN is_date(COALESCE(fabs.action_date, '0'))
                  THEN CAST(fabs.action_date AS DATE)
-                 END) < CAST(sam_recipient.expiration_date AS DATE)))),
-special_exception_{0} AS
-    (SELECT *
-    FROM active_recipient_{0} AS fabs
-    WHERE NOT (
-         UPPER(legal_entity_country_code) <> 'USA'
-         AND (CASE WHEN is_date(COALESCE(action_date, '0'))
-              THEN CAST(action_date AS DATE)
-              END) > CAST('10/01/2024' AS DATE)
-         AND EXISTS (
-            SELECT 1
-            FROM sam_recipient_unregistered AS sru
-            WHERE UPPER(fabs.uei) = UPPER(sru.uei))))
+                 END) < CAST(sam_recipient.expiration_date AS DATE))))
 SELECT
     row_number,
     action_date,
@@ -51,9 +39,13 @@ SELECT
     legal_entity_country_code,
     uei,
     afa_generated_unique AS "uniqueid_AssistanceTransactionUniqueKey"
-FROM special_exception_{0} AS fabs
+FROM active_recipient_{0} AS fabs
 WHERE NOT (
-          (CASE WHEN is_date(COALESCE(action_date, '0'))
-           THEN CAST(action_date AS DATE)
-           END) < CAST('01/01/2017' AS DATE)
-           AND UPPER(COALESCE(correction_delete_indicatr, '')) = 'C');
+     UPPER(legal_entity_country_code) <> 'USA'
+     AND (CASE WHEN is_date(COALESCE(action_date, '0'))
+          THEN CAST(action_date AS DATE)
+          END) > CAST('10/01/2024' AS DATE)
+     AND EXISTS (
+        SELECT 1
+        FROM sam_recipient_unregistered AS sru
+        WHERE UPPER(fabs.uei) = UPPER(sru.uei)));
