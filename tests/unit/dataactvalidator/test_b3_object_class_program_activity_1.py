276b1ec902149e01f4ca00170dee011bf15a7e0b
Daniel Boos <29313570+dpb-bah@users.noreply.github.com>
2024-09-20 14:25:13 -0400

    B3

--- tests/unit/dataactvalidator/test_b3_object_class_program_activity_1.py
+++ tests/unit/dataactvalidator/test_b3_object_class_program_activity_1.py
@@ -7,8 +7,9 @@ _FILE = 'b3_object_class_program_activity_1'
 
 
 def test_column_headers(database):
-    expected_subset = {'row_number', 'obligations_undelivered_or_fyb', 'ussgl480100_undelivered_or_fyb', 'difference',
-                       'uniqueid_TAS', 'uniqueid_DisasterEmergencyFundCode', 'uniqueid_ProgramActivityCode',
+    expected_subset = {'row_number', 'prior_year_adjustment', 'obligations_undelivered_or_fyb',
+                       'ussgl480100_undelivered_or_fyb', 'difference', 'uniqueid_TAS',
+                       'uniqueid_DisasterEmergencyFundCode', 'uniqueid_ProgramActivityCode',
                        'uniqueid_ProgramActivityName', 'uniqueid_ObjectClass',
                        'uniqueid_ByDirectReimbursableFundingSource'}
     actual = set(query_columns(_FILE, database))
@@ -19,17 +20,20 @@ def test_success(database):
     """ Test that calculation passes with equal values and with a null """
 
     value = Decimal('100.23')
-    ocpa = ObjectClassProgramActivityFactory(obligations_undelivered_or_fyb=value,
-                                             ussgl480100_undelivered_or_fyb=value)
-    ocpa_null = ObjectClassProgramActivityFactory(obligations_undelivered_or_fyb=0,
-                                                  ussgl480100_undelivered_or_fyb=None)
+    ocpa = ObjectClassProgramActivityFactory(obligations_undelivered_or_fyb=value, ussgl480100_undelivered_or_fyb=value,
+                                             prior_year_adjustment='X')
+    ocpa_null = ObjectClassProgramActivityFactory(obligations_undelivered_or_fyb=0, ussgl480100_undelivered_or_fyb=None,
+                                                  prior_year_adjustment='x')
+    # Different values, different PYA
+    ocpa_pya = ObjectClassProgramActivityFactory(obligations_undelivered_or_fyb=1, ussgl480100_undelivered_or_fyb=2)
 
-    assert number_of_errors(_FILE, database, models=[ocpa, ocpa_null]) == 0
+    assert number_of_errors(_FILE, database, models=[ocpa, ocpa_null, ocpa_pya]) == 0
 
 
 def test_failure(database):
     """ Test that calculation fails for unequal values """
     ocpa = ObjectClassProgramActivityFactory(obligations_undelivered_or_fyb=Decimal('101.23'),
-                                             ussgl480100_undelivered_or_fyb=Decimal('102.34'))
+                                             ussgl480100_undelivered_or_fyb=Decimal('102.34'),
+                                             prior_year_adjustment='x')
 
     assert number_of_errors(_FILE, database, models=[ocpa]) == 1
