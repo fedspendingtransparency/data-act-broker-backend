ccc61ac7cd5b70a7f41c9b289fe425e47cb142b3
Daniel Boos <29313570+dpb-bah@users.noreply.github.com>
2024-09-24 14:06:56 -0400

    C6

--- dataactvalidator/config/sqlrules/c6_award_financial_2.sql
+++ dataactvalidator/config/sqlrules/c6_award_financial_2.sql
@@ -10,6 +10,7 @@ SELECT
                                                   COALESCE(ussgl488200_upward_adjustm_cpe, 0)) AS "difference",
     display_tas AS "uniqueid_TAS",
     disaster_emergency_fund_code AS "uniqueid_DisasterEmergencyFundCode",
+    prior_year_adjustment AS "uniqueid_PriorYearAdjustment",
     piid AS "uniqueid_PIID",
     fain AS "uniqueid_FAIN",
     uri AS "uniqueid_URI"
