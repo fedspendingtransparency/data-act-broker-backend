ddd739709a639ae893429691498a7b5dfc654ed3
Alisa Burdeyny <Burdeyny_Alisa@bah.com>
2024-09-24 15:20:50 -0400

    Making it a CTE for speed

--- dataactvalidator/config/sqlrules/b29_award_financial.sql
+++ dataactvalidator/config/sqlrules/b29_award_financial.sql
@@ -1,4 +1,15 @@
 -- If PYA is provided in File C, it must be on all (non-TOA) balance rows. See RSS for valid domain values.
+WITH award_financial_b29_{0} AS
+    (SELECT transaction_obligated_amou,
+        row_number,
+        prior_year_adjustment,
+        display_tas,
+        disaster_emergency_fund_code,
+        piid,
+        fain,
+        uri
+    FROM award_financial
+    WHERE submission_id = {0})
 SELECT
     row_number,
     prior_year_adjustment,
@@ -7,13 +18,11 @@ SELECT
     piid AS "uniqueid_PIID",
     fain AS "uniqueid_FAIN",
     uri AS "uniqueid_URI"
-FROM award_financial
-WHERE submission_id = {0}
-    AND transaction_obligated_amou IS NULL
+FROM award_financial_b29_{0}
+WHERE transaction_obligated_amou IS NULL
     AND COALESCE(prior_year_adjustment, '') = ''
     AND EXISTS (
         SELECT 1
-        FROM award_financial
-        WHERE submission_id = {0}
-            AND COALESCE(prior_year_adjustment, '') <> ''
+        FROM award_financial_b29_{0}
+        WHERE COALESCE(prior_year_adjustment, '') <> ''
     );
