5f772947b8dd9be0bf7599677b22d4f80e0d89a9
Alisa Burdeyny <Burdeyny_Alisa@bah.com>
2024-10-10 11:25:42 -0400

    Updating C rules for USSGL 480110 and 490110

--- dataactvalidator/config/sqlrules/c4_award_financial_2.sql
+++ dataactvalidator/config/sqlrules/c4_award_financial_2.sql
@@ -1,11 +1,13 @@
--- ObligationsDeliveredOrdersUnpaidTotal (CPE) = USSGL(4901 + 4931+ 4981). This applies to the award level.
+-- ObligationsDeliveredOrdersUnpaidTotal (CPE) = USSGL(4901 + 490110 + 4931 + 4981). This applies to the award level.
 SELECT
     row_number,
     obligations_delivered_orde_cpe,
     ussgl490100_delivered_orde_cpe,
+    ussgl490110_reinstated_del_cpe,
     ussgl493100_delivered_orde_cpe,
     ussgl498100_upward_adjustm_cpe,
     COALESCE(obligations_delivered_orde_cpe, 0) - (COALESCE(ussgl490100_delivered_orde_cpe, 0) +
+                                                   COALESCE(ussgl490110_reinstated_del_cpe, 0) +
                                                    COALESCE(ussgl493100_delivered_orde_cpe, 0) +
                                                    COALESCE(ussgl498100_upward_adjustm_cpe, 0)) AS "difference",
     display_tas AS "uniqueid_TAS",
@@ -18,5 +20,6 @@ FROM award_financial
 WHERE submission_id = {0}
     AND COALESCE(obligations_delivered_orde_cpe, 0) <>
         COALESCE(ussgl490100_delivered_orde_cpe, 0) +
+        COALESCE(ussgl490110_reinstated_del_cpe, 0) +
         COALESCE(ussgl493100_delivered_orde_cpe, 0) +
         COALESCE(ussgl498100_upward_adjustm_cpe, 0);
