5d19272ce6eba656888f2295db9ba39d0e6655f6
Daniel Boos <29313570+dpb-bah@users.noreply.github.com>
2024-11-06 14:32:06 -0500

    Copying updates from USAS otem for testing purposes

--- dataactcore/broker_logging.py
+++ dataactcore/broker_logging.py
@@ -26,6 +26,16 @@ def deep_merge(left, right):
     else:
         return right
 
+# CUSTOM Logging EXPORTER for debugging
+class LoggingSpanProcessor(SpanProcessor):
+    def on_end(self, span: ReadableSpan) -> None:
+        trace_id = span.context.trace_id
+        span_id = span.context.span_id
+        logger = logging.getLogger(__name__)
+        logger.info(f"Span ended: trace_id={trace_id}, span_id={span_id}, {span.name}_attributes={span.attributes}")
+
+    def force_flush(self, timeout_millis: int = 30000) -> bool:
+        return True
 
 # Reasonable defaults to avoid clutter in our config files
 DEFAULT_CONFIG = {
@@ -71,26 +81,34 @@ DEFAULT_CONFIG = {
 
 def configure_logging(service_name='broker'):
     resource = Resource.create(attributes={"service.name": service_name})
-
     provider = TracerProvider(resource=resource)
     trace.set_tracer_provider(provider)
 
+    # Set up the OTLP exporter
+    # Check out https://opentelemetry.io/docs/languages/sdk-configuration/otlp-exporter/
+    # for more exporter configuration
     if CONFIG_BROKER['local']:
+        # if local print the traces to the console
         exporter = ConsoleSpanExporter()
+
+        # custom debug information
+        logging_span_processor = LoggingSpanProcessor()
+        trace.get_tracer_provider().add_span_processor(logging_span_processor)
     else:
+        # if prod or non-prod send trace information to the endpoint
         exporter = OTLPSpanExporter(
             endpoint=os.getenv("OTEL_EXPORTER_OTLP_TRACES_ENDPOINT"),
         )
-    span_processor = BatchSpanProcessor(exporter)
-    trace.get_tracer_provider().add_span_processor(span_processor)
+    trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(exporter))
 
+    LoggingInstrumentor(logging_format="%(msg)s [span_id=%(otelSpanID)s trace_id=%(otelTraceID)s]")
     LoggingInstrumentor().instrument(tracer_provider=trace.get_tracer_provider(), set_logging_format=True)
     URLLibInstrumentor().instrument(tracer_provider=trace.get_tracer_provider())
 
-    config = DEFAULT_CONFIG
-    if 'python_config' in CONFIG_LOGGING:
-        config = deep_merge(config, CONFIG_LOGGING['python_config'])
-    logging.config.dictConfig(config)
+    # config = DEFAULT_CONFIG
+    # if 'python_config' in CONFIG_LOGGING:
+    #     config = deep_merge(config, CONFIG_LOGGING['python_config'])
+    # logging.config.dictConfig(config)
 
     logging.getLogger('boto3').setLevel(logging.CRITICAL)
     logging.getLogger('botocore').setLevel(logging.CRITICAL)
