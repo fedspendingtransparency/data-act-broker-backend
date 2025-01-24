59a9cb99a36e21ff91d17e28a6cde44dff943d28
Zach Flanders <zachary.flanders@kc.frb.org>
2025-01-24 14:19:09 -0600

    [DEV-11948] - Fixing SQL style.

--- dataactvalidator/config/sqlrules/b29_award_financial_1.sql
+++ dataactvalidator/config/sqlrules/b29_award_financial_1.sql
@@ -4,6 +4,5 @@ SELECT
     prior_year_adjustment,
     display_tas AS "uniqueid_TAS"
 FROM award_financial
-WHERE (submission_id = {0}
-    AND UPPER(COALESCE(prior_year_adjustment, '')) NOT IN ('X', 'B', 'P', '')
-);
+WHERE submission_id = {0}
+    AND UPPER(COALESCE(prior_year_adjustment, '')) NOT IN ('X', 'B', 'P', '');
--- dataactvalidator/config/sqlrules/b29_object_class_program_activity.sql
+++ dataactvalidator/config/sqlrules/b29_object_class_program_activity.sql
@@ -4,6 +4,5 @@ SELECT
     prior_year_adjustment,
     display_tas AS "uniqueid_TAS"
 FROM object_class_program_activity
-WHERE (submission_id = {0}
-    AND UPPER(prior_year_adjustment) NOT IN ('X', 'B', 'P')
-);
+WHERE submission_id = {0}
+    AND UPPER(prior_year_adjustment) NOT IN ('X', 'B', 'P');
