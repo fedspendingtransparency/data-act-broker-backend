c83fbb698d0367d1ab26b7382fabf85575ea21b3
Burdeyny <alisa.burdeyny@kc.frb.org>
2025-04-01 15:20:57 -0400

    Adding tests

--- tests/unit/dataactcore/scripts/test_load_full_sam_subaward_dump.py
+++ tests/unit/dataactcore/scripts/test_load_full_sam_subaward_dump.py
@@ -1,24 +1,41 @@
-import pytest
 import os
+
+from unittest.mock import Mock
 
 from dataactcore.config import CONFIG_BROKER
 from dataactcore.scripts.ad_hoc import load_full_sam_subaward_dump
-from dataactcore.models.domainModels import CGAC, FREC, SubTierAgency, ExternalDataType, ExternalDataLoadDate
+from dataactcore.models.fsrs import SAMSubcontract, SAMSubgrant
 
 
 def test_load_full_sam_subaward_dump_assistance(database, monkeypatch):
     """ Test loading the assistance data file """
-    filename = 'SAM_Subward_Bulk_Import_Assistance.csv'
+    sess = database.session
+    filename = 'SAM_Subaward_Bulk_Import_Assistance.csv'
     monkeypatch.setattr('dataactcore.scripts.ad_hoc.load_full_sam_subaward_dump.get_full_dump_filepath',
-                        os.path.join(CONFIG_BROKER['path'], 'tests', 'data', 'config', filename))
+                        Mock(return_value=os.path.join(CONFIG_BROKER['path'], 'tests', 'unit', 'data', 'fake_sam_files',
+                                                       'subaward', filename)))
+    load_full_sam_subaward_dump.load_full_dump_file(sess, 'assistance', {'assistance_subawards': 0})
 
-    assert True
+    results = sess.query(SAMSubgrant).all()
+
+    assert len(results) == 1
+    assert results[0].uei == 'VENDORUEI'
+    assert results[0].award_amount == '27354.55'
+    assert len(results[0].business_types_codes) == 2
+    assert 'XX' in results[0].business_types_codes
 
 
 def test_load_full_sam_subaward_dump_contracts(database, monkeypatch):
     """ Test loading the contracts data file """
-    filename = 'SAM_Subward_Bulk_Import_Contracts.csv'
+    sess = database.session
+    filename = 'SAM_Subaward_Bulk_Import_Contracts.csv'
     monkeypatch.setattr('dataactcore.scripts.ad_hoc.load_full_sam_subaward_dump.get_full_dump_filepath',
-                        os.path.join(CONFIG_BROKER['path'], 'tests', 'data', 'config', filename))
+                        Mock(return_value=os.path.join(CONFIG_BROKER['path'], 'tests', 'unit', 'data', 'fake_sam_files',
+                                                       'subaward', filename)))
+    load_full_sam_subaward_dump.load_full_dump_file(sess, 'contract', {'contract_subawards': 0})
 
-    assert True
+    results = sess.query(SAMSubcontract).all()
+
+    assert len(results) == 1
+    assert results[0].uei == 'ABCDEFGHIJK'
+    assert results[0].award_amount == '69693.47'
