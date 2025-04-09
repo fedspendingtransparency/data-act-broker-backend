000c07da76a99ea57b530c782c723eccc34bcbd6
Burdeyny <alisa.burdeyny@kc.frb.org>
2025-04-09 14:14:09 -0400

    Fixing upload location

--- dataactcore/scripts/ad_hoc/archive_fsrs_tables.py
+++ dataactcore/scripts/ad_hoc/archive_fsrs_tables.py
@@ -25,17 +25,19 @@ def main():
     logger.info('Starting FSRS archive')
 
     for fsrs_table in [FSRSProcurement, FSRSSubcontract, FSRSGrant, FSRSSubgrant]:
-        local_file = os.path.join(os.getcwd(), f'{fsrs_table.__table__.name}_archive.csv')
+        filename = f'{fsrs_table.__table__.name}_archive.csv'
+        local_file = os.path.join(os.getcwd(), filename)
         fsrs_query = sess.query(fsrs_table)
-        write_stream_query(sess, fsrs_query, local_file, local_file, CONFIG_BROKER['local'], generate_headers=True,
+        write_stream_query(sess, fsrs_query, local_file, filename, CONFIG_BROKER['local'], generate_headers=True,
                            generate_string=True, bucket='dti-da-data-archive-prod')
 
     subaward_fsrs_old_query = """
         SELECT *
         FROM subaward_fsrs_old
     """
-    local_file = os.path.join(os.getcwd(), 'subaward_fsrs_old_archive.csv')
-    write_stream_query(sess, subaward_fsrs_old_query, local_file, local_file, CONFIG_BROKER['local'], generate_headers=True,
+    filename = 'subaward_fsrs_old_archive.csv'
+    local_file = os.path.join(os.getcwd(), filename)
+    write_stream_query(sess, subaward_fsrs_old_query, local_file, filename, CONFIG_BROKER['local'], generate_headers=True,
                        generate_string=False, bucket='dti-da-data-archive-prod')
     logger.info('Completed SQL query, file written')
 
