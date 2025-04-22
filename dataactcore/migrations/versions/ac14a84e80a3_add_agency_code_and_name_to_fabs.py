3b58d479d567af77ab78a07baf65a413c4196d28
Zach Flanders <zachary.flanders@kc.frb.org>
2025-04-22 12:07:21 -0500

    [DEV-12100] - Adding alembic migration

---
+++ dataactcore/migrations/versions/ac14a84e80a3_add_agency_code_and_name_to_fabs.py
@@ -0,0 +1,42 @@
+"""empty message
+
+Revision ID: ac14a84e80a3
+Revises: 97955355bc51
+Create Date: 2025-04-22 17:02:58.951709
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'ac14a84e80a3'
+down_revision = '97955355bc51'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade(engine_name):
+    globals()["upgrade_%s" % engine_name]()
+
+
+def downgrade(engine_name):
+    globals()["downgrade_%s" % engine_name]()
+
+
+
+
+
+def upgrade_data_broker():
+    op.add_column('fabs', sa.Column('awarding_agency_code', sa.Text(), nullable=True))
+    op.add_column('fabs', sa.Column('awarding_agency_name', sa.Text(), nullable=True))
+    op.create_index(op.f('ix_fabs_awarding_agency_code'), 'fabs', ['awarding_agency_code'], unique=False)
+    # ### end Alembic commands ###
+
+
+def downgrade_data_broker():
+    op.drop_index(op.f('ix_fabs_awarding_agency_code'), table_name='fabs')
+    op.drop_column('fabs', 'awarding_agency_name')
+    op.drop_column('fabs', 'awarding_agency_code')
+    # ### end Alembic commands ###
+
