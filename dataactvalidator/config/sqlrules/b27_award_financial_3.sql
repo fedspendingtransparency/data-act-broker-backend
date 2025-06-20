48bcb08501b7ccea7deeb7176716cb2fbeaee4d6
Zach Flanders <zachary.flanders@kc.frb.org>
2025-06-20 13:40:18 -0500

    [DEV-12793] - Add new rule b27

---
+++ dataactvalidator/config/sqlrules/b27_award_financial_3.sql
@@ -0,0 +1,17 @@
+-- As of FY26, each row in file C must contain a PARK and must not contain a PAC/PAN.
+SELECT
+    row_number,
+    program_activity_reporting_key,
+    program_activity_code,
+    program_activity_name,
+    display_tas AS "uniqueid_TAS",
+    object_class AS "uniqueid_ObjectClass"
+FROM award_financial AS af
+    JOIN submission AS sub
+        ON sub.submission_id = af.submission_id
+WHERE af.submission_id = {0}
+    AND reporting_fiscal_year >= 2026
+    AND (COALESCE(program_activity_reporting_key, '') = ''
+        OR COALESCE(program_activity_code, '') != ''
+        OR COALESCE(program_activity_name, '') != ''
+    );
---
+++ dataactvalidator/config/sqlrules/b27_object_class_program_activity_3.sql
@@ -0,0 +1,17 @@
+-- As of FY26, each row in file B must contain a PARK and must not contain a PAC/PAN.
+SELECT
+    row_number,
+    program_activity_reporting_key,
+    program_activity_code,
+    program_activity_name,
+    display_tas AS "uniqueid_TAS",
+    object_class AS "uniqueid_ObjectClass"
+FROM object_class_program_activity AS ocpa
+    JOIN submission AS sub
+        ON sub.submission_id = ocpa.submission_id
+WHERE ocpa.submission_id = {0}
+    AND reporting_fiscal_year >= 2026
+    AND (COALESCE(program_activity_reporting_key, '') = ''
+        OR COALESCE(program_activity_code, '') != ''
+        OR COALESCE(program_activity_name, '') != ''
+    );
