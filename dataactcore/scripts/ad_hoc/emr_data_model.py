03a716fdaabc2a342daf3f245d71767d7c42c82b
dpb-bah <boos_daniel@bah.com>
2025-10-20 18:00:57 -0400

    Update emr_data_model.py

--- dataactcore/scripts/ad_hoc/emr_data_model.py
+++ dataactcore/scripts/ad_hoc/emr_data_model.py
@@ -382,7 +382,7 @@ if __name__ == "__main__":
 
     # jaydebee
     driver_class = "org.postgresql.Driver"
-    jar_file = os.path.join(os.path.abspath(__file__), 'postgresql-42.7.8.jar')
+    jar_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'postgresql-42.7.8.jar')
     print(jar_file)
     conn = jaydebeapi.connect(
         driver_class,
