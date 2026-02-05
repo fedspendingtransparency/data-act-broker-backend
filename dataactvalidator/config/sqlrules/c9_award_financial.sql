7da1dacce23a1e99c1f393136e38e043abbc5354
alburde1 <alisa.burdeyny@kc.frb.org>
2026-02-05 11:37:44 -0500

    Adding new assistance types and updating some rules
    + All DABS rules and basic FABS "valid assistance type" check

--- dataactvalidator/config/sqlrules/c9_award_financial.sql
+++ dataactvalidator/config/sqlrules/c9_award_financial.sql
@@ -16,10 +16,10 @@ FROM award_financial_assistance AS afa
 WHERE submission_id = {0}
 	AND afa.uri IS NOT NULL
 	AND afa.record_type = '1'
-	AND ((afa.assistance_type NOT IN ('07', '08')
+	AND ((afa.assistance_type NOT IN ('07', '08', 'F003', 'F004')
             AND COALESCE(afa.federal_action_obligation, 0) <> 0
         )
-        OR (afa.assistance_type IN ('07', '08')
+        OR (afa.assistance_type IN ('07', '08', 'F003', 'F004')
             AND COALESCE(CAST(afa.original_loan_subsidy_cost AS NUMERIC), 0) > 0
         )
     )
@@ -43,10 +43,10 @@ FROM award_financial_assistance AS afa
 WHERE submission_id = {0}
 	AND afa.record_type IN ('2', '3')
 	AND afa.fain IS NOT NULL
-	AND ((afa.assistance_type NOT IN ('07', '08')
+	AND ((afa.assistance_type NOT IN ('07', '08', 'F003', 'F004')
             AND COALESCE(afa.federal_action_obligation, 0) <> 0
         )
-        OR (afa.assistance_type IN ('07', '08')
+        OR (afa.assistance_type IN ('07', '08', 'F003', 'F004')
             AND COALESCE(CAST(afa.original_loan_subsidy_cost AS NUMERIC), 0) > 0
         )
     )
