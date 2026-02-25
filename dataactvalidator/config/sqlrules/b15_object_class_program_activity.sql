101195139914321ea07198f6604e5950f835599e
dpb-bah <boos_daniel@bah.com>
2026-02-25 18:46:15 -0500

    [DEV-14636] Cleaning up B14/B15 to match the other rules

--- dataactvalidator/config/sqlrules/b15_object_class_program_activity.sql
+++ dataactvalidator/config/sqlrules/b15_object_class_program_activity.sql
@@ -4,8 +4,7 @@
 -- where PYA = "X". If the DEFC is other than Q or QQQ, this value should be $0 since there should not be reimbursable
 -- work reported for disasters or emergencies.
 WITH object_class_program_activity_b15_{0} AS
-    (SELECT sub.reporting_fiscal_year,
-        sub.reporting_fiscal_period,
+    (SELECT submission_id,
         display_tas,
         UPPER(disaster_emergency_fund_code) AS disaster_emergency_fund_code,
         UPPER(prior_year_adjustment) AS prior_year_adjustment,
@@ -32,59 +31,63 @@ WITH object_class_program_activity_b15_{0} AS
             SUM(ussgl490800_authority_outl_cpe) - SUM(ussgl490800_authority_outl_fyb) +
             SUM(ussgl498100_upward_adjustm_cpe) +
             SUM(ussgl498200_upward_adjustm_cpe)
-        ) AS total
+        ) AS sum_amount
     FROM object_class_program_activity AS op
-    JOIN submission sub ON sub.submission_id = op.submission_id
     WHERE op.submission_id = {0}
         AND UPPER(prior_year_adjustment) = 'X'
         AND UPPER(by_direct_reimbursable_fun) = 'R'
-    GROUP BY UPPER(disaster_emergency_fund_code),
+    GROUP BY submission_id,
         display_tas,
-        UPPER(prior_year_adjustment),
-        sub.reporting_fiscal_year,
-        sub.reporting_fiscal_period),
-sf_133_b15_{0} AS (
-    SELECT display_tas,
-        UPPER(disaster_emergency_fund_code) AS disaster_emergency_fund_code,
-        UPPER(prior_year_adjustment) AS prior_year_adjustment,
-        SUM(amount) AS total
-    FROM sf_133
-    WHERE EXISTS (
-            SELECT 1
-            FROM object_class_program_activity_b15_{0} AS op
-            WHERE sf_133.period = op.reporting_fiscal_period
-                AND sf_133.fiscal_year = op.reporting_fiscal_year
-        )
-        AND line = 2104
-    GROUP BY UPPER(disaster_emergency_fund_code),
-        display_tas,
+        UPPER(disaster_emergency_fund_code),
         UPPER(prior_year_adjustment))
 SELECT DISTINCT
     NULL AS row_number,
     op.display_tas AS "tas",
     UPPER(op.prior_year_adjustment) AS "prior_year_adjustment",
-    op.ussgl480100_undelivered_or_cpe_sum,
-    op.ussgl480100_undelivered_or_fyb_sum,
-    op.ussgl480200_undelivered_or_cpe_sum,
-    op.ussgl480200_undelivered_or_fyb_sum,
-    op.ussgl488100_upward_adjustm_cpe_sum,
-    op.ussgl488200_upward_adjustm_cpe_sum,
-    op.ussgl490100_delivered_orde_cpe_sum,
-    op.ussgl490100_delivered_orde_fyb_sum,
-    op.ussgl490200_delivered_orde_cpe_sum,
-    op.ussgl490800_authority_outl_cpe_sum,
-    op.ussgl490800_authority_outl_fyb_sum,
-    op.ussgl498100_upward_adjustm_cpe_sum,
-    op.ussgl498200_upward_adjustm_cpe_sum,
-    COALESCE(sf.total, 0) AS "expected_value_GTAS SF133 Line 2104",
-    op.total + COALESCE(sf.total, 0) AS "difference",
+    ussgl480100_undelivered_or_cpe_sum,
+    ussgl480100_undelivered_or_fyb_sum,
+    ussgl480200_undelivered_or_cpe_sum,
+    ussgl480200_undelivered_or_fyb_sum,
+    ussgl488100_upward_adjustm_cpe_sum,
+    ussgl488200_upward_adjustm_cpe_sum,
+    ussgl490100_delivered_orde_cpe_sum,
+    ussgl490100_delivered_orde_fyb_sum,
+    ussgl490200_delivered_orde_cpe_sum,
+    ussgl490800_authority_outl_cpe_sum,
+    ussgl490800_authority_outl_fyb_sum,
+    ussgl498100_upward_adjustm_cpe_sum,
+    ussgl498200_upward_adjustm_cpe_sum,
+    SUM(COALESCE(sf.amount, 0)) AS "expected_value_GTAS SF133 Line 2104",
+    sum_amount + SUM(COALESCE(sf.amount, 0)) AS "difference",
     op.display_tas AS "uniqueid_TAS",
     UPPER(op.disaster_emergency_fund_code) AS "uniqueid_DisasterEmergencyFundCode"
 FROM object_class_program_activity_b15_{0} AS op
-LEFT OUTER JOIN sf_133_b15_{0} AS sf
-    ON op.display_tas = sf.display_tas
-    AND op.disaster_emergency_fund_code = COALESCE(sf.disaster_emergency_fund_code, '')
-WHERE (UPPER(op.disaster_emergency_fund_code) IN ('Q', 'QQQ')
-        AND op.total <> (-1 * COALESCE(sf.total, 0)))
+    INNER JOIN submission AS sub
+        ON op.submission_id = sub.submission_id
+    LEFT OUTER JOIN sf_133 AS sf
+        ON op.display_tas = sf.display_tas
+        AND UPPER(op.disaster_emergency_fund_code) = UPPER(COALESCE(sf.disaster_emergency_fund_code, ''))
+        AND sf.period = sub.reporting_fiscal_period
+        AND sf.fiscal_year = sub.reporting_fiscal_year
+        AND sf.line = 2104
+GROUP BY op.display_tas,
+    ussgl480100_undelivered_or_cpe_sum,
+    ussgl480100_undelivered_or_fyb_sum,
+    ussgl480200_undelivered_or_cpe_sum,
+    ussgl480200_undelivered_or_fyb_sum,
+    ussgl488100_upward_adjustm_cpe_sum,
+    ussgl488200_upward_adjustm_cpe_sum,
+    ussgl490100_delivered_orde_cpe_sum,
+    ussgl490100_delivered_orde_fyb_sum,
+    ussgl490200_delivered_orde_cpe_sum,
+    ussgl490800_authority_outl_cpe_sum,
+    ussgl490800_authority_outl_fyb_sum,
+    ussgl498100_upward_adjustm_cpe_sum,
+    ussgl498200_upward_adjustm_cpe_sum,
+    sum_amount,
+    UPPER(op.disaster_emergency_fund_code),
+    UPPER(op.prior_year_adjustment)
+HAVING (UPPER(op.disaster_emergency_fund_code) IN ('Q', 'QQQ')
+        AND sum_amount <> (-1 * SUM(COALESCE(sf.amount, 0))))
     OR (UPPER(op.disaster_emergency_fund_code) NOT IN ('Q', 'QQQ')
-        AND op.total <> 0);
+        AND sum_amount <> 0);
