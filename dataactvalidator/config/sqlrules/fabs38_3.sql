a4b119e1161abc5c04b83ad1519d08828a187bcf
Burdeyny <alisa.burdeyny@kc.frb.org>
2026-07-29 11:11:57 -0400

    Update FABS38 rules for new ActionTypes

--- dataactvalidator/config/sqlrules/fabs38_3.sql
+++ dataactvalidator/config/sqlrules/fabs38_3.sql
@@ -1,4 +1,4 @@
--- AwardingOfficeCode must be submitted for new awards (ActionType = A) or mixed aggregate records (ActionType = E)
+-- AwardingOfficeCode must be submitted for new awards (ActionType = A1/A2) or mixed aggregate records (ActionType = G1)
 -- whose ActionDate is on or after October 1, 2018, and whose CorrectionDeleteIndicator is either Blank or C.
 SELECT
     row_number,
@@ -10,7 +10,7 @@ SELECT
 FROM fabs
 WHERE submission_id = {0}
     AND COALESCE(awarding_office_code, '') = ''
-    AND UPPER(COALESCE(action_type, '')) IN ('A', 'E')
+    AND UPPER(COALESCE(action_type, '')) IN ('A1', 'A2', 'G1')
     AND UPPER(COALESCE(correction_delete_indicatr, '')) <> 'D'
     AND (CASE WHEN is_date(COALESCE(action_date, '0'))
             THEN CAST(action_date AS DATE)
