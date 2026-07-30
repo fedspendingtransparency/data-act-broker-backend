b9f3bc2743a559a292edf15ff9183a2753ce5c65
Alisa Burdeyny <Burdeyny_Alisa@bah.com>
2026-07-30 14:53:47 -0400

    Add USSGLs to B rules that are missing them

--- dataactvalidator/config/sqlrules/b11_object_class_program_activity_2.sql
+++ dataactvalidator/config/sqlrules/b11_object_class_program_activity_2.sql
@@ -19,12 +19,13 @@ WHERE submission_id = {0}
         is_zero(obligations_delivered_orde_fyb) + is_zero(obligations_incurred_by_pr_cpe) +
         is_zero(obligations_undelivered_or_cpe) + is_zero(obligations_undelivered_or_fyb) +
         is_zero(ussgl480100_undelivered_or_cpe) + is_zero(ussgl480100_undelivered_or_fyb) +
-        is_zero(ussgl480200_undelivered_or_cpe) + is_zero(ussgl480200_undelivered_or_fyb) +
-        is_zero(ussgl480210_rein_undel_obs_cpe) + is_zero(ussgl483100_undelivered_or_cpe) +
-        is_zero(ussgl483200_undelivered_or_cpe) + is_zero(ussgl487100_downward_adjus_cpe) +
-        is_zero(ussgl487200_downward_adjus_cpe) + is_zero(ussgl488100_upward_adjustm_cpe) +
-        is_zero(ussgl488200_upward_adjustm_cpe) + is_zero(ussgl490100_delivered_orde_cpe) +
-        is_zero(ussgl490100_delivered_orde_fyb) + is_zero(ussgl490200_delivered_orde_cpe) +
+        is_zero(ussgl480110_rein_undel_ord_cpe) + is_zero(ussgl480200_undelivered_or_cpe) +
+        is_zero(ussgl480200_undelivered_or_fyb) + is_zero(ussgl480210_rein_undel_obs_cpe) +
+        is_zero(ussgl483100_undelivered_or_cpe) + is_zero(ussgl483200_undelivered_or_cpe) +
+        is_zero(ussgl487100_downward_adjus_cpe) + is_zero(ussgl487200_downward_adjus_cpe) +
+        is_zero(ussgl488100_upward_adjustm_cpe) + is_zero(ussgl488200_upward_adjustm_cpe) +
+        is_zero(ussgl490100_delivered_orde_cpe) + is_zero(ussgl490100_delivered_orde_fyb) +
+        is_zero(ussgl490110_rein_deliv_ord_cpe) + is_zero(ussgl490200_delivered_orde_cpe) +
         is_zero(ussgl490800_authority_outl_cpe) + is_zero(ussgl490800_authority_outl_fyb) +
         is_zero(ussgl493100_delivered_orde_cpe) + is_zero(ussgl497100_downward_adjus_cpe) +
         is_zero(ussgl497200_downward_adjus_cpe) + is_zero(ussgl497210_down_adj_refun_cpe) +
