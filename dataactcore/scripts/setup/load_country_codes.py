d6ab721ef72191e5518ab1178ab50ee6385116ad
dpb-bah <boos_daniel@bah.com>
2026-07-31 15:02:49 -0400

    [DEV-13342] Adding region

--- dataactcore/scripts/setup/load_country_codes.py
+++ dataactcore/scripts/setup/load_country_codes.py
@@ -76,7 +76,7 @@ def load_country_codes(base_path=None, force_reload=False):
         genc_file_name = "genc.xml"
         if CONFIG_BROKER["use_aws"]:
             logger.info('pulling from s3')
-            s3 = boto3.client("s3")
+            s3 = boto3.client("s3", region_name=CONFIG_BROKER["aws_region"])
             s3.download_file(
                 Bucket=CONFIG_BROKER["public_files_bucket"],
                 Key=f"broker_reference_data/{genc_file_name}",
