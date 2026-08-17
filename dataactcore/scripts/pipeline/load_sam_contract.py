1c81cea99fc1488af6c57d0e53b0fdcc4577091e
Alisa Burdeyny <Burdeyny_Alisa@bah.com>
2026-08-17 14:08:55 -0400

    Removing quote replacement
    + It seems like the quote replacement was a holdover from a previous method of inserting data. Removing it for now

--- dataactcore/scripts/pipeline/load_sam_contract.py
+++ dataactcore/scripts/pipeline/load_sam_contract.py
@@ -488,7 +488,7 @@ def insert_into_db(sess, contract_df):
         row_values = []
         # split it up by value for sql injection checking
         for value in row:
-            params_dict[f"param{index}"] = str(value).replace('"', "'") if value != "NULL" else None
+            params_dict[f"param{index}"] = str(value) if value != "NULL" else None
             row_values.append(f":param{index}")
             index += 1
         # build the row of *params* that will be populated by the binding params_dict
