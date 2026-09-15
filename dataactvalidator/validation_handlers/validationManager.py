029a12c08b4b13e9aceeca1cbfe2f9b9f48b30d5
Daniel Boos <boos_daniel@bah.com>
2026-09-15 01:15:34 -0400

    [DEV-16278] Reworking and testing long/short rows via merging dfs

--- dataactvalidator/validation_handlers/validationManager.py
+++ dataactvalidator/validation_handlers/validationManager.py
@@ -481,6 +481,16 @@ class ValidationManager:
         self.total_rows = 1 + len(self.long_rows)
         self.total_data_rows = 0
 
+        # Build long_row_increment outside the chunks which will correct row_numbers ignored for being too long
+        self.long_row_df = pd.DataFrame({"row_number": range(2, file_row_count + 1)})
+        self.long_row_df["long_row"] = self.long_row_df["row_number"].isin(self.long_rows)
+        self.long_row_df["long_row_increment"] = self.long_row_df["long_row"].cumsum()
+        self.long_row_df = (
+            self.long_row_df[~self.long_row_df["long_row"]]
+            .drop(columns=["row_number", "long_row"])
+            .reset_index(drop=True)
+        )
+
         # Making base error/warning files
         self.error_file_name = report_file_name(self.submission_id, False, self.file_type.name)
         self.error_file_path = "".join([CONFIG_SERVICES["error_report_path"], self.error_file_name])
@@ -555,12 +565,6 @@ class ValidationManager:
             on_bad_lines="skip",
             skiprows=skiprows,
         )
-        # Doing this once outside instead of multiple times with each chunk
-        reader_obj['short_row'] = reader_obj['row_number'].isin(self.short_rows)
-        # Increments +1 for every time a long_row appears
-        # which we can add to the row_number to get the *real* row_number
-        reader_obj['long_row_increment'] = reader_obj['row_number'].isin(self.long_rows).cumsum()
-
         # Setting this outside of reader/file type objects which may not be used during processing
         self.flex_fields = self.reader.flex_fields
         self.header_dict = self.reader.header_dict
@@ -820,8 +824,9 @@ class ValidationManager:
         with lockable:
             shared_data["total_rows"] += len(chunk_df.index)
 
-        # Increment row numbers if any were ignored being too long
-        # This syncs the row numbers back to their original values
+        # Increments +1 for every row ignored being too long
+        # which we can add to the row_number to get the original row_number
+        chunk_df = chunk_df.merge(self.long_row_df, how="left", left_index=True, right_index=True)
         chunk_df["row_number"] = chunk_df["row_number"] + chunk_df["long_row_increment"]
 
         logger.info(
@@ -837,10 +842,10 @@ class ValidationManager:
         )
 
         # Drop rows that were too short and pandas filled in with Nones
-        chunk_df = chunk_df[~chunk_df["short_row"]]
+        chunk_df = chunk_df[~chunk_df["row_number"].isin(self.short_rows)]
 
-        # Drop the index, short_row, and long_row_increment columns
-        chunk_df = chunk_df.drop(["index", "short_row", "long_row_increment"], axis=1)
+        # Drop the index and long_row_increment columns
+        chunk_df = chunk_df.drop(["index", "long_row_increment"], axis=1)
 
         # Drop all rows that have 1 or less filled in values (row_number is always filled in so this is how
         # we have to drop all rows that are just empty)
